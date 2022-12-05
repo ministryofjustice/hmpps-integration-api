5aa121e9c577b944fab15b561a1b264656825cd6
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2022-12-05 15:40:04 +0000

    Add root API endpoint for testing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/HmppsIntegrationApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/HmppsIntegrationApi.kt
@@ -2,10 +2,18 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication()
 class HmppsIntegrationApi
 
 fun main(args: Array<String>) {
   runApplication<HmppsIntegrationApi>(*args)
+}
+
+@RestController
+class TestApiController {
+  @GetMapping("/")
+  fun index(): String = "Test!"
 }
