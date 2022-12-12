9da5b702caf26b581e9d2523febd453399cc3066
mcoffey-mt <matthew.coffey@madetech.com>
2022-12-12 12:58:12 +0000

    Piped request from prison api through to hmpps-integration-api - renamed person controller to offences - updated authentication token to string on output

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagename/controllers/OffenceController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagename/controllers/OffenceController.kt
@@ -4,21 +4,22 @@
 
 package uk.gov.justice.digital.hmpps.hmppstemplatepackagename.controllers
 
+import org.apache.tomcat.util.json.JSONParser
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
 
 @RestController
-@RequestMapping("/persons")
-class PersonController {
+@RequestMapping("/offences")
+class OffenceController {
   private fun fetchAuthToken() : String {
 
     // todo grab from env var, add url query strings right and what the headers
     val tokenUrl = "http://oauth-server:8080/auth/oauth/token?grant_type=client_credentials"
 
     val builder: WebClient.Builder = WebClient.builder()
-    val token = builder.build()
+    val response = builder.build()
       .post()
       .uri(tokenUrl)
       .header("Authorization", "Basic ZGVsaXVzOmNsaWVudHNlY3JldA==")
@@ -26,13 +27,25 @@ class PersonController {
       .bodyToMono(String::class.java)
       .block()
 
-    print(token)
-
-    return token
+    print(response)
+    val tokenObj = JSONParser(response).parseObject()["access_token"].toString()
+    print(tokenObj)
+    return tokenObj
   }
 
   @GetMapping("/all")
-  fun getPerson() {
-    val token : String = fetchAuthToken()
+  fun getOffences(): String? {
+    val token: String = fetchAuthToken()
+
+    val offenceUrl = "http://prison-api:8080/api/offences"
+    val builder: WebClient.Builder = WebClient.builder()
+
+    return builder.build()
+      .get()
+      .uri(offenceUrl)
+      .header("Authorization", "Bearer $token")
+      .retrieve()
+      .bodyToMono(String::class.java)
+      .block();
   }
 }
