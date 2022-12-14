9267408a892fef81e4d4c34b7853b4cf032cb595
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2022-12-14 11:48:00 +0000

    Refactor PersonController test to use kotest

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/persons")
-class PersonController {
+class PersonController() {
 
   @GetMapping("{id}")
   fun getPerson() {
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonControllerTest.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers
 
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -8,12 +10,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest
-internal class PersonControllerTest(@Autowired val mockMvc: MockMvc) {
-
-    @Test
-    fun `retrieve a person`() {
+internal class PersonControllerTest(@Autowired val mockMvc: MockMvc) : DescribeSpec({
+  describe("GET /persons/{id}") {
+    it("responds with a 200 OK status") {
       val id=1
-      val result = mockMvc.perform(get("/persons/$id"))
-        .andExpect(status().isOk())
+      val result = mockMvc.perform(get("/persons/$id")).andReturn();
+
+      result.response.status.shouldBe(200);
     }
-}
+  }
+})
