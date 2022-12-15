47c0210b6b374aca04b9eaa7a268dd6a26ae2ae0
Emile <emile.swarts@digital.justice.gov.uk>
2022-12-15 15:46:58 +0000

    More linting errors

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonControllerTest.kt
@@ -9,10 +9,7 @@ import org.mockito.internal.verification.VerificationModeFactory.times
 import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.http.MediaType
-import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
@@ -43,7 +40,7 @@ internal class PersonControllerTest(
     it("responds with a 404 NOT FOUND status") {
       val id_that_does_not_exist = 777
 
-      mockMvc.perform(get("/persons/${id_that_does_not_exist}"))
+      mockMvc.perform(get("/persons/$id_that_does_not_exist"))
         .andDo(print())
         .andExpect(status().isNotFound)
     }
