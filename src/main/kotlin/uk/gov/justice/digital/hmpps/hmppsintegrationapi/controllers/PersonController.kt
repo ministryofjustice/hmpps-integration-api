9314dac97f2b469d75cd658de01e085295a2fa30
Emile Swarts <emile.swarts@digital.justice.gov.uk>
2022-12-15 16:06:11 +0000

    404 exception (#6)
    Throw an exception and return HTTP status code 404 to the client

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonController.kt
@@ -5,15 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.EntityNotFoundException
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetPersonService
 
 @RestController
 @RequestMapping("/persons")
 class PersonController(@Autowired val getPersonService: GetPersonService) {
-
   @GetMapping("{id}")
   fun getPerson(@PathVariable id: Int): Person? {
-    return getPersonService.execute(id)
+    val result = getPersonService.execute(id) ?: throw EntityNotFoundException("Could not find person with id: $id")
+
+    return result
   }
 }
