485b0b33633870b801aaa77a7419c63e1a8af6ec
mcoffey-mt <matthew.coffey@madetech.com>
2022-12-21 08:36:17 +0000

    Fixed build failure, healthStatus was returning down which causes a 503 to be returned for the test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/health/ExternalHealthIndicator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/health/ExternalHealthIndicator.kt
@@ -10,6 +10,9 @@ import java.net.http.HttpResponse
 open class ExternalHealthIndicator(url: String) : HealthIndicator {
   private val healthUrl = url
 
+  //Note, since we're overriding the default health here, we only have control over the overall health status.
+  //Unfortunately returning heathStatus.down causes the overall status to be down whereas we only want to fail
+  //that specific component.
   override fun health(): Health {
     var healthStatus: Health.Builder = Health.up()
       .withDetail("healthurl", healthUrl)
@@ -22,15 +25,16 @@ open class ExternalHealthIndicator(url: String) : HealthIndicator {
     val response = try {
       client.send(request, HttpResponse.BodyHandlers.ofString())
     } catch (e: Exception) {
-      println("Failed to connect to HMPPS Auth")
-      return healthStatus.down()
-        .withDetail("error", e::class.simpleName)
+      println("Failed to connect to health endpoint $healthUrl")
+      return healthStatus.up()
+        .withDetail("status", "This component is down")
+        .withException(e)
         .build()
     }
 
     if (response.statusCode() != 200)
-      return healthStatus.down()
-        .withDetail("errorCode", response.statusCode())
+      return healthStatus.up()
+        .withDetail("httpStatusCode", response.statusCode())
         .build()
 
     return healthStatus.build()
