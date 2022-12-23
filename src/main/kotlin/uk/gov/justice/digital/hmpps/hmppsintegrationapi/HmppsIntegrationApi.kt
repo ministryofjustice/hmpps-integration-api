2f8c7e489ee7d991cb5a249ccdd1442086ae2855
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2022-12-23 11:16:26 +0000

    Remove test API endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/HmppsIntegrationApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/HmppsIntegrationApi.kt
@@ -2,18 +2,10 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@SpringBootApplication()
+@SpringBootApplication
 class HmppsIntegrationApi
 
 fun main(args: Array<String>) {
   runApplication<HmppsIntegrationApi>(*args)
-}
-
-@RestController
-class TestApiController {
-  @GetMapping("/")
-  fun index(): String = "Test!"
 }
