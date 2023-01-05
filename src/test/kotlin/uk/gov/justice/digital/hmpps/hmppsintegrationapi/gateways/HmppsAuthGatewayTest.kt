8ac4ebe908a46a463f792ea0a6c7ad7aafe544a8
Emile Swarts <emile.swarts@digital.justice.gov.uk>
2023-01-05 10:59:32 +0000

    Move test application config file to config directory (#22)
    This configuration file will be picked up automatically and saves having
    to explicitly reference the active profile in tests.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/HmppsAuthGatewayTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/HmppsAuthGatewayTest.kt
@@ -4,14 +4,12 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.AuthenticationFailedException
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.mockservers.HmppsAuthMockServer
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Credentials
 
 @ContextConfiguration(initializers = [ConfigDataApplicationContextInitializer::class], classes = [(HmppsAuthGateway::class)])
-@ActiveProfiles("test")
 class HmppsAuthGatewayTest(hmppsAuthGateway: HmppsAuthGateway) :
   DescribeSpec({
     val hmppsAuthMockServer = HmppsAuthMockServer()
