50dc865258d74296fee3aa12ad183c80c2dda95f
Emile Swarts <emile.swarts@digital.justice.gov.uk>
2023-01-06 13:54:33 +0000

    Enable smoke tests (#25)
    * Enable smoke test
    
    This waits for the prison api to be up.
    Exit if failed to return expected payload from NOMIS, otherwise proceed.
    
    * Add running smoke tests as part of pipeline
    
    Co-authored-by: Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
    Co-authored-by: AprilMD <95350189+AprilMD@users.noreply.github.com>

---
+++ scripts/wait_for_prison_api_to_be_ready.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+max_retry=100
+tries=0
+
+until $(curl --output /dev/null --silent --head --fail http://prison-api:8080/health); do
+  if [ $tries -gt $max_retry ]; then
+    echo ""
+    exit 1
+  fi
+  ((tries=tries+1))
+
+  printf '.'
+  sleep 2
+done
