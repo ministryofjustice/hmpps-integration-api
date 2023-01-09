bb3f78a86b23dd3dd60110a2922473b90b5e5575
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-01-09 11:24:25 +0000

    match on substring for smoke test to prevent unnecessary failures in the future

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -4,7 +4,7 @@ set -e
 
 existing_person_id=A1234AL
 curl http://hmpps-integration-api:8080/persons/${existing_person_id} > /tmp/result
-expected_substring='{"firstName":"DANIEL","lastName":"SMELLEY"}'
+expected_substring='"firstName":"DANIEL","lastName":"SMELLEY"'
 
 echo "Running smoke test on prison API.."
 cat /tmp/result
