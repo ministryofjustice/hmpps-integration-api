64888145ed59880fb47b1f1a1ec82873ea81514c
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-01-10 09:25:33 +0000

    Refactor: Extract string extension for removing whitespace and newlines

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/StringExtensionTest.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class StringExtensionTest : DescribeSpec({
+  describe("#removeWhitespaceAndNewlines") {
+    it("removes all whitespace and new lines") {
+      """
+      {
+        "never": "gonna",
+        "give": "you",
+        "up": null
+      }
+      """.removeWhitespaceAndNewlines().shouldBe("{\"never\":\"gonna\",\"give\":\"you\",\"up\":null}")
+    }
+  }
+})
