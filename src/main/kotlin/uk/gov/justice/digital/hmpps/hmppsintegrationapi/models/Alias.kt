f3bae1f18400a4601e6b6e2f7de227e4b6b4144a
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-01-31 09:15:04 +0000

    Remove OpenAPI documentation and annotations
    We've decided to move away from generating our OpenAPI spec file
    from annotations - https://github.com/ministryofjustice/hmpps-integration-api/pull/37.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/Alias.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/Alias.kt
@@ -1,26 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models
 
 import com.fasterxml.jackson.annotation.JsonAlias
-import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
 data class Alias(
-  @field:Schema(
-    description = "First name",
-    example = "John",
-    type = "string"
-  )
   val firstName: String,
-
-  @field:Schema(
-    description = "Last name",
-    example = "Marston",
-    type = "string"
-  )
   val lastName: String,
-
   val middleName: String? = null,
-
   @JsonAlias("dob")
   var dateOfBirth: LocalDate? = null
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/Person.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/Person.kt
@@ -1,26 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models
 
-import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
 data class Person(
-  @field:Schema(
-    description = "First name",
-    example = "Arthur",
-    type = "string"
-  )
   val firstName: String,
-
-  @field:Schema(
-    description = "Last name",
-    example = "Morgan",
-    type = "string"
-  )
   val lastName: String,
-
   val middleName: String? = null,
-
   val dateOfBirth: LocalDate? = null,
-
   val aliases: List<Alias> = listOf()
 )
