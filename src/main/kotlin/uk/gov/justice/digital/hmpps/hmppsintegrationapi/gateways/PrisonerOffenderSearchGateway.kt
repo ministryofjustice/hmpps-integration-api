28ea3dddb4530754dbe4beb702d12f2a1507d577
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-02-01 11:57:06 +0000

    Add Prisoner Offender Search gateway

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/PrisonerOffenderSearchGateway.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
+
+import org.springframework.stereotype.Component
+
+@Component
+class PrisonerOffenderSearchGateway {
+  fun getPerson(id: String) = null
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetPersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetPersonService.kt
@@ -3,11 +3,17 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
 
 @Service
-class GetPersonService(@Autowired val nomisGateway: NomisGateway) {
+class GetPersonService(
+  @Autowired val nomisGateway: NomisGateway,
+  @Autowired val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway
+) {
   fun execute(id: String): Person? {
+    prisonerOffenderSearchGateway.getPerson(id)
+
     return nomisGateway.getPerson(id)
   }
 }
