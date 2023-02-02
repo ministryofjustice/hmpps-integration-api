244b3045c8bb0768c1dcc70b3e01663a282e6090
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-02-02 14:55:21 +0000

    Inline script to prepare prisoner-offender-search mock api endpoint

--- scripts/setup_prisoner_offender_search.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-curl https://prisoner-offender-search-dev.prison.service.justice.gov.uk/v3/api-docs > prisoner-offender-search.json
-
-sed -i "s+*/*+application/json+g" prisoner-offender-search.json
-
