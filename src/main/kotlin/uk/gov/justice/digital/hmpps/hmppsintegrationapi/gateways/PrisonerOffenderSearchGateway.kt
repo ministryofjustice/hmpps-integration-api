8f6b82497a207bd421ebe22c60a7596f33f821ec
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-02-02 10:51:02 +0000

    Fix values for username and password for Prisoner Offender Search

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/PrisonerOffenderSearchGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/PrisonerOffenderSearchGateway.kt
@@ -14,10 +14,10 @@ class PrisonerOffenderSearchGateway(@Value("\${services.prisoner-offender-search
   @Autowired
   lateinit var hmppsAuthGateway: HmppsAuthGateway
 
-  @Value("\${services.prison-api.hmpps-auth.username}")
+  @Value("\${services.prisoner-offender-search.hmpps-auth.username}")
   private lateinit var username: String
 
-  @Value("\${services.prison-api.hmpps-auth.password}")
+  @Value("\${services.prisoner-offender-search.hmpps-auth.password}")
   private lateinit var password: String
 
   fun getPerson(id: String): Person? {
