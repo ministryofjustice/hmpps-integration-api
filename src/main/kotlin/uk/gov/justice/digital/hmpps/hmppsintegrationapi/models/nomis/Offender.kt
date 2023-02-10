d408fb13b35495d5d18e650fe42fce4a37968c24
mcoffey-mt <matthew.coffey@madetech.com>
2023-02-10 07:58:05 +0000

    Added alias to imported namespace to remove huge in line import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/Offender.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/Offender.kt
@@ -3,13 +3,14 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.nomis
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Alias
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
 import java.time.LocalDate
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.nomis.Alias as nomisAlias
 
 data class Offender(
   val firstName: String,
   val lastName: String,
   val middleName: String? = null,
   val dateOfBirth: LocalDate? = null,
-  val aliases: List<uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.nomis.Alias> = listOf()
+  val aliases: List<nomisAlias> = listOf()
 ) {
   fun toPerson(): Person = Person(
     this.firstName,
