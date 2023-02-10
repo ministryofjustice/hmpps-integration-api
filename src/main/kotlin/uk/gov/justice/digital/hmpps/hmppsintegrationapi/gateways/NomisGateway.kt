bb02f9e5fbfd545356b99ae2a3d2bbc77c4daa29
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-02-10 14:26:19 +0000

    Update NOMIS gateway to return null when bad request or not found

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/NomisGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/NomisGateway.kt
@@ -1,15 +1,20 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
 
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.nomis.Offender
 
 @Component
 class NomisGateway(@Value("\${services.prison-api.base-url}") baseUrl: String) {
   private val webClient: WebClient = WebClient.builder().baseUrl(baseUrl).build()
+  private val log = LoggerFactory.getLogger(this::class.java)
 
   @Autowired
   lateinit var hmppsAuthGateway: HmppsAuthGateway
@@ -17,13 +22,20 @@ class NomisGateway(@Value("\${services.prison-api.base-url}") baseUrl: String) {
   fun getPerson(id: String): Person? {
     val token = hmppsAuthGateway.getClientToken("NOMIS")
 
-    return webClient
-      .get()
-      .uri("/api/offenders/$id")
-      .header("Authorization", "Bearer $token")
-      .retrieve()
-      .bodyToFlux(Offender::class.java)
-      .map { offender -> offender.toPerson() }
-      .blockFirst()
+    return try {
+      webClient
+        .get()
+        .uri("/api/offenders/$id")
+        .header("Authorization", "Bearer $token")
+        .retrieve()
+        .bodyToFlux(Offender::class.java)
+        .map { offender -> offender.toPerson() }
+        .blockFirst()
+    } catch (exception: WebClientResponseException.BadRequest) {
+      log.error("${exception.message} - ${Json.parseToJsonElement(exception.responseBodyAsString).jsonObject["developerMessage"]}")
+      null
+    } catch (exception: WebClientResponseException.NotFound) {
+      null
+    }
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/NomisGatewayTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/NomisGatewayTest.kt
@@ -2,12 +2,14 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import org.mockito.internal.verification.VerificationModeFactory
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.ContextConfiguration
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.mockservers.HmppsAuthMockServer
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.mockservers.NomisApiMockServer
@@ -15,7 +17,7 @@ import java.time.LocalDate
 
 @ContextConfiguration(
   initializers = [ConfigDataApplicationContextInitializer::class],
-  classes = [NomisGateway::class, HmppsAuthGateway::class]
+  classes = [NomisGateway::class]
 )
 class NomisGatewayTest(@MockBean val hmppsAuthGateway: HmppsAuthGateway, private val nomisGateway: NomisGateway) :
   DescribeSpec({
@@ -88,6 +90,38 @@ class NomisGatewayTest(@MockBean val hmppsAuthGateway: HmppsAuthGateway, private
         val person = nomisGateway.getPerson(offenderNo)
 
         person?.aliases.shouldBeEmpty()
+      }
+
+      it("returns null when 400 Bad Request is returned") {
+        nomisApiMockServer.stubGetOffender(
+          offenderNo,
+          """
+          {
+            "developerMessage": "reason for bad request"
+          }
+          """,
+          HttpStatus.BAD_REQUEST
+        )
+
+        val person = nomisGateway.getPerson(offenderNo)
+
+        person?.shouldBeNull()
+      }
+
+      it("returns null when 404 Not Found is returned") {
+        nomisApiMockServer.stubGetOffender(
+          offenderNo,
+          """
+          {
+            "developerMessage": "cannot find person"
+          }
+          """,
+          HttpStatus.NOT_FOUND
+        )
+
+        val person = nomisGateway.getPerson(offenderNo)
+
+        person?.shouldBeNull()
       }
     }
   })
