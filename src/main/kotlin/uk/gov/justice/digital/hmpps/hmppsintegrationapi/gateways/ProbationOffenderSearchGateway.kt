6cd25345fb7a7426ad368feca81e71f768eaf051
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-02-10 14:27:07 +0000

    Update Probation Offender Search gateway to return null when bad request or not found

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/ProbationOffenderSearchGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/ProbationOffenderSearchGateway.kt
@@ -1,16 +1,21 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
 
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.probationoffendersearch.Offender
 
 @Component
 class ProbationOffenderSearchGateway(@Value("\${services.probation-offender-search.base-url}") baseUrl: String) {
   private val webClient: WebClient = WebClient.builder().baseUrl(baseUrl).build()
+  private val log = LoggerFactory.getLogger(this::class.java)
 
   @Autowired
   lateinit var hmppsAuthGateway: HmppsAuthGateway
@@ -18,14 +23,21 @@ class ProbationOffenderSearchGateway(@Value("\${services.probation-offender-sear
   fun getPerson(id: String): Person? {
     val token = hmppsAuthGateway.getClientToken("Probation Offender Search")
 
-    return webClient
-      .post()
-      .uri("/search")
-      .header("Authorization", "Bearer $token")
-      .body(BodyInserters.fromValue(mapOf("nomsNumber" to id, "valid" to true)))
-      .retrieve()
-      .bodyToFlux(Offender::class.java)
-      .map { offender -> offender.toPerson() }
-      .blockFirst()
+    return try {
+      webClient
+        .post()
+        .uri("/search")
+        .header("Authorization", "Bearer $token")
+        .body(BodyInserters.fromValue(mapOf("nomsNumber" to id, "valid" to true)))
+        .retrieve()
+        .bodyToFlux(Offender::class.java)
+        .map { offender -> offender.toPerson() }
+        .blockFirst()
+    } catch (exception: WebClientResponseException.BadRequest) {
+      log.error("${exception.message} - ${Json.parseToJsonElement(exception.responseBodyAsString).jsonObject["developerMessage"]}")
+      null
+    } catch (exception: WebClientResponseException.NotFound) {
+      null
+    }
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/mockservers/ProbationOffenderSearchApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/mockservers/ProbationOffenderSearchApiMockServer.kt
@@ -5,13 +5,14 @@ import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.equalToJson
 import com.github.tomakehurst.wiremock.client.WireMock.matching
 import com.github.tomakehurst.wiremock.client.WireMock.post
+import org.springframework.http.HttpStatus
 
 class ProbationOffenderSearchApiMockServer : WireMockServer(WIREMOCK_PORT) {
   companion object {
     private const val WIREMOCK_PORT = 4002
   }
 
-  fun stubPostOffenderSearch(requestBody: String, responseBody: String) {
+  fun stubPostOffenderSearch(requestBody: String, responseBody: String, status: HttpStatus = HttpStatus.OK) {
     stubFor(
       post("/search")
         .withHeader(
@@ -22,7 +23,7 @@ class ProbationOffenderSearchApiMockServer : WireMockServer(WIREMOCK_PORT) {
         .willReturn(
           aResponse()
             .withHeader("Content-Type", "application/json")
-            .withStatus(200)
+            .withStatus(status.value())
             .withBody(responseBody.trimIndent()),
         ),
     )
