e8e457fdb00582742f03aafab68568abed290ea0
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-02-17 13:14:57 +0000

    Remove nullable for captureData

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ImageMetadata.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ImageMetadata.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models
 import java.time.LocalDate
 
 data class ImageMetadata(
-  val captureDate: LocalDate?,
+  val captureDate: LocalDate,
   val view: String,
   val orientation: String,
   val type: String,
