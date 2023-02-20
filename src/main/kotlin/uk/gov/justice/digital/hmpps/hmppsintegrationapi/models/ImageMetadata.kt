aa6408137aeec6021e6d40407c3388d32c7ae88d
mcoffey-mt <matthew.coffey@madetech.com>
2023-02-20 11:08:17 +0000

    Reintroduced imageId to image metadata, using local version of the prison-api schema

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ImageMetadata.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ImageMetadata.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models
 import java.time.LocalDate
 
 data class ImageMetadata(
+  val imageId : Long,
   val captureDate: LocalDate,
   val view: String,
   val orientation: String,
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/ImageDetail.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/ImageDetail.kt
@@ -4,12 +4,14 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.ImageMetadata
 import java.time.LocalDate
 
 class ImageDetail(
+  val imageId: Long,
   val captureDate: LocalDate,
   val imageView: String,
   val imageOrientation: String,
   val imageType: String,
 ) {
   fun toImageMetadata(): ImageMetadata = ImageMetadata(
+    imageId = this.imageId,
     captureDate = this.captureDate,
     view = this.imageView,
     orientation = this.imageOrientation,
