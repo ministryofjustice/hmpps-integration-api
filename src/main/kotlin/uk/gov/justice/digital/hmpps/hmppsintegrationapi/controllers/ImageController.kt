3a60e8f6cf691b5078dfad7087b7787260ce2743
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-02-21 12:33:52 +0000

    Update get image endpoint to return 404

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/ImageController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/ImageController.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.EntityNotFoundException
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetImageService
 
 @RestController
@@ -12,7 +14,13 @@ class ImageController(
   @Autowired val getImageService: GetImageService
 ) {
   @GetMapping("{id}")
-  fun getImage(@PathVariable id: String): ByteArray {
-    return byteArrayOf()
+  fun getImage(@PathVariable id: String): ByteArray? {
+    val image = getImageService.execute(id)
+
+    if (image == null) {
+      throw EntityNotFoundException("Could not find image with id: $id")
+    }
+
+    return image
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/ImageControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/ImageControllerTest.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import org.mockito.Mockito
+import org.mockito.internal.verification.VerificationModeFactory
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -16,12 +18,10 @@ internal class ImageControllerTest(
   @Autowired val mockMvc: MockMvc,
   @MockBean val getImageService: GetImageService,
 ) : DescribeSpec({
-
   val id = "2461788"
   val image = byteArrayOf(0x48, 101, 108, 108, 111)
 
   describe("GET /images/{id}") {
-
     beforeTest {
       Mockito.reset(getImageService)
       whenever(getImageService.execute(id)).thenReturn(image)
@@ -31,6 +31,27 @@ internal class ImageControllerTest(
       val result = mockMvc.perform(get("/images/$id")).andReturn()
 
       result.response.status.shouldBe(200)
+    }
+
+    it("responds with a 404 NOT FOUND status") {
+      val idThatDoesNotExist = "98765"
+      whenever(getImageService.execute(id)).thenReturn(null)
+
+      val result = mockMvc.perform(get("/images/$idThatDoesNotExist")).andReturn()
+
+      result.response.status.shouldBe(404)
+    }
+
+    it("retrieves a image with the matching ID") {
+      mockMvc.perform(get("/images/$id")).andReturn()
+
+      verify(getImageService, VerificationModeFactory.times(1)).execute(id)
+    }
+
+    it("returns an image with the matching ID") {
+      val result = mockMvc.perform(get("/images/$id")).andReturn()
+
+      result.response.contentAsByteArray.shouldBe(image)
     }
   }
 })
