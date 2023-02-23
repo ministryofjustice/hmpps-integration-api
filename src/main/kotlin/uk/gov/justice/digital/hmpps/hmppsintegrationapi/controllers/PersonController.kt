55c50bd85d24551a883c2b86277c8b8092565817
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-02-23 14:59:13 +0000

    Add controller action for getting person addresses

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/PersonController.kt
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.EntityNotFoundException
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Address
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.ImageMetadata
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetAddressesForPersonService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetImageMetadataForPersonService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetPersonService
 
@@ -15,7 +17,8 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetPersonServic
 @RequestMapping("/persons")
 class PersonController(
   @Autowired val getPersonService: GetPersonService,
-  @Autowired val getImageMetadataForPersonService: GetImageMetadataForPersonService
+  @Autowired val getImageMetadataForPersonService: GetImageMetadataForPersonService,
+  @Autowired val getAddressesForPersonService: GetAddressesForPersonService
 ) {
   @GetMapping("{id}")
   fun getPerson(@PathVariable id: String): Map<String, Person?> {
@@ -33,5 +36,12 @@ class PersonController(
     val images = getImageMetadataForPersonService.execute(id)
 
     return mapOf("images" to images)
+  }
+
+  @GetMapping("{id}/addresses")
+  fun getPersonAddresses(@PathVariable id: String): Map<String, List<Address>> {
+    val addresses = getAddressesForPersonService.execute(id)
+
+    return mapOf("addresses" to addresses)
   }
 }
