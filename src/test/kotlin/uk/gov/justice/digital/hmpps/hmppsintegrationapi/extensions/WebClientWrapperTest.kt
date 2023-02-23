b352c100679e95df013386de52bd69072bf387df
mcoffey-mt <matthew.coffey@madetech.com>
2023-02-23 10:02:54 +0000

    Generic mock server added

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/WebClientWrapperTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/WebClientWrapperTest.kt
@@ -1,15 +1,47 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.mockservers.GenericApiMockServer
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
 
+class TestModel(val sourceName: String) {
+  // fun toTestDomainModel() => TestDomainModel(sourceName)
+}
+
+class TestDomainModel(val name: String)
+
 class WebClientWrapperTest : DescribeSpec({
+  val mockServer = GenericApiMockServer()
+  val id = "ABC1234"
+
+  beforeTest() {
+    mockServer.stubGetTest(
+      id,
+      """
+        {
+          "sourceName" : "Harold"
+        }
+        """
+    )
+  }
+
+  beforeEach() {
+    mockServer.start()
+  }
+
+  afterTest() {
+    mockServer.stop()
+  }
+
   describe("#makeWebClientRequest") {
     it("makes a get request") {
-      val id = "1234"
       val token = "4567"
-      val webClient = WebClientWrapper(baseUrl = "http://localhost:4030", uri = "/api/offenders/$id", authToken = token)
-      webClient.get<Person>()
+
+      val webClient = WebClientWrapper(baseUrl = mockServer.baseUrl(), uri = "/test/$id", authToken = token)
+      val person = webClient.get<Person>()
+
+      person?.firstName.shouldBe("Frank")
     }
   }
 })
