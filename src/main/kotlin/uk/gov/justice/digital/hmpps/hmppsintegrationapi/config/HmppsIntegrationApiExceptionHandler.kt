b2cb312a0f4cbc8e137ae6287929dea8b323ec7b
Emile Swarts <emile.swarts@digital.justice.gov.uk>
2023-03-07 12:41:18 +0000

    Upgrade to Spring Boot 3 (#79)
    * Upgrade to Spring Boot 3
    
    This upgrades the application to use Spring Boot 3.x
    Upgrade Gradle
    Use latest version of wiremock, which is required with the upgrade,
    sadly kotest hasn't upgraded their implementation.
    Remove microsoft log appender, which is no longer compatible.
    
    * Remove Spring boot properties migrator
    
    * Replace all instances of logAppender with ConsoleAppender

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/HmppsIntegrationApiExceptionHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/HmppsIntegrationApiExceptionHandler.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.config
 
+import jakarta.validation.ValidationException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.BAD_REQUEST
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.AuthenticationFailedException
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.EntityNotFoundException
-import javax.validation.ValidationException
 
 @RestControllerAdvice
 class HmppsIntegrationApiExceptionHandler {
