6aa0eaaddf12b8bd14b8b62b53365be88dfa94ba
Emile <emile.swarts@digital.justice.gov.uk>
2023-03-08 13:03:43 +0000

    Revert "Revert "Upgrade to Spring Boot 3 (#79)""
    This reverts commit 2592e4cca439a1ac01353ff5ceec6cbb7787910e.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.4-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.0"
   kotlin("plugin.spring") version "1.8.0"
 }
 
@@ -13,7 +13,7 @@ dependencies {
 
   testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.4")
   testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.4")
-  testImplementation("io.kotest.extensions:kotest-extensions-wiremock:1.0.3")
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
   testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
   testImplementation("org.mockito:mockito-core:4.9.0")
 
