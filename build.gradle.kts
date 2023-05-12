05368495ccc083b8b192632f8a2bd0e7903db6a2
Emile <emile.swarts@digital.justice.gov.uk>
2023-05-12 09:05:33 +0100

    Bump Gradle Spring Boot version
    Our Owasp security scan is currently failing, bumping versions to
    address this.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.2"
-  kotlin("plugin.spring") version "1.8.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4"
+  kotlin("plugin.spring") version "1.8.21"
 }
 
 configurations {
