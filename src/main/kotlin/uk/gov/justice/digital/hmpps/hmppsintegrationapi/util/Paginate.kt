f53e08d40e7d465844dddbaef76a311376231bb8
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-05-26 09:34:43 +0100

    Update pagination for searching for a person endpoint (#154)
    This improves pagination by removing unnecessary properties, duplicated
    values and adding more meaningful property names.
    
    It also updates our OpenAPI spec.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/Paginate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/Paginate.kt
@@ -1,14 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.util
 
 import org.springframework.data.domain.PageImpl
-import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.PageRequest
 
-infix fun <T> List<T>.paginateWith(paginationOptions: Pageable): PageImpl<T> {
+fun <T> List<T>.paginateWith(page: Int = 1, perPage: Int = 10): PaginatedResponse<T> {
+  val paginationOptions = PageRequest.of(page - 1, perPage)
   val start = paginationOptions.pageSize * paginationOptions.pageNumber
   val end = (start + paginationOptions.pageSize).coerceAtMost(this.size)
 
   if (start > end) {
-    return PageImpl(listOf<T>(), paginationOptions, this.size.toLong())
+    return PaginatedResponse(PageImpl(listOf<T>(), paginationOptions, this.size.toLong()))
   }
-  return PageImpl(this.subList(start, end), paginationOptions, this.count().toLong())
+
+  return PaginatedResponse(PageImpl(this.subList(start, end), paginationOptions, this.count().toLong()))
 }
