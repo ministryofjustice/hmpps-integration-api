42702d5eadb60e38f457716bcad38904250098d2
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-06-01 12:42:54 +0100

    fail on purpose

--- scripts/smoke-test-deployed-environment.sh
+++ scripts/smoke-test-deployed-environment.sh
@@ -2,4 +2,8 @@
 
 set -o pipefail
 
-curl -s -o /dev/null -w "%{http_code}%" -L "https://google.com";
+#curl -s -o /dev/null -w "%{http_code}%" -L "https://google.com";
+
+echo "fail on purpose"
+
+exit 1
