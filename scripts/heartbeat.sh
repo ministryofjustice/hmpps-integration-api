5389d5a2c566c7c59ba296effe154f1f590dfbff
mcoffey-mt <matthew.coffey@madetech.com>
2023-06-02 11:57:31 +0100

    Log fake apikey

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -6,12 +6,12 @@ set -o pipefail
 #curl -s -o /dev/null -w "%{http_code}%" -L "https://google.com";
 
 echo "Generating certificates";
-./generate-client.certificate.sh "${CIRCLE_PROJECT_REPONAME}" "heartbeat";
+./scripts/generate-client.certificate.sh "${CIRCLE_PROJECT_REPONAME}" "heartbeat";
 echo "Certificates generated";
 
 echo "Retrieving API key from Circle CI secret";
 API_KEY=${HEARTBEAT_API_KEY}
-echo "Got api key"
+echo "Got test fake api key: ${API_KEY}"
 
 echo "fail on purpose"
 
