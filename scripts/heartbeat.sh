11deb131a90530d977b47c73b01bb63cb953d531
mcoffey-mt <matthew.coffey@madetech.com>
2023-06-02 14:16:18 +0100

    Tweaks to heartbeat script

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -6,14 +6,14 @@ set -o pipefail
 #curl -s -o /dev/null -w "%{http_code}%" -L "https://google.com";
 
 echo "Retrieving certificates from context";
-CERT=${MTLS_CERT} > /tmp/client.pem
-KEY=${MTLS_KEY} >  /tmp/client.key
+echo -n "${MTLS_CERT}" > /tmp/client.pem &&
+echo -n "${MTLS_KEY}" >  /tmp/client.key
 echo "Certificates retrieved";
 
 echo "Retrieving API key from Circle CI secret";
 API_KEY=${API_KEY}
 
-curl ${SERVICE_URL} -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key |grep firstName &&
+curl "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key |grep firstName &&
 echo $?
 
 echo "fail on purpose"
