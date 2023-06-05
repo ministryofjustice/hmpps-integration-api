866fd0bd56c97c71bb706324d02bb3f41edb1a04
mcoffey-mt <matthew.coffey@madetech.com>
2023-06-05 12:07:22 +0100

    Decode certs from circleci

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -5,15 +5,11 @@ set -o pipefail
 #curl -s -o /dev/null -w "%{http_code}%" -L "https://google.com";
 
 echo "Retrieving certificates from context";
-echo -n "${MTLS_CERT}" > /tmp/client.pem &&
-echo -n "${MTLS_KEY}" >  /tmp/client.key
+echo -n "${MTLS_CERT}" | base64 --decode > /tmp/client.pem
+echo -n "${MTLS_KEY}" | base64 --decode > /tmp/client.key
 echo "Certificates retrieved";
-cat /tmp/client.pem;
-cat /tmp/client.key;
 
 echo "Retrieving API key from Circle CI secret";
-API_KEY=${API_KEY}
-
 curl "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key |grep firstName &&
 echo $?
 
