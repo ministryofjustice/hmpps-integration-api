c16278d818f3f4206f72fbfccb0d265942274631
mcoffey-mt <matthew.coffey@madetech.com>
2023-06-05 10:32:55 +0100

    Log certs

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -2,18 +2,19 @@
 
 set -o pipefail
 
-
 #curl -s -o /dev/null -w "%{http_code}%" -L "https://google.com";
 
 echo "Retrieving certificates from context";
 echo -n "${MTLS_CERT}" > /tmp/client.pem &&
 echo -n "${MTLS_KEY}" >  /tmp/client.key
 echo "Certificates retrieved";
+cat /tmp/client.pem;
+cat /tmp/client.key;
 
 echo "Retrieving API key from Circle CI secret";
 API_KEY=${API_KEY}
 
-curl "https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons?first_name=Frank" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key |grep firstName &&
+curl "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key |grep firstName &&
 echo $?
 
 echo "fail on purpose"
