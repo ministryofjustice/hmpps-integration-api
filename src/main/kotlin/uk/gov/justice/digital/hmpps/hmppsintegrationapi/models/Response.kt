2f601de6c9c08b2e08f3f07d718520fd20d54304
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-06-05 10:40:44 +0100

    Update get person addresses condition when not found is returned
    Only return 404 NOT FOUND when service returns errors for both upstream
    APIs as it might be that addresses is found in one API but not another.
    This is still valid.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/Response.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/Response.kt
@@ -2,8 +2,12 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models
 
 data class Response<T>(val data: T, val errors: List<UpstreamApiError> = emptyList(), val description: String? = null) {
   companion object {
-    fun <T> merge(responses: List<Response<List<T>>>): Response<List<T>> = Response(data = responses.flatMap { it.data }, errors = responses.flatMap { it.errors })
+    fun <T> merge(responses: List<Response<List<T>>>): Response<List<T>> =
+      Response(data = responses.flatMap { it.data }, errors = responses.flatMap { it.errors })
   }
 
   fun hasError(type: UpstreamApiError.Type): Boolean = this.errors.any { it.type == type }
+
+  fun hasErrorCausedBy(type: UpstreamApiError.Type, causedBy: UpstreamApi): Boolean =
+    this.errors.any { it.type == type && it.causedBy == causedBy }
 }
