9efcbfdbf0ba8867162f5bc199f729333548e2e4
mcoffey-mt <matthew.coffey@madetech.com>
2023-06-06 12:26:45 +0100

    Add checks for existance of environment variables

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -4,14 +4,17 @@ set -o pipefail
 
 requiredVars=("MTLS_KEY" "MTLS_CERT" "SERVICE_URL" "API_KEY")
 
-for str in requiredVars[@]; do
-  if [[ -z "${!$str}" ]]; then
+for str in "${requiredVars[@]}"; do
+  if [[ -z "${!str}" ]]; then
     echo "CIRCLECI Context variable was undefined: $str"
-    exit 1
-  else
-    echo "Found secret $str"
+    fail=1
   fi
 done
+
+if [ $fail -eq 1 ]; then
+  echo "**FAIL**: Missing env variables"
+  exit 1
+fi
 
 echo "Retrieving certificates from context";
 echo -n "${MTLS_CERT}" | base64 --decode > /tmp/client.pem
