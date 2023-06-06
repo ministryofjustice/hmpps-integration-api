9e2339665a2d04f56b2bb1f3b8916e9d3b4399fc
mcoffey-mt <matthew.coffey@madetech.com>
2023-06-06 11:46:31 +0100

    Validate context variables exist before executing

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -2,7 +2,14 @@
 
 set -o pipefail
 
-#curl -s -o /dev/null -w "%{http_code}%" -L "https://google.com";
+requiredVars=("MTLS_KEY" "MTLS_CERT" "SERVICE_URL" "API_KEY")
+
+for str in requiredVars[@]; do
+  if [[ -z "${!$str}" ]]; then
+    echo "CIRCLECI Context variable was undefined: $str"
+    exit 1
+  fi
+done
 
 echo "Retrieving certificates from context";
 echo -n "${MTLS_CERT}" | base64 --decode > /tmp/client.pem
@@ -11,10 +18,9 @@ echo "Certificates retrieved";
 
 curl --silent "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key | grep firstName
 
-if [ $? -eq 0 ]
-then
-echo "Located firstName in response"
-echo "**SUCCESS**"
+if [ $? -eq 0 ]; then
+  echo "Located firstName in response"
+  echo "**SUCCESS**"
 fi
 
 exit $?
