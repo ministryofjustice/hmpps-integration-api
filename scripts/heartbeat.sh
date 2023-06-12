689daa57cd63da7ee46c1b98c61a5d041d35dc42
mcoffey-mt <matthew.coffey@madetech.com>
2023-06-12 09:11:05 +0100

    Hide response

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -21,7 +21,7 @@ echo -n "${MTLS_CERT}" | base64 --decode > /tmp/client.pem
 echo -n "${MTLS_KEY}" | base64 --decode > /tmp/client.key
 echo "Certificates retrieved";
 
-curl --silent -o /dev/null "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key | grep firstName
+curl --silent "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key | grep firstName > /dev/null
 
 if [ $? -eq 0 ]; then
   echo "Located firstName in response"
