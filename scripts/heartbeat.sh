a84915e399da246387ab3c19fc50138df53169de
mcoffey-mt <matthew.coffey@madetech.com>
2023-06-13 09:21:29 +0100

    Tidy up naming and logging

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -6,13 +6,13 @@ requiredVars=("MTLS_KEY" "MTLS_CERT" "SERVICE_URL" "API_KEY")
 
 for str in "${requiredVars[@]}"; do
   if [[ -z "${!str}" ]]; then
-    echo "CIRCLECI Context variable was undefined: $str"
+    echo "CircleCI context variable was undefined: $str"
     fail=1
   fi
 done
 
 if [ $fail = 1 ]; then
-  echo "**FAIL**: Missing env variables"
+  echo "**FAIL** Missing CircleCI context variable(s)"
   exit 1
 fi
 
@@ -24,10 +24,9 @@ echo "Certificates retrieved";
 curl --silent "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key | grep firstName > /dev/null
 
 if [ $? -eq 0 ]; then
-  echo "Located firstName in response"
-  echo "**SUCCESS**"
+  echo "**SUCCESS** Located firstName in response"
   exit 0
 else
-  echo "Failed to locate firstName in response"
+  echo "**FAIL** Failed to locate firstName in response"
   exit 1
 fi
