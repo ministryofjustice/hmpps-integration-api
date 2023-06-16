ab071ba6113807df1f7311929bf6cde6ba36b490
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-06-16 12:02:32 +0100

    Update heartbeat to retry 5 times in 10 secs intervals before failing (#183)
    * Fix unary operator expected error
    
    * Update heartbeat to retry 5 times in 10 secs intervals before failing
    
    * Update logging to output retry configuration
    
    * Update heartbeat job names to be more distinguishable

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -2,31 +2,50 @@
 
 set -o pipefail
 
+retry_attempts=5
+retry_after_seconds=10
 requiredVars=("MTLS_KEY" "MTLS_CERT" "SERVICE_URL" "API_KEY")
+
+echo "Heartbeat Checker"
+echo -e "=================\n"
+
+echo "[Config] Maximum retry attempts: $retry_attempts"
+echo "[Config] Retry after: $retry_after_seconds seconds"
 
 for str in "${requiredVars[@]}"; do
   if [[ -z "${!str}" ]]; then
-    echo "CircleCI context variable was undefined: $str"
+    echo "[Config] CircleCI context variable was undefined: $str"
     fail=1
   fi
 done
 
-if [ $fail = 1 ]; then
-  echo "**FAIL** Missing CircleCI context variable(s)"
+if [[ $fail == 1 ]]; then
+  echo "[Config] 💔️️ Failed! Missing CircleCI context variable(s)"
   exit 1
 fi
 
-echo "Retrieving certificates from context";
+echo -e "\n[Setup] Retrieving certificates from context";
 echo -n "${MTLS_CERT}" | base64 --decode > /tmp/client.pem
 echo -n "${MTLS_KEY}" | base64 --decode > /tmp/client.key
-echo "Certificates retrieved";
+echo -e "[Setup] Certificates retrieved\n";
 
-curl --silent "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key | grep firstName > /dev/null
+for (( attempts=1;attempts<=retry_attempts;attempts++ ))
+do
+  echo "[Attempt ${attempts}] 🧑‍⚕️ Checking for a heartbeat..."
+  curl --silent "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key | grep firstName > /dev/null
 
-if [ $? -eq 0 ]; then
-  echo "**SUCCESS** Located firstName in response"
-  exit 0
-else
-  echo "**FAIL** Failed to locate firstName in response"
-  exit 1
-fi
+  if [[ $? == 0 ]]; then
+    echo "[Attempt ${attempts}] ✅ Success! Located firstName in response"
+    exit 0
+  else
+    echo -e "[Attempt ${attempts}] 💔️ Failed! Unable to locate firstName in response"
+
+    if [[ $attempts != "$retry_attempts" ]]; then
+      sleep $retry_after_seconds
+      echo -e "[Attempt ${attempts}] 🔁 Retrying..."
+    fi
+    echo
+  fi
+done
+
+exit 1
