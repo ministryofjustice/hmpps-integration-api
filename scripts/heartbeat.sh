823725e725440b119077be38008a8397326e98a7
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-06-16 10:16:04 +0100

    Update heartbeat to retry 5 times in 10 secs intervals before failing

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -2,6 +2,8 @@
 
 set -o pipefail
 
+retry_attempts=5
+retry_after_seconds=10
 requiredVars=("MTLS_KEY" "MTLS_CERT" "SERVICE_URL" "API_KEY")
 
 for str in "${requiredVars[@]}"; do
@@ -21,12 +23,23 @@ echo -n "${MTLS_CERT}" | base64 --decode > /tmp/client.pem
 echo -n "${MTLS_KEY}" | base64 --decode > /tmp/client.key
 echo "Certificates retrieved";
 
-curl --silent "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key | grep firstName > /dev/null
+for (( attempts=1;attempts<=retry_attempts;attempts++ ))
+do
+  echo "[Attempt ${attempts}] 🧑‍⚕️ Checking for a heartbeat..."
+  curl --silent "${SERVICE_URL}" -H "x-api-key: ${API_KEY}" --cert /tmp/client.pem --key /tmp/client.key | grep firstName > /dev/null
 
-if [ $? -eq 0 ]; then
-  echo "**SUCCESS** Located firstName in response"
-  exit 0
-else
-  echo "**FAIL** Failed to locate firstName in response"
-  exit 1
-fi
+  if [[ $? == 0 ]]; then
+    echo "[Attempt ${attempts}] ✅ Success! Located firstName in response"
+    exit 0
+  else
+    echo -e "[Attempt ${attempts}] 💔️ Failed! Unable to locate firstName in response"
+
+    if [[ $attempts != "$retry_attempts" ]]; then
+      sleep $retry_after_seconds
+      echo -e "[Attempt ${attempts}] 🔁 Retrying..."
+    fi
+    echo
+  fi
+done
+
+exit 1
