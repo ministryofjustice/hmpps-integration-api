94a34018d5af682631be82ca22a7a33920bcb8e5
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-06-21 16:03:43 +0100

    Add tests for Prisoner model from Prisoner Offender Search

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/prisoneroffendersearch/PrisonerTest.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisoneroffendersearch
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import java.time.LocalDate
+
+class PrisonerTest : DescribeSpec(
+  {
+    describe("#toPerson") {
+      it("maps one-to-one attributes to person attributes") {
+        val prisoner = Prisoner(
+          firstName = "First Name",
+          lastName = "Last Name",
+          middleNames = "Middle Name",
+          dateOfBirth = LocalDate.parse("2023-03-01"),
+          prisonerNumber = "prisonerNumber",
+          pncNumber = "pncNumber",
+          aliases = listOf(
+            PrisonerAlias(
+              firstName = "Alias First Name",
+              lastName = "Alias Last Name",
+              middleNames = "Alias Middle Names",
+              dateOfBirth = LocalDate.parse("2023-01-01"),
+            ),
+          ),
+        )
+
+        val person = prisoner.toPerson()
+
+        person.firstName.shouldBe(prisoner.firstName)
+        person.lastName.shouldBe(prisoner.lastName)
+        person.middleName.shouldBe(prisoner.middleNames)
+        person.dateOfBirth.shouldBe(prisoner.dateOfBirth)
+        person.aliases.first().firstName.shouldBe("Alias First Name")
+        person.aliases.first().lastName.shouldBe("Alias Last Name")
+        person.aliases.first().middleName.shouldBe("Alias Middle Names")
+        person.aliases.first().dateOfBirth.shouldBe(LocalDate.parse("2023-01-01"))
+        person.prisonerId.shouldBe(prisoner.prisonerNumber)
+        person.pncId.shouldBe(prisoner.pncNumber)
+      }
+    }
+  },
+)
