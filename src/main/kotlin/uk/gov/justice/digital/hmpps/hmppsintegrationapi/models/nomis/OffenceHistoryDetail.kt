352b45d0e8d7af33531d5ed95124bfd2c58f9c65
Luke Williams <108728588+luke-a-williams@users.noreply.github.com>
2023-06-29 14:30:25 +0100

    Update offence endpoint to include more fields from our golden record (#195)
    * update offence endpoint to include more fields from our golden record
    
    * fix for the linting issues

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/OffenceHistoryDetail.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/OffenceHistoryDetail.kt
@@ -4,13 +4,19 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Offence
 import java.time.LocalDate
 
 data class OffenceHistoryDetail(
-  val offenceDate: LocalDate,
+  val courtDate: LocalDate,
   val offenceCode: String,
+  val offenceDate: LocalDate,
   val offenceDescription: String,
+  val offenceRangeDate: LocalDate,
+  val statuteCode: String,
 ) {
   fun toOffence(): Offence = Offence(
-    date = offenceDate,
-    code = offenceCode,
-    description = offenceDescription,
+    cjsCode = this.offenceCode,
+    courtDate = this.courtDate,
+    description = this.offenceDescription,
+    endDate = this.offenceRangeDate,
+    startDate = this.offenceDate,
+    statuteCode = this.statuteCode,
   )
 }
