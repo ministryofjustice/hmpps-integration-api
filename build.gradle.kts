7d600a86f05465c2d8bb52fb3f13394cd01f4217
Emile <emile.swarts@digital.justice.gov.uk>
2023-07-05 11:41:02 +0100

    Bump HMPPS Gradle Spring Boot and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
-  kotlin("plugin.spring") version "1.8.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.2"
+  kotlin("plugin.spring") version "1.8.22"
 }
 
 configurations {
@@ -9,8 +9,8 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.15.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+  implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.24.0")
   implementation("io.sentry:sentry-logback:6.15.0")
   implementation("org.springframework.data:spring-data-commons:3.1.0")
 
