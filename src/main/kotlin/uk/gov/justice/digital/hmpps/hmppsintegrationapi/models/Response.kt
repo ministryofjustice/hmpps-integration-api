cb74b38879215036e0ea3690571f13fc72de776a
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-07-10 14:53:04 +0100

    Remove unused property in Response class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/Response.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/Response.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models
 
-data class Response<T>(val data: T, val errors: List<UpstreamApiError> = emptyList(), val description: String? = null) {
+data class Response<T>(val data: T, val errors: List<UpstreamApiError> = emptyList()) {
   companion object {
     fun <T> merge(responses: List<Response<List<T>>>): Response<List<T>> =
       Response(data = responses.flatMap { it.data }, errors = responses.flatMap { it.errors })
