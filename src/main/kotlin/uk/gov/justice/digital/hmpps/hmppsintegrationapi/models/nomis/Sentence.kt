8969a92e2f56acc4ab4dcefdea8acbadbb5b1280
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-07-11 15:33:44 +0100

    Extract sentence detail model from sentence model

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/Sentence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/Sentence.kt
@@ -1,16 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.nomis
 
-import java.time.LocalDate
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Sentence as IntegrationApiSentence
 
 data class Sentence(
   val sentenceDetail: SentenceDetail?,
 ) {
-  fun toSentence(): IntegrationApiSentence = IntegrationApiSentence(
+  fun toSentence() = IntegrationApiSentence(
     startDate = this.sentenceDetail?.sentenceStartDate,
-  )
-
-  data class SentenceDetail(
-    val sentenceStartDate: LocalDate?,
   )
 }
