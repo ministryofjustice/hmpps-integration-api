969a998faa2867acefb10d91e190b99b17b8d1f4
Luke Williams <lawilliams1993@gmail.com>
2023-07-17 19:29:27 +0100

    Add a new tooling directory, declare constants properly, create requirements.txt for virtual environment and initialise a simple script for information retrieval

---
+++ tooling/retrieve_schema_fields.py
@@ -0,0 +1,16 @@
+from constants import common
+
+import pandas as pd
+import requests
+import yaml
+
+json_extract = common.extract_data(common.URL)
+
+schema_field_df = pd.DataFrame(columns=["Schema", "Field"])
+for schema in json_extract["components"]["schemas"]:
+    for field in json_extract["components"]["schemas"][schema]["properties"]:
+        new_row_dict = {"Schema": [schema], "Field": [field]}
+        schema_field_df = pd.concat([schema_field_df, pd.DataFrame(data=new_row_dict)], axis=0)
+schema_field_df = schema_field_df.reset_index(drop=True)
+
+schema_field_df.to_csv("outputs/Schema_Field.csv")
