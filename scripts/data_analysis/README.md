fc76061193b76cb7da04633e91e4e94719d9ebcb
Luke Williams <108728588+luke-a-williams@users.noreply.github.com>
2023-07-24 13:20:38 +0100

    initialise schema hierarchy script (#214)
    * initialise schema hierarchy script
    
    * updated python version to match graphviz dependency

--- scripts/data_analysis/README.md
+++ scripts/data_analysis/README.md
@@ -7,6 +7,7 @@ This repository contains Python scripts and modules for various tooling purposes
 - [Installation](#installation)
 - [Scripts](#scripts)
   - [Retrieve Schemas and their fields from Prison API](#retrieve-schemas-and-their-fields-from-prison-api)
+  - [Generate a Schema space diagram, output child-parent relations](#generate-a-schema-space-diagram-output-child-parent-relations)
 - [Modules](#modules)
   - [Constants](#constants)
 - [Contributing](#contributing)
@@ -15,7 +16,7 @@ This repository contains Python scripts and modules for various tooling purposes
 
 Before running the scripts in this repository, make sure you have the following:
 
-- Python 3.6 or above
+- Python 3.7 or above
 - Virtual environment - included in Installation instructions below
 
 ## Installation
@@ -34,8 +35,33 @@ To run this script *from within the tooling folder*, based on your python distri
 ```shell
 python3 retrieve_schema_fields.py
 ```
-Outputs:
-A csv file in the [outputs](outputs) directory containing
+>Outputs:
+> - A csv file in the [outputs](outputs) directory containing schemas and fields within them
+
+### Generate a Schema space diagram, output child-parent relations
+[generate_schema_diagram.py](generate_schema_diagram.py)
+There are several options for the running of this script:
+- Search for one schema
+- Search for multiple schema
+- No Search option - generate full diagram
+
+To run this script *from within the tooling folder*, based on your Python distribution:
+- Search for one schema, where the argument is a schema name in a string format
+```shell
+python3 generate_schema_diagram.py "AddressDto"
+```
+- Search for mulitiple schemas, where the arguments are all strings of schema names:
+```shell
+python3 generate_schema_diagram.py "AddressDto" "SentenceCalcDates"
+```
+- No search option, generating a full diagram:
+```shell
+python3 generate_schema_diagram.py
+```
+
+>Outputs
+> - A csv file with the parent child relations of schemas.
+> - A diagram in .dot format, renderable in plantuml or local graphviz renderer, of the schema relations
 
 ## Modules
 This repository contains the following Python modules:
@@ -46,4 +72,4 @@ This allows you to edit constants in one location, without having to amend other
 Feel free to explore them for more functionality.
 
 ## Contributing
-Contributions to this tooling section are welcome, as long as they can be executed in python. Autodocumentation is a potential and the desire is to keep this option open as this tooling section expands.
+Contributions to this tooling section are welcome, as long as they can be executed in Python. Autodocumentation is a potential and the desire is to keep this option open as this tooling section expands.
--- scripts/data_analysis/constants/common.py
+++ scripts/data_analysis/constants/common.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import requests
 import yaml
+import os
 
 URL = 'https://api-dev.prison.service.justice.gov.uk/v3/api-docs'
+SCHEMA_FIELD_FILE = "outputs/schema_field.csv"
+SCHEMA_PARENT_CHILD_FILE = "outputs/schema_parent_child.csv"
+SCHEMA_DIAGRAM = "outputs/schema_hierachy.dot"
+
+def prepare_directory(filename=""):
+  if os.path.exists("outputs/") == False:
+      os.mkdir("outputs/")
+  if os.path.exists(f"outputs/{filename}") and str(filename) != "":
+      os.remove(f"filename")
 
 def extract_data(url=URL):
     """
@@ -25,3 +35,47 @@ def extract_data(url=URL):
     else:
         print(url, " responded with ", response.status_code)
         return '{}'
+
+def findParentSchema(response_dict, child_schema):
+    """
+    Search all schemas for any reference to the child schema (i.e. find all direct parents) and return results in a dataframe.
+    This function assumes the dictionary object passed in is generated from OpenAPI Swagger spec
+
+        Parameters:
+            response_dict (dict): A dictionary object representing the extract from API Docs, such as the output of the extract_data function above
+            child_schema (str): A string of the exact schema name to search for
+
+        Returns:
+            data_frame (pd.DataFrame): a dataframe of schema, field and reference information.
+                Default value is an empty dataframe if no results are found, to avoid type errors in concatenation
+
+    """
+    data_frames = []
+
+    for schema in response_dict["components"]["schemas"]:
+        for field in response_dict["components"]["schemas"][schema]["properties"]:
+            try:
+                if type(response_dict["components"]["schemas"][schema]["properties"][field]["$ref"]) is str:
+                    nested_ref = response_dict["components"]["schemas"][schema]["properties"][field]["$ref"]
+            except KeyError as ke:
+                nested_ref = ""
+
+            try:    
+                if type(response_dict["components"]["schemas"][schema]["properties"][field]["items"]["$ref"] is str):
+                    nested_item_ref = response_dict["components"]["schemas"][schema]["properties"][field]["items"]["$ref"]
+            except KeyError as ke:
+                nested_item_ref = ""
+
+            if child_schema in nested_ref: #Note that this has the increased benefit of a partial match effect too 
+                data_dict = {'Parent_Schema': [schema], 'Field': [field], 'Child_Schema': [nested_ref.split('/')[-1]]}
+                data_frames.append(pd.DataFrame(data=data_dict))
+            elif child_schema in nested_item_ref:
+                data_dict = {'Parent_Schema': [schema], 'Field': [field], 'Child_Schema': [nested_item_ref.split('/')[-1]]}
+                data_frames.append(pd.DataFrame(data=data_dict))
+    
+    if data_frames == []:
+        return pd.DataFrame()
+    else:
+        data_frame = pd.concat(data_frames, axis=0)
+        data_frame = data_frame.reset_index(drop=True)
+        return data_frame
