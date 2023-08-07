b2e503cd9a2be1e8690052d70cab6232d4c98ebc
Emile Swarts <emile.swarts@digital.justice.gov.uk>
2023-08-07 14:51:42 +0100

    Offences can have multiple court dates (#224)
    An offence in Nomis only has one associated court date, where nDelius
    has multiple. Update our schema to support multiple dates, where Nomis
    would always be a list of one item.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/helpers/OffenceHelper.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/helpers/OffenceHelper.kt
@@ -8,13 +8,13 @@ fun generateTestOffence(
   description: String? = "Some description",
   startDate: LocalDate? = LocalDate.parse("2020-02-03"),
   endDate: LocalDate? = LocalDate.parse("2020-03-03"),
-  courtDate: LocalDate? = LocalDate.parse("2020-04-03"),
+  courtDates: List<LocalDate?> = listOf(LocalDate.parse("2020-04-03")),
   statuteCode: String? = "RR12",
 ): Offence = Offence(
   cjsCode = cjsCode,
   description = description,
   startDate = startDate,
   endDate = endDate,
-  courtDate = courtDate,
+  courtDates = courtDates,
   statuteCode = statuteCode,
 )
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ndelius/SupervisionsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ndelius/SupervisionsTest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.ndelius
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Offence
+import java.time.LocalDate
 
 class SupervisionsTest : DescribeSpec(
   {
@@ -10,8 +11,18 @@ class SupervisionsTest : DescribeSpec(
       it("maps one-to-one attributes to integration API attributes") {
         val supervisions = Supervisions(
           supervisions = listOf(
-            Supervision(mainOffence = MainOffence(description = "foobar")),
-            Supervision(mainOffence = MainOffence(description = "barbaz")),
+            Supervision(
+              mainOffence = MainOffence(description = "foobar"),
+              courtAppearances = listOf(
+                CourtAppearance(date = "2009-07-07T00:00:00+01:00"),
+              ),
+            ),
+            Supervision(
+              mainOffence = MainOffence(description = "barbaz"),
+              courtAppearances = listOf(
+                CourtAppearance(date = "2010-07-07T00:00:00+01:00"),
+              ),
+            ),
           ),
         )
 
@@ -19,8 +30,14 @@ class SupervisionsTest : DescribeSpec(
 
         integrationApiOffences.shouldBe(
           listOf(
-            Offence(description = "foobar"),
-            Offence(description = "barbaz"),
+            Offence(
+              description = "foobar",
+              courtDates = listOf(LocalDate.parse("2009-07-07")),
+            ),
+            Offence(
+              description = "barbaz",
+              courtDates = listOf(LocalDate.parse("2010-07-07")),
+            ),
           ),
         )
       }
