a4b8b91ac3c73b49f87305fb4023744de1c9e93c
Emile <emile.swarts@digital.justice.gov.uk>
2023-08-08 11:09:06 +0100

    Remove unused method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ndelius/Supervisions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ndelius/Supervisions.kt
@@ -1,11 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.ndelius
 
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Offence
-
-data class Supervisions (
+data class Supervisions(
   val supervisions: List<Supervision>,
-) {
-  fun toOffences(): List<Offence> {
-    return this.supervisions.flatMap { it.toOffences() }
-  }
-}
+)
