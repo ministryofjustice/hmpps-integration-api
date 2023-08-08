9b0b578756a13477c541fa884cdb62d82b0c96f8
Emile <emile.swarts@digital.justice.gov.uk>
2023-08-08 10:32:47 +0100

    WIP model structure Offences

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ndelius/Supervisions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ndelius/Supervisions.kt
@@ -1,5 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.ndelius
 
-data class Supervisions(
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Offence
+
+data class Supervisions (
   val supervisions: List<Supervision>,
-)
+) {
+  fun toOffences(): List<Offence> {
+    return this.supervisions.flatMap { it.toOffences() }
+  }
+}
