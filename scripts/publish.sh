52432d90103459fb69503a5645a5f36dc937ebe6
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-08-31 16:11:20 +0100

    Update logging for publish Docker image script
    - Remove outputting ECR endpoint as it gets obfuscated.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,13 +2,11 @@
 
 set -e
 
+echo "Logging into ECR."
 aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ECR_REGISTRY_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 
-echo "ecr endpoint"
-echo $ECR_ENDPOINT
-
-echo "we are about to tag"
+echo "Tagging Docker image."
 docker tag hmpps-integration-api "${ECR_ENDPOINT}:${APP_VERSION}"
-echo "we are about to push"
-echo "${ECR_ENDPOINT}:${APP_VERSION}"
+
+echo "Pushing Docker image to ECR."
 docker push "${ECR_ENDPOINT}:${APP_VERSION}"
