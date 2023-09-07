92d7cc8f833f2eff0970a7406f2909f7cb40902d
Luke Williams <lawilliams1993@gmail.com>
2023-09-07 10:55:08 +0100

    added docstrings for functions

--- scripts/data_analysis/search_apis_for_phrase.py
+++ scripts/data_analysis/search_apis_for_phrase.py
@@ -1,4 +1,4 @@
-"""TODO"""
+"""Module to search all APIs on Structurizer, hardcoded on 05/09/2023"""
 import sys
 import pandas as pd
 
@@ -8,8 +8,20 @@ SCHEMA_RESULT_COLS = ["Schema", "Field", "Field_metadata"]
 PATH_RESULT_COLS = ["Path", "Http_method", "Path_metadata"]
 
 # Define a search function
-def search_string(string_to_search, search_phrase) -> str:
-    """TODO"""
+def search_string(string_to_search, search_phrase) -> bool:
+    """
+    Searches a string for a particular phrase.
+    Search logic includes logic to mitigate risk of missed searches in the following ways:
+        - Allows for phrases to be searched for in both field names and descriptions.
+        - Allows mixed case searches to be conducted
+
+    Parameters:
+        string_to_search (str): String to search.
+        search_phrase (str): The word, or phrase, to search for in the String to search.
+
+    Returns:
+        True/False (boolean): Boolean conditional on the phrase being found
+    """
     return str(search_phrase).lower().replace(" ", "") in str(string_to_search).lower().replace(" ", "")
 
 def get_schema_or_path_data(dict_resp,
@@ -17,11 +29,30 @@ def get_schema_or_path_data(dict_resp,
                             schema_bool=True,
                             metadata_divider='|') -> pd.DataFrame:
     """ 
-    TODO
-    Currently only returns all schemas, fields and nested metadata values (not metadata keys).
-    Outputs as a dataframe. 
+    Expects a dictionary response object along with a list of dictionary keys 
+    used to drill down into the nested Dictionary for either the Schemas or Paths.
+    Everything deper than either Schema/Field or Path/HTPP is considered as 'Metadata'
+    And each value associated with a different key is simply seperated with the metadata_divider.
+    This long metadata string is much easier to search and more flexibly generated regardless
+    of dictionary complexity or nested depth > 3.
     If errenous schema or field data is attempted to be appended the row will just be empty.
-    Needs to handle an empty dictionary.
+    
+    Parameters:
+        dict_resp (dictionary): The API response in Dictionary format
+        dict_keys_list (List(str)): The ordered keys listed, to retrieve the
+            appropriate depth of the Dictionary.
+            Default/nothing found: {}
+        schema_bool (boolean): Boolean used to indicate whether it is a 
+            'Schema' or 'Path' search the keys are being provided for.
+            Default: True
+        metadata_divider (str): A single character string used as the desired
+            seperator between dictionary values that comprise the metadata string.
+            Default: '|'
+
+    Returns:
+        data_frame (pd.DateFrame): A dataframe containing all of the relevant 
+            Schema or Path data ready to be searched.
+            
     """
     if schema_bool:
         data_frame_cols = SCHEMA_RESULT_COLS
@@ -61,12 +92,25 @@ def get_schema_or_path_data(dict_resp,
         data_frame = pd.DataFrame(list_of_lists, columns=data_frame_cols)
 
     except KeyError:
-        sys.exit()
+        print("Error in provided keys, please check your input values. Exiting")
+        sys.exit(1)
     return data_frame
 
 def search_df_for_phrase(data_frame, search_phrase) -> pd.DataFrame:
     """
-    TODO
+    Searches target data_frame cell by cell for a search_phrase. 
+    creates a mask of True/False values for each and returns a dataframe
+    where only the rows contained at least 1 true value in the mask.
+    See the "search_string" function to understand the search logic applied.
+
+    Parameters:
+        data_frame (pd.DataFrame): The data frame to search.
+        search_phrase (str): The phrase to search the data frame for.
+
+    Returns:
+        filtered_data_frame (pd.DataFrame): The filtered data_frame
+            based on the successful search results. Can be empty if no
+            results are found.
     """
     mask = data_frame.applymap(lambda df_cell: search_string(df_cell, search_phrase))
     filtered_data_frame = data_frame.loc[mask.any(axis=1)]
@@ -74,15 +118,41 @@ def search_df_for_phrase(data_frame, search_phrase) -> pd.DataFrame:
 
 def add_column_value(data_frame, column_name, value) -> pd.DataFrame:
     """ 
-    #TODO - this function is just responsible for adding an extra column to a dataframe for a phrase
+    This function forces the logic of adding a column using a simple assignment into a function.
+    This prevents it failing to add a column to a view of the dataframe.
+    There is probably a better way than this, but it could not be found quickly.
+
+    Parameters:
+        data_frame (pd.DataFrame): The data frame to which you want to add a permanent column.
+        column_name (str): The name of the column you wish to add.
+        value (any primitive): The value you wish to add for each row in this new column.
+
+    Returns:
+        data_frame (pd.DataFrame): The original data_frame with the new column added.
     """
     data_frame[column_name] = value
     return data_frame
 
 def find_context(target_str, search_str, col_sep='|') -> str:
     """ 
-    TODO
-    #Currently doesn't work for enums?
+    A powerful function designed to highlight in a string divided
+    into parts with a divider where the search string is.
+    (divider exmaples: the ',' in a csv, or a custom one like the '|' in the metadata string)
+
+    Parameters:
+        target_str (str): The divided string that is to be searched
+        search_str (str): The search phrase to search the target_str
+
+    Returns:
+        context (str): The context of the search_str.
+        If the string is not divided it will simply return the full string.
+
+    Example:
+        find_context('| Hello | Cruel | World |', 'Cru') -> ' Cruel '
+
+    Limitations:
+        - Currently isn't perfect for nested lists within the string.
+        - divider character cannot be anywhere in the string other than as a divider.
     """
     context = ''
     formatted_target = str.lower(target_str)
@@ -98,7 +168,16 @@ def find_context(target_str, search_str, col_sep='|') -> str:
 
 def search_api_for_phrase(url=common.DEFAULT_URL, search_phrase="") -> (pd.DataFrame, pd.DataFrame):
     """
-    TODO
+    Specifically searches the api-docs of a desired API url for a particular search phrase contained anywhere 
+    within either the components/schema section or paths section of the json/yaml response.
+    Returns a tabulated response for the result from each of these sections
+
+    Parameters:
+        url (str): Default is Prison API, but can specify any API url that contains components/schema or paths
+        search_phrase (str): The phrase you wish to search for
+
+    Returns:
+        tuple(pd.DataFrame, pd.DataFrame): The dataframes for the schema and path search respectively.
     """
     json_extract = common.extract_data(url)
     schema_data_frame = get_schema_or_path_data(
@@ -122,15 +201,23 @@ def search_api_for_phrase(url=common.DEFAULT_URL, search_phrase="") -> (pd.DataF
 def retrieve_context(data_frame, column_name, search_phrase, col_sep='|') -> pd.DataFrame:
 
     """ 
-    #TODO returns a dataframe with a Context column added
+    Implements the find_context logic on a target data_frame column based on a search phrase.
+    For a string contained within each row of that column, that has a divider defined as col_sep,
+    it will return the substring between such dividers that the first search result is in.
+    It returns this on a row by row basis as a new column, "Context".
+
+    Parameters:
+        data_frame (pd.DataFrame): The dataframe over which you wish to iteratively search
+        column_name (str): The column name of the data frame you wish to search
+        search_phrase (str): The phrase you wish to search for within the metadata of the row
+        col_sep (str): The single character divider within the string within which to retrieve context
     """
-    ##Currently not filtering context properly, look into this.
-    #started happening since this was .loc
 
     copy_data_frame = data_frame.copy()
     copy_data_frame["Context"] = ''
     column_index = copy_data_frame.columns.get_loc(column_name)
-    context_series = copy_data_frame.loc[:, column_name].apply(lambda x: find_context(x, search_phrase, col_sep))
+    context_series = copy_data_frame.loc[:, column_name].apply(
+        lambda x: find_context(x, search_phrase, col_sep).replace(search_phrase, f">>>{search_phrase}<<<"))
     copy_data_frame["Context"] = context_series
     for index, row in copy_data_frame.iterrows():
         if str.lower(search_phrase) in str.lower(row[column_index]):
