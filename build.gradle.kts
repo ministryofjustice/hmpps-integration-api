d305e2125019a3844969799e226b9d6b2ea382b7
Emile <emile.swarts@digital.justice.gov.uk>
2023-09-14 10:05:53 +0100

    Bump HMPPS Gradle Spring Boot to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.1"
   kotlin("plugin.spring") version "1.8.22"
 }
 
