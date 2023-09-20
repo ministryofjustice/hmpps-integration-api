eb2414950b2748441c9ea60fa4ed2de64276ec99
popey2700 <alex.pope@madetech.com>
2023-09-20 11:28:45 +0100

    Created Sexual Predictor model

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/assessRisksAndNeeds/SexualPredictorScore.kt
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.assessRisksAndNeeds
+
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.SexualPredictor as IntegrationAPISexualPredictor
+
+data class SexualPredictorScore(
+  val ospIndecentScoreLevel: String? = null,
+  val ospContactScoreLevel: String? = null,
+) {
+  fun toSexualPredictor(): IntegrationAPISexualPredictor = IntegrationAPISexualPredictor(
+    indecentScoreLevel = this.ospIndecentScoreLevel,
+    contactScoreLevel = this.ospContactScoreLevel
+  )
+}
