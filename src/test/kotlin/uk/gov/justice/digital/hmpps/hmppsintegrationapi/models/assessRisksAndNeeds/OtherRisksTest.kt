77a4de163c45983f4b47264391b053f8c073910f
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-09-22 12:40:56 +0100

    Add OtherRisks model for ARN and HMPPS

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/assessRisksAndNeeds/OtherRisksTest.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.assessRisksAndNeeds
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.assessRisksAndNeeds.OtherRisks as ArnOtherRisks
+
+class OtherRisksTest : DescribeSpec(
+  {
+    describe("#toOtherRisks") {
+      it("maps one-to-one attributes to Integration API attributes") {
+        val arnOtherRisks = ArnOtherRisks(
+          escapeOrAbscond = "YES",
+          controlIssuesDisruptiveBehaviour = "YES",
+          breachOfTrust = "YES",
+          riskToOtherPrisoners = "YES",
+        )
+
+        val integrationApiOtherRisks = arnOtherRisks.toOtherRisks()
+
+        integrationApiOtherRisks.escapeOrAbscond.shouldBe(arnOtherRisks.escapeOrAbscond)
+        integrationApiOtherRisks.controlIssuesDisruptiveBehaviour.shouldBe(arnOtherRisks.controlIssuesDisruptiveBehaviour)
+        integrationApiOtherRisks.breachOfTrust.shouldBe(arnOtherRisks.breachOfTrust)
+        integrationApiOtherRisks.riskToOtherPrisoners.shouldBe(arnOtherRisks.riskToOtherPrisoners)
+      }
+    }
+  },
+)
