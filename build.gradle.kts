436ceb4655ad7e0c26b0ae2a76cce55f9ea5eff3
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-10-31 15:24:27 +0000

    Update WireMock dependency to use official package

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
   testImplementation("io.kotest:kotest-assertions-json-jvm:5.7.2")
   testImplementation("io.kotest:kotest-runner-junit5-jvm:5.7.2")
   testImplementation("io.kotest:kotest-assertions-core-jvm:5.7.2")
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
+  testImplementation("org.wiremock:wiremock:3.2.0")
   testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
   testImplementation("org.mockito:mockito-core:5.6.0")
 
