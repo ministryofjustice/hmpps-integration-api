032e2105c16e37d5139ae03db709abacea0fc3fd
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-11-02 14:56:42 +0000

    Add service for getting latest sentence key dates and adjustments

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/LatestSentenceKeyDatesAndAdjustments.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models
+
+data class LatestSentenceKeyDatesAndAdjustments(
+  val adjustments: SentenceAdjustment? = null,
+  val automaticRelease: SentenceKeyDate? = null,
+)
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLatestSentenceKeyDatesAndAdjustmentsForPersonService.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.ProbationOffenderSearchGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.LatestSentenceKeyDatesAndAdjustments
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Response
+
+@Service
+class GetLatestSentenceKeyDatesAndAdjustmentsForPersonService(
+  @Autowired val probationOffenderSearchGateway: ProbationOffenderSearchGateway,
+  @Autowired val nomisGateway: NomisGateway,
+) {
+  fun execute(hmppsId: String): Response<LatestSentenceKeyDatesAndAdjustments?> {
+    val (person, personErrors) = probationOffenderSearchGateway.getPerson(hmppsId)
+
+    if (personErrors.isNotEmpty()) {
+      return Response(data = null, personErrors)
+    }
+
+    val nomisNumber = person?.identifiers?.nomisNumber ?: return Response(data = null)
+
+    val (latestSentenceKeyDates, latestSentenceKeyDatesErrors) = nomisGateway.getLatestSentenceKeyDatesForPerson(
+      nomisNumber,
+    )
+    val (latestSentenceAdjustments, latestSentenceAdjustmentsErrors) = nomisGateway.getLatestSentenceAdjustmentsForPerson(
+      nomisNumber,
+    )
+
+    if (latestSentenceKeyDatesErrors.isNotEmpty() && latestSentenceAdjustmentsErrors.isNotEmpty()) {
+      return Response(data = null, latestSentenceKeyDatesErrors + latestSentenceAdjustmentsErrors)
+    }
+
+    return Response(
+      data = LatestSentenceKeyDatesAndAdjustments(
+        adjustments = latestSentenceAdjustments,
+        automaticRelease = latestSentenceKeyDates?.automaticRelease,
+      ),
+    )
+  }
+}
