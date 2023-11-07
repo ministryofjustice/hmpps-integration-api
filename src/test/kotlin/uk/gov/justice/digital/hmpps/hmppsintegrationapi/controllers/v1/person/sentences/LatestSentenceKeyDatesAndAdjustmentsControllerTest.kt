6b69b7e01ee44a54ef8a02e89cebead7c950056d
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-11-07 11:29:48 +0000

    Add basic endpoint for getting latest sentence key dates and adjustments (#288)
    * Add service for getting latest sentence key dates and adjustments
    
    * Move sentences controller into person directory and fix some formatting.
    
    * Add endpoint for returning latest sentence key dates and adjustments
    
    * Add smoke test
    
    * Remove duplicate risks endpoint in OpenAPI spec
    
    * Add API endpoint to OpenAPI spec

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/person/sentences/LatestSentenceKeyDatesAndAdjustmentsControllerTest.kt
@@ -0,0 +1,100 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers.v1.person.sentences
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.mockito.Mockito
+import org.mockito.internal.verification.VerificationModeFactory
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.HttpStatus
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers.v1.person.SentencesController
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.removeWhitespaceAndNewlines
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.LatestSentenceKeyDatesAndAdjustments
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.SentenceAdjustment
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.SentenceKeyDate
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.UpstreamApiError
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetLatestSentenceKeyDatesAndAdjustmentsForPersonService
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetSentencesForPersonService
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import java.time.LocalDate
+
+@WebMvcTest(controllers = [SentencesController::class])
+internal class LatestSentenceKeyDatesAndAdjustmentsControllerTest(
+  @Autowired val mockMvc: MockMvc,
+  @MockBean val getSentencesForPersonService: GetSentencesForPersonService,
+  @MockBean val getLatestSentenceKeyDatesAndAdjustmentsForPersonService: GetLatestSentenceKeyDatesAndAdjustmentsForPersonService,
+) : DescribeSpec(
+  {
+    val hmppsId = "9999/11111A"
+    val encodedHmppsId = URLEncoder.encode(hmppsId, StandardCharsets.UTF_8)
+    val path = "/v1/persons/$encodedHmppsId/sentences/latest-key-dates-and-adjustments"
+
+    beforeTest {
+      Mockito.reset(getLatestSentenceKeyDatesAndAdjustmentsForPersonService)
+      whenever(getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)).thenReturn(
+        Response(
+          data = LatestSentenceKeyDatesAndAdjustments(
+            adjustments = SentenceAdjustment(additionalDaysAwarded = 7),
+            automaticRelease = SentenceKeyDate(date = LocalDate.parse("2023-04-01")),
+          ),
+        ),
+      )
+    }
+
+    it("responds with a 200 OK status") {
+      val result = mockMvc.perform(MockMvcRequestBuilders.get(path)).andReturn()
+
+      result.response.status.shouldBe(HttpStatus.OK.value())
+    }
+
+    it("retrieves the latest sentence key dates and adjustments for a person with the matching ID") {
+      mockMvc.perform(MockMvcRequestBuilders.get(path)).andReturn()
+
+      verify(getLatestSentenceKeyDatesAndAdjustmentsForPersonService, VerificationModeFactory.times(1)).execute(hmppsId)
+    }
+
+    it("returns the latest sentence key dates and adjustments for a person with the matching ID") {
+      val result = mockMvc.perform(MockMvcRequestBuilders.get(path)).andReturn()
+
+      result.response.contentAsString.shouldContain(
+        """
+          "data": {
+            "adjustments": {
+              "additionalDaysAwarded": 7
+            },
+            "automaticRelease": {
+              "date": "2023-04-01"
+            }
+          }
+          """.removeWhitespaceAndNewlines(),
+      )
+    }
+
+    it("responds with a 404 NOT FOUND status when person isn't found in the upstream API") {
+      whenever(getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)).thenReturn(
+        Response(
+          data = null,
+          errors = listOf(
+            UpstreamApiError(
+              causedBy = UpstreamApi.NOMIS,
+              type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
+            ),
+          ),
+        ),
+      )
+
+      val result = mockMvc.perform(MockMvcRequestBuilders.get(path)).andReturn()
+
+      result.response.status.shouldBe(HttpStatus.NOT_FOUND.value())
+    }
+  },
+)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLatestSentenceKeyDatesAndAdjustmentsForPersonServiceTest.kt
@@ -0,0 +1,156 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import org.mockito.Mockito
+import org.mockito.internal.verification.VerificationModeFactory
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.ContextConfiguration
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.ProbationOffenderSearchGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Identifiers
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.SentenceAdjustment
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.SentenceKeyDate
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.SentenceKeyDates
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.UpstreamApiError
+import java.time.LocalDate
+
+@ContextConfiguration(
+  initializers = [ConfigDataApplicationContextInitializer::class],
+  classes = [GetLatestSentenceKeyDatesAndAdjustmentsForPersonService::class],
+)
+internal class GetLatestSentenceKeyDatesAndAdjustmentsForPersonServiceTest(
+  @MockBean val probationOffenderSearchGateway: ProbationOffenderSearchGateway,
+  @MockBean val nomisGateway: NomisGateway,
+  private val getLatestSentenceKeyDatesAndAdjustmentsForPersonService: GetLatestSentenceKeyDatesAndAdjustmentsForPersonService,
+) : DescribeSpec(
+  {
+    val hmppsId = "2003/13116M"
+    val nomisNumber = "abc123"
+
+    beforeEach {
+      Mockito.reset(nomisGateway)
+
+      whenever(probationOffenderSearchGateway.getPerson(id = hmppsId)).thenReturn(
+        Response(
+          data = Person(
+            firstName = "Baylan",
+            lastName = "Skoll",
+            identifiers = Identifiers(nomisNumber = nomisNumber),
+          ),
+        ),
+      )
+      whenever(nomisGateway.getLatestSentenceKeyDatesForPerson(nomisNumber)).thenReturn(
+        Response(
+          data = SentenceKeyDates(
+            automaticRelease = SentenceKeyDate(date = LocalDate.parse("2023-11-02")),
+          ),
+        ),
+      )
+      whenever(nomisGateway.getLatestSentenceAdjustmentsForPerson(nomisNumber)).thenReturn(
+        Response(
+          data = SentenceAdjustment(additionalDaysAwarded = 10),
+        ),
+      )
+    }
+
+    it("retrieves NOMIS number from Probation Offender Search") {
+      getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)
+
+      verify(probationOffenderSearchGateway, VerificationModeFactory.times(1)).getPerson(id = hmppsId)
+    }
+
+    it("retrieves latest sentence key dates from NOMIS") {
+      getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)
+
+      verify(nomisGateway, VerificationModeFactory.times(1)).getLatestSentenceKeyDatesForPerson(id = nomisNumber)
+    }
+
+    it("retrieves latest sentence adjustments from NOMIS") {
+      getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)
+
+      verify(nomisGateway, VerificationModeFactory.times(1)).getLatestSentenceAdjustmentsForPerson(id = nomisNumber)
+    }
+
+    it("returns latest sentence key dates and adjustments from NOMIS") {
+      val response = getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)
+
+      response.data?.adjustments?.additionalDaysAwarded.shouldBe(10)
+      response.data?.automaticRelease?.date.shouldBe(LocalDate.parse("2023-11-02"))
+    }
+
+    it("returns an error when person cannot be found in probation") {
+      whenever(probationOffenderSearchGateway.getPerson(id = hmppsId)).thenReturn(
+        Response(
+          data = null,
+          errors = listOf(
+            UpstreamApiError(
+              causedBy = UpstreamApi.PROBATION_OFFENDER_SEARCH,
+              type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
+            ),
+          ),
+        ),
+      )
+
+      val response = getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)
+
+      response.errors.shouldHaveSize(1)
+      response.errors.first().causedBy.shouldBe(UpstreamApi.PROBATION_OFFENDER_SEARCH)
+      response.errors.first().type.shouldBe(UpstreamApiError.Type.ENTITY_NOT_FOUND)
+    }
+
+    it("returns null when person doesn't have a NOMIS number") {
+      whenever(probationOffenderSearchGateway.getPerson(id = hmppsId)).thenReturn(
+        Response(
+          data = Person(
+            firstName = "Shin",
+            lastName = "Hati",
+            identifiers = Identifiers(nomisNumber = null),
+          ),
+          errors = emptyList(),
+        ),
+      )
+
+      val response = getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)
+
+      response.data.shouldBeNull()
+    }
+
+    it("returns all errors when latest sentence key dates and adjustments cannot be found for NOMIS number") {
+      whenever(nomisGateway.getLatestSentenceKeyDatesForPerson(nomisNumber)).thenReturn(
+        Response(
+          data = null,
+          errors = listOf(
+            UpstreamApiError(
+              causedBy = UpstreamApi.NOMIS,
+              type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
+            ),
+          ),
+        ),
+      )
+      whenever(nomisGateway.getLatestSentenceAdjustmentsForPerson(nomisNumber)).thenReturn(
+        Response(
+          data = null,
+          errors = listOf(
+            UpstreamApiError(
+              causedBy = UpstreamApi.NOMIS,
+              type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
+            ),
+          ),
+        ),
+      )
+
+      val response = getLatestSentenceKeyDatesAndAdjustmentsForPersonService.execute(hmppsId)
+
+      response.errors.shouldHaveSize(2)
+    }
+  },
+)
