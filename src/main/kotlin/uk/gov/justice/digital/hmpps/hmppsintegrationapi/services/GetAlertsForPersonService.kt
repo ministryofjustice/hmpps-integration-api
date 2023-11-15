7f8131b8a92f9bfd472331fe286fdef4870982a9
chiaramapellimt <chiara.mapelli@madetech.com>
2023-11-15 14:53:40 +0000

    Refactored getAlertsForPersonService to behave consistently like other services

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetAlertsForPersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetAlertsForPersonService.kt
@@ -3,18 +3,17 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Alert
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Response
 
 @Service
 class GetAlertsForPersonService(
   @Autowired val nomisGateway: NomisGateway,
-  @Autowired val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway,
+  @Autowired val getPersonService: GetPersonService,
 ) {
   fun execute(hmppsId: String): Response<List<Alert>> {
-    val responseFromPrisonerOffenderSearch = prisonerOffenderSearchGateway.getPersons(hmppsId = hmppsId)
-    val nomisNumber = responseFromPrisonerOffenderSearch.data.firstOrNull()?.identifiers?.nomisNumber
+    val personResponse = getPersonService.execute(hmppsId = hmppsId)
+    val nomisNumber = personResponse.data?.identifiers?.nomisNumber
     var nomisAlerts: Response<List<Alert>> = Response(data = emptyList())
 
     if (nomisNumber != null) {
@@ -23,7 +22,7 @@ class GetAlertsForPersonService(
 
     return Response(
       data = nomisAlerts.data,
-      errors = nomisAlerts.errors + responseFromPrisonerOffenderSearch.errors,
+      errors = nomisAlerts.errors + personResponse.errors,
     )
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetAlertsForPersonServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetAlertsForPersonServiceTest.kt
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.ConfigDataApplicationContextInitial
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ContextConfiguration
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Alert
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Identifiers
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.Person
@@ -24,21 +23,23 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.UpstreamApiError
 )
 internal class GetAlertsForPersonServiceTest(
   @MockBean val nomisGateway: NomisGateway,
-  @MockBean val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway,
+  @MockBean val personService: GetPersonService,
   private val getAlertsForPersonService: GetAlertsForPersonService,
 ) : DescribeSpec(
   {
     val hmppsId = "1234/56789B"
     val prisonerNumber = "Z99999ZZ"
+    val deliusCrn = "X777776"
     val alert = Alert()
+
+    val person = Person(firstName = "Qui-gon", lastName = "Jin", identifiers = Identifiers(nomisNumber = prisonerNumber, deliusCrn = deliusCrn))
 
     beforeEach {
       Mockito.reset(nomisGateway)
-      Mockito.reset(prisonerOffenderSearchGateway)
+      Mockito.reset(personService)
 
-      whenever(prisonerOffenderSearchGateway.getPersons(hmppsId = hmppsId)).thenReturn(
-        Response(data = listOf(Person(firstName = "Chandler", lastName = "Bing", identifiers = Identifiers(nomisNumber = prisonerNumber)))),
-      )
+      whenever(personService.execute(hmppsId = deliusCrn)).thenReturn(Response(person))
+      whenever(personService.execute(hmppsId = hmppsId)).thenReturn(Response(person))
 
       whenever(nomisGateway.getAlertsForPerson(prisonerNumber)).thenReturn(
         Response(
@@ -49,10 +50,10 @@ internal class GetAlertsForPersonServiceTest(
       )
     }
 
-    it("retrieves prisoner ID from Prisoner Offender Search using a Hmpps Id") {
+    it("retrieves a person from getPersonService") {
       getAlertsForPersonService.execute(hmppsId)
 
-      verify(prisonerOffenderSearchGateway, VerificationModeFactory.times(1)).getPersons(hmppsId = hmppsId)
+      verify(personService, VerificationModeFactory.times(1)).execute(hmppsId = hmppsId)
     }
 
     it("retrieves alerts from NOMIS using a prisoner number") {
@@ -63,9 +64,9 @@ internal class GetAlertsForPersonServiceTest(
 
     describe("when an upstream API returns an error when looking up a person by a Hmmps Id") {
       beforeEach {
-        whenever(prisonerOffenderSearchGateway.getPersons(hmppsId = hmppsId)).thenReturn(
+        whenever(personService.execute(hmppsId = hmppsId)).thenReturn(
           Response(
-            data = emptyList(),
+            data = null,
             errors = listOf(
               UpstreamApiError(
                 causedBy = UpstreamApi.PRISONER_OFFENDER_SEARCH,
