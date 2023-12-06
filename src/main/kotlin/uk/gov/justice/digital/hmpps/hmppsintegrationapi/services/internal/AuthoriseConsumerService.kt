317511a3e43212e6df5cbd875ab59d649606ede8
Chiara <chiara.mapelli@madetech.com>
2023-12-06 14:04:32 +0000

    fixing lint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/internal/AuthoriseConsumerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/internal/AuthoriseConsumerService.kt
@@ -10,8 +10,8 @@ class AuthoriseConsumerService(
   fun execute(subjectDistinguishedName: String, consumerPathConfig: Map<String, List<String>>, requestedPath: String): Boolean {
     val consumer = extractConsumerFromSubjectDistinguishedNameService.execute(subjectDistinguishedName)
 
-    println("consumer: ${consumer}")
-    println("requestedPath: ${requestedPath}")
+    println("consumer: $consumer")
+    println("requestedPath: $requestedPath")
 
     consumerPathConfig[consumer]?.forEach {
       if (Regex(it).matches(requestedPath)) {
