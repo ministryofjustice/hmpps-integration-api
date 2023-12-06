fd0321182869360f153723dd27f04166294b81f4
Chiara <chiara.mapelli@madetech.com>
2023-12-06 14:37:09 +0000

    Refactor

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/internal/AuthoriseConsumerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/internal/AuthoriseConsumerService.kt
@@ -1,14 +1,13 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.internal
 
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
+@Component
 @Service
-class AuthoriseConsumerService(
-  @Autowired private val extractConsumerFromSubjectDistinguishedNameService: ExtractConsumerFromSubjectDistinguishedNameService,
-) {
+class AuthoriseConsumerService {
   fun execute(subjectDistinguishedName: String, consumerPathConfig: Map<String, List<String>>, requestedPath: String): Boolean {
-    val consumer = extractConsumerFromSubjectDistinguishedNameService.execute(subjectDistinguishedName)
+    val consumer = extractConsumerFromSubjectDistinguishedNameService(subjectDistinguishedName)
 
     println("consumer: $consumer")
     println("requestedPath: $requestedPath")
@@ -20,5 +19,19 @@ class AuthoriseConsumerService(
     }
 
     return false
+  }
+
+  fun extractConsumerFromSubjectDistinguishedNameService(subjectDistinguishedName: String?): String? {
+    if (subjectDistinguishedName.isNullOrEmpty()) {
+      return null
+    }
+
+    val match = Regex("^.*,CN=(.*)$").find(subjectDistinguishedName)
+
+    if (match?.groupValues == null) {
+      return null
+    }
+
+    return match.groupValues[1]
   }
 }
