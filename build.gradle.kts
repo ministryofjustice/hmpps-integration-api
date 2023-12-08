6feeea1b42b0ee21268a0b6ec409b28b87702b5e
popey2700 <alex.pope@madetech.com>
2023-12-08 13:21:33 +0000

    Updated gradle spring boot version to 5.10.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.10.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.10.1"
   kotlin("plugin.spring") version "1.9.20"
 }
 
