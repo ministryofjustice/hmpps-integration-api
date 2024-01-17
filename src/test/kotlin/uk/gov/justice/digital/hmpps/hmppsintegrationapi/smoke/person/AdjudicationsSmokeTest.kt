0f10652832079b622917c742f1a637876bf18d6a
Chiara <chiara.mapelli@madetech.com>
2024-01-17 11:28:21 +0000

    Adding new endpoint to authorised routes

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/smoke/person/AdjudicationsSmokeTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/smoke/person/AdjudicationsSmokeTest.kt
@@ -1,3 +1,36 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.smoke.person
 
-class AdjudicationsSmokeTest
+import io.kotest.assertions.json.shouldEqualJson
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.http.HttpStatus
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.removeWhitespaceAndNewlines
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.helpers.IntegrationAPIHttpClient
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+class AdjudicationsSmokeTest : DescribeSpec(
+  {
+    val hmppsId = "2004/13116M"
+    val encodedHmppsId = URLEncoder.encode(hmppsId, StandardCharsets.UTF_8)
+    val basePath = "/v1/persons/$encodedHmppsId/reported-adjudications"
+    val httpClient = IntegrationAPIHttpClient()
+
+    it("returns adjudications for a person") {
+      val response = httpClient.performAuthorised(basePath)
+
+      response.statusCode().shouldBe(HttpStatus.OK.value())
+      response.body().shouldEqualJson(
+        """
+         {
+          "data": [
+            "incidentDetails": {
+              "dateTimeOfIncident": "2021-01-01T12:00:00"
+              }
+            ]
+          }
+        """.removeWhitespaceAndNewlines(),
+      )
+    }
+  },
+)
