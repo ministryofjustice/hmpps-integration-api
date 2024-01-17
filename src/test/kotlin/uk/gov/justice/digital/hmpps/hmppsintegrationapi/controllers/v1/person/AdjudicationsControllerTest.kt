93e8389ea58dbe46884980a5e75e7d0b4959acb4
Chiara <chiara.mapelli@madetech.com>
2024-01-17 10:14:42 +0000

    Added test setup

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/person/AdjudicationsControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/person/AdjudicationsControllerTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers.v1.person
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import org.mockito.Mockito
 import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -10,6 +11,8 @@ import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.helpers.IntegrationAPIMockMvc
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Adjudication
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.IncidentDetailsDto
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
@@ -31,17 +34,39 @@ internal class AdjudicationsControllerTest(
     val path = "/v1/persons/$encodedHmppsId/reported-adjudications"
     val mockMvc = IntegrationAPIMockMvc(springMockMvc)
 
-    it("throws exception when no person found") {
-      whenever(getAdjudicationsForPersonService.execute(hmppsId = "notfound")).thenReturn(
-        Response(
-          data = emptyList(),
-          errors = listOf(UpstreamApiError(type = UpstreamApiError.Type.ENTITY_NOT_FOUND, causedBy = UpstreamApi.ADJUDICATIONS)),
-        ),
-      )
-      val noFoundPath = "/v1/persons/notfound/reported-adjudications"
-      val result = mockMvc.performAuthorised(noFoundPath)
-      result.response.status.shouldBe(HttpStatus.NOT_FOUND.value())
+    describe("GET $path") {
+      beforeTest {
+        Mockito.reset(getAdjudicationsForPersonService)
+        Mockito.reset(auditService)
+        whenever(getAdjudicationsForPersonService.execute(hmppsId)).thenReturn(
+          Response(
+            data = listOf(
+              Adjudication(
+                incidentDetails = IncidentDetailsDto(
+                  dateTimeOfIncident = "2021-04-03T10:00:00",
+                ),
+              ),
+            ),
+          ),
+        )
+      }
+
+      it("throws exception when no person found") {
+        whenever(getAdjudicationsForPersonService.execute(hmppsId = "notfound")).thenReturn(
+          Response(
+            data = emptyList(),
+            errors = listOf(
+              UpstreamApiError(
+                type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
+                causedBy = UpstreamApi.ADJUDICATIONS,
+              ),
+            ),
+          ),
+        )
+        val noFoundPath = "/v1/persons/notfound/reported-adjudications"
+        val result = mockMvc.performAuthorised(noFoundPath)
+        result.response.status.shouldBe(HttpStatus.NOT_FOUND.value())
+      }
     }
   },
-
 )
