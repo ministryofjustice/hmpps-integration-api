163d39cb7731bc5b2f816fdd720bab3952b78d35
popey2700 <alex.pope@madetech.com>
2024-02-06 09:39:41 +0000

    Update Sentence model
    Updated sentence model to have a system source as well as renaming the dataSource field to be service source

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Sentence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Sentence.kt
@@ -3,7 +3,8 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 import java.time.LocalDate
 
 data class Sentence(
-  val dataSource: UpstreamApi,
+  val serviceSource: UpstreamApi,
+  val systemSource: SystemSource,
   val dateOfSentencing: LocalDate? = null,
   val description: String? = null,
   val isActive: Boolean? = null,
