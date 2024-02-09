a595fbee1a82a476a75dbe095af0568e044a02ba
Chiara <chiara.mapelli@madetech.com>
2024-02-09 13:18:59 +0000

    Fixed smoke test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/NomisPageCaseNote.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/nomis/NomisPageCaseNote.kt
@@ -4,10 +4,10 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.CaseNote
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.PageCaseNote
 
 data class NomisPageCaseNote(
-  val caseNotes: List<NomisCaseNote> ? = emptyList(),
+  val content: List<NomisCaseNote> = listOf(),
 ) {
   fun toPageCaseNote(): PageCaseNote = PageCaseNote(
-    caseNotes = this.caseNotes?.map {
+    caseNotes = this.content.map {
       CaseNote(caseNoteId = it.caseNoteId)
     },
   )
