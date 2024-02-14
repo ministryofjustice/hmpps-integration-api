bac1e0cfe9772ce9fb87cc0e5238edfc86f84d76
Chiara <chiara.mapelli@madetech.com>
2024-02-14 10:29:34 +0000

    Removed potential extra field

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ContactDetails.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ContactDetails.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
 data class ContactDetails(
-
   val phoneNumbers: List<PhoneNumber> = emptyList(),
   val emailAddresses: List<String> = emptyList(),
-
 )
