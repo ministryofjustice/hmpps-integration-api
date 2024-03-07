e9aa8964e799b73110f23382101af9061eec6ccb
Chiara <chiara.mapelli@madetech.com>
2024-03-07 13:28:14 +0000

    Added NDelius model

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/ndelius/NDeliusMappaDetail.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.ndelius
+
+data class NDeliusMappaDetail(
+  val level: Int? = null,
+  val levelDescription: String? = null,
+  val category: Int? = null,
+  val categoryDescription: String? = null,
+  val startDate: String? = null,
+  val reviewDate: String? = null,
+  val notes: String? = null,
+)
