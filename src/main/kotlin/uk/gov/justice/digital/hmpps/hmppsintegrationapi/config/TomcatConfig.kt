c258064754793f5c779c5bedb59dc0d747e57095
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-03-13 14:57:52 +0000

    [HIA-257]
    Changing tack and getting more success using an interceptor than a filter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/TomcatConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/TomcatConfig.kt
@@ -2,14 +2,24 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.config
 
 import org.apache.catalina.connector.Connector
 import org.apache.tomcat.util.buf.EncodedSolidusHandling
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.ServiceDownInterceptor
 
 @Configuration
-class TomcatConfig {
+class TomcatConfig(
+  @Autowired val serviceDownInterceptor: ServiceDownInterceptor,
+) : WebMvcConfigurer {
+  override fun addInterceptors(registry: InterceptorRegistry) {
+    registry.addInterceptor(serviceDownInterceptor)
+  }
+
   @Bean
   fun tomcatCustomizer(): WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
     return WebServerFactoryCustomizer { factory: TomcatServletWebServerFactory ->
