b06b1a566fd810467fc58e18152de8eb150f86f3
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-03-15 16:39:38 +0000

    [HIA-257]
    Removing code that's no longer needed

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/TomcatConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/TomcatConfig.kt
@@ -7,11 +7,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class TomcatConfig() : WebMvcConfigurer {
-
+class TomcatConfig {
   @Bean
   fun tomcatCustomizer(): WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
     return WebServerFactoryCustomizer { factory: TomcatServletWebServerFactory ->
