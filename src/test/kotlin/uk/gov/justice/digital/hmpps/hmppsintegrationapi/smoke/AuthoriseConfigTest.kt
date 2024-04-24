d77c78a1644ae597e429cc50b2c1717b84ba68e9
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-04-24 13:25:57 +0100

    Refactor to not use wildcard at end of path for url authorization con… (#405)
    * Refactor to not use wildcard at end of path for url authorization config and remove bsi and cymulate access
    
    * fix smoke tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/smoke/AuthoriseConfigTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/smoke/AuthoriseConfigTest.kt
@@ -19,33 +19,38 @@ class AuthoriseConfigTest : DescribeSpec(
       response.body().shouldEqualJson(
         """
       {
-      "automated-test-client": [
-        "/v1/persons",
-        "/v1/persons/.*",
-        "/v1/persons/.*/images",
-        "/v1/images/.*",
-        "/v1/persons/.*/addresses",
-        "/v1/persons/.*/offences",
-        "/v1/persons/.*/alerts",
-        "/v1/persons/.*/sentences",
-        "/v1/persons/.*/sentences/latest-key-dates-and-adjustments",
-        "/v1/persons/.*/risks/scores",
-        "/v1/persons/.*/needs",
-        "/v1/persons/.*/risks",
-        "/v1/persons/.*/reported-adjudications",
-        "/v1/persons/.*/case-notes",
-        "/v1/persons/.*/person-responsible-officer",
-        "/v1/epf/person-details/.*/.*",
-        "/health",
-        "/health/ping",
-        "/health/readiness",
-        "/health/liveness",
-        "/info"
-      ],
-      "config-test": [
-        "/v1/config/authorisation"
-      ]
-    }
+        "automated-test-client": [
+          "/v1/persons",
+          "/v1/persons/\\.*+[^/]*${'$'}",
+          "/v1/persons/.*/images",
+          "/v1/images/\\.*+[^/]*${'$'}",
+          "/v1/persons/.*/addresses",
+          "/v1/persons/.*/offences",
+          "/v1/persons/.*/alerts",
+          "/v1/persons/.*/sentences",
+          "/v1/persons/.*/sentences/latest-key-dates-and-adjustments",
+          "/v1/persons/.*/risks/scores",
+          "/v1/persons/.*/needs",
+          "/v1/persons/.*/risks",
+          "/v1/persons/.*/reported-adjudications",
+          "/v1/persons/.*/adjudications",
+          "/v1/persons/.*/licences/conditions",
+          "/v1/persons/.*/protected-characteristics",
+          "/v1/persons/.*/risks/mappadetail",
+          "/v1/persons/.*/risks/categories",
+          "/v1/persons/.*/case-notes",
+          "/v1/persons/.*/person-responsible-officer",
+          "/v1/epf/person-details/.*/\\.*+[^/]*${'$'}",
+          "/health",
+          "/health/ping",
+          "/health/readiness",
+          "/health/liveness",
+          "/info"
+        ],
+        "config-test": [
+          "/v1/config/authorisation"
+        ]
+      }
       """.removeWhitespaceAndNewlines(),
       )
     }
