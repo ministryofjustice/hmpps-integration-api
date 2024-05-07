e947ebf2751ee6f3ce27bbe843713b56fd7f6667
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-05-07 17:07:37 +0100

    [HIA-721]
    Initial commit and adding data objects

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/RiskManagementController.kt
@@ -0,0 +1,21 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers.v1
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.AuthorisationConfig
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.RiskManagementPlan
+
+@RestController
+@EnableConfigurationProperties(AuthorisationConfig::class)
+class RiskManagementController {
+
+  @RequestMapping("/v1/persons/{encodedHmppsId}/risk-management-plan")
+  fun getRiskManagementPlans(
+    @PathVariable encodedHmppsId: String,
+  ) : Response<List<RiskManagementPlan>> {
+    return Response(emptyList(), emptyList() )
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/RiskManagementPlan.kt
@@ -0,0 +1,19 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
+
+data class RiskManagementPlan (
+
+  val assessmentId: String,
+  val dateCompleted: String,
+  val initiationDate: String,
+  val assessmentStatus: String,
+  val assessmentType: String,
+  val keyInformationCurrentSituation: String,
+  val furtherConsiderationsCurrentSituation: String,
+  val supervision: String,
+  val monitoringAndControl: String,
+  val interventionsAndTreatment: String,
+  val victimSafetyPlanning: String,
+  val latestSignLockDate: String,
+  val latestCompleteDate: String,
+
+)
