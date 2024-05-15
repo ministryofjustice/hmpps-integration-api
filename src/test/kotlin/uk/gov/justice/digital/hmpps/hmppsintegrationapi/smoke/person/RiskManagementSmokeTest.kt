d1437b6cbc6fccb3f7a246ea7cf44db820b473d7
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-05-15 11:03:51 +0100

    [HIA-721]
    WIP Adding smoke test; checking it runs in CI

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/smoke/person/RiskManagementSmokeTest.kt
@@ -0,0 +1,37 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.smoke.person
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.springframework.http.HttpStatus
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.removeWhitespaceAndNewlines
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.helpers.IntegrationAPIHttpClient
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+class RiskManagementSmokeTest : DescribeSpec(
+  {
+    val basePath = "v1/persons"
+    val httpClient = IntegrationAPIHttpClient()
+    val hmppsId = "2004/13116M"
+    val tailPath = "risk-management-plan"
+    val encodedHmppsId = URLEncoder.encode(hmppsId, StandardCharsets.UTF_8)
+
+    it("returns a list of risk management plans using crn / hmppsId as search parameters") {
+
+      val response = httpClient.performAuthorised("$basePath/$encodedHmppsId/$tailPath")
+
+      response.statusCode().shouldBe(HttpStatus.OK.value())
+      response.body().shouldContain("\"data\":[")
+      response.body().shouldContain(
+        """
+          "assessmentStatus": "string",
+          "assessmentType": "string",
+          "initiationDate": "2024-05-04T01:04:20",
+          "assessmentStatus": "string",
+          "assessmentType": "string",
+        """.removeWhitespaceAndNewlines(),
+      )
+    }
+  }
+)
