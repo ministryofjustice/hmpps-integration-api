67b4a8fc97f805813bc5cc54ea2af3602c7e54dc
Chiara <chiara.mapelli@madetech.com>
2024-05-23 14:33:03 +0100

    Format

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLicenceConditionServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLicenceConditionServiceTest.kt
@@ -1,6 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.mockito.Mockito
+import org.mockito.internal.verification.VerificationModeFactory
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ContextConfiguration
@@ -9,6 +14,9 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Identifiers
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Licence
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.LicenceCondition
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Person
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 
 @ContextConfiguration(
   initializers = [ConfigDataApplicationContextInitializer::class],
@@ -17,8 +25,6 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Person
 internal class GetLicenceConditionServiceTest(
   @MockBean val createAndVaryLicenceGateway: CreateAndVaryLicenceGateway,
   @MockBean val getPersonService: GetPersonService,
-  @MockBean val getPersonService: GetPersonService,
-
   private val getLicenceConditionService: GetLicenceConditionService,
 ) : DescribeSpec(
     {
@@ -28,18 +34,9 @@ internal class GetLicenceConditionServiceTest(
       val licences = listOf(Licence(id = "12345"))
       val conditions = listOf(LicenceCondition(condition = "MockCondition", category = "AP"))
 
-     beforeEach {
-       Mockito.reset(getPersonService)
-       Mockito.reset(createAndVaryLicenceGateway)
-
-       whenever(getPersonService.execute(hmppsId = hmppsId)).thenReturn(Response(person))
-       whenever(createAndVaryLicenceGateway.getLicenceSummaries(id = crn)).thenReturn(Response(licences))
-       whenever(createAndVaryLicenceGateway.getLicenceConditions(id = 12345)).thenReturn(Response(conditions))
-     }
-     it("performs a search according to hmpps Id") {
-       getLicenceConditionService.execute(hmppsId)
-       verify(getPersonService, VerificationModeFactory.times(1)).execute(hmppsId = hmppsId)
-     }
+      beforeEach {
+        Mockito.reset(getPersonService)
+        Mockito.reset(createAndVaryLicenceGateway)
 
         whenever(getPersonService.execute(hmppsId = hmppsId)).thenReturn(Response(person))
         whenever(createAndVaryLicenceGateway.getLicenceSummaries(id = crn)).thenReturn(Response(licences))
