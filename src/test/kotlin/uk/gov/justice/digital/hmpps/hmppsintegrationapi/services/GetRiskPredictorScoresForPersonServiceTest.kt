80c976a762b9dc05a20f7ea7c9b1c8ecf0f7a93f
Chiara <chiara.mapelli@madetech.com>
2024-06-14 14:44:45 +0100

    Now targeting the right error

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetRiskPredictorScoresForPersonServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetRiskPredictorScoresForPersonServiceTest.kt
@@ -119,7 +119,7 @@ internal class GetRiskPredictorScoresForPersonServiceTest(
           it("records upstream 403 API error for probation offender search") {
             val response = getRiskPredictorScoresForPersonService.execute(hmppsId)
 
-            response.hasErrorCausedBy(UpstreamApiError.Type.FORBIDDEN, UpstreamApi.PROBATION_OFFENDER_SEARCH).shouldBe(true)
+            response.hasErrorCausedBy(UpstreamApiError.Type.FORBIDDEN, UpstreamApi.ASSESS_RISKS_AND_NEEDS).shouldBe(true)
           }
 
           it("does not get risk predictor scores from ARN") {
