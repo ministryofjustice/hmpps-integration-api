1f631ef82bb25cab155e693262fde3e1f11a64a9
popey2700 <alex.pope@madetech.com>
2024-06-14 13:24:59 +0100

    Fix script bug

--- scripts/check_certs_expiry.sh
+++ scripts/check_certs_expiry.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# File to check
 CERT_FILE="client_certificates/dev-event-service-client.pem"
 
 if [ ! -f "$CERT_FILE" ]; then
@@ -10,7 +9,6 @@ fi
 
 EXPIRY_DATE=$(openssl x509 -in "$CERT_FILE" -noout -enddate | cut -d= -f2)
 
-# convert date to seconds
 date_to_seconds() {
   if date --version >/dev/null 2>&1; then
     # GNU date
@@ -21,12 +19,23 @@ date_to_seconds() {
   fi
 }
 
-# Convert expiry date to seconds
 EXPIRY_DATE_SECONDS=$(date_to_seconds "$EXPIRY_DATE")
-THIRTY_DAYS_SECONDS=$(date_to_seconds "$(date -v +30d)")
 
-if [ "$EXPIRY_DATE_SECONDS" -le "$THIRTY_DAYS_SECONDS" ]; then
-  echo "The certificate will expire within the next 30 days."
+if date --version >/dev/null 2>&1; then
+  # GNU date
+  CURRENT_DATE_SECONDS=$(date +%s)
+  THIRTY_DAYS_SECONDS=$(date -d "+30 days" +%s)
+else
+  # BSD date (macOS)
+  CURRENT_DATE_SECONDS=$(date +%s)
+  THIRTY_DAYS_SECONDS=$(date -v +30d +%s)
+fi
+
+DIFFERENCE=$((EXPIRY_DATE_SECONDS - CURRENT_DATE_SECONDS))
+
+if [ "$DIFFERENCE" -le $((30 * 24 * 60 * 60)) ]; then
+  DAYS_LEFT=$((DIFFERENCE / (24 * 60 * 60)))
+  echo "The certificate will expire on $EXPIRY_DATE in $DAYS_LEFT days."
 else
   echo "The certificate is valid for more than 30 days."
 fi
