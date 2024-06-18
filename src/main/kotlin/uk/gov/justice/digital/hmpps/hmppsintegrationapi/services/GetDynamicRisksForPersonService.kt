8b8b3db802647f3edb1d5d95752f899f1172b42c
Chiara <chiara.mapelli@madetech.com>
2024-06-18 13:25:38 +0100

    Service

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetDynamicRisksForPersonService.kt
@@ -0,0 +1,52 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NDeliusGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.DynamicRisk
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
+
+@Service
+class GetDynamicRisksForPersonService (
+  @Autowired val nDeliusGateway: NDeliusGateway,
+  @Autowired val getPersonService: GetPersonService,
+) {
+  fun execute(hmppsId: String): Response<List<DynamicRisk>> {
+    val personResponse = getPersonService.execute(hmppsId = hmppsId)
+    val deliusCrn = personResponse.data?.identifiers?.deliusCrn
+
+    var nDeliusDynamicRisks: Response<List<DynamicRisk>> = Response(data = emptyList())
+
+    if (deliusCrn != null) {
+      val allNDeliusDynamicRisks = nDeliusGateway.getDynamicRisksForPerson(deliusCrn)
+      val filteredNDeliusDynamicRisks = allNDeliusDynamicRisks.data?.filter {
+        it.code in
+          listOf(
+            "RCCO", "RCPR", "REG22", "RVLN", "ALT8", "STRG", "AVIS", "ALT1", "WEAP",
+            "RHRH", "RLRH", "RMRH", "RVHR", "RCHD", "REG15", "REG16", "REG17",
+            "ALT4", "AVS2", "ALT7", "ALSH"
+          )
+      }.orEmpty()
+      if (filteredNDeliusDynamicRisks.isEmpty()) {
+        return Response(
+          data = emptyList(),
+          errors =
+            listOf(
+              UpstreamApiError(
+                causedBy = UpstreamApi.NDELIUS,
+                type = UpstreamApiError.Type.ENTITY_NOT_FOUND
+              ),
+            ),
+        )
+      }
+      nDeliusDynamicRisks = Response(data = filteredNDeliusDynamicRisks, errors = allNDeliusDynamicRisks.errors)
+    }
+
+    return Response(
+      data = nDeliusDynamicRisks.data,
+      errors = personResponse.errors + nDeliusDynamicRisks.errors
+    )
+  }
+}
