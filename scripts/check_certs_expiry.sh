a74092a45e36bd4e52145de05c21985d86b919c2
popey2700 <alex.pope@madetech.com>
2024-06-26 17:56:26 +0100

    Add slack notification functionality

--- scripts/check_certs_expiry.sh
+++ scripts/check_certs_expiry.sh
@@ -1,67 +1,89 @@
 #!/bin/bash
 
-check_certificate_file_exists() {
-  if [ ! -f "$1" ]; then
-    echo "Certificate file not found: $1"
-    exit 1
-  fi
+configure_aws_credentials() {
+    local environment="$1"
+    access_key_id=$(kubectl get secret aws-services -n hmpps-integration-api-"$environment" -o json | jq -r '.data."api-gateway"' | base64 --decode | jq -r '."access-credentials"."access-key-id"')
+    secret_access_key=$(kubectl get secret aws-services -n hmpps-integration-api-"$environment" -o json | jq -r '.data."api-gateway"' | base64 --decode | jq -r '."access-credentials"."secret-access-key"')
+    export AWS_ACCESS_KEY_ID="$access_key_id"
+    export AWS_SECRET_ACCESS_KEY="$secret_access_key"
+}
+
+get_certificate_from_s3() {
+    local environment="$1"
+    local client="$2"
+    local s3_bucket="hmpps-integration-api-$environment-certificates-backup"
+    local s3_key="$client/client.pem"
+    local file_path="./tmp/client.pem"
+
+    aws s3 cp "s3://$s3_bucket/$s3_key" "$file_path"
+    if [ $? -ne 0 ]; then
+        echo "Failed to download certificate for $client from $s3_bucket."
+        exit 1
+    fi
 }
 
 get_certificate_expiry_date() {
-  local cert_file="$1"
-  local expiry_date
+    local expiry_date
 
-  expiry_date=$(openssl x509 -in "$cert_file" -noout -enddate | cut -d= -f2)
-  if [ $? -ne 0 ]; then
-    echo "Failed to read certificate expiry date."
-    exit 1
-  fi
+    expiry_date=$(openssl x509 -in "./tmp/client.pem" -noout -enddate | cut -d= -f2)
+    if [ $? -ne 0 ]; then
+        echo "Failed to read certificate expiry date."
+        exit 1
+    fi
 
-  echo "$expiry_date"
+    echo "$expiry_date"
 }
 
 convert_date_to_seconds() {
-  local date_str="$1"
-  if date --version >/dev/null 2>&1; then
-    # GNU date
-    date -d "$date_str" +%s
-  else
-    # BSD date (macOS)
-    date -jf "%b %d %H:%M:%S %Y %Z" "$date_str" +%s 2>/dev/null || date -jf "%b %d %H:%M:%S %Y %Z" "$date_str" "+%s"
-  fi
+    local date_str="$1"
+    if date --version >/dev/null 2>&1; then
+        # GNU date
+        date -d "$date_str" +%s
+    else
+        # BSD date (macOS)
+        date -jf "%b %d %H:%M:%S %Y %Z" "$date_str" +%s 2>/dev/null || date -jf "%b %d %H:%M:%S %Y %Z" "$date_str" "+%s"
+    fi
 }
 
 check_certificate_expiry() {
-  local expiry_seconds="$1"
-  local current_seconds="$2"
-  local difference=$((expiry_seconds - current_seconds))
+    local environment="$1"
+    local client="$2"
+    local slack_webhook_url="$3"
 
-  if [ "$difference" -le $((30 * 24 * 60 * 60)) ]; then
-    local days_left=$((difference / (24 * 60 * 60)))
-    echo "The certificate will expire within the next 30 days (in $days_left days)."
-  else
-    echo "The certificate is valid for more than 30 days."
-  fi
+    get_certificate_from_s3 "$environment" "$client"
+
+    local expiry_date
+    expiry_date="$(get_certificate_expiry_date)"
+
+    local expiry_seconds
+    expiry_seconds="$(convert_date_to_seconds "$expiry_date")"
+
+    local current_seconds
+    current_seconds="$(date +%s)"
+
+    local difference=$((expiry_seconds - current_seconds))
+
+    if [ "$difference" -le $((30 * 24 * 60 * 60)) ]; then
+        message=" **TEST** The certificate for $client in $environment will expire within the next 30 days (in $((difference / (24 * 60 * 60))) days)."
+#        curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$slack_webhook_url"
+        echo "$message"
+    else
+        message=" **TEST** The certificate for $client in $environment is valid for more than 30 days and expires on $expiry_date."
+#        curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$slack_webhook_url"
+        echo "$message"
+    fi
 }
 
-# Main function
 main() {
-  local cert_file="client_certificates/dev-notification-test-client.pem"
+    environments=("dev" "preprod" "prod")
+    clients=("ctrlo")
 
-  check_certificate_file_exists "$cert_file"
-
-  local expiry_date
-  expiry_date=$(get_certificate_expiry_date "$cert_file")
-
-  local expiry_date_seconds
-  expiry_date_seconds=$(convert_date_to_seconds "$expiry_date")
-
-  local current_date_seconds
-  current_date_seconds=$(date +%s)
-
-  echo "The certificate will expire on $expiry_date."
-
-  check_certificate_expiry "$expiry_date_seconds" "$current_date_seconds"
+    for environment in "${environments[@]}"; do
+        configure_aws_credentials "$environment"
+        for client in "${clients[@]}"; do
+            check_certificate_expiry "$environment" "$client" "$(kubectl -n hmpps-integration-api-dev get secrets slack-webhook-url -o json  | jq -r '.data."slack_webhook_url"' | base64 --decode)"
+        done
+    done
 }
 
 main
