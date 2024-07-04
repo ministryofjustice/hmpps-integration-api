5fe9f7aaccdb606448d0ee4805600fdae6fe6a88
popey2700 <alex.pope@madetech.com>
2024-07-04 12:08:55 +0100

    Update jsonInclude SentenceKeyDate

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/SentenceKeyDate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/SentenceKeyDate.kt
@@ -3,9 +3,9 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 import com.fasterxml.jackson.annotation.JsonInclude
 import java.time.LocalDate
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class SentenceKeyDate(
   val date: LocalDate? = null,
   val overrideDate: LocalDate? = null,
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   val calculatedDate: LocalDate? = null,
 )
