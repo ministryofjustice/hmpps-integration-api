3ebb37173c0d169598570f1c31423e9189984aa9
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-08-05 14:48:49 +0100

    PI-2377: Get client name and set to clientId (#460)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/telemetry/ClientTrackingInterceptor.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.telemetry
+
+import io.opentelemetry.api.trace.Span
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class ClientTrackingConfiguration(private val clientTrackingInterceptor: ClientTrackingInterceptor) : WebMvcConfigurer {
+  override fun addInterceptors(registry: InterceptorRegistry) {
+    registry.addInterceptor(clientTrackingInterceptor).addPathPatterns("/**")
+  }
+}
+
+@Component
+class ClientTrackingInterceptor : HandlerInterceptor {
+  override fun preHandle(
+    request: HttpServletRequest,
+    response: HttpServletResponse,
+    handler: Any,
+  ): Boolean {
+    val subjectDistinguishedName = request.getAttribute("clientName") as String?
+    subjectDistinguishedName?.let {
+      try {
+        Span.current().setAttribute("clientId", it)
+      } catch (ignored: Exception) {
+        // Do nothing - don't create client id span
+      }
+    }
+    return true
+  }
+}
