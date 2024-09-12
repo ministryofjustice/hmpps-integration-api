95c9ee7625c3cfdca017b14942bf000055952349
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-09-12 18:05:37 +0100

    PI-2517 - update integration tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/AdjudicationsIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/AdjudicationsIntegrationTest.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
 
 import org.junit.jupiter.api.Test
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
@@ -11,7 +10,6 @@ class AdjudicationsIntegrationTest : IntegrationTestBase() {
   fun `returns adjudications for a person`() {
     callApi("$basePath/$nomsId/reported-adjudications")
       .andExpect(status().isOk)
-      .andDo(print())
       .andExpect(content().json(getExpectedResponse("person-adjudications"), true))
   }
 }
