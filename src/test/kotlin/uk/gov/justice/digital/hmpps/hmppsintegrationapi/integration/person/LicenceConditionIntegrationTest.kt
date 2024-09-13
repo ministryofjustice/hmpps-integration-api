ed65e0287ed3a6bc95a22898eec89995cdc6f374
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-09-13 12:25:45 +0100

    PI-2517 - update integration tests

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/LicenceConditionIntegrationTest.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
+
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
+
+class LicenceConditionIntegrationTest : IntegrationTestBase() {
+
+  @Test
+  fun `returns alerts for a person`() {
+    callApi("$basePath/$crn/licences/conditions")
+      .andExpect(status().isOk)
+      .andExpect(content().json(getExpectedResponse("person-licence-conditions")))
+  }
+
+}
