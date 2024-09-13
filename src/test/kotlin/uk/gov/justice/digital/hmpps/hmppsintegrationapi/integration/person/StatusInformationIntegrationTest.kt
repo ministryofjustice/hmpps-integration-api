c5c60bcdeb8a23aaa35619ca69a6b79556564a16
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-09-13 16:56:47 +0100

    PI-2517 - update integration tests

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/StatusInformationIntegrationTest.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
+
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
+
+class StatusInformationIntegrationTest : IntegrationTestBase() {
+
+  @Test
+  fun `returns status information for a person`() {
+    callApi("$basePath/$pnc/status-information")
+      .andExpect(status().isOk)
+      .andExpect(content().json(getExpectedResponse("person-status-information")))
+  }
+
+}
