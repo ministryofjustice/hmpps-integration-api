a31da0fc2d2f247a58711f0edb69793cd1314f71
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-09-13 13:34:04 +0100

    PI-2517 - update integration tests

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/RiskManagementIntegrationTest.kt
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
+
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
+
+class RiskManagementIntegrationTest : IntegrationTestBase() {
+
+  @Test
+  fun `returns protected characteristics for a person`() {
+    callApi("$basePath/$crn/risk-management-plan")
+      .andExpect(status().isOk)
+      .andExpect(content().json(getExpectedResponse("person-risk-plan")))
+  }
+}
