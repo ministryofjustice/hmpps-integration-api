008e686a154c0a275726354c65395fcae0baedbd
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-09-13 13:15:13 +0100

    PI-2517 - update integration tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/ProtectedCharacteristicsIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/ProtectedCharacteristicsIntegrationTest.kt
@@ -5,13 +5,13 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
 
-class OffencesIntegrationTest : IntegrationTestBase() {
+class ProtectedCharacteristicsIntegrationTest : IntegrationTestBase() {
 
   @Test
-  fun `returns offences for a person`() {
-    callApi("$basePath/$pnc/offences")
+  fun `returns protected characteristics for a person`() {
+    callApi("$basePath/$pnc/protected-characteristics")
       .andExpect(status().isOk)
-      .andExpect(content().json(getExpectedResponse("person-offences")))
+      .andExpect(content().json(getExpectedResponse("person-protected-characteristics")))
   }
 
 }
