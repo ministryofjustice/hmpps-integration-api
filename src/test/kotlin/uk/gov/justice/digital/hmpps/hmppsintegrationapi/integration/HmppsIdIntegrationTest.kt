4660ea39887badc05eeedb22d21864949e98ba65
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-11-11 12:53:41 +0000

    Added tests.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/HmppsIdIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/HmppsIdIntegrationTest.kt
@@ -17,4 +17,56 @@ class HmppsIdIntegrationTest : IntegrationTestBase() {
         ),
       )
   }
+
+  @Test
+  fun `gets the nomis Id for a HMPPSID where the id is a crn`() {
+    callApi("/v1/hmpps/id/$crn/nomis-number")
+      .andExpect(status().isOk)
+      .andExpect(
+        content().json(
+          """
+        {"data":{"nomisNumber":"G5555TT"}}
+      """,
+        ),
+      )
+  }
+
+  @Test
+  fun `gets the nomis Id for a HMPPSID where the id is a NOMIS id`() {
+    callApi("/v1/hmpps/id/$nomsId/nomis-number")
+      .andExpect(status().isOk)
+      .andExpect(
+        content().json(
+          """
+        {"data":{"nomisNumber":"G2996UX"}}
+      """,
+        ),
+      )
+  }
+
+  @Test
+  fun `gets the nomis Id for a HMPPSID where the id is invalid`() {
+    callApi("/v1/hmpps/id/invalidId/nomis-number")
+      .andExpect(status().is4xxClientError)
+      .andExpect(
+        content().json(
+          """
+        {"userMessage":"Invalid HMPPS ID: invalidId","developerMessage":"Validation failure: Invalid HMPPS ID: invalidId"}
+      """,
+        ),
+      )
+  }
+
+  @Test
+  fun `gets the nomis Id for a HMPPSID where the id not found`() {
+    callApi("/v1/hmpps/id/invalidId/nomis-number")
+      .andExpect(status().is4xxClientError)
+      .andExpect(
+        content().json(
+          """
+        {"userMessage":"Invalid HMPPS ID: invalidId","developerMessage":"Validation failure: Invalid HMPPS ID: invalidId","moreInfo":null}
+      """,
+        ),
+      )
+  }
 }
