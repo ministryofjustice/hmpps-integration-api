ea7710fe90d375ed49bea770bbfee426d205bf52
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2024-11-12 12:06:17 +0000

    Added endpoint to return NOMS number. (#512)
    * Added endpoint to return NOMS number.
    
    * Added tests.
    
    * Update HmppsIdController.kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/HmppsIdController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/HmppsIdController.kt
@@ -1,6 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers.v1
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.ValidationException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,6 +13,7 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.EntityNotFound
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.decodeUrlCharacters
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.DataResponse
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsId
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.NomisNumber
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetHmppsIdService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.internal.AuditService
@@ -34,6 +38,36 @@ class HmppsIdController(
     }
 
     auditService.createEvent("GET_HMPPS_ID_BY_NOMIS_NUMBER", mapOf("nomisNumber" to nomisNumber))
+
+    return DataResponse(response.data)
+  }
+
+  @GetMapping("{encodedHmppsId}/nomis-number")
+  @Operation(
+    summary = "Return NOMS number for a given hmpps Id",
+    description = """Accepts a HMPPS Id (hmppsId) and looks up the corresponding NOMS number.
+    """,
+    responses = [
+      ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+      ApiResponse(responseCode = "404", description = "NOMS number could not be found."),
+      ApiResponse(responseCode = "400", description = "Invalid hmppsId."),
+    ],
+  )
+  fun getNomisNumberByHMPPSID(
+    @PathVariable encodedHmppsId: String,
+  ): DataResponse<NomisNumber?> {
+    val hmppsId = encodedHmppsId.decodeUrlCharacters()
+
+    val response = getHmppsIdService.getNomisNumber(hmppsId)
+
+    if (response.hasError(UpstreamApiError.Type.ENTITY_NOT_FOUND)) {
+      throw EntityNotFoundException("Could not find nomis number for HMPPS ID: $hmppsId")
+    }
+    if (response.hasError(UpstreamApiError.Type.BAD_REQUEST)) {
+      throw ValidationException("Invalid HMPPS ID: $hmppsId")
+    }
+
+    auditService.createEvent("GET_NOMIS_NUMBER_BY_HMPPS_ID", mapOf("hmppsId" to hmppsId))
 
     return DataResponse(response.data)
   }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHmppsIdService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHmppsIdService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsId
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.NomisNumber
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 
 @Service
@@ -15,6 +16,15 @@ class GetHmppsIdService(
     return Response(
       data = HmppsId(hmppsId = personResponse.data?.hmppsId),
       errors = personResponse.errors,
+    )
+  }
+
+  fun getNomisNumber(hmppsId: String): Response<NomisNumber?> {
+    val nomisResponse = getPersonService.getNomisNumber(hmppsId = hmppsId)
+
+    return Response(
+      data = NomisNumber(nomisNumber = nomisResponse.data?.nomisNumber),
+      errors = nomisResponse.errors,
     )
   }
 }
