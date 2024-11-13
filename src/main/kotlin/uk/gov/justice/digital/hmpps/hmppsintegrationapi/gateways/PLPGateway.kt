f13a6ef0fc28a81d2cd934ab96f786e3724bac90
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2024-11-13 09:20:55 +0000

    Added endpoint to return plp induction schedule. (#514)
    * Added endpoint to return NOMS number.
    
    * Added tests.
    
    * Added plp induction schedule endpoint.
    
    * Added plp induction schedule endpoint.
    
    * Commit changes made by code formatters
    
    * Update HmppsIdController.kt
    
    * manually copied over json file
    
    * Commit changes made by code formatters
    
    * reverted prism change
    
    * added prisoner number to response
    
    * lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/PLPGateway.kt
@@ -0,0 +1,52 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper.WebClientWrapperResponse
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.InductionSchedule
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+
+@Component
+class PLPGateway(
+  @Value("\${services.plp.base-url}") baseUrl: String,
+) {
+  private val webClient = WebClientWrapper(baseUrl)
+
+  @Autowired
+  lateinit var hmppsAuthGateway: HmppsAuthGateway
+
+  fun getInductionSchedule(prisonerNumber: String): Response<InductionSchedule> {
+    val result =
+      webClient.request<InductionSchedule>(
+        HttpMethod.GET,
+        "/inductions/$prisonerNumber/induction-schedule",
+        authenticationHeader(),
+        UpstreamApi.PLP,
+      )
+
+    return when (result) {
+      is WebClientWrapperResponse.Success -> {
+        val inductionSchedule = result.data
+        Response(data = inductionSchedule)
+      }
+
+      is WebClientWrapperResponse.Error -> {
+        Response(
+          data = InductionSchedule(),
+          errors = result.errors,
+        )
+      }
+    }
+  }
+
+  private fun authenticationHeader(): Map<String, String> {
+    val token = hmppsAuthGateway.getClientToken("PLP")
+    return mapOf(
+      "Authorization" to "Bearer $token",
+    )
+  }
+}
