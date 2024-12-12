56d07cddcc2bbc8be3eb402b4abb8c88ae55f2af
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2024-12-12 14:01:37 +0000

    updated to return actual results from PLP (#535)
    * updated to return actual results from PLP
    
    * updated to return actual results from PLP
    
    * updated to return actual results from PLP
    
    * updated to return actual results from PLP

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ReviewSchedule.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ReviewSchedule.kt
@@ -2,17 +2,30 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
-import io.swagger.v3.oas.annotations.media.Schema
+import java.time.Instant
 import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+data class ReviewSchedules(
+  val reviewSchedules: List<ReviewSchedule>,
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.USE_DEFAULTS)
 data class ReviewSchedule(
-  @Schema(
-    description = "An ISO-8601 date representing when the Review should be completed by.",
-    example = "2023-09-01",
-  )
-  val deadlineDate: LocalDate? = null,
-  val nomisNumber: String? = null,
-  val description: String? = null,
+  val reference: String,
+  val reviewDateFrom: LocalDate,
+  val reviewDateTo: LocalDate,
+  val status: String,
+  val calculationRule: String,
+  val createdBy: String,
+  val createdByDisplayName: String,
+  val createdAt: Instant,
+  val createdAtPrison: String,
+  val updatedBy: String,
+  val updatedByDisplayName: String,
+  val updatedAt: Instant,
+  val updatedAtPrison: String,
+  val version: Int,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetReviewScheduleForPersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetReviewScheduleForPersonService.kt
@@ -4,27 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PLPGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.ReviewSchedule
-import java.time.LocalDate
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.ReviewSchedules
 
 @Service
 class GetReviewScheduleForPersonService(
   @Autowired val plpGateway: PLPGateway,
   @Autowired val getPersonService: GetPersonService,
 ) {
-  fun execute(hmppsId: String): Response<ReviewSchedule> {
+  fun execute(hmppsId: String): Response<ReviewSchedules> {
     val response = getPersonService.getNomisNumber(hmppsId = hmppsId)
 
     response.data?.nomisNumber?.let {
-      return Response(
-        ReviewSchedule(
-          deadlineDate = LocalDate.now().plusMonths(1),
-          nomisNumber = it,
-          description = "This is a hardcoded response.",
-        ),
-      )
-//      return plpGateway.getReviewSchedule(it) // <-- this will be call the PLP service once the downstream code is written
+      return plpGateway.getReviewSchedules(it)
     }
-    return Response(ReviewSchedule(), response.errors)
+    return Response(ReviewSchedules(listOf()), response.errors)
   }
 }
