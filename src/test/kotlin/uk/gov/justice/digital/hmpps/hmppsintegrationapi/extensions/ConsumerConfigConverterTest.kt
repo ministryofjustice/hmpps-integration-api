bcd539490331ece2082ce3dfda9908d5dce7ae9d
Ben Madley <ben.madley@madetech.com>
2024-12-17 15:25:52 +0000

    Test for ConsumerConfigConverter.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/ConsumerConfigConverterTest.kt
@@ -0,0 +1,20 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerConfig
+
+@ActiveProfiles("test")
+class ConsumerConfigConverterTest() {
+  @Test
+  fun `converts empty string to empty list`() {
+    //  authorisation:
+    //    no-include:
+
+    val consumerConfig = ""
+    val actual = ConsumerConfigConverter().convert(consumerConfig)
+
+    actual.shouldBe(ConsumerConfig(emptyList()))
+  }
+}
