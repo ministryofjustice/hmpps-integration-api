798531388c5a048dfe187f6a60c4332e42d829ca
wcdkj <will.clark@digital.justice.gov.uk>
2024-12-19 14:28:33 +0000

    Added fitlers extraction filter for incoming requests and unit tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/FiltersExtractionFilter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/FiltersExtractionFilter.kt
@@ -1,17 +1,45 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
 
-import jakarta.servlet.*
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.AuthorisationConfig
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 import java.io.IOException
 
-class FiltersExtractionFilter : Filter {
-  @Throws(IOException::class, ServletException::class)
-  override fun doFilter(
-    request: ServletRequest,
-    response: ServletResponse?,
-    chain: FilterChain,
-  ) {
-    request.setAttribute("filters", ConsumerFilters(mapOf("example-filter" to listOf("filter-1", "filter-2"))))
-    chain.doFilter(request, response)
+@Component
+@Order(2)
+@EnableConfigurationProperties(AuthorisationConfig::class)
+class FiltersExtractionFilter
+  @Autowired
+  constructor(
+    var authorisationConfig: AuthorisationConfig,
+  ) : Filter {
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(
+      request: ServletRequest,
+      response: ServletResponse?,
+      chain: FilterChain,
+    ) {
+      val subjectDistinguishedName = request.getAttribute("clientName") as String?
+      val requestingConsumer: ConsumerConfig? = authorisationConfig.consumers[subjectDistinguishedName]
+
+      if (requestingConsumer == null) {
+        (response as HttpServletResponse).sendError(HttpServletResponse.SC_FORBIDDEN, "TODO")
+        return
+      }
+
+      val requestingConsumersFilters: ConsumerFilters? = requestingConsumer.filters
+
+      request.setAttribute("filters", requestingConsumersFilters)
+      chain.doFilter(request, response)
+    }
   }
-}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/FiltersExtractionFilterTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/FiltersExtractionFilterTest.kt
@@ -1,26 +1,37 @@
+@file:Suppress("ktlint:standard:no-wildcard-imports")
+
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
 
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
 import org.mockito.Mockito.*
+import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.AuthorisationConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 
 class FiltersExtractionFilterTest {
+  @Mock
+  private var authorisationConfig: AuthorisationConfig = AuthorisationConfig()
+
+  @InjectMocks
+  private var filtersExtractionFilter: FiltersExtractionFilter = FiltersExtractionFilter(authorisationConfig)
+
   @Test
   fun `calls the onward chain`() {
     // Arrange
     val mockRequest = mock(HttpServletRequest::class.java)
+    whenever(mockRequest.getAttribute("clientName")).thenReturn("consumer-name")
+
     val mockResponse = mock(HttpServletResponse::class.java)
     val mockChain = mock(FilterChain::class.java)
 
-    val filter = FiltersExtractionFilter()
-
     // Act
-    filter.doFilter(mockRequest, mockResponse, mockChain)
+    filtersExtractionFilter.doFilter(mockRequest, mockResponse, mockChain)
 
     // Assert
     verify(mockChain, times(1)).doFilter(mockRequest, mockResponse)
@@ -30,18 +41,35 @@ class FiltersExtractionFilterTest {
   fun `can get filters attribute from the test`() {
     // Arrange
     val mockRequest = mock(HttpServletRequest::class.java)
+    whenever(mockRequest.getAttribute("clientName")).thenReturn("consumer-name")
+
     val mockChain = mock(FilterChain::class.java)
 
-    val filter = FiltersExtractionFilter()
-
-    val authorisationConfig = AuthorisationConfig()
     val expectedFilters = ConsumerFilters(mapOf("example-filter" to listOf("filter-1", "filter-2")))
     authorisationConfig.consumers = mapOf("consumer-name" to ConsumerConfig(include = null, filters = expectedFilters))
 
     // Act
-    filter.doFilter(mockRequest,  null, mockChain)
+    filtersExtractionFilter.doFilter(mockRequest, null, mockChain)
 
     // Assert
     verify(mockRequest, times(1)).setAttribute("filters", expectedFilters)
+  }
+
+  @Test
+  fun `tolerates when no consumer config values matched with clientName`() {
+    // Arrange
+    val mockRequest = mock(HttpServletRequest::class.java)
+    whenever(mockRequest.getAttribute("clientName")).thenReturn("invalid-consumer-name")
+
+    val mockChain = mock(FilterChain::class.java)
+
+    val expectedFilters = ConsumerFilters(mapOf("example-filter" to listOf("filter-1", "filter-2")))
+    authorisationConfig.consumers = mapOf("consumer-name" to ConsumerConfig(include = null, filters = expectedFilters))
+
+    // Act
+    filtersExtractionFilter.doFilter(mockRequest, null, mockChain)
+
+    // Assert
+    verify(mockRequest, times(0)).setAttribute("filters", expectedFilters)
   }
 }
