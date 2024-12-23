8e94341a3957435ee1d3e5a5ca2eee5529b96014
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2024-12-23 15:23:11 +0000

    return additional conducted by/role (#548)
    * return additional conductedby/role
    
    * return additional conductedby/role
    
    * return additional conductedby/role

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ReviewSchedule.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ReviewSchedule.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import java.time.Instant
 import java.time.LocalDate
+import java.util.UUID
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
@@ -14,7 +15,7 @@ data class ReviewSchedules(
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
 data class ReviewSchedule(
-  val reference: String,
+  val reference: UUID,
   val reviewDateFrom: LocalDate,
   val reviewDateTo: LocalDate,
   val status: String,
@@ -28,4 +29,41 @@ data class ReviewSchedule(
   val updatedAt: Instant,
   val updatedAtPrison: String,
   val version: Int,
+  var conductedBy: String?,
+  var conductedRole: String?,
+)
+
+data class ActionPlanReviewsResponse(
+  val latestReviewSchedule: ScheduledActionPlanReviewResponse? = null,
+  val completedReviews: List<CompletedActionPlanReviewResponse>,
+)
+
+data class ScheduledActionPlanReviewResponse(
+  val reviewDateFrom: LocalDate,
+  val reviewDateTo: LocalDate,
+  val status: String,
+  val calculationRule: String,
+  val createdBy: String,
+  val createdByDisplayName: String,
+  val createdAt: java.time.OffsetDateTime,
+  val createdAtPrison: String,
+  val updatedBy: String,
+  val updatedByDisplayName: String,
+  val updatedAt: java.time.OffsetDateTime,
+  val updatedAtPrison: String,
+  val exemptionReason: String? = null,
+  val version: Int? = null,
+)
+
+data class CompletedActionPlanReviewResponse(
+  val reference: UUID,
+  val deadlineDate: LocalDate,
+  val completedDate: LocalDate,
+  val createdBy: String,
+  val createdByDisplayName: String,
+  val createdAt: java.time.OffsetDateTime,
+  val createdAtPrison: String,
+  val reviewScheduleReference: UUID? = null,
+  val conductedBy: String? = null,
+  val conductedByRole: String? = null,
 )
