d160fc74302cbfa543f9e317a5685ca2a30d23b7
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2024-12-23 16:02:29 +0000

    WIP Co-authored-by: emma.lanigan <emma.lanigan@justice.gov.uk>

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/ConsumerNameExtractionFilterTest.kt
@@ -0,0 +1,49 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.AuthorisationConfig
+
+class ConsumerNameExtractionFilterTest {
+  private var authorisationConfig: AuthorisationConfig = AuthorisationConfig()
+  private var consumerNameExtractionFilter: ConsumerNameExtractionFilter = ConsumerNameExtractionFilter()
+
+  @Test
+  fun `calls the onward chain`() {
+    // Arrange
+    val mockRequest = mock(HttpServletRequest::class.java)
+    whenever(mockRequest.getAttribute("clientName")).thenReturn("consumer-name")
+
+    val mockResponse = mock(HttpServletResponse::class.java)
+    val mockChain = mock(FilterChain::class.java)
+
+    // Act
+    consumerNameExtractionFilter.doFilter(mockRequest, mockResponse, mockChain)
+
+    // Assert
+    verify(mockChain, times(1)).doFilter(mockRequest, mockResponse)
+  }
+
+//  @Test
+//  fun `can get subject distinguished name from request and set as consumer name `() {
+//    // Arrange
+//    val mockRequest = mock(HttpServletRequest::class.java)
+//    whenever(mockRequest.getHeader("subject-distinguished-name")).thenReturn("CN=consumer-name"))
+//    whenever(mockRequest.getAttribute("clientName")).thenReturn("consumer-name")
+//
+//    val mockResponse = mock(HttpServletResponse::class.java)
+//    val mockChain = mock(FilterChain::class.java)
+//
+//    // Act
+//    consumerNameExtractionFilter.doFilter(mockRequest, mockResponse, mockChain)
+//
+//    // Assert
+//    verify(mockRequest, times(1)).setAttribute("client-name", "consumer-name")
+//  }
+}
