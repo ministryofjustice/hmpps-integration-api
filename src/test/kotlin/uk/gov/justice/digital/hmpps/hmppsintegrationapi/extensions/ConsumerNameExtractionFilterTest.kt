22632ed3a15211ef9d5c7af108d21cb3f2631ee6
emmalanigan <emma.lanigan@justice.gov.uk>
2024-12-24 10:12:20 +0000

    add test to check consumer name is set using subject distinguished name

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/ConsumerNameExtractionFilterTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/ConsumerNameExtractionFilterTest.kt
@@ -8,10 +8,8 @@ import org.mockito.Mockito.mock
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.kotlin.whenever
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.AuthorisationConfig
 
 class ConsumerNameExtractionFilterTest {
-  private var authorisationConfig: AuthorisationConfig = AuthorisationConfig()
   private var consumerNameExtractionFilter: ConsumerNameExtractionFilter = ConsumerNameExtractionFilter()
 
   @Test
@@ -30,20 +28,18 @@ class ConsumerNameExtractionFilterTest {
     verify(mockChain, times(1)).doFilter(mockRequest, mockResponse)
   }
 
-//  @Test
-//  fun `can get subject distinguished name from request and set as consumer name `() {
-//    // Arrange
-//    val mockRequest = mock(HttpServletRequest::class.java)
-//    whenever(mockRequest.getHeader("subject-distinguished-name")).thenReturn("CN=consumer-name"))
-//    whenever(mockRequest.getAttribute("clientName")).thenReturn("consumer-name")
-//
-//    val mockResponse = mock(HttpServletResponse::class.java)
-//    val mockChain = mock(FilterChain::class.java)
-//
-//    // Act
-//    consumerNameExtractionFilter.doFilter(mockRequest, mockResponse, mockChain)
-//
-//    // Assert
-//    verify(mockRequest, times(1)).setAttribute("client-name", "consumer-name")
-//  }
+  @Test
+  fun `can get subject distinguished name from request and set as consumer name `() {
+    // Arrange
+    val mockRequest = mock(HttpServletRequest::class.java)
+    whenever(mockRequest.getHeader("subject-distinguished-name")).thenReturn(",CN=consumer-name")
+
+    val mockResponse = mock(HttpServletResponse::class.java)
+    val mockChain = mock(FilterChain::class.java)
+    // Act
+    consumerNameExtractionFilter.doFilter(mockRequest, mockResponse, mockChain)
+
+    // Assert
+    verify(mockRequest, times(1)).setAttribute("clientName", "consumer-name")
+  }
 }
