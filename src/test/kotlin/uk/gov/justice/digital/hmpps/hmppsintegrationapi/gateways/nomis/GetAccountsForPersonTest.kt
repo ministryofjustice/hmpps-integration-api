9aab6a57ea0a2af43c7e7c19c4b524fba3076cad
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-01-06 16:22:34 +0000

    complete tests for getAccountsForPerson

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/nomis/GetAccountsForPersonTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/nomis/GetAccountsForPersonTest.kt
@@ -2,12 +2,14 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.nomis
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import org.mockito.Mockito
 import org.mockito.internal.verification.VerificationModeFactory
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.bean.override.mockito.MockitoBean
@@ -16,6 +18,8 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.HmppsAuthGatewa
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.mockservers.HmppsAuthMockServer
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.mockservers.NomisApiMockServer
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 
 @ActiveProfiles("test")
 @ContextConfiguration(
@@ -68,23 +72,15 @@ class GetAccountsForPersonTest(
           response.data?.savings.shouldBe(2234)
           response.data?.cash.shouldBe(1000)
         }
-//
-//      it("returns a person with an empty list of offences when no offences are found") {
-//        nomisApiMockServer.stubNomisApiResponse(offenceHistoryPath, "[]")
-//
-//        val response = nomisGateway.getOffencesForPerson(offenderNo)
-//
-//        response.data.shouldBeEmpty()
-//      }
 
-//      it("returns an error when 404 Not Found is returned because no person is found") {
-//        nomisApiMockServer.stubNomisApiResponse(offenceHistoryPath, "", HttpStatus.NOT_FOUND)
-//
-//        val response = nomisGateway.getOffencesForPerson(offenderNo)
-//
-//        response.errors.shouldHaveSize(1)
-//        response.errors.first().causedBy.shouldBe(UpstreamApi.NOMIS)
-//        response.errors.first().type.shouldBe(UpstreamApiError.Type.ENTITY_NOT_FOUND)
-//      }
+        it("returns an error when 404 Not Found is returned because no person is found") {
+          nomisApiMockServer.stubNomisApiResponse(accountsPath, "", HttpStatus.NOT_FOUND)
+
+          val response = nomisGateway.getAccountsForPerson(prisonId, nomisNumber)
+
+          response.errors.shouldHaveSize(1)
+          response.errors.first().causedBy.shouldBe(UpstreamApi.NOMIS)
+          response.errors.first().type.shouldBe(UpstreamApiError.Type.ENTITY_NOT_FOUND)
+        }
       },
     )
