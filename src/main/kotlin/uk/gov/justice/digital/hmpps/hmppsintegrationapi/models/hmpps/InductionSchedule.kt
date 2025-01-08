c79fc2d7f54d6ecec89b24b8977579b5b7cb7d12
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-01-08 11:14:11 +0000

    added schema documentation (#566)
    * added schema documentation
    
    * added schema documentation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/InductionSchedule.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/InductionSchedule.kt
@@ -14,6 +14,12 @@ import java.time.LocalDate
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
 data class InductionSchedules(
+  @Schema(
+    description = """
+       A list of induction schedule versions for this prisoner. The prisoner will only have one induction schedule but for various reasons the schedule can be updated.
+       This list will show each change the schedule has been through. This allows for reports to be generated.
+    """,
+  )
   val inductionSchedules: List<InductionSchedule>,
 )
 
@@ -27,12 +33,42 @@ data class InductionSchedule(
   )
   val deadlineDate: LocalDate? = null,
   @Schema(
-    description = "The current status of the Induction Schedule",
+    description = """
+      The current status of the Induction Schedule.
+      list of values:
+          SCHEDULED
+          COMPLETED
+          EXEMPT_PRISONER_DRUG_OR_ALCOHOL_DEPENDENCY
+          EXEMPT_PRISONER_OTHER_HEALTH_ISSUES
+          EXEMPT_PRISONER_FAILED_TO_ENGAGE
+          EXEMPT_PRISONER_ESCAPED_OR_ABSCONDED
+          EXEMPT_PRISONER_SAFETY_ISSUES
+          EXEMPT_PRISON_REGIME_CIRCUMSTANCES
+          EXEMPT_PRISON_STAFF_REDEPLOYMENT
+          EXEMPT_PRISON_OPERATION_OR_SECURITY_ISSUE
+          EXEMPT_SECURITY_ISSUE_RISK_TO_STAFF
+          EXEMPT_SYSTEM_TECHNICAL_ISSUE
+          EXEMPT_PRISONER_TRANSFER
+          EXEMPT_PRISONER_RELEASE
+          EXEMPT_PRISONER_DEATH
+          EXEMPT_SCREENING_AND_ASSESSMENT_IN_PROGRESS
+          EXEMPT_SCREENING_AND_ASSESSMENT_INCOMPLETE
+    """,
     example = "SCHEDULED",
   )
   val status: String? = null,
   @Schema(
-    description = "The Induction Schedule rule used to determine deadline date.",
+    description = """
+        The Induction Schedule rule used to determine deadline date.
+        list of values are:
+          NEW_PRISON_ADMISSION
+          EXISTING_PRISONER_LESS_THAN_6_MONTHS_TO_SERVE
+          EXISTING_PRISONER_BETWEEN_6_AND_12_MONTHS_TO_SERVE
+          EXISTING_PRISONER_BETWEEN_12_AND_60_MONTHS_TO_SERVE
+          EXISTING_PRISONER_INDETERMINATE_SENTENCE
+          EXISTING_PRISONER_ON_REMAND
+          EXISTING_PRISONER_UN_SENTENCED
+    """,
     example = "NEW_PRISON_ADMISSION",
   )
   val calculationRule: String? = null,
@@ -62,6 +98,13 @@ data class InductionSchedule(
   )
   val inductionPerformedAt: LocalDate? = null,
   val exemptionReason: String? = null,
+  @Schema(
+    description = """
+      The induction schedule can change status numerous times.
+      When looking at the plp-induction-schedule/history of the inductions.
+      The version number indicates which version of the induction schedule this one is, the higher the number the newer the update.
+    """,
+  )
   val version: Int? = null,
 )
 
