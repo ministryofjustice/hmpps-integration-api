ff0702f10d1308614476302b35e1a7c86658b708
wcdkj <will.clark@digital.justice.gov.uk>
2025-01-29 15:30:37 +0000

    Added consumer filter access control to sentences endpoint and updated relevant tests (#613)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/SentencesIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/SentencesIntegrationTest.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
 
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
@@ -20,5 +22,25 @@ class SentencesIntegrationTest : IntegrationTestBase() {
     callApi("$path/latest-key-dates-and-adjustments")
       .andExpect(status().isOk)
       .andExpect(content().json(getExpectedResponse("person-sentence-key-dates")))
+  }
+
+  @Test
+  fun `return a 404 for if consumer has empty list of prisons on latest sentence key dates and adjustments `() {
+    val headers = HttpHeaders()
+    headers.set("subject-distinguished-name", "C=GB,ST=London,L=London,O=Home Office,CN=no-prisons")
+    mockMvc
+      .perform(
+        get("$path/latest-key-dates-and-adjustments").headers(headers),
+      ).andExpect(status().isNotFound)
+  }
+
+  @Test
+  fun `return a 404 for prisoner in wrong prison on latest sentence key dates and adjustments`() {
+    val headers = HttpHeaders()
+    headers.set("subject-distinguished-name", "C=GB,ST=London,L=London,O=Home Office,CN=limited-prisons")
+    mockMvc
+      .perform(
+        get("$path/latest-key-dates-and-adjustments").headers(headers),
+      ).andExpect(status().isNotFound)
   }
 }
