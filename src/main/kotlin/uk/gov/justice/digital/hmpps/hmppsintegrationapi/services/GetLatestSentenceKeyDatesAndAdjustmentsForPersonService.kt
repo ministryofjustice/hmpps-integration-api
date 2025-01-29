7f2f472a5e296ed1bc5cb7953989cbf2127fc619
wcdkj <will.clark@digital.justice.gov.uk>
2025-01-29 14:58:45 +0000

    Added consumer filter access control to sentences endpoint and updated relevant tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLatestSentenceKeyDatesAndAdjustmentsForPersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLatestSentenceKeyDatesAndAdjustmentsForPersonService.kt
@@ -2,18 +2,26 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.ProbationOffenderSearchGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.KeyDatesAndAdjustmentsDTO
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.LatestSentenceKeyDatesAndAdjustments
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 
 @Service
 class GetLatestSentenceKeyDatesAndAdjustmentsForPersonService(
   @Autowired val probationOffenderSearchGateway: ProbationOffenderSearchGateway,
+  @Autowired val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway,
   @Autowired val nomisGateway: NomisGateway,
+  @Autowired val consumerPrisonAccessService: ConsumerPrisonAccessService,
 ) {
-  fun execute(hmppsId: String): Response<LatestSentenceKeyDatesAndAdjustments?> {
+  fun execute(
+    hmppsId: String,
+    filters: ConsumerFilters?,
+  ): Response<LatestSentenceKeyDatesAndAdjustments?> {
     val (person, personErrors) = probationOffenderSearchGateway.getPerson(hmppsId)
 
     if (personErrors.isNotEmpty()) {
@@ -21,6 +29,19 @@ class GetLatestSentenceKeyDatesAndAdjustmentsForPersonService(
     }
 
     val nomisNumber = person?.identifiers?.nomisNumber ?: return Response(data = null)
+
+    val (prisonerOffender, prisonOffenderErrors) = prisonerOffenderSearchGateway.getPrisonOffender(nomisNumber)
+    if (prisonOffenderErrors.isNotEmpty()) {
+      return Response(data = null, prisonOffenderErrors)
+    }
+    val prisonId = prisonerOffender?.prisonId
+
+    if (!prisonId.isNullOrEmpty()) {
+      val consumerPrisonFilterCheck = consumerPrisonAccessService.checkConsumerHasPrisonAccess<LatestSentenceKeyDatesAndAdjustments>(prisonId, filters)
+      if (consumerPrisonFilterCheck.errors.isNotEmpty()) {
+        return consumerPrisonFilterCheck
+      }
+    }
 
     val (latestSentenceKeyDates, latestSentenceKeyDatesErrors) =
       nomisGateway.getLatestSentenceKeyDatesForPerson(
