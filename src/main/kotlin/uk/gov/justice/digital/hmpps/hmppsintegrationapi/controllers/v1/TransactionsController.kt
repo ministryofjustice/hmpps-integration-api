72c56ca228908abe7e4cf23c9b78ff71a5f4c70d
wcdkj <will.clark@digital.justice.gov.uk>
2025-01-30 11:16:05 +0000

    Updating request body docs on transaction post controller

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/TransactionsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/TransactionsController.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers.v1
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.parameters.RequestBody
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.ValidationException
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestAttribute
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -178,6 +179,17 @@ class TransactionsController(
   @Operation(
     summary = "Post a transaction.",
     description = "<b>Applicable filters</b>: <ul><li>prisons</li></ul>",
+    requestBody =
+      RequestBody(
+        description = "The transaction request body.",
+        required = true,
+        content = [
+          Content(
+            schema =
+              Schema(ref = "#/components/schemas/TransactionRequest"),
+          ),
+        ],
+      ),
     responses = [
       ApiResponse(responseCode = "200", useReturnTypeSchema = true, description = "Successfully created a transaction."),
       ApiResponse(
