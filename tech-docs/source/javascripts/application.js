0ca76770248753d69e048639984fc0d69db14e62
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-02-03 16:03:15 +0000

    Add JS to show response bodies (#619)

--- tech-docs/source/javascripts/application.js
+++ tech-docs/source/javascripts/application.js
@@ -1,1 +1,29 @@
 //= require govuk_tech_docs
+
+window.addEventListener("load", (event) => {
+  const endpoints = [{
+    schemaName: 'transactionrequest',
+    endpoint: 'v1/prison/{prisonId}/prisoners/hmppsid/transactions',
+    method: 'post'
+  }, {
+    schemaName: 'transactiontransferrequest',
+    endpoint: 'v1/prison/{prisonId}/prisoners/{hmppsId}/transactions/transfer',
+    method: 'post'
+  }];
+  endpoints.forEach(body => {
+    const endpointFormatted = body.endpoint
+      .replaceAll('/', '-')
+      .replaceAll('{', '')
+      .replaceAll('}', '')
+      .toLowerCase()
+      + '-' + body.method.toLowerCase();
+    const heading = document.createElement("h4");
+    heading.id = `${endpointFormatted}-request-body`
+    heading.classList.add('anchored-heading');
+    const text = document.createTextNode('Request Body')
+    heading.appendChild(text)
+    const requestBody = document.querySelector(`.schema-${body.schemaName}`).cloneNode(true);
+    const responses = document.querySelector(`#${endpointFormatted}-responses`);
+    responses.before(heading, requestBody);
+  })
+});
