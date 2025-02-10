c8d3da9c89d374be58d07367023bf1949ac682d3
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-02-10 15:13:23 +0000

    start test for non-associations gateway

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/NonAssociationsGateway.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper
+
+@Component
+class NonAssociationsGateway(
+  @Value("\${services.non-associations.base-url}") baseUrl: String,
+) {
+  private val webClient = WebClientWrapper(baseUrl)
+
+  @Autowired
+  lateinit var hmppsAuthGateway: HmppsAuthGateway
+
+  fun getNonAssociationsForPerson() {}
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/nonAssociations/NonAssociationsGatewayTest.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.nonAssociations
+
+import io.kotest.core.spec.style.DescribeSpec
+import org.mockito.internal.verification.VerificationModeFactory
+import org.mockito.kotlin.verify
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.HmppsAuthGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NonAssociationsGateway
+
+@ActiveProfiles("test")
+@ContextConfiguration(
+  initializers = [ConfigDataApplicationContextInitializer::class],
+  classes = [NonAssociationsGateway::class],
+)
+class NonAssociationsGatewayTest(
+  @MockitoBean val hmppsAuthGateway: HmppsAuthGateway,
+  private val nonAssociationsGateway: NonAssociationsGateway,
+) : DescribeSpec({
+    it("authenticates using HMPPS Auth with credentials") {
+
+      nonAssociationsGateway.getNonAssociationsForPerson()
+
+      verify(hmppsAuthGateway, VerificationModeFactory.times(1)).getClientToken("NON-ASSOCIATIONS")
+    }
+  })
