99fd339af97d0d2b1231812387a7d9544ae36c60
Tom Watkins <tom.watkins@justice.gov.uk>
2025-02-10 12:00:56 +0000

    Add schema descriptions on PersonInPrison

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PersonInPrison.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PersonInPrison.kt
@@ -24,11 +24,18 @@ data class PersonInPrison(
   val identifiers: Identifiers = Identifiers(),
   @Schema(description = "An identifier from the Police National Computer (PNC)")
   val pncId: String? = null,
+  @Schema(description = "Category", example = "C")
   val category: String? = null,
+  @Schema(description = "Cell sharing risk assessment", example = "HIGH")
   val csra: String? = null,
+  @Schema(description = "Date prisoner was received into the prison", example = "2021-12-01")
   val receptionDate: String? = null,
+  @Schema(description = "Status of the prisoner", example = "ACTIVE IN")
   val status: String? = null,
+  @Schema(description = "Prison ID", example = "MDI")
   val prisonId: String? = null,
+  @Schema(description = "Name of the prison", example = "HMP Leeds")
   val prisonName: String? = null,
+  @Schema(description = "In prison cell location", example = "A-1-002")
   val cellLocation: String? = null,
 )
