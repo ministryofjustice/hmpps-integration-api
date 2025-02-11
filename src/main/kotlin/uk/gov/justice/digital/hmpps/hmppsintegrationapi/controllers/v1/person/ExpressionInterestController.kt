4e67b5f71eff4f8d195a5c19ea54735081755e50
gorton.brown <gorton.brown@justice.gov.uk>
2025-02-11 16:28:06 +0000

    API reponse refactor

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/person/ExpressionInterestController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/person/ExpressionInterestController.kt
@@ -2,14 +2,16 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers.v1.person
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.PutExpressionInterestService
 
 @RestController
@@ -22,18 +24,33 @@ class ExpressionInterestController(
   @Operation(
     summary = "Returns completed response",
     responses = [
-      ApiResponse(responseCode = "200", useReturnTypeSchema = true, description = "Successfully submitted an expression of interest"),
-      ApiResponse(responseCode = "403", useReturnTypeSchema = true, description = "Access is forbidden"),
-      ApiResponse(responseCode = "400", useReturnTypeSchema = true, description = "Bad Request"),
-      ApiResponse(responseCode = "404", useReturnTypeSchema = true, description = "Not found"),
+      ApiResponse(
+        responseCode = "200",
+        useReturnTypeSchema = true,
+        description = "Successfully submitted an expression of interest",
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad Request",
+        content = [Content(schema = Schema(ref = "#/components/schemas/BadRequest"))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Access is forbidden",
+        content = [Content(schema = Schema(ref = "#/components/schemas/ForbiddenResponse"))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        content = [Content(schema = Schema(ref = "#/components/schemas/PersonNotFound"))],
+      ),
     ],
   )
   fun submitExpressionOfInterest(
-    @Parameter(description = "A URL-encoded HMPPS identifier", example = "2008%2F0545166T") @PathVariable hmppsId: String,
+    @Parameter(description = "A HMPPS identifier", example = "A1234AA") @PathVariable hmppsId: String,
     @Parameter(description = "A job identifier") @PathVariable jobid: String,
-  ): ResponseEntity<Void> {
+  ): Response<Unit> {
     putExpressionInterestService.sendExpressionOfInterest(hmppsId, jobid)
 
-    return ResponseEntity.ok().build()
+    return Response(data = Unit, errors = emptyList())
   }
 }
