c49a05825359ec5b1e4775638a2a7ae58d3c22ad
wcdkj <will.clark@digital.justice.gov.uk>
2025-02-12 10:57:01 +0000

    add controller tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/OffenderRestrictionsControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/OffenderRestrictionsControllerTest.kt
@@ -18,6 +18,7 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.nonAssociation.NonAssociation
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.nonAssociation.NonAssociationPrisonerDetails
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.nonAssociation.NonAssociations
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetPrisonersNonAssociationsService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.internal.AuditService
 
@@ -76,37 +77,45 @@ class OffenderRestrictionsControllerTest(
           data = nonAssociations,
         ),
       )
-      val result = mockMvc.performAuthorised(nonAssociationsPath)
+      val result = mockMvc.performAuthorised("$nonAssociationsPath?includeOpen=true&includeClosed=false")
       result.response.contentAsString.shouldContain(
         """
-        {
-          "data":
-                [
-                  {
-                    "id": 42,
-                    "role": "VICTIM",
-                    "roleDescription": "Victim",
-                    "reason": "BULLYING",
-                    "reasonDescription": "Bullying",
-                    "restrictionType": "CELL",
-                    "restrictionTypeDescription": "Cell only",
-                    "comment": "John and Luke always end up fighting",
-                    "isClosed": false,
-                    "closedReason": null,
-                    "otherPrisonerDetails": {
-                      "prisonerNumber": "D5678EF",
-                      "role": "PERPETRATOR",
-                      "roleDescription": "Perpetrator",
-                      "firstName": "Joseph",
-                      "lastName": "Bloggs",
-                      "prisonId": "MDI",
-                      "prisonName": "Moorland (HMP & YOI)",
-                      "cellLocation": "B-2-007"
-                    },
-                    "isOpen": true
-                  }
-                ]
-        }
+          {
+            "data": {
+              "nonAssociations": [
+                {
+                  "id": 42,
+                  "role": "VICTIM",
+                  "roleDescription": "Victim",
+                  "reason": "BULLYING",
+                  "reasonDescription": "Bullying",
+                  "restrictionType": "CELL",
+                  "restrictionTypeDescription": "Cell only",
+                  "comment": "John and Luke always end up fighting",
+                  "authorisedBy": "Unknown",
+                  "whenCreated": "Unknown",
+                  "whenUpdated": "Unknown",
+                  "updatedBy": "Unknown",
+                  "isClosed": false,
+                  "closedBy": null,
+                  "closedReason": null,
+                  "closedAt": null,
+                  "otherPrisonerDetails": {
+                    "prisonerNumber": "D5678EF",
+                    "role": "PERPETRATOR",
+                    "roleDescription": "Perpetrator",
+                    "firstName": "Joseph",
+                    "lastName": "Bloggs",
+                    "prisonId": "MDI",
+                    "prisonName": "Moorland (HMP & YOI)",
+                    "cellLocation": "B-2-007"
+                  },
+                  "isOpen": true
+                }
+              ]
+            }
+          }
+
         """.removeWhitespaceAndNewlines(),
       )
     }
@@ -119,9 +128,34 @@ class OffenderRestrictionsControllerTest(
           errors,
         ),
       )
-      val result = mockMvc.performAuthorised(nonAssociationsPath + "?includeOpen=false&includeClosed=false")
+      val result = mockMvc.performAuthorised("$nonAssociationsPath?includeOpen=false&includeClosed=false")
       result.response.status.shouldBe(HttpStatus.BAD_REQUEST.value())
     }
-    // returns 404 not found if person not found
-    // calls the non associations endpoint with the correct filters
+
+    it("returns 404 not found if person not found with that specific hmpps id") {
+      val errors = listOf(UpstreamApiError(type = UpstreamApiError.Type.ENTITY_NOT_FOUND, causedBy = UpstreamApi.NON_ASSOCIATIONS, description = "not found."))
+      whenever(getPrisonersNonAssociationsService.execute(hmppsId, prisonId, includeOpen = "true", includeClosed = "false", filters = null)).thenReturn(
+        Response(
+          data = null,
+          errors,
+        ),
+      )
+
+      val result = mockMvc.performAuthorised("$nonAssociationsPath?includeOpen=true&includeClosed=false")
+      result.response.status.shouldBe(HttpStatus.NOT_FOUND.value())
+    }
+
+    it("returns 404 not found if person not found in prison") {
+      val errors = listOf(UpstreamApiError(type = UpstreamApiError.Type.ENTITY_NOT_FOUND, causedBy = UpstreamApi.NON_ASSOCIATIONS, description = "No prison associated with prisoner"))
+      val wrongPrisonId = "MDI"
+      whenever(getPrisonersNonAssociationsService.execute(hmppsId, wrongPrisonId, includeOpen = "true", includeClosed = "false", filters = ConsumerFilters(listOf("XYZ")))).thenReturn(
+        Response(
+          data = null,
+          errors,
+        ),
+      )
+
+      val result = mockMvc.performAuthorisedWithCN("/v1/prison/$wrongPrisonId/prisoners/$hmppsId/non-associations?includeOpen=true&includeClosed=false", "limited-prisons")
+      result.response.status.shouldBe(HttpStatus.NOT_FOUND.value())
+    }
   })
