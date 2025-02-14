caec45dc589fd8c63ce41d4bcc2707189fca0630
wcdkj <will.clark@digital.justice.gov.uk>
2025-02-14 16:17:35 +0000

    init vistor restrictions service and tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitorRestrictionsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitorRestrictionsService.kt
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PersonalRelationshipsGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.personalRelationships.PrisonerContactRestrictions
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 
 class GetVisitorRestrictionsService(
@@ -12,12 +14,46 @@ class GetVisitorRestrictionsService(
   @Autowired val getPersonService: GetPersonService,
 ) {
   fun execute(
-    prisonId: String,
     hmppsId: String,
     contactId: Long,
     filters: ConsumerFilters?,
-  ): Response<List<Any>> {
+  ): Response<List<PrisonerContactRestrictions>?> {
     val personResponse = getPersonService.getPrisoner(hmppsId, filters)
-    return Response(data = emptyList())
+
+    if (personResponse.errors.isNotEmpty()) {
+      return Response(emptyList(), personResponse.errors)
+    }
+    val prisonId = personResponse.data?.prisonId
+
+    val consumerPrisonFilterCheck = consumerPrisonAccessService.checkConsumerHasPrisonAccess<List<PrisonerContactRestrictions>>(prisonId, filters)
+    if (consumerPrisonFilterCheck.errors.isNotEmpty()) {
+      return consumerPrisonFilterCheck
+    }
+
+    val (linkedPrisoners, linkedPrisonersErrors) = personalRelationshipsGateway.getLinkedPrisoner(contactId)
+
+    if (linkedPrisonersErrors.isNotEmpty()) {
+      return Response(emptyList(), linkedPrisonersErrors)
+    }
+
+    // revise
+    var linkedPrisonerIds = linkedPrisoners.map { it.relationships?.map { it.prisonerContactId } }.flatMap { it ?: emptyList() }
+
+    var restrictionsResult = mutableListOf<PrisonerContactRestrictions>()
+    // maybe revise with coroutine depending on webclient behavior
+    for (prisonerContactId in linkedPrisonerIds) {
+      val gatewayResult = personalRelationshipsGateway.getPrisonerContactRestrictions(prisonerContactId!!)
+      if (gatewayResult.errors.isEmpty() && gatewayResult.data != null) {
+        restrictionsResult.add(gatewayResult.data)
+      }
+      // continue to call gateway in the case the error is 404
+      if (gatewayResult.errors.isNotEmpty() && !gatewayResult.hasError(UpstreamApiError.Type.ENTITY_NOT_FOUND)) {
+        return Response(emptyList(), gatewayResult.errors)
+      }
+    }
+    // A. return 404 not found if none of prisonerContactIds find data
+    // B. find data for some prisonerContactIds, what do we do about the ones we don't find? send 404 or ignore and only send data we have
+    // C. what to do when we get back a non 404 error, error out immediately or keep making calls
+    return Response(data = restrictionsResult)
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitorRestrictionsServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitorRestrictionsServiceTest.kt
@@ -1,14 +1,19 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import org.mockito.Mockito
 import org.mockito.internal.verification.VerificationModeFactory
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PersonalRelationshipsGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 
 @ContextConfiguration(
@@ -25,6 +30,7 @@ class GetVisitorRestrictionsServiceTest(
       val hmppsId = "A1234AA"
       val prisonId = "ABC"
       val contactId = 123456L
+      val nomisNumber = "Z99999ZZ"
       val filters = ConsumerFilters(null)
 
       beforeEach {
@@ -36,9 +42,32 @@ class GetVisitorRestrictionsServiceTest(
 //      )
       }
       it("gets a person using a Hmpps ID") {
-        getVisitorRestrictionsService.execute(prisonId, hmppsId, contactId, filters)
+        getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
 
         verify(getPersonService, VerificationModeFactory.times(1)).getPrisoner(hmppsId = hmppsId, filters = filters)
+      }
+
+      it("returns an error from person service when prisoner is not found") {
+        val errors = listOf(UpstreamApiError(type = UpstreamApiError.Type.ENTITY_NOT_FOUND, causedBy = UpstreamApi.PRISONER_OFFENDER_SEARCH, description = "not found."))
+        whenever(getPersonService.getPrisoner(hmppsId, filters)).thenReturn(Response(data = null, errors = errors))
+
+        val result = getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
+        result.data.shouldBe(emptyList())
+        result.errors.shouldBe(errors)
+      }
+
+      it("returns null when a person in an unapproved prison") {
+        val consumerFilters = ConsumerFilters(prisons = listOf("ABC"))
+        val wrongPrisonId = "XYZ"
+
+        whenever(consumerPrisonAccessService.checkConsumerHasPrisonAccess<Any>(wrongPrisonId, consumerFilters)).thenReturn(
+          Response(data = null, errors = listOf(UpstreamApiError(UpstreamApi.NOMIS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "Not found"))),
+        )
+
+        val response = getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
+
+        response.data.shouldBe(null)
+        response.errors.shouldBe(listOf(UpstreamApiError(UpstreamApi.NOMIS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "Not found")))
       }
     },
   )
