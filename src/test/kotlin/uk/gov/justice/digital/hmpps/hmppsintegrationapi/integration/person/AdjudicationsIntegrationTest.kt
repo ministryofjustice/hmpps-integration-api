7cce4e146093cada097330748d427bd9f1545881
Tom Watkins <tom.watkins@justice.gov.uk>
2025-02-17 11:40:19 +0000

    Fix deprecation warning post dependency upgrade

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/AdjudicationsIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/AdjudicationsIntegrationTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
 
 import org.junit.jupiter.api.Test
+import org.springframework.test.json.JsonCompareMode
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
@@ -10,6 +11,6 @@ class AdjudicationsIntegrationTest : IntegrationTestBase() {
   fun `returns adjudications for a person`() {
     callApi("$basePath/$nomsId/reported-adjudications")
       .andExpect(status().isOk)
-      .andExpect(content().json(getExpectedResponse("person-adjudications"), true))
+      .andExpect(content().json(getExpectedResponse("person-adjudications"), JsonCompareMode.STRICT))
   }
 }
