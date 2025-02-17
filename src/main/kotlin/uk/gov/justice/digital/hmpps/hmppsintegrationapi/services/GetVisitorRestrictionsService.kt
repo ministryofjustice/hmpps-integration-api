7c54c584e13b08ddd7774fccb461939bc1a39c1e
wcdkj <will.clark@digital.justice.gov.uk>
2025-02-17 11:38:40 +0000

    Service layer tests for visitor restrictions endpoint and logic

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitorRestrictionsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitorRestrictionsService.kt
@@ -36,24 +36,20 @@ class GetVisitorRestrictionsService(
       return Response(emptyList(), linkedPrisonersErrors)
     }
 
-    // revise
-    var linkedPrisonerIds = linkedPrisoners.map { it.relationships?.map { it.prisonerContactId } }.flatMap { it ?: emptyList() }
+    val linkedPrisonerIds = linkedPrisoners.map { it.relationships?.map { it.prisonerContactId } }.flatMap { it ?: emptyList() }
 
-    var restrictionsResult = mutableListOf<PrisonerContactRestrictions>()
-    // maybe revise with coroutine depending on webclient behavior
+    val restrictionsResult = mutableListOf<PrisonerContactRestrictions>()
     for (prisonerContactId in linkedPrisonerIds) {
       val gatewayResult = personalRelationshipsGateway.getPrisonerContactRestrictions(prisonerContactId!!)
       if (gatewayResult.errors.isEmpty() && gatewayResult.data != null) {
         restrictionsResult.add(gatewayResult.data)
       }
-      // continue to call gateway in the case the error is 404
+      // Continue to loop through ids and call gateway in the case the error is 404
       if (gatewayResult.errors.isNotEmpty() && !gatewayResult.hasError(UpstreamApiError.Type.ENTITY_NOT_FOUND)) {
         return Response(emptyList(), gatewayResult.errors)
       }
     }
-    // A. return 404 not found if none of prisonerContactIds find data
-    // B. find data for some prisonerContactIds, what do we do about the ones we don't find? send 404 or ignore and only send data we have
-    // C. what to do when we get back a non 404 error, error out immediately or keep making calls
+
     return Response(data = restrictionsResult)
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitorRestrictionsServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitorRestrictionsServiceTest.kt
@@ -14,6 +14,12 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PersonalRelatio
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.personalRelationships.ContactGlobalRestriction
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.personalRelationships.LinkedPrisoner
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.personalRelationships.PrisonerContactRestriction
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.personalRelationships.PrisonerContactRestrictions
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.personalRelationships.Relationship
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisoneroffendersearch.POSPrisoner
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 
 @ContextConfiguration(
@@ -30,20 +36,98 @@ class GetVisitorRestrictionsServiceTest(
       val hmppsId = "A1234AA"
       val prisonId = "ABC"
       val contactId = 123456L
-      val nomisNumber = "Z99999ZZ"
       val filters = ConsumerFilters(null)
+      val posPrisoner = POSPrisoner(firstName = "Test", lastName = "Person", prisonId = prisonId)
+      val listOfRelationships = listOf(Relationship(prisonerContactId = contactId, relationshipType = "FAM", relationshipTypeDescription = "Family", relationshipToPrisoner = "SON", relationshipToPrisonerDescription = "Son"))
+      val listOfLinkedPrisoners = listOf(LinkedPrisoner(prisonerNumber = "1234567", relationships = listOfRelationships, firstName = "Test", lastName = "Person", middleNames = null))
+      val prisonerContactRestrictionsResponse =
+        listOf(
+          PrisonerContactRestriction(
+            prisonerContactRestrictionId = 1L,
+            prisonerContactId = contactId,
+            contactId = contactId,
+            prisonerNumber = "A1234BC",
+            restrictionType = "Restriction 1",
+            restrictionTypeDescription = "Description for Restriction 1",
+            startDate = "2024-01-01",
+            expiryDate = "2025-01-01",
+            comments = "Some comments",
+            enteredByUsername = "user123",
+            enteredByDisplayName = "User Name",
+            createdBy = "admin",
+            createdTime = "2024-01-01T12:00:00Z",
+            updatedBy = "admin",
+            updatedTime = "2024-01-02T12:00:00Z",
+          ),
+          PrisonerContactRestriction(
+            prisonerContactRestrictionId = 2L,
+            prisonerContactId = contactId,
+            contactId = contactId,
+            prisonerNumber = "A1234BC",
+            restrictionType = "Restriction 2",
+            restrictionTypeDescription = "Description for Restriction 2",
+            startDate = "2024-02-01",
+            expiryDate = "2025-02-01",
+            comments = "Some other comments",
+            enteredByUsername = "user123",
+            enteredByDisplayName = "User Name",
+            createdBy = "admin",
+            createdTime = "2024-02-01T12:00:00Z",
+            updatedBy = "admin",
+            updatedTime = "2024-02-02T12:00:00Z",
+          ),
+        )
 
+      val contactGlobalRestrictionsResponse =
+        listOf(
+          ContactGlobalRestriction(
+            contactRestrictionId = 1L,
+            contactId = contactId,
+            restrictionType = "Restriction 1",
+            restrictionTypeDescription = "Description for Restriction 1",
+            startDate = "2024-01-01",
+            expiryDate = "2025-01-01",
+            comments = "Some comments",
+            enteredByUsername = "user123",
+            enteredByDisplayName = "User Name",
+            createdBy = "admin",
+            createdTime = "2024-01-01T12:00:00Z",
+            updatedBy = "admin",
+            updatedTime = "2024-01-02T12:00:00Z",
+          ),
+          ContactGlobalRestriction(
+            contactRestrictionId = 2L,
+            contactId = contactId,
+            restrictionType = "Restriction 2",
+            restrictionTypeDescription = "Description for Restriction 2",
+            startDate = "2024-02-01",
+            expiryDate = "2025-02-01",
+            comments = "Some other comments",
+            enteredByUsername = "user123",
+            enteredByDisplayName = "User Name",
+            createdBy = "admin",
+            createdTime = "2024-02-01T12:00:00Z",
+            updatedBy = "admin",
+            updatedTime = "2024-02-02T12:00:00Z",
+          ),
+        )
+      val prisonerContactRestrictions = PrisonerContactRestrictions(prisonerContactRestrictionsResponse, contactGlobalRestrictionsResponse)
       beforeEach {
         Mockito.reset(personalRelationshipsGateway)
         Mockito.reset(getPersonService)
 
-//      whenever(consumerPrisonAccessService.checkConsumerHasPrisonAccess<Any>(prisonId, filters)).thenReturn(
-//        Response(data = null),
-//      )
-      }
-      it("gets a person using a Hmpps ID") {
-        getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
+        whenever(consumerPrisonAccessService.checkConsumerHasPrisonAccess<Any>(prisonId, filters)).thenReturn(
+          Response(data = null),
+        )
 
+        whenever(getPersonService.getPrisoner(hmppsId, filters)).thenReturn(Response(data = posPrisoner.toPersonInPrison(), errors = emptyList()))
+      }
+
+      it("gets a person using a Hmpps ID") {
+        whenever(personalRelationshipsGateway.getLinkedPrisoner(contactId)).thenReturn(Response(data = listOfLinkedPrisoners, errors = emptyList()))
+        whenever(personalRelationshipsGateway.getPrisonerContactRestrictions(contactId)).thenReturn(Response(data = prisonerContactRestrictions, errors = emptyList()))
+
+        getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
         verify(getPersonService, VerificationModeFactory.times(1)).getPrisoner(hmppsId = hmppsId, filters = filters)
       }
 
@@ -57,17 +141,53 @@ class GetVisitorRestrictionsServiceTest(
       }
 
       it("returns null when a person in an unapproved prison") {
-        val consumerFilters = ConsumerFilters(prisons = listOf("ABC"))
-        val wrongPrisonId = "XYZ"
+        val consumerFilters = ConsumerFilters(prisons = listOf("XYZ"))
+        val wrongPrisonId = "ABC"
 
-        whenever(consumerPrisonAccessService.checkConsumerHasPrisonAccess<Any>(wrongPrisonId, consumerFilters)).thenReturn(
+        whenever(getPersonService.getPrisoner(hmppsId, consumerFilters)).thenReturn(Response(data = posPrisoner.toPersonInPrison(), errors = emptyList()))
+
+        whenever(consumerPrisonAccessService.checkConsumerHasPrisonAccess<List<PrisonerContactRestrictions>>(wrongPrisonId, consumerFilters)).thenReturn(
           Response(data = null, errors = listOf(UpstreamApiError(UpstreamApi.NOMIS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "Not found"))),
         )
 
-        val response = getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
+        val response = getVisitorRestrictionsService.execute(hmppsId, contactId, consumerFilters)
 
         response.data.shouldBe(null)
         response.errors.shouldBe(listOf(UpstreamApiError(UpstreamApi.NOMIS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "Not found")))
+      }
+
+      it("returns an error when linked prisoners are not found") {
+        val errors = listOf(UpstreamApiError(UpstreamApi.PERSONAL_RELATIONSHIPS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "Not found"))
+
+        whenever(personalRelationshipsGateway.getLinkedPrisoner(contactId)).thenReturn(Response(data = emptyList(), errors = errors))
+
+        val response = getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
+
+        response.data.shouldBe(emptyList())
+        response.errors.shouldBe(listOf(UpstreamApiError(UpstreamApi.PERSONAL_RELATIONSHIPS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "Not found")))
+      }
+
+      it("returns visitor restrictions") {
+        whenever(personalRelationshipsGateway.getLinkedPrisoner(contactId)).thenReturn(Response(data = listOfLinkedPrisoners, errors = emptyList()))
+        whenever(personalRelationshipsGateway.getPrisonerContactRestrictions(contactId)).thenReturn(Response(data = prisonerContactRestrictions, errors = emptyList()))
+
+        val response = getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
+
+        response.data.shouldBe(listOf(prisonerContactRestrictions))
+      }
+
+      it("returns multiple relationships and queries getPrisonerContactRestrictions accordingly") {
+        val scopedPrisonerContactId = 123457L
+        val listOfManyRelationships = listOf(Relationship(prisonerContactId = contactId, relationshipType = "FAM", relationshipTypeDescription = "Family", relationshipToPrisoner = "SON", relationshipToPrisonerDescription = "Son"), Relationship(prisonerContactId = scopedPrisonerContactId, relationshipType = "FAM", relationshipTypeDescription = "Family", relationshipToPrisoner = "BRO", relationshipToPrisonerDescription = "Brother"))
+        val listOfLinkedPrisonerWithManyRelationships = listOf(LinkedPrisoner(prisonerNumber = "1234567", relationships = listOfManyRelationships, firstName = "Test", lastName = "Person", middleNames = null))
+
+        whenever(personalRelationshipsGateway.getLinkedPrisoner(contactId)).thenReturn(Response(data = listOfLinkedPrisonerWithManyRelationships, errors = emptyList()))
+        whenever(personalRelationshipsGateway.getPrisonerContactRestrictions(contactId)).thenReturn(Response(data = prisonerContactRestrictions, errors = emptyList()))
+        whenever(personalRelationshipsGateway.getPrisonerContactRestrictions(scopedPrisonerContactId)).thenReturn(Response(data = prisonerContactRestrictions, errors = emptyList()))
+
+        getVisitorRestrictionsService.execute(hmppsId, contactId, filters)
+
+        verify(personalRelationshipsGateway, VerificationModeFactory.times(2)).getPrisonerContactRestrictions(Mockito.anyLong())
       }
     },
   )
