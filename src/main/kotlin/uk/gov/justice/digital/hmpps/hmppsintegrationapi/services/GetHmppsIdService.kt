4be4398098ffa081b8503c88b36a7a873aba87b1
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-02-18 10:33:31 +0000

    HMAI-224 - Add prison filter to /v1/hmpps/id/nomis-number/{nomisNumber} (#663)
    * Service changes
    
    * Add filter to controller
    
    * Add new integration test for filters
    
    * Add safe call
    
    * Simplified service
    
    * Move null check
    
    * Dont call POS if no prison filters passed in and we already have hmppsId

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHmppsIdService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHmppsIdService.kt
@@ -2,40 +2,70 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsId
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.NomisNumber
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 
 @Service
 class GetHmppsIdService(
   @Autowired val getPersonService: GetPersonService,
+  @Autowired val consumerPrisonAccessService: ConsumerPrisonAccessService,
 ) {
-  fun execute(hmppsId: String): Response<HmppsId?> {
-    val identifierType = getPersonService.identifyHmppsId(hmppsId)
+  fun execute(
+    nomisNumber: String,
+    filters: ConsumerFilters? = null,
+  ): Response<HmppsId?> {
+    val identifierType = getPersonService.identifyHmppsId(nomisNumber)
     if (identifierType != GetPersonService.IdentifierType.NOMS) {
       return Response(
         data = null,
         errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.BAD_REQUEST)),
       )
     }
-    val personResponse = getPersonService.execute(hmppsId.uppercase())
-    var hmppsIdToReturn =
-      personResponse.data?.hmppsId
-    return if (hmppsIdToReturn != null) {
-      Response(
-        data = HmppsId(hmppsIdToReturn),
+
+    val personResponse = getPersonService.execute(nomisNumber.uppercase())
+    if (personResponse.errors.isNotEmpty() && !personResponse.hasError(UpstreamApiError.Type.ENTITY_NOT_FOUND)) {
+      return Response(
+        data = null,
         errors = personResponse.errors,
       )
-    } else {
-      val prisonerResponse = getPersonService.getPersonFromNomis(hmppsId.uppercase())
-      hmppsIdToReturn = prisonerResponse.data?.prisonerNumber
-      Response(
-        data = HmppsId(hmppsIdToReturn),
-        errors = prisonerResponse.errors,
+    }
+    var hmppsId = personResponse.data?.hmppsId
+    // If no filters passed in, then we don't need to get the prison ID
+    if (hmppsId != null && filters == null) {
+      return Response(data = HmppsId(hmppsId))
+    }
+
+    val (prisoner, prisonerErrors) = getPersonService.getPersonFromNomis(nomisNumber.uppercase())
+    if (prisonerErrors.isNotEmpty()) {
+      return Response(
+        data = null,
+        errors = prisonerErrors,
       )
     }
+
+    if (hmppsId == null) {
+      hmppsId = prisoner?.prisonerNumber
+      if (hmppsId == null) {
+        return Response(
+          data = null,
+          errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND)),
+        )
+      }
+    }
+
+    val consumerPrisonFilterCheck = consumerPrisonAccessService.checkConsumerHasPrisonAccess<HmppsId>(prisoner?.prisonId, filters)
+    if (consumerPrisonFilterCheck.errors.isNotEmpty()) {
+      return consumerPrisonFilterCheck
+    }
+
+    return Response(
+      data = HmppsId(hmppsId),
+    )
   }
 
   fun getNomisNumber(hmppsId: String): Response<NomisNumber?> {
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHmppsIdServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHmppsIdServiceTest.kt
@@ -3,10 +3,13 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import org.mockito.Mockito
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.bean.override.mockito.MockitoBean
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsId
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Identifiers
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Person
@@ -14,6 +17,7 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisoneroffendersearch.POSPrisoner
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 
 @ContextConfiguration(
   initializers = [ConfigDataApplicationContextInitializer::class],
@@ -21,28 +25,41 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisoneroffenders
 )
 internal class GetHmppsIdServiceTest(
   @MockitoBean val getPersonService: GetPersonService,
+  @MockitoBean val consumerPrisonAccessService: ConsumerPrisonAccessService,
   private val getHmppsIdService: GetHmppsIdService,
 ) : DescribeSpec(
     {
       val id = "A7777ZZ"
       val hmppsId = HmppsId(hmppsId = id)
+      val prisonId = "ABC"
+      val filters = ConsumerFilters(listOf("ABC"))
 
       beforeEach {
         Mockito.reset(getPersonService)
 
+        whenever(consumerPrisonAccessService.checkConsumerHasPrisonAccess<Nothing>(prisonId, filters)).thenReturn(
+          Response(data = null, errors = emptyList()),
+        )
+
         whenever(getPersonService.identifyHmppsId(id)).thenReturn(
           GetPersonService.IdentifierType.NOMS,
         )
+
         whenever(getPersonService.execute(id)).thenReturn(
           Response(
             data = Person(firstName = "Qui-gon", lastName = "Jin", hmppsId = id, identifiers = Identifiers(nomisNumber = id)),
           ),
         )
+
+        whenever(getPersonService.getPersonFromNomis(id)).thenReturn(
+          Response(
+            data = POSPrisoner(firstName = "Test", lastName = "Test", prisonerNumber = id, prisonId = prisonId),
+          ),
+        )
       }
 
       it("Returns a hmpps id for the given id") {
-        val result = getHmppsIdService.execute(id)
-
+        val result = getHmppsIdService.execute(id, filters)
         result.shouldBe(Response(data = hmppsId))
       }
 
@@ -50,11 +67,11 @@ internal class GetHmppsIdServiceTest(
         whenever(getPersonService.identifyHmppsId(id)).thenReturn(
           GetPersonService.IdentifierType.UNKNOWN,
         )
-        val result = getHmppsIdService.execute(id)
+        val result = getHmppsIdService.execute(id, filters)
         result.shouldBe(Response(data = null, errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.BAD_REQUEST))))
       }
 
-      it("Returns a 404 when valid nomis number but no hmppsId is found in either service") {
+      it("Returns a 404 when valid nomis number supplied but when 404 returned found in both services") {
         whenever(getPersonService.execute(id)).thenReturn(
           Response(
             data = null,
@@ -67,27 +84,62 @@ internal class GetHmppsIdServiceTest(
             errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND)),
           ),
         )
-        val result = getHmppsIdService.execute(id)
-        result.shouldBe(Response(data = HmppsId(null), errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND))))
+        val result = getHmppsIdService.execute(id, filters)
+        result.shouldBe(Response(data = null, errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND))))
       }
 
-      it("Returns a 200 when valid nomis number but no 404 from getPersonService.execute and found in getPersonService.getPersonFromNomis") {
+      it("Returns an error when getPersonService.execute() returns a non 404 error") {
+        val errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.INTERNAL_SERVER_ERROR))
+        whenever(getPersonService.execute(id)).thenReturn(
+          Response(
+            data = null,
+            errors = errors,
+          ),
+        )
+        val result = getHmppsIdService.execute(id, filters)
+        result.shouldBe(Response(data = null, errors = errors))
+      }
+
+      it("Returns an error when getPersonService.getPersonFromNomis() returns any error") {
+        val errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND))
+        whenever(getPersonService.getPersonFromNomis(id)).thenReturn(
+          Response(
+            data = null,
+            errors = errors,
+          ),
+        )
+        val result = getHmppsIdService.execute(id, filters)
+        result.shouldBe(Response(data = null, errors = errors))
+      }
+
+      it("Returns a 200 when 404 returned from getPersonService.execute but hmppsId found in getPersonService.getPersonFromNomis") {
         whenever(getPersonService.execute(id)).thenReturn(
           Response(
             data = null,
             errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND)),
           ),
         )
-        whenever(getPersonService.getPersonFromNomis(id)).thenReturn(
-          Response(
-            data = POSPrisoner(firstName = "Test", lastName = "Test", prisonerNumber = id),
-          ),
-        )
-        val result = getHmppsIdService.execute(id)
+        val result = getHmppsIdService.execute(id, filters)
         result.shouldBe(Response(data = hmppsId))
       }
 
-      it("Returns a 200 when valid nomis number but no hmppsid from getPersonService.execute and found in getPersonService.getPersonFromNomis") {
+      it("Returns a 200 when no hmppsId returned from getPersonService.execute but found in getPersonService.getPersonFromNomis") {
+        whenever(getPersonService.execute(id)).thenReturn(
+          Response(
+            data = Person(firstName = "Qui-gon", lastName = "Jin"),
+          ),
+        )
+        val result = getHmppsIdService.execute(id, filters)
+        result.shouldBe(Response(data = hmppsId))
+      }
+
+      it("Returns a 200 when hmppsId returned from getPersonService.execute and no filters passed in") {
+        val result = getHmppsIdService.execute(id, null)
+        verify(getPersonService, never()).getNomisNumber(id)
+        result.shouldBe(Response(data = hmppsId))
+      }
+
+      it("Returns a 404 when no neither service returns an error but hmppsId is null still") {
         whenever(getPersonService.execute(id)).thenReturn(
           Response(
             data = Person(firstName = "Qui-gon", lastName = "Jin"),
@@ -95,11 +147,20 @@ internal class GetHmppsIdServiceTest(
         )
         whenever(getPersonService.getPersonFromNomis(id)).thenReturn(
           Response(
-            data = POSPrisoner(firstName = "Test", lastName = "Test", prisonerNumber = id),
+            data = POSPrisoner(firstName = "Test", lastName = "Test"),
           ),
         )
-        val result = getHmppsIdService.execute(id)
-        result.shouldBe(Response(data = hmppsId))
+        val result = getHmppsIdService.execute(id, filters)
+        result.shouldBe(Response(data = null, errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND))))
+      }
+
+      it("Returns 404 when person is in an unapproved prison") {
+        whenever(consumerPrisonAccessService.checkConsumerHasPrisonAccess<Nothing>(prisonId, filters)).thenReturn(
+          Response(data = null, errors = listOf(UpstreamApiError(UpstreamApi.NOMIS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "Not found"))),
+        )
+        val result = getHmppsIdService.execute(id, filters)
+        result.data.shouldBe(null)
+        result.errors.shouldBe(listOf(UpstreamApiError(UpstreamApi.NOMIS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "Not found")))
       }
     },
   )
