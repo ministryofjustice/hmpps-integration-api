99d6d8e658314a0c01faf3ce964af0313d808ca9
wcdkj <will.clark@digital.justice.gov.uk>
2025-02-18 17:51:58 +0000

    Service tests for get visits byref (#668)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetVisitInformationByReferenceService.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonVisitsGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisonVisits.Visit
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+
+@Service
+class GetVisitInformationByReferenceService(
+  @Autowired val consumerPrisonAccessService: ConsumerPrisonAccessService,
+  @Autowired val prisonVisitsGateway: PrisonVisitsGateway,
+) {
+  fun execute(
+    visitReference: String,
+    filters: ConsumerFilters? = null,
+  ): Response<Visit?> {
+    val prisonVisitsResponse = prisonVisitsGateway.getVisitByReference(visitReference)
+
+    if (!prisonVisitsResponse.errors.isNullOrEmpty()) {
+      return Response(data = null, errors = prisonVisitsResponse.errors)
+    }
+
+    val prisonId = prisonVisitsResponse.data?.prisonId
+
+    if (prisonId.isNullOrBlank()) {
+      return Response(data = null, errors = listOf(UpstreamApiError(UpstreamApi.MANAGE_PRISON_VISITS, UpstreamApiError.Type.ENTITY_NOT_FOUND, "PrisonId not found")))
+    }
+
+    val consumerPrisonFilterCheck = consumerPrisonAccessService.checkConsumerHasPrisonAccess<Visit>(prisonId, filters, UpstreamApi.MANAGE_PRISON_VISITS)
+
+    if (consumerPrisonFilterCheck.errors.isNotEmpty()) {
+      return consumerPrisonFilterCheck
+    }
+
+    return Response(data = prisonVisitsResponse.data)
+  }
+}
