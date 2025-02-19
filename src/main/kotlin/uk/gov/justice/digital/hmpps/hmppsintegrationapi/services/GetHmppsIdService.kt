8f70d9aede285eadc65c594ff5b7a66e27cafd36
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-02-19 11:56:29 +0000

    Changed hmppsId service to use getPersonWithFilter method (#670)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHmppsIdService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHmppsIdService.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsId
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.NomisNumber
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
@@ -13,59 +12,26 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.Consum
 @Service
 class GetHmppsIdService(
   @Autowired val getPersonService: GetPersonService,
-  @Autowired val consumerPrisonAccessService: ConsumerPrisonAccessService,
 ) {
   fun execute(
     nomisNumber: String,
     filters: ConsumerFilters? = null,
   ): Response<HmppsId?> {
-    val identifierType = getPersonService.identifyHmppsId(nomisNumber)
-    if (identifierType != GetPersonService.IdentifierType.NOMS) {
+    val (person, personErrors) = getPersonService.getPersonWithPrisonFilter(nomisNumber, filters)
+    if (personErrors.isNotEmpty()) {
       return Response(
         data = null,
-        errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.BAD_REQUEST)),
+        errors = personErrors,
       )
     }
 
-    val personResponse = getPersonService.execute(nomisNumber.uppercase())
-    if (personResponse.errors.isNotEmpty() && !personResponse.hasError(UpstreamApiError.Type.ENTITY_NOT_FOUND)) {
-      return Response(
+    val hmppsId =
+      person?.hmppsId ?: return Response(
         data = null,
-        errors = personResponse.errors,
+        errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND)),
       )
-    }
-    var hmppsId = personResponse.data?.hmppsId
-    // If no filters passed in, then we don't need to get the prison ID
-    if (hmppsId != null && filters == null) {
-      return Response(data = HmppsId(hmppsId))
-    }
 
-    val (prisoner, prisonerErrors) = getPersonService.getPersonFromNomis(nomisNumber.uppercase())
-    if (prisonerErrors.isNotEmpty()) {
-      return Response(
-        data = null,
-        errors = prisonerErrors,
-      )
-    }
-
-    if (hmppsId == null) {
-      hmppsId = prisoner?.prisonerNumber
-      if (hmppsId == null) {
-        return Response(
-          data = null,
-          errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND)),
-        )
-      }
-    }
-
-    val consumerPrisonFilterCheck = consumerPrisonAccessService.checkConsumerHasPrisonAccess<HmppsId>(prisoner?.prisonId, filters)
-    if (consumerPrisonFilterCheck.errors.isNotEmpty()) {
-      return consumerPrisonFilterCheck
-    }
-
-    return Response(
-      data = HmppsId(hmppsId),
-    )
+    return Response(data = HmppsId(hmppsId))
   }
 
   fun getNomisNumber(hmppsId: String): Response<NomisNumber?> {
