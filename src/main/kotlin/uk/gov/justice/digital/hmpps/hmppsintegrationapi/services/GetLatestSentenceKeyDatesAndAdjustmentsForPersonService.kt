8ed377d1680510bae95996bbb954daffa1e28bf8
emmalanigan <emma.lanigan@justice.gov.uk>
2025-02-21 15:23:24 +0000

    amend GetLatestSentenceKeyDatesAndAdjustmentsForPersonService to use getPersonWithPrisonFilter; amend unit tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLatestSentenceKeyDatesAndAdjustmentsForPersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLatestSentenceKeyDatesAndAdjustmentsForPersonService.kt
@@ -2,10 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.ProbationOffenderSearchGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.KeyDatesAndAdjustmentsDTO
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.LatestSentenceKeyDatesAndAdjustments
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
@@ -15,33 +12,20 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.Consum
 
 @Service
 class GetLatestSentenceKeyDatesAndAdjustmentsForPersonService(
-  @Autowired val probationOffenderSearchGateway: ProbationOffenderSearchGateway,
-  @Autowired val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway,
   @Autowired val nomisGateway: NomisGateway,
-  @Autowired val consumerPrisonAccessService: ConsumerPrisonAccessService,
+  private val getPersonService: GetPersonService,
 ) {
   fun execute(
     hmppsId: String,
     filters: ConsumerFilters?,
   ): Response<LatestSentenceKeyDatesAndAdjustments?> {
-    val (person, personErrors) = probationOffenderSearchGateway.getPerson(hmppsId)
+    val (person, personErrors) = getPersonService.getPersonWithPrisonFilter(hmppsId, filters)
 
     if (personErrors.isNotEmpty()) {
       return Response(data = null, personErrors)
     }
 
-    val nomisNumber = person?.identifiers?.nomisNumber ?: return Response(data = null)
-
-    val (prisonerOffender, prisonOffenderErrors) = prisonerOffenderSearchGateway.getPrisonOffender(nomisNumber)
-    if (prisonOffenderErrors.isNotEmpty()) {
-      return Response(data = null, prisonOffenderErrors)
-    }
-    val prisonId = prisonerOffender?.prisonId
-
-    val consumerPrisonFilterCheck = consumerPrisonAccessService.checkConsumerHasPrisonAccess<LatestSentenceKeyDatesAndAdjustments>(prisonId, filters)
-    if (consumerPrisonFilterCheck.errors.isNotEmpty()) {
-      return consumerPrisonFilterCheck
-    }
+    val nomisNumber = person?.identifiers?.nomisNumber ?: return Response(data = null, errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.ENTITY_NOT_FOUND)))
 
     val (latestSentenceKeyDates, latestSentenceKeyDatesErrors) =
       nomisGateway.getLatestSentenceKeyDatesForPerson(
