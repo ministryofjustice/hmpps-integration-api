df3f0c51136d69aa2a516491d4caabefad56e69a
Tom Watkins <tom.watkins@justice.gov.uk>
2025-02-21 00:35:54 +0000

    Tidy up auth header code

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/helpers/IntegrationAPIWebClient.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/helpers/IntegrationAPIWebClient.kt
@@ -1,68 +1,70 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.helpers
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec
 
 class IntegrationAPIWebClient(
   @Autowired val client: WebTestClient,
 ) {
-  fun performAuthorised(path: String): ResponseSpec {
-    val subjectDistinguishedName = "C=GB,ST=London,L=London,O=Home Office,CN=automated-test-client"
-    return client
+  private fun setAuthHeader(
+    headers: HttpHeaders,
+    cn: String = "automated-test-client",
+  ) {
+    headers.set("subject-distinguished-name", "C=GB,ST=London,L=London,O=Home Office,CN=$cn")
+  }
+
+  fun performAuthorised(path: String): ResponseSpec =
+    client
       .get()
       .uri(path)
-      .header("subject-distinguished-name", subjectDistinguishedName)
+      .headers { headers -> setAuthHeader(headers) }
       .exchange()
-  }
 
   fun performAuthorisedPut(path: String): ResponseSpec {
     val subjectDistinguishedName = "C=GB,ST=London,L=London,O=Home Office,CN=automated-test-client"
     return client
       .put()
       .uri(path)
-      .header("subject-distinguished-name", subjectDistinguishedName)
+      .headers { headers -> setAuthHeader(headers) }
       .exchange()
   }
 
   fun performAuthorisedWithCN(
     path: String,
     cn: String,
-  ): ResponseSpec {
-    val subjectDistinguishedName = "C=GB,ST=London,L=London,O=Home Office,CN=$cn"
-    return client
+  ): ResponseSpec =
+    client
       .get()
       .uri(path)
-      .header("subject-distinguished-name", subjectDistinguishedName)
+      .headers { headers -> setAuthHeader(headers, cn) }
       .exchange()
-  }
 
   fun <T : Any> performAuthorisedPost(
     path: String,
     requestBody: T,
-  ): ResponseSpec {
-    val subjectDistinguishedName = "C=GB,ST=London,L=London,O=Home Office,CN=automated-test-client"
-    return client
+  ): ResponseSpec =
+    client
       .post()
       .uri(path)
-      .header("subject-distinguished-name", subjectDistinguishedName)
-      .header("content-type", "application/json")
-      .bodyValue(requestBody)
+      .headers { headers ->
+        setAuthHeader(headers)
+        headers.set("content-type", "application/json")
+      }.bodyValue(requestBody)
       .exchange()
-  }
 
   fun <T : Any> performAuthorisedPostWithCN(
     path: String,
     cn: String,
     requestBody: T,
-  ): ResponseSpec {
-    val subjectDistinguishedName = "C=GB,ST=London,L=London,O=Home Office,CN=$cn"
-    return client
+  ): ResponseSpec =
+    client
       .post()
       .uri(path)
-      .header("subject-distinguished-name", subjectDistinguishedName)
-      .header("content-type", "application/json")
-      .bodyValue(requestBody)
+      .headers { headers ->
+        setAuthHeader(headers, cn)
+        headers.set("content-type", "application/json")
+      }.bodyValue(requestBody)
       .exchange()
-  }
 }
