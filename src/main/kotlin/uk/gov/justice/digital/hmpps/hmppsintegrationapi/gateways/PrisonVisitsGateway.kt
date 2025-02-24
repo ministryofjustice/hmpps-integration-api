c35b232fd1ed6fd487772abe22f373c09a9f4979
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-02-24 16:49:25 +0000

    HMAI-232 - /v1/persons/{hmppsId}/visit/future - Gateway changes (#682)
    * Gateway tests
    
    * add gateway function and tests
    
    * refactor visits model
    
    * Remove 500 test
    
    * Fix lint issues
    
    ---------
    
    Co-authored-by: BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/PrisonVisitsGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/PrisonVisitsGateway.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpMethod
@@ -8,6 +10,7 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrap
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper.WebClientWrapperResponse
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisonVisits.FutureVisit
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisonVisits.PaginatedVisit
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisonVisits.Visit
 
@@ -16,6 +19,7 @@ class PrisonVisitsGateway(
   @Value("\${services.manage-prison-visits.base-url}") baseUrl: String,
 ) {
   private val webClient = WebClientWrapper(baseUrl)
+  private val mapper: ObjectMapper = ObjectMapper()
 
   @Autowired
   lateinit var hmppsAuthGateway: HmppsAuthGateway
@@ -81,11 +85,43 @@ class PrisonVisitsGateway(
     }
   }
 
+  fun getFutureVisits(prisonerId: String): Response<List<FutureVisit>?> {
+    val result =
+      webClient.request<List<FutureVisit>?>(
+        HttpMethod.GET,
+        "/visits/search/future/$prisonerId",
+        authenticationHeader(),
+        UpstreamApi.MANAGE_PRISON_VISITS,
+        badRequestAsError = true,
+      )
+
+    return when (result) {
+      is WebClientWrapperResponse.Success -> {
+        Response(
+          data =
+            mapToFutureVisits(result),
+        )
+      }
+
+      is WebClientWrapperResponse.Error -> {
+        Response(
+          data = null,
+          errors = result.errors,
+        )
+      }
+    }
+  }
+
   private fun authenticationHeader(): Map<String, String> {
     val token = hmppsAuthGateway.getClientToken("MANAGE-PRISON-VISITS")
 
     return mapOf(
       "Authorization" to "Bearer $token",
     )
+  }
+
+  fun mapToFutureVisits(result: WebClientWrapper.WebClientWrapperResponse.Success<List<FutureVisit>?>): List<FutureVisit> {
+    val mappedResult: List<FutureVisit> = mapper.convertValue(result.data, object : TypeReference<List<FutureVisit>>() {})
+    return mappedResult
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/prisonVisits/Visit.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/prisonVisits/Visit.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisonVisits
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Visit(
   val applicationReference: String?,
   val reference: String?,
@@ -23,22 +25,73 @@ data class Visit(
   val visitorSupport: VisitorSupport,
 )
 
+data class FutureVisit(
+  @JsonProperty("applicationReference")
+  val applicationReference: String,
+  @JsonProperty("reference")
+  val reference: String,
+  @JsonProperty("prisonerId")
+  val prisonerId: String,
+  @JsonProperty("prisonId")
+  val prisonId: String,
+  @JsonProperty("prisonName")
+  val prisonName: String,
+  @JsonProperty("sessionTemplateReference")
+  val sessionTemplateReference: String,
+  @JsonProperty("visitRoom")
+  val visitRoom: String,
+  @JsonProperty("visitType")
+  val visitType: String,
+  @JsonProperty("visitStatus")
+  val visitStatus: String,
+  @JsonProperty("outcomeStatus")
+  val outcomeStatus: String,
+  @JsonProperty("visitRestriction")
+  val visitRestriction: String,
+  @JsonProperty("startTimestamp")
+  val startTimestamp: String,
+  @JsonProperty("endTimestamp")
+  val endTimestamp: String,
+  @JsonProperty("createdTimestamp")
+  val createdTimestamp: String,
+  @JsonProperty("modifiedTimestamp")
+  val modifiedTimestamp: String,
+  @JsonProperty("firstBookedDateTime")
+  val firstBookedDateTime: String,
+  @JsonProperty("visitNotes")
+  val visitNotes: List<VisitNotes>,
+  @JsonProperty("visitContact")
+  val visitContact: VisitContact,
+  @JsonProperty("visitors")
+  val visitors: List<Visitors>,
+  @JsonProperty("visitorSupport")
+  val visitorSupport: VisitorSupport,
+)
+
 data class VisitNotes(
+  @JsonProperty("type")
   val type: String,
+  @JsonProperty("text")
   val text: String,
 )
 
 data class Visitors(
+  @JsonProperty("nomisPersonId")
   val nomisPersonId: Long,
+  @JsonProperty("visitContact")
   val visitContact: Boolean?,
 )
 
 data class VisitContact(
+  @JsonProperty("name")
   val name: String,
+  @JsonProperty("telephone")
   val telephone: String,
+  @JsonProperty("email")
   val email: String,
 )
 
 data class VisitorSupport(
+  @JsonProperty("description")
   val description: String,
 )
