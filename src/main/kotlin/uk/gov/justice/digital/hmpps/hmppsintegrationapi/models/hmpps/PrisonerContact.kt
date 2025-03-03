d4d6e70b052c4516ec4abb3bdc6caf77d0f8295d
Tom Watkins <tom.watkins@justice.gov.uk>
2025-03-03 18:21:21 +0000

    PrisonerContact metadata

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonerContact.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonerContact.kt
@@ -1,6 +1,10 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class PrisonerContact(
+  @Schema(description = "The details of a contact as an individual")
   val contact: Contact,
+  @Schema(description = "The details of the relationship between the prisoner and this contact")
   val relationship: PrisonerContactRelationship,
 )
