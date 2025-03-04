c29d9c16c588f5afe4ca27ee25d67fb6dd6b5eb1
Ben Madley <ben.madley@madetech.com>
2025-03-04 11:13:27 +0000

    Update sentry dependencies to 8.3.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ dependencies {
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   implementation("org.springframework.boot:spring-boot-starter-webflux:3.4.3")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.20.0")
-  implementation("io.sentry:sentry-logback:7.20.0")
+  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.3.0")
+  implementation("io.sentry:sentry-logback:8.3.0")
   implementation("org.springframework.data:spring-data-commons:3.4.3")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.2") {
     exclude("org.springframework.security", "spring-security-config")
