e34633fba2899448740ada22731d844da951581e
wcdkj <will.clark@digital.justice.gov.uk>
2025-03-05 11:56:05 +0000

    First pass role filtering on endpoints for consumer config

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/RolesConfig.kt
@@ -0,0 +1,35 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.config
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.env.PropertiesPropertySource
+import org.springframework.core.io.support.EncodedResource
+import org.springframework.core.io.support.PropertySourceFactory
+import java.io.IOException
+
+@Configuration
+@PropertySource(value = ["classpath:/roles.yml"], factory = MultipleYamlPropertySourceFactory::class)
+class RolesConfig(
+  val roles: List<Role>,
+)
+
+data class Role(
+  val name: String,
+  val endpoints: List<String>,
+)
+
+class MultipleYamlPropertySourceFactory : PropertySourceFactory {
+  @Throws(IOException::class)
+  override fun createPropertySource(
+    name: String,
+    encodedResource: EncodedResource,
+  ): org.springframework.core.env.PropertySource<*> {
+    val factory = YamlPropertiesFactoryBean()
+    factory.setResources(encodedResource.resource)
+
+    val properties = factory.getObject()
+
+    return PropertiesPropertySource(encodedResource.resource.filename, properties)
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/prisonVisits/PaginatedVisit.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/prisonVisits/PaginatedVisit.kt
@@ -1,34 +1,15 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisonVisits
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.interfaces.IPaginatedObject
 
-// A lot of these fields seem overkill?
 data class PaginatedVisit(
-  val totalElements: Long,
-  val totalPages: Long,
-  val first: Boolean,
-  val last: Boolean,
-  val size: Long,
-  val number: Long,
-  val sort: Sort,
-  val numberOfElements: Long,
-  val pageable: Pageable,
-  val empty: Boolean,
+  override val isLastPage: Boolean,
+  override val count: Int,
+  override val page: Int,
+  override val perPage: Int,
+  override val totalCount: Long,
+  override val totalPages: Int,
   @JsonProperty("content")
-  val visits: List<Visit?>,
-)
-
-data class Sort(
-  val empty: Boolean,
-  val sorted: Boolean,
-  val unsorted: Boolean,
-)
-
-data class Pageable(
-  val offset: Long,
-  val sort: Sort,
-  val pageSize: Long,
-  val paged: Boolean,
-  val pageNumber: Long,
-  val unpaged: Boolean,
-)
+  override val content: List<Visit?>,
+) : IPaginatedObject<Visit?>
