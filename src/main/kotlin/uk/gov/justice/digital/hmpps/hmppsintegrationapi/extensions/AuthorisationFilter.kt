4388eba27b28815306babcf28ad23e7964c44327
wcdkj <will.clark@digital.justice.gov.uk>
2025-03-05 17:28:01 +0000

    Moved roles extraction filter into auth filter due to cross concerns

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/AuthorisationFilter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/AuthorisationFilter.kt
@@ -8,20 +8,24 @@ import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.AuthorisationConfig
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.RolesConfig
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.internal.AuthoriseConsumerService
 import java.io.IOException
 
 @Component
 @Order(1)
-@EnableConfigurationProperties(AuthorisationConfig::class)
+@EnableConfigurationProperties(AuthorisationConfig::class, RolesConfig::class)
 class AuthorisationFilter
   @Autowired
   constructor(
     var authorisationConfig: AuthorisationConfig,
+    @Qualifier("data-uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.RolesConfig") var rolesConfig: RolesConfig,
   ) : Filter {
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(
@@ -34,15 +38,34 @@ class AuthorisationFilter
       val authoriseConsumerService = AuthoriseConsumerService()
       val subjectDistinguishedName = req.getAttribute("clientName") as String?
       val requestedPath = req.requestURI
+      val consumerConfig: ConsumerConfig? = authorisationConfig.consumers[subjectDistinguishedName]
 
       if (subjectDistinguishedName == null) {
         res.sendError(HttpServletResponse.SC_FORBIDDEN, "No subject-distinguished-name header provided for authorisation")
         return
       }
+      val includesResult = authoriseConsumerService.doesConsumerHaveIncludesAccess(authorisationConfig.consumers[subjectDistinguishedName], requestedPath)
 
-      val result = authoriseConsumerService.doesConsumerHaveIncludesAccess(authorisationConfig.consumers[subjectDistinguishedName], requestedPath)
+      if (includesResult) {
+        chain.doFilter(request, response)
+        return
+      }
 
-      if (!result) {
+      val consumersRoles = consumerConfig?.roles
+      val roleEndpoints =
+        buildList {
+          for (consumerRole in consumersRoles.orEmpty()) {
+            for (role in rolesConfig.roles) {
+              if (role.name == consumerRole) {
+                addAll(role.endpoints)
+              }
+            }
+          }
+        }
+
+      val roleResult =
+        authoriseConsumerService.doesConsumerHaveRoleAccess(roleEndpoints, requestedPath)
+      if (!roleResult) {
         res.sendError(HttpServletResponse.SC_FORBIDDEN, "Unable to authorise $requestedPath for $subjectDistinguishedName")
         return
       }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/AuthorisationFilterTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/AuthorisationFilterTest.kt
@@ -9,18 +9,21 @@ import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.AuthorisationConfig
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.RolesConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.Role
 
 class AuthorisationFilterTest {
-  private var authorisationConfig: AuthorisationConfig = AuthorisationConfig()
-  private var authorisationFilter: AuthorisationFilter = AuthorisationFilter(authorisationConfig)
   private var examplePath: String = "/v1/persons"
+  private var authorisationConfig: AuthorisationConfig = AuthorisationConfig()
+  private var exampleRoleConfig = RolesConfig(listOf(Role("private-prison", listOf(examplePath))))
+  private var authorisationFilter: AuthorisationFilter = AuthorisationFilter(authorisationConfig, exampleRoleConfig)
   private var exampleConsumer: String = "consumer-name"
+  private var exampleRoles: List<String> = listOf("private-prison")
 
   @Test
-  fun `calls the onward chain`() {
-    // Arrange
+  fun `calls the onward chain when path found in either`() {
     val mockRequest = mock(HttpServletRequest::class.java)
     whenever(mockRequest.requestURI).thenReturn(examplePath)
     whenever(mockRequest.getAttribute("clientName")).thenReturn(exampleConsumer)
@@ -28,31 +31,57 @@ class AuthorisationFilterTest {
     val mockChain = mock(FilterChain::class.java)
 
     authorisationConfig.consumers = mapOf(exampleConsumer to ConsumerConfig(include = listOf(examplePath), filters = ConsumerFilters(prisons = null), roles = listOf()))
-
-    // Act
     authorisationFilter.doFilter(mockRequest, mockResponse, mockChain)
 
-    // Assert
     verify(mockChain, times(1)).doFilter(mockRequest, mockResponse)
   }
 
   @Test
-  fun `generates error when consumer is unauthorised for requested path`() {
+  fun `calls the onward chain when path found in roles (but not in includes)`() {
     val mockRequest = mock(HttpServletRequest::class.java)
     whenever(mockRequest.requestURI).thenReturn(examplePath)
     whenever(mockRequest.getAttribute("clientName")).thenReturn(exampleConsumer)
     val mockResponse = mock(HttpServletResponse::class.java)
     val mockChain = mock(FilterChain::class.java)
 
-    authorisationConfig.consumers = mapOf(exampleConsumer to ConsumerConfig(include = emptyList(), filters = ConsumerFilters(prisons = null), roles = emptyList()))
-
-    val authorisationFilter = AuthorisationFilter(authorisationConfig)
-
-    // Act
+    authorisationConfig.consumers = mapOf(exampleConsumer to ConsumerConfig(include = emptyList(), filters = ConsumerFilters(prisons = null), roles = exampleRoles))
+    val authorisationFilter = AuthorisationFilter(authorisationConfig, exampleRoleConfig)
     authorisationFilter.doFilter(mockRequest, mockResponse, mockChain)
 
-    // Assert
-    verify(mockResponse, times(1)).sendError(403, "Unable to authorise /v1/persons for consumer-name")
+    verify(mockChain, times(1)).doFilter(mockRequest, mockResponse)
+  }
+
+  @Test
+  fun `calls the onward chain when path not found in roles, but found in includes`() {
+    val mockRequest = mock(HttpServletRequest::class.java)
+    whenever(mockRequest.requestURI).thenReturn(examplePath)
+    whenever(mockRequest.getAttribute("clientName")).thenReturn(exampleConsumer)
+    val mockResponse = mock(HttpServletResponse::class.java)
+    val mockChain = mock(FilterChain::class.java)
+
+    authorisationConfig.consumers = mapOf(exampleConsumer to ConsumerConfig(include = listOf(examplePath), filters = ConsumerFilters(prisons = null), roles = listOf()))
+    val invalidRoleConfig = RolesConfig(listOf(Role("private-prison", emptyList())))
+    val authorisationFilter = AuthorisationFilter(authorisationConfig, invalidRoleConfig)
+    authorisationFilter.doFilter(mockRequest, mockResponse, mockChain)
+
+    verify(mockChain, times(1)).doFilter(mockRequest, mockResponse)
+  }
+
+  @Test
+  fun `returns error when the roles or consumer includes do not contain the path`() {
+    val invalidPath = "v1/invalid"
+    val mockRequest = mock(HttpServletRequest::class.java)
+    whenever(mockRequest.requestURI).thenReturn(invalidPath)
+    whenever(mockRequest.getAttribute("clientName")).thenReturn(exampleConsumer)
+    val mockResponse = mock(HttpServletResponse::class.java)
+    val mockChain = mock(FilterChain::class.java)
+
+    val authorisationConfig = AuthorisationConfig()
+    authorisationConfig.consumers = mapOf(exampleConsumer to ConsumerConfig(include = listOf(examplePath), filters = ConsumerFilters(prisons = null), roles = exampleRoles))
+    val authorisationFilter = AuthorisationFilter(authorisationConfig = authorisationConfig, rolesConfig = exampleRoleConfig)
+    authorisationFilter.doFilter(mockRequest, mockResponse, mockChain)
+
+    verify(mockResponse, times(1)).sendError(403, "Unable to authorise $invalidPath for $exampleConsumer")
   }
 
   @Test
