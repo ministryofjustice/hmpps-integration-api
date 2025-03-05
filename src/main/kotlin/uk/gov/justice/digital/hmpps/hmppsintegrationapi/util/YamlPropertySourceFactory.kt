058061d1d6b59b47ce5ce5b62a3b639fa43eb15b
Tom Watkins <tom.watkins@justice.gov.uk>
2025-03-05 19:08:30 +0000

    Undo annotation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/YamlPropertySourceFactory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/YamlPropertySourceFactory.kt
@@ -3,7 +3,6 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.util
 
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
-import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.PropertiesPropertySource
 import org.springframework.core.env.PropertySource
 import org.springframework.core.io.support.EncodedResource
@@ -11,7 +10,6 @@ import org.springframework.core.io.support.PropertySourceFactory
 import java.io.IOException
 import java.util.*
 
-@Configuration
 class YamlPropertySourceFactory : PropertySourceFactory {
   override fun createPropertySource(
     name: String?,
