fd90c7a5f02dfa01ca72798524e1fd3999c15ad5
Tom Watkins <tom.watkins@justice.gov.uk>
2025-03-05 19:01:44 +0000

    Add configuration annotation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/YamlPropertySourceFactory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/YamlPropertySourceFactory.kt
@@ -3,6 +3,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.util
 
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
+import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.PropertiesPropertySource
 import org.springframework.core.env.PropertySource
 import org.springframework.core.io.support.EncodedResource
@@ -10,6 +11,7 @@ import org.springframework.core.io.support.PropertySourceFactory
 import java.io.IOException
 import java.util.*
 
+@Configuration
 class YamlPropertySourceFactory : PropertySourceFactory {
   override fun createPropertySource(
     name: String?,
