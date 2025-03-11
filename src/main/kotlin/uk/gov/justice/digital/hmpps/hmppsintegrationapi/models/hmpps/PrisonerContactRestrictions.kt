0f3353276893f7deca3104efa9d98b6a80363564
Tom Watkins <tom.watkins@justice.gov.uk>
2025-03-11 18:04:56 +0000

    Remove JSONProperty annotations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonerContactRestrictions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonerContactRestrictions.kt
@@ -1,7 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class PrisonerContactRestrictions(
   var prisonerContactRestrictions: List<PrisonerContactRestriction>? = emptyList(),
   var contactGlobalRestrictions: List<ContactGlobalRestriction>? = emptyList(),
@@ -26,30 +24,17 @@ data class PrisonerContactRestriction(
 )
 
 data class ContactGlobalRestriction(
-  @JsonProperty("contactRestrictionId")
   val contactRestrictionId: Long,
-  @JsonProperty("contactId")
   val contactId: Long,
-  @JsonProperty("restrictionType")
   val restrictionType: String,
-  @JsonProperty("restrictionTypeDescription")
   val restrictionTypeDescription: String,
-  @JsonProperty("startDate")
   val startDate: String,
-  @JsonProperty("expiryDate")
   val expiryDate: String,
-  @JsonProperty("comments")
   val comments: String,
-  @JsonProperty("enteredByUsername")
   val enteredByUsername: String,
-  @JsonProperty("enteredByDisplayName")
   val enteredByDisplayName: String,
-  @JsonProperty("createdBy")
   val createdBy: String,
-  @JsonProperty("createdTime")
   val createdTime: String,
-  @JsonProperty("updatedBy")
   val updatedBy: String,
-  @JsonProperty("updatedTime")
   val updatedTime: String,
 )
