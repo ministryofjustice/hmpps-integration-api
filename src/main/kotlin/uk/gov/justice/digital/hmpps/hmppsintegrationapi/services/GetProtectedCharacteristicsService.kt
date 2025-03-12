e980a364ab23c576296eb4b512ad235244985fd2
Emma Lanigan <98022697+emmalanigan@users.noreply.github.com>
2025-03-12 16:54:55 +0000

    HMAI-307 Add Prisons filter to /v1/persons/{hmppsId}/protected-characteristics so can be used by kilco (#745)
    * remove encodedhmppsid from controller and add filters
    
    * add filtering to service layer
    
    * add /v1/persons/.*/protected-characteristics to private prison role

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetProtectedCharacteristicsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetProtectedCharacteristicsService.kt
@@ -2,26 +2,51 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.NomisGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.ProbationOffenderSearchGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.PersonProtectedCharacteristics
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetPersonService.IdentifierType
 
 @Service
 class GetProtectedCharacteristicsService(
   @Autowired val probationOffenderSearchGateway: ProbationOffenderSearchGateway,
   @Autowired val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway,
   @Autowired val nomisGateway: NomisGateway,
+  @Autowired val consumerPrisonAccessService: ConsumerPrisonAccessService,
+  @Autowired val getPersonService: GetPersonService,
 ) {
-  fun execute(hmppsId: String): Response<PersonProtectedCharacteristics?> {
+  fun execute(
+    hmppsId: String,
+    filters: ConsumerFilters?,
+  ): Response<PersonProtectedCharacteristics?> {
+    val hmppsIdType = getPersonService.identifyHmppsId(hmppsId)
+    if (hmppsIdType == IdentifierType.UNKNOWN) {
+      return Response(
+        data = null,
+        errors = listOf(UpstreamApiError(causedBy = UpstreamApi.NOMIS, type = UpstreamApiError.Type.BAD_REQUEST)),
+      )
+    }
+
     val probationOffender = probationOffenderSearchGateway.getOffender(hmppsId)
 
     if (probationOffender.data != null) {
-      val result = probationOffender.data.toPersonProtectedCharacteristics()
       val prisonOffender =
         probationOffender.data.otherIds.nomsNumber
           ?.let { prisonerOffenderSearchGateway.getPrisonOffender(it) }
+      if (filters != null) {
+        val consumerPrisonFilterCheck = consumerPrisonAccessService.checkConsumerHasPrisonAccess<PersonProtectedCharacteristics>(prisonOffender?.data?.prisonId, filters)
+        if (consumerPrisonFilterCheck.errors.isNotEmpty()) {
+          return consumerPrisonFilterCheck
+        }
+      }
+
+      val result = probationOffender.data.toPersonProtectedCharacteristics()
       if (prisonOffender?.data != null) {
         result.maritalStatus = prisonOffender.data.maritalStatus
 
@@ -31,7 +56,6 @@ class GetProtectedCharacteristicsService(
       }
       return Response(data = result, errors = probationOffender.errors)
     }
-
     return Response(data = null, errors = probationOffender.errors)
   }
 }
