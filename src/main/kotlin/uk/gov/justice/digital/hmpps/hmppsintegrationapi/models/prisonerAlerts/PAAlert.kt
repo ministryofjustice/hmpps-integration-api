b43ddbbd904d546531615a85d99aaca92e1e9a45
Tom Watkins <tom.watkins@justice.gov.uk>
2025-03-13 18:25:02 +0000

    Add paginated model conversion method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/prisonerAlerts/PAAlert.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/prisonerAlerts/PAAlert.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.prisonerAlerts
 
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Alert
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class PAAlert(
@@ -8,8 +10,8 @@ data class PAAlert(
   val alertCode: PAAlertCode,
   val description: String? = null,
   val authorisedBy: String? = null,
-  val activeFrom: String,
-  val activeTo: String? = null,
+  val activeFrom: LocalDate,
+  val activeTo: LocalDate? = null,
   val isActive: Boolean,
   val createdAt: LocalDateTime,
   val createdBy: String,
@@ -21,4 +23,18 @@ data class PAAlert(
   val activeToLastSetBy: String? = null,
   val activeToLastSetByDisplayName: String? = null,
   val prisonCodeWhenCreated: String? = null,
-)
+) {
+  fun toAlert() =
+    Alert(
+      offenderNo = this.prisonNumber,
+      type = this.alertCode.alertTypeCode,
+      typeDescription = this.alertCode.alertTypeDescription,
+      code = this.alertCode.code,
+      codeDescription = this.alertCode.description,
+      comment = this.description,
+      dateCreated = this.activeFrom,
+      dateExpired = this.activeTo,
+      expired = this.activeTo?.isBefore(LocalDate.now().plusDays(1)) ?: false,
+      active = this.isActive,
+    )
+}
