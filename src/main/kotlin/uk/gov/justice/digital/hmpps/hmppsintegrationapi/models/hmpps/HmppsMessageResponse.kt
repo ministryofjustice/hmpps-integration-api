a748aab82b417eb53aec23d73a575659c1b16b9e
Tom Watkins <tom.watkins@justice.gov.uk>
2025-03-19 11:54:46 +0000

    Reworked visit queue models and implemented sendCreateVisit in queue service

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/HmppsMessageResponse.kt
@@ -0,0 +1,5 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
+
+data class HmppsMessageResponse(
+  val message: String,
+)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/VisitQueueService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/VisitQueueService.kt
@@ -1,12 +1,17 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.stereotype.Service
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.MessageFailedException
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.CreateVisitRequest
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsMessageResponse
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 import uk.gov.justice.hmpps.sqs.HmppsQueue
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.eventTypeMessageAttributes
 
 @Service
 class VisitQueueService(
@@ -18,15 +23,39 @@ class VisitQueueService(
   private val visitsQueueUrl by lazy { visitsQueue.queueUrl }
 
   companion object {
-    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+    private val objectMapper: ObjectMapper = jacksonObjectMapper()
   }
 
   fun sendCreateVisit(
     visit: CreateVisitRequest,
+    who: String,
     consumerFilters: ConsumerFilters?,
-  ) {
+  ): Response<HmppsMessageResponse?> {
     val visitPrisonerId = visit.prisonerId
 
     val personResponse = getPersonService.getNomisNumberWithPrisonFilter(hmppsId = visitPrisonerId, filters = consumerFilters)
+
+    if (personResponse.errors.isNotEmpty()) {
+      return Response(data = null, errors = personResponse.errors)
+    }
+
+    val message = visit.toHmppsMessage(who)
+
+    try {
+      val stringifiedMessage = objectMapper.writeValueAsString(message)
+
+      visitsQueueSqsClient.sendMessage(
+        SendMessageRequest
+          .builder()
+          .queueUrl(visitsQueueUrl)
+          .messageBody(stringifiedMessage)
+          .eventTypeMessageAttributes(message.eventType.toString())
+          .build(),
+      )
+
+      return Response(HmppsMessageResponse(message = "Visit creation written to queue"))
+    } catch (e: Exception) {
+      throw MessageFailedException("Could not send Visit message to queue", e)
+    }
   }
 }
