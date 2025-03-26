269de67ba74822e511713627570afc3586c3936a
ben-madley-mt <91620321+ben-madley-mt@users.noreply.github.com>
2025-03-26 12:16:54 +0000

    Cleanup Authorisation Filter to only call doFilter in a single place (#769)
    * Add test for current covered case of Limited access.
    
    * Catch other case of limited access requests being made.
    
    * Move different authorisation methods to own functions.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/AuthorisationFilter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/AuthorisationFilter.kt
@@ -43,20 +43,30 @@ class AuthorisationFilter(
     val authoriseConsumerService = AuthoriseConsumerService()
     val requestedPath = req.requestURI
 
-    val includesResult = authoriseConsumerService.doesConsumerHaveIncludesAccess(authorisationConfig.consumers[subjectDistinguishedName], requestedPath)
-    if (includesResult) {
+    if (authorisedThroughIncludes(authoriseConsumerService, subjectDistinguishedName, requestedPath) ||
+      authorisedThroughRole(authoriseConsumerService, subjectDistinguishedName, requestedPath)
+    ) {
       try {
         chain.doFilter(request, response)
       } catch (e: Throwable) {
-        if (e.cause is LimitedAccessException) {
-          res.sendError(HttpServletResponse.SC_FORBIDDEN, e.message)
+        val cause = e.cause
+        if (cause is LimitedAccessException) {
+          res.sendError(HttpServletResponse.SC_FORBIDDEN, cause.message)
         } else {
           throw e
         }
       }
       return
+    } else {
+      res.sendError(HttpServletResponse.SC_FORBIDDEN, "Unable to authorise $requestedPath for $subjectDistinguishedName")
     }
+  }
 
+  private fun authorisedThroughRole(
+    authoriseConsumerService: AuthoriseConsumerService,
+    subjectDistinguishedName: String?,
+    requestedPath: String,
+  ): Boolean {
     val consumerConfig: ConsumerConfig? = authorisationConfig.consumers[subjectDistinguishedName]
     val consumersRoles = consumerConfig?.roles
     val rolesInclude =
@@ -67,11 +77,12 @@ class AuthorisationFilter(
       }
     val roleResult =
       authoriseConsumerService.doesConsumerHaveRoleAccess(rolesInclude, requestedPath)
-    if (!roleResult) {
-      res.sendError(HttpServletResponse.SC_FORBIDDEN, "Unable to authorise $requestedPath for $subjectDistinguishedName")
-      return
-    }
-
-    chain.doFilter(request, response)
+    return roleResult
   }
+
+  private fun authorisedThroughIncludes(
+    authoriseConsumerService: AuthoriseConsumerService,
+    subjectDistinguishedName: String?,
+    requestedPath: String,
+  ) = authoriseConsumerService.doesConsumerHaveIncludesAccess(authorisationConfig.consumers[subjectDistinguishedName], requestedPath)
 }
