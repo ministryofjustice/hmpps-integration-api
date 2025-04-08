ab7a68fbd9a129b24cb4da729ebb17e715c094d0
emmalanigan <emma.keeley@madetech.com>
2025-04-08 11:29:34 +0100

    add client auth to config

--- smoke_tests/run.js
+++ smoke_tests/run.js
@@ -6,6 +6,8 @@ export const errorRate = new Rate('errors')
 
 const ExecutionOptions_Scenarios = getScenarios()
 
+getCerts()
+
 export const options = {
     scenarios: ExecutionOptions_Scenarios,
     thresholds: {
@@ -23,4 +25,40 @@ export function getScenarios() {
             maxDuration: '30s',
         },
     }
+}
+
+export function getCerts() {
+  console.log("\n[Setup] Retrieving certificates from context");
+
+  // Retrieve environment variables
+  const mtlsCertBase64 = __ENV.MTLS_CERT;
+  const mtlsKeyBase64 = __ENV.MTLS_KEY;
+
+  console.log(__ENV.SERVICE_URL);
+
+
+  if (!mtlsCertBase64 || !mtlsKeyBase64) {
+    console.error("Error: MTLS_CERT or MTLS_KEY environment variables not set.");
+    return; // Exit setup if environment variables are missing
+  }
+
+  try {
+    // Decode Base64 and write to files
+    const decodedCert = b64decode(mtlsCertBase64);
+    const decodedKey = b64decode(mtlsKeyBase64);
+
+    open('/client.pem', 'w').write(decodedCert);
+    open('/client.key', 'w').write(decodedKey);
+
+    console.log("[Setup] Certificates retrieved\n");
+
+    // You might want to return some data from setup to be used in VU code
+    return {
+      clientCertPath: '/client.pem',
+      clientKeyPath: '/client.key',
+    };
+  } catch (error) {
+    console.error("Error during certificate setup:", error);
+    return; // Exit setup on error
+  }
 }
--- smoke_tests/smoke-tests.js
+++ smoke_tests/smoke-tests.js
@@ -1,16 +1,32 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+// export const options = {
+//   tlsAuth: [
+//     {
+//       domains: [__ENV.SERVICE_URL], // Specify the domain(s) requiring mTLS
+//       cert: open('/client.pem'),
+//       key: open('/client.key'),
+//     },
+//   ],
+// };
 
 const get_urls = [
-    'https://www.google.com',
+    'https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons/X828566',
+    'https://www.google.com'
 ];
 
 
 export function mainSmokeTest() {
-
     get_urls.forEach((url) => {
-        const res = http.get(url);
+        const res = http.get(url, {
+            tlsAuth: [
+              {
+                cert: '/client.pem',
+                key: '/client.key',
+              },
+            ],
+          })
         check(res, {'status code MUST be 200': (res) => res.status === 200})
 
     });
