0f73c542661addb2f5010c9a2c9f6d234cfbbdf2
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-04-10 16:29:20 +0100

    Re use isLastPage value (#809)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/VisitQueueService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/VisitQueueService.kt
@@ -149,7 +149,7 @@ class VisitQueueService(
             return Response(data = null, errors = response.errors)
           }
           addAll(response.data?.contacts.orEmpty())
-          isLastPage = response.data?.pageMetadata?.totalPages == (response.data?.pageMetadata?.number ?: true)
+          isLastPage = response.data?.toPaginatedPrisonerContacts()?.isLastPage ?: true
           page++
         }
       }
