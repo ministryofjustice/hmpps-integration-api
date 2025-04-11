d4c6f27977885836c5f03fc981f1010932007db4
Tom Watkins <tom.watkins@justice.gov.uk>
2025-04-11 14:00:34 +0100

    Added test for when data is present

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/personalRelationships/PRPaginatedPrisonerContactsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/personalRelationships/PRPaginatedPrisonerContactsTest.kt
@@ -12,7 +12,7 @@ internal class PRPaginatedPrisonerContactsTest {
   @Nested
   inner class TestPaginatedPrisonerContacts {
     @Test
-    fun `Handle converting to PaginatedPrisonerContacts`() {
+    fun `Handle empty data`() {
       val prPaginatedPrisonerContacts =
         PRPaginatedPrisonerContacts(
           contacts = emptyList(),
@@ -32,6 +32,74 @@ internal class PRPaginatedPrisonerContactsTest {
           page = 1,
           totalCount = 0,
           totalPages = 0,
+          isLastPage = true,
+          perPage = 10,
+        )
+
+      prPaginatedPrisonerContacts.toPaginatedPrisonerContacts().shouldBe(paginatedPrisonerContacts)
+    }
+
+    @Test
+    fun `Handle data`() {
+      val visitorContactId = 3L
+      val prPaginatedPrisonerContacts =
+        PRPaginatedPrisonerContacts(
+          contacts =
+            listOf(
+              PRPrisonerContact(
+                prisonerContactId = 123456,
+                contactId = visitorContactId,
+                prisonerNumber = "A1234BC",
+                lastName = "Doe",
+                firstName = "John",
+                middleNames = "William",
+                dateOfBirth = "1980-01-01",
+                relationshipTypeCode = "S",
+                relationshipTypeDescription = "Friend",
+                relationshipToPrisonerCode = "FRI",
+                relationshipToPrisonerDescription = "Friend",
+                flat = "Flat 1",
+                property = "123",
+                street = "Baker Street",
+                area = "Marylebone",
+                cityCode = "25343",
+                cityDescription = "Sheffield",
+                countyCode = "S.YORKSHIRE",
+                countyDescription = "South Yorkshire",
+                postCode = "NW1 6XE",
+                countryCode = "ENG",
+                countryDescription = "England",
+                primaryAddress = true,
+                mailAddress = true,
+                phoneType = "MOB",
+                phoneTypeDescription = "Mobile",
+                phoneNumber = "+1234567890",
+                extNumber = "123",
+                isApprovedVisitor = true,
+                isNextOfKin = false,
+                isEmergencyContact = true,
+                isRelationshipActive = true,
+                currentTerm = true,
+                comments = "Close family friend",
+                restrictionSummary = RestrictionSummary(active = emptyList(), totalActive = 0, totalExpired = 0),
+              ),
+            ),
+          pageMetadata =
+            PagedModel.PageMetadata(
+              10,
+              0,
+              1,
+              1,
+            ),
+        )
+
+      val paginatedPrisonerContacts =
+        PaginatedPrisonerContacts(
+          content = prPaginatedPrisonerContacts.contacts.map { it.toPrisonerContact() },
+          count = 1,
+          page = 1,
+          totalCount = 1,
+          totalPages = 1,
           isLastPage = true,
           perPage = 10,
         )
