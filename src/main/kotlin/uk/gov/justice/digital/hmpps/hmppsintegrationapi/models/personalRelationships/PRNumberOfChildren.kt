7f1d57d52d51176d7f6b2c6ebad92414362b5bec
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-04-14 14:35:14 +0100

    add service function and tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/personalRelationships/PRNumberOfChildren.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/personalRelationships/PRNumberOfChildren.kt
@@ -1,9 +1,13 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.personalRelationships
 
 data class PRNumberOfChildren(
-  val id: Int,
+  val id: Int?,
   val numberOfChildren: String,
-  val active: Boolean,
-  val createdTime: String,
-  val createdBy: String,
+  val active: Boolean?,
+  val createdTime: String?,
+  val createdBy: String?,
+)
+
+data class PRNumberOfChildrenDto(
+  val numberOfChildren: String,
 )
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetNumberOfChildrenForPersonService.kt
@@ -0,0 +1,29 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PersonalRelationshipsGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.personalRelationships.PRNumberOfChildrenDto
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+
+@Service
+class GetNumberOfChildrenForPersonService(
+  @Autowired val personalRelationshipsGateway: PersonalRelationshipsGateway,
+  @Autowired val getPersonService: GetPersonService,
+) {
+  fun execute(
+    hmppsId: String,
+    filters: ConsumerFilters? = null,
+  ): Response<PRNumberOfChildrenDto?> {
+    val personResponse = getPersonService.getPersonWithPrisonFilter(hmppsId = hmppsId, filters = filters)
+    val nomisNumber = personResponse.data?.identifiers?.nomisNumber ?: return Response(data = null, errors = personResponse.errors)
+
+    val numberOfChildren = personalRelationshipsGateway.getNumberOfChildren(nomisNumber)
+
+    return Response(
+      data = numberOfChildren.data?.let { PRNumberOfChildrenDto(it.numberOfChildren) },
+      errors = numberOfChildren.errors,
+    )
+  }
+}
