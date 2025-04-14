b94474819dba6a6f28b1aa4913cb25b1a010bab5
wcdkj <will.clark@digital.justice.gov.uk>
2025-04-14 16:52:26 +0100

    Readded case-notes smoke test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/CaseNotesIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/CaseNotesIntegrationTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
 
 import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
 import java.time.LocalDateTime
@@ -13,7 +14,9 @@ class CaseNotesIntegrationTest : IntegrationTestBase() {
 
   @Test
   fun `returns case notes for a person`() {
-    callApi(path).andExpect(status().isOk)
+    callApi(path)
+      .andExpect(status().isOk)
+      .andExpect(content().json(getExpectedResponse("person-case-notes")))
   }
 
   @Test
