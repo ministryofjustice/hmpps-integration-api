1ef02a2d1ef777fdb4c0e5b2030c4ffba3feea1e
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-04-22 10:41:41 +0100

    write integration tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/ImageController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/ImageController.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.ValidationException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -32,6 +33,7 @@ class ImageController(
     description = "<b>Applicable filters</b>: <ul><li>prisons</li></ul>",
     responses = [
       ApiResponse(responseCode = "200", description = "Successfully found an image with the provided ID.", content = [Content(mediaType = "image/jpeg", schema = Schema(type = "string", format = "binary"))]),
+      ApiResponse(responseCode = "400", content = [Content(schema = Schema(ref = "#/components/schemas/BadRequest"))]),
       ApiResponse(responseCode = "404", description = "Failed to find an image with the provided ID.", content = [Content(schema = Schema(ref = "#/components/schemas/PersonNotFound"))]),
       ApiResponse(responseCode = "500", content = [Content(schema = Schema(ref = "#/components/schemas/InternalServerError"))]),
     ],
@@ -48,6 +50,10 @@ class ImageController(
 
     if (response.hasError(UpstreamApiError.Type.ENTITY_NOT_FOUND)) {
       throw EntityNotFoundException("Could not find image with id: $id")
+    }
+
+    if (response.hasError(UpstreamApiError.Type.BAD_REQUEST)) {
+      throw ValidationException("Invalid HMPPS ID: $hmppsId")
     }
 
     auditService.createEvent("GET_PERSON_IMAGE", mapOf("imageId" to id.toString()))
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/ImageIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/ImageIntegrationTest.kt
@@ -6,9 +6,37 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 class ImageIntegrationTest : IntegrationTestBase() {
   @Test
-  fun `returns an image from NOMIS`() {
+  fun `returns an image from NOMIS for old endpoint`() {
     callApi("/v1/images/2461788")
       .andExpect(status().isOk)
       .andExpect(content().contentType("image/jpeg"))
+  }
+
+  private val id = 2461788
+  private val path = "/v1/persons/$nomsId/images/$id"
+
+  @Test
+  fun `returns an image from NOMIS`() {
+    callApi(path )
+      .andExpect(status().isOk)
+      .andExpect(content().contentType("image/jpeg"))
+  }
+
+  @Test
+  fun `returns a 400 if the hmppsId is invalid`() {
+    callApi("/v1/persons/$invalidNomsId/images/$id")
+      .andExpect(status().isBadRequest)
+  }
+
+  @Test
+  fun `returns a 404 if consumer is in the wrong prison`() {
+    callApiWithCN(path, limitedPrisonsCn)
+      .andExpect(status().isNotFound)
+  }
+
+  @Test
+  fun `returns a 404 if consumer has empty list of prisons`() {
+    callApiWithCN(path, noPrisonsCn)
+      .andExpect(status().isNotFound)
   }
 }
