1c411b2f2a920baeb3499791b5be7b282f404c38
Tom Watkins <tom.watkins@justice.gov.uk>
2025-04-23 13:25:27 +0100

    Add integration tests for endpoints using with feature flags

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/FeatureFlaggedEndpointsIntegrationTest.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration
+
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.whenever
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.FeatureFlagConfig
+
+internal class FeatureFlaggedEndpointsIntegrationTest : IntegrationTestBase() {
+  @MockitoBean
+  private lateinit var featureFlagConfig: FeatureFlagConfig
+
+  @Test
+  fun `number of children endpoint should return 503`() {
+    whenever(featureFlagConfig.useNumberOfChildrenEndpoints).thenReturn(false)
+    callApi("$basePath/$nomsId/number-of-children")
+      .andExpect(status().isServiceUnavailable)
+  }
+
+  @Test
+  fun `physical characteristics endpoint should return 503`() {
+    whenever(featureFlagConfig.usePhysicalCharacteristicsEndpoints).thenReturn(false)
+    callApi("$basePath/$nomsId/physical-characteristics")
+      .andExpect(status().isServiceUnavailable)
+  }
+
+  @Test
+  fun `images by idendpoint should return 503`() {
+    whenever(featureFlagConfig.useImageEndpoints).thenReturn(false)
+    callApi("$basePath/$nomsId/images/2461788")
+      .andExpect(status().isServiceUnavailable)
+  }
+}
