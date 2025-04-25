0d2629086004ea9b416752d5ac04f451e83efc8f
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-04-25 11:29:32 +0100

    add model for location

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/locationsInsidePrison/LIPCapacity.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.locationsInsidePrison
+
+data class LIPCapacity(
+  val maxCapacity: Int,
+  val workingCapacity: Int
+)
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/locationsInsidePrison/LIPCertification.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.locationsInsidePrison
+
+data class LIPCertification(
+  val certified: Boolean,
+  val capacityOfCertifiedCell: Int
+)
