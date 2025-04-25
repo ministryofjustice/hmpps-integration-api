b544a48d04e2de4619714b43feea870423ab6f71
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-04-25 11:53:16 +0100

    add location function in gateway

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/LocationsInsidePrisonGateway.kt
@@ -0,0 +1,52 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper.WebClientWrapperResponse
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.locationsInsidePrison.LIPLocation
+
+@Component
+class LocationsInsidePrisonGateway(
+  @Value("\${services.locations-inside-prison.base-url}") baseUrl: String,
+) {
+  private val webClient = WebClientWrapper(baseUrl)
+
+  @Autowired
+  lateinit var hmppsAuthGateway: HmppsAuthGateway
+
+  fun getLocationByKey(key: String): Response<LIPLocation?> {
+    val result =
+      webClient.request<LIPLocation>(
+        HttpMethod.GET,
+        "location/key/$key",
+        authenticationHeader(),
+        UpstreamApi.LOCATIONS_INSIDE_PRISON,
+      )
+
+    return when (result) {
+      is WebClientWrapperResponse.Success -> {
+        Response(data = result.data)
+      }
+
+      is WebClientWrapperResponse.Error -> {
+        Response(
+          data = null,
+          errors = result.errors,
+        )
+      }
+    }
+  }
+
+  private fun authenticationHeader(): Map<String, String> {
+    val token = hmppsAuthGateway.getClientToken("LOCATIONS-INSIDE-PRISON")
+
+    return mapOf(
+      "Authorization" to "Bearer $token",
+    )
+  }
+}
