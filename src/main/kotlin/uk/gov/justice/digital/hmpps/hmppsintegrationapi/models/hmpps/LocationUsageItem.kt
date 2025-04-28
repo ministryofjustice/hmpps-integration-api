92fe1e3b933c5a46d6f19924b99240d013a5449e
Tom Watkins <tom.watkins@justice.gov.uk>
2025-04-28 18:18:33 +0100

    Added schema descriptions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/LocationUsageItem.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/LocationUsageItem.kt
@@ -1,7 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class LocationUsageItem(
+  @Schema(description = "Usage Type", examples = ["ADJUDICATION_HEARING", "APPOINTMENT", "MOVEMENT", "OCCURRENCE", "PROGRAMMES_ACTIVITIES", "PROPERTY", "VISIT", "OTHER"])
   val usageType: String,
+  @Schema(description = "Capacity")
   val capacity: Int?,
+  @Schema(description = "Sequence")
   val sequence: Int,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ResidentialDetails.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ResidentialDetails.kt
@@ -1,8 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class ResidentialDetails(
+  @Schema(description = "The top level type of locations", example = "Wings")
   val topLevelLocationType: String,
+  @Schema(description = "The description of the type of sub locations most common", examples = ["Wings", "Landings", "Spurs", "Cells"])
   val subLocationName: String?,
+  @Schema(description = "The current parent location (e.g Wing or Landing) details")
   val parentLocation: Location?,
+  @Schema(description = "All residential locations under this parent")
   val subLocations: List<Location>,
 )
