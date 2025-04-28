f5ff88200cc6abdbabc60c65c48121162c3c402a
Tom Watkins <tom.watkins@justice.gov.uk>
2025-04-28 15:48:04 +0100

    Added feature flag

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfig.kt
@@ -8,17 +8,20 @@ data class FeatureFlagConfig(
   val usePhysicalCharacteristicsEndpoints: Boolean,
   val useImageEndpoints: Boolean,
   val useEducationAssessmentsEndpoints: Boolean,
+  val useResidentialHierarchyEndpoints: Boolean,
 ) {
   companion object {
     const val USE_ARNS_ENDPOINTS = "use-arns-endpoints"
     const val USE_PHYSICAL_CHARACTERISTICS_ENDPOINTS = "use-physical-characteristics-endpoints"
     const val USE_IMAGE_ENDPOINTS = "use-image-endpoints"
     const val USE_EDUCATION_ASSESSMENTS_ENDPOINTS = "use-education-assessments-endpoints"
+    const val USE_RESIDENTIAL_HIERARCHY_ENDPOINTS = "use-residential-hierarchy-endpoints"
   }
 
   fun getConfigFlagValue(name: String): Boolean? =
     when (name) {
       USE_ARNS_ENDPOINTS -> this.useArnsEndpoints
+      USE_RESIDENTIAL_HIERARCHY_ENDPOINTS -> this.useResidentialHierarchyEndpoints
       else -> null
     }
 }
