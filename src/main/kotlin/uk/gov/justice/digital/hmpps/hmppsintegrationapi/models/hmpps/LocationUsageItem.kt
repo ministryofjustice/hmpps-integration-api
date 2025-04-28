fa3fe2eceb76b390720f20919cde54222bba62e8
Tom Watkins <tom.watkins@justice.gov.uk>
2025-04-28 18:23:24 +0100

    Add example

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/LocationUsageItem.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/LocationUsageItem.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class LocationUsageItem(
-  @Schema(description = "Usage Type", examples = ["ADJUDICATION_HEARING", "APPOINTMENT", "MOVEMENT", "OCCURRENCE", "PROGRAMMES_ACTIVITIES", "PROPERTY", "VISIT", "OTHER"])
+  @Schema(description = "Usage Type", example = "ADJUDICATION_HEARING", examples = ["ADJUDICATION_HEARING", "APPOINTMENT", "MOVEMENT", "OCCURRENCE", "PROGRAMMES_ACTIVITIES", "PROPERTY", "VISIT", "OTHER"])
   val usageType: String,
   @Schema(description = "Capacity")
   val capacity: Int?,
