a1e351613df422321ea6cd2479fdb25423becf8b
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-04-29 11:23:35 +0100

    add service function

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLocationByKeyService.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.LocationsInsidePrisonGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Location
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+
+@Service
+class GetLocationByKeyService(
+  @Autowired val locationsInsidePrisonGateway: LocationsInsidePrisonGateway,
+  @Autowired val consumerPrisonAccessService: ConsumerPrisonAccessService,
+)
+
+fun execute(
+  prisonId: String,
+  filters: ConsumerFilters?,
+  key: String,
+): Response<Location?> {
+  val checkAccess = consumerPrisonAccessService.checkConsumerHasPrisonAccess<Location?>(prisonId, filters)
+
+  if (checkAccess.errors.isNotEmpty()) {
+    return Response(data = checkAccess.data, errors = checkAccess.errors)
+  }
+
+  val prisonCombinedWithLocationKey = "$prisonId-$key"
+  val result = locationsInsidePrisonGateway.getLocationByKey(prisonCombinedWithLocationKey)
+
+  if (result.errors.isNotEmpty()) {
+    return Response(data = null, errors = result.errors)
+  }
+
+  return Response(data = result.data)
+}
