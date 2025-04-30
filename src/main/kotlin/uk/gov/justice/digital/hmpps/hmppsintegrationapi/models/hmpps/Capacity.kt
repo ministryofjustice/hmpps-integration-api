b14c5b71328d36dfe1d6e9f6e93e36cab0c9df66
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-04-30 10:02:51 +0100

    create capacity model

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Capacity.kt
@@ -0,0 +1,12 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class Capacity(
+  @Schema(description = "Max capacity of the location", example = "1000")
+  val maxCapacity: Int,
+  @Schema(description = "Signed capacity of the location", example = "1000")
+  val signedCapacity: Int,
+  @Schema(description = "Working capacity of the location", example = "1000")
+  val workingCapacity: Int,
+)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/locationsInsidePrison/LIPPrisonSummary.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/locationsInsidePrison/LIPPrisonSummary.kt
@@ -1,9 +1,18 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.locationsInsidePrison
 
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Capacity
+
 data class LIPPrisonSummary(
   val prisonName: String,
   val workingCapacity: Int,
   val signedOperationalCapacity: Int,
   val maxCapacity: Int,
   val numberOfCellLocations: Int,
-)
+) {
+  fun toCapacity(): Capacity =
+    Capacity(
+      maxCapacity = this.maxCapacity,
+      signedCapacity = this.signedOperationalCapacity,
+      workingCapacity = this.workingCapacity,
+    )
+}
