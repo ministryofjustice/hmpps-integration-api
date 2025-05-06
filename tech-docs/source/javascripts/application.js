4baef0a1f790ae1b67772c36fbeecb06ef573891
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-06 11:39:05 +0100

    Get swagger showing in page

--- tech-docs/source/javascripts/application.js
+++ tech-docs/source/javascripts/application.js
@@ -1,6 +1,17 @@
 //= require govuk_tech_docs
 
 window.addEventListener("load", (event) => {
+  const ui = SwaggerUIBundle({
+    url: 'https://ministryofjustice.github.io/hmpps-integration-api/openapi/api-docs.json',
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout",
+    displayOperationId: false
+  })
+
   const endpoints = [
     {
       schemaName: 'TransactionRequest',
