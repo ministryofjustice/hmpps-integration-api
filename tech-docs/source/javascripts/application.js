70b066c4e8f54ff94e047061fd86b5800f2425c0
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-06 14:10:13 +0100

    Remove commented out JS

--- tech-docs/source/javascripts/application.js
+++ tech-docs/source/javascripts/application.js
@@ -1,14 +1,1 @@
 //= require govuk_tech_docs
-
-// window.addEventListener("load", (event) => {
-//   SwaggerUIBundle({
-//     url: 'https://ministryofjustice.github.io/hmpps-integration-api/openapi/api-docs.json',
-//     dom_id: '#swagger-ui',
-//     presets: [
-//       SwaggerUIBundle.presets.apis,
-//       SwaggerUIStandalonePreset
-//     ],
-//     layout: "StandaloneLayout",
-//     displayOperationId: false
-//   })
-// });
