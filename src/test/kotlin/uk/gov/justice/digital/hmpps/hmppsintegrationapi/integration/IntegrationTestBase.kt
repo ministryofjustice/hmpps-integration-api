9803289f7eefa1f3e0bbdfe2b0bd21cdf71e369c
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-13 16:57:52 +0100

    Mocked prisoner by ID

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/IntegrationTestBase.kt
@@ -17,7 +17,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.removeWhitespaceAndNewlines
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.mockservers.ApiMockServer
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.mockservers.HmppsAuthMockServer
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import java.io.File
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
@@ -43,19 +45,32 @@ abstract class IntegrationTestBase {
   final val contactId = 123456L
 
   companion object {
+    private val nomsId = "G2996UX"
+
+    private val gatewaysFolder = "src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways"
     private val hmppsAuthMockServer = HmppsAuthMockServer()
+    val prisonerOffenderSearchMockServer = ApiMockServer.create(UpstreamApi.PRISONER_OFFENDER_SEARCH)
 
     @BeforeAll
     @JvmStatic
     fun startMockServers() {
       hmppsAuthMockServer.start()
       hmppsAuthMockServer.stubGetOAuthToken("client", "client-secret")
+
+      prisonerOffenderSearchMockServer.start()
+      prisonerOffenderSearchMockServer.stubForGet(
+        "/prisoner/$nomsId",
+        File(
+          "$gatewaysFolder/prisoneroffendersearch/fixtures/PrisonerByIdResponse.json",
+        ).readText(),
+      )
     }
 
     @AfterAll
     @JvmStatic
     fun stopMockServers() {
       hmppsAuthMockServer.stop()
+      prisonerOffenderSearchMockServer.stop()
     }
   }
 
