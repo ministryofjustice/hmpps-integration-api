fce4f22d510193bdeaa15c4976babce64ede60d9
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-15 17:05:07 +0100

    Add method to gateway

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/HealthAndMedicationGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/HealthAndMedicationGateway.kt
@@ -2,8 +2,12 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.healthandmedication.HAMHealthAndMedication
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 
 @Component
 class HealthAndMedicationGateway(
@@ -19,5 +23,30 @@ class HealthAndMedicationGateway(
     return mapOf(
       "Authorization" to "Bearer $token",
     )
+  }
+
+  fun getHealthAndMedicationData(prisonerNumber: String): Response<HAMHealthAndMedication?> {
+    val result =
+      webClient.request<HAMHealthAndMedication>(
+        HttpMethod.GET,
+        "/prisoners/$prisonerNumber",
+        authenticationHeader(),
+        UpstreamApi.HEALTH_AND_MEDICATION,
+        badRequestAsError = true,
+      )
+
+    return when (result) {
+      is WebClientWrapper.WebClientWrapperResponse.Success -> {
+        return Response(
+          data = result.data,
+        )
+      }
+      is WebClientWrapper.WebClientWrapperResponse.Error -> {
+        Response(
+          data = null,
+          errors = result.errors,
+        )
+      }
+    }
   }
 }
