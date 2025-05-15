3cad192c71a3727388401a4f648a182c86f5f91a
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-15 12:00:15 +0100

    Fixed empty response test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/prison/PrisonIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/prison/PrisonIntegrationTest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.prison
 import org.junit.jupiter.api.Test
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.removeWhitespaceAndNewlines
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
 
 class PrisonIntegrationTest : IntegrationTestBase() {
@@ -34,13 +35,54 @@ class PrisonIntegrationTest : IntegrationTestBase() {
   @Test
   fun `return a 200 for empty but successful upstream response`() {
     val deliberateMissVal = "nope"
-    callApi("$basePrisonPath/prisoners?first_name=$deliberateMissVal&last_name=$lastName&dateOfBirth=$dateOfBirth")
+    prisonerOffenderSearchMockServer.stubForPost(
+      "/global-search?size=9999",
+      """
+            {
+              "firstName": "$deliberateMissVal",
+              "lastName": "$lastName",
+              "includeAliases": false,
+              "dateOfBirth": "$dateOfBirth"
+            }
+          """.removeWhitespaceAndNewlines(),
+      """
+      {
+        "content": [],
+        "pageable": {
+          "sort": {
+            "empty": true,
+            "unsorted": true,
+            "sorted": false
+          },
+          "offset": 0,
+          "pageSize": 10,
+          "pageNumber": 0,
+          "paged": true,
+          "unpaged": false
+        },
+        "totalPages": 1,
+        "last": false,
+        "totalElements": 0,
+        "size": 10,
+        "number": 0,
+        "sort": {
+          "empty": true,
+          "unsorted": true,
+          "sorted": false
+        },
+        "first": true,
+        "numberOfElements": 0,
+        "empty": false
+      }
+      """.trimIndent(),
+    )
+    callApi("$basePrisonPath/prisoners?first_name=$deliberateMissVal&last_name=$lastName&date_of_birth=$dateOfBirth")
       .andExpect(status().isOk)
   }
 
   @Test
   fun `return multiple prisoners when querying by complex parameters`() {
-    callApi("$basePrisonPath/prisoners?first_name=$firstName&last_name=$lastName&dateOfBirth=$dateOfBirth")
+    callApi("$basePrisonPath/prisoners?first_name=$firstName&last_name=$lastName&date_of_birth=$dateOfBirth")
       .andExpect(status().isOk)
       .andExpect(content().json(getExpectedResponse("prisoners-response")))
   }
