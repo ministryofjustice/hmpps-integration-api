373dffa1dc0964e14b4eef214ddde74e796db8ff
Emmalanigan <emma.lanigan@justice.gov.uk>
2025-05-16 12:02:13 +0100

    WIP GetHealthAndDietService

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/HealthAndDiet.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/HealthAndDiet.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class HealthAndDiet(
-  val diet: Diet,
+  val diet: Diet? = null,
   val smoking: Smoker?,
 )
 
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetHealthAndDietService.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.HealthAndMedicationGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HealthAndDiet
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+
+@Service
+class GetHealthAndDietService(
+  @Autowired val getPersonService: GetPersonService,
+  @Autowired val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway,
+  @Autowired val healthAndMedicationGateway: HealthAndMedicationGateway,
+) {
+  fun execute(
+    hmppsId: String,
+    filters: ConsumerFilters?,
+  ): Response<HealthAndDiet?> {
+    val personResponse = getPersonService.getPersonWithPrisonFilter(hmppsId = hmppsId, filters)
+
+    if (personResponse.errors.isNotEmpty()) {
+      return Response(data = null, errors = personResponse.errors)
+    }
+
+    val smokerResponse = prisonerOffenderSearchGateway.getPrisonOffender(hmppsId)
+    val dietResponse = healthAndMedicationGateway.getHealthAndMedicationData(hmppsId)
+
+    val healthAndDiet =
+      HealthAndDiet(
+        diet = dietResponse.data?.dietAndAllergy?.toDiet(),
+        smoking = smokerResponse.data?.toSmoker(),
+      )
+
+    return Response(data = healthAndDiet, errors = smokerResponse.errors + dietResponse.errors)
+  }
+}
