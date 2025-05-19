a46e6966c8cf60c58a65b5dbd4fb9531113e9ff9
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-05-19 11:14:34 +0100

    add service function

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetLanguagesForPersonService.kt
@@ -0,0 +1,34 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.PersonalCareNeed
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+
+@Service
+class GetLanguagesForPersonService(
+  @Autowired val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway,
+  @Autowired val getPersonService: GetPersonService,
+) {
+  fun execute(
+    hmppsId: String,
+    filters: ConsumerFilters? = null,
+  ): Response<List<PersonalCareNeed>?> {
+    val personResponse = getPersonService.getPersonWithPrisonFilter(hmppsId = hmppsId, filters = filters)
+    if (personResponse.errors.isNotEmpty()) {
+      return Response(data = null, errors = personResponse.errors)
+    }
+    val nomisNumber = personResponse.data?.identifiers?.nomisNumber ?: return Response(data = null, errors = listOf(UpstreamApiError(UpstreamApi.PRISONER_OFFENDER_SEARCH, UpstreamApiError.Type.ENTITY_NOT_FOUND)))
+
+    val languages = prisonerOffenderSearchGateway.getPrisonOffender(nomisNumber)
+
+    return Response(
+      data = languages.data?.toPersonalCareNeeds(),
+      errors = languages.errors,
+    )
+  }
+}
