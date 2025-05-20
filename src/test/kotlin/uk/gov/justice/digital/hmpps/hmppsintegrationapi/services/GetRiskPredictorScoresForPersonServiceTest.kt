02ffe6a88993f83af4405c0b0e4e037f1b1c5233
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-20 19:15:27 +0100

    Corrected get risk predictor service tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetRiskPredictorScoresForPersonServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetRiskPredictorScoresForPersonServiceTest.kt
@@ -1,10 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import org.mockito.Mockito
-import org.mockito.internal.verification.VerificationModeFactory
+import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
@@ -13,7 +12,6 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.AssessRisksAndNeedsGateway
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.GeneralPredictor
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.GroupReconviction
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Identifiers
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Person
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.RiskOfSeriousRecidivism
@@ -22,6 +20,7 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.SexualPredi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.ViolencePredictor
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.personas.personInProbationOnlyPersona
 import java.time.LocalDateTime
 
 @ContextConfiguration(
@@ -34,11 +33,15 @@ internal class GetRiskPredictorScoresForPersonServiceTest(
   private val getRiskPredictorScoresForPersonService: GetRiskPredictorScoresForPersonService,
 ) : DescribeSpec(
     {
-      val hmppsId = "1234/56789B"
-      val deliusCrn = "X123456"
-
       val personFromProbationOffenderSearch =
-        Person(firstName = "Phoebe", lastName = "Buffay", identifiers = Identifiers(deliusCrn = deliusCrn))
+        Person(
+          firstName = personInProbationOnlyPersona.firstName,
+          lastName = personInProbationOnlyPersona.lastName,
+          identifiers = personInProbationOnlyPersona.identifiers,
+        )
+
+      val deliusCrn = personFromProbationOffenderSearch.identifiers.deliusCrn!!
+      val hmppsId = deliusCrn
 
       beforeEach {
         Mockito.reset(getPersonService)
@@ -49,20 +52,17 @@ internal class GetRiskPredictorScoresForPersonServiceTest(
             data = personFromProbationOffenderSearch,
           ),
         )
-
         whenever(assessRisksAndNeedsGateway.getRiskPredictorScoresForPerson(deliusCrn)).thenReturn(Response(data = emptyList()))
       }
 
       it("gets a person from getPersonService") {
         getRiskPredictorScoresForPersonService.execute(hmppsId)
-
-        verify(getPersonService, VerificationModeFactory.times(1)).execute(hmppsId = hmppsId)
+        verify(getPersonService, times(1)).execute(hmppsId = hmppsId)
       }
 
       it("gets risk predictor scores for a person from ARN API using CRN") {
         getRiskPredictorScoresForPersonService.execute(hmppsId)
-
-        verify(assessRisksAndNeedsGateway, VerificationModeFactory.times(1)).getRiskPredictorScoresForPerson(deliusCrn)
+        verify(assessRisksAndNeedsGateway, times(1)).getRiskPredictorScoresForPerson(deliusCrn)
       }
 
       it("returns risk predictor scores for a person") {
@@ -83,80 +83,55 @@ internal class GetRiskPredictorScoresForPersonServiceTest(
         )
 
         val response = getRiskPredictorScoresForPersonService.execute(hmppsId)
-
         response.data.shouldBe(riskPredictors)
       }
 
       describe("when an upstream API returns an error") {
-
-        xdescribe("when a person cannot be found by hmpps Id in probation offender search") {
-
+        describe("when a person cannot be found getPersonService") {
+          val errors =
+            listOf(
+              UpstreamApiError(
+                causedBy = UpstreamApi.PRISONER_OFFENDER_SEARCH,
+                type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
+              ),
+            )
           beforeEach {
             whenever(getPersonService.execute(hmppsId)).thenReturn(
               Response(
                 data = null,
-                errors =
-                  listOf(
-                    UpstreamApiError(
-                      causedBy = UpstreamApi.PRISONER_OFFENDER_SEARCH,
-                      type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
-                    ),
-                    UpstreamApiError(
-                      causedBy = UpstreamApi.PROBATION_OFFENDER_SEARCH,
-                      type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
-                    ),
-                  ),
+                errors,
               ),
             )
           }
 
           it("records upstream 404 API error for probation offender search") {
             val response = getRiskPredictorScoresForPersonService.execute(hmppsId)
-
-            response.hasErrorCausedBy(UpstreamApiError.Type.ENTITY_NOT_FOUND, UpstreamApi.PROBATION_OFFENDER_SEARCH).shouldBe(true)
-          }
-
-          it("records upstream 403 API error for probation offender search") {
-            val response = getRiskPredictorScoresForPersonService.execute(hmppsId)
-
-            response.data.isEmpty()
-            response.errors.shouldHaveSize(1)
-            response.hasErrorCausedBy(UpstreamApiError.Type.FORBIDDEN, UpstreamApi.ASSESS_RISKS_AND_NEEDS).shouldBe(true)
+            response.errors.shouldBe(errors)
           }
 
           it("does not get risk predictor scores from ARN") {
             getRiskPredictorScoresForPersonService.execute(hmppsId)
-
-            verify(assessRisksAndNeedsGateway, VerificationModeFactory.times(0)).getRiskPredictorScoresForPerson(id = deliusCrn)
+            verify(assessRisksAndNeedsGateway, times(0)).getRiskPredictorScoresForPerson(id = deliusCrn)
           }
         }
 
         it("returns error from ARN API when person/crn cannot be found in ARN") {
-
+          val errors =
+            listOf(
+              UpstreamApiError(
+                causedBy = UpstreamApi.ASSESS_RISKS_AND_NEEDS,
+                type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
+              ),
+            )
           whenever(assessRisksAndNeedsGateway.getRiskPredictorScoresForPerson(deliusCrn)).thenReturn(
             Response(
               data = emptyList(),
-              errors =
-                listOf(
-                  UpstreamApiError(
-                    causedBy = UpstreamApi.ASSESS_RISKS_AND_NEEDS,
-                    type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
-                  ),
-                ),
+              errors,
             ),
           )
 
           val response = getRiskPredictorScoresForPersonService.execute(hmppsId)
-
-          response.errors.shouldHaveSize(1)
-          response.errors
-            .first()
-            .causedBy
-            .shouldBe(UpstreamApi.ASSESS_RISKS_AND_NEEDS)
-          response.errors
-            .first()
-            .type
-            .shouldBe(UpstreamApiError.Type.ENTITY_NOT_FOUND)
+          response.errors.shouldBe(errors)
         }
       }
     },
