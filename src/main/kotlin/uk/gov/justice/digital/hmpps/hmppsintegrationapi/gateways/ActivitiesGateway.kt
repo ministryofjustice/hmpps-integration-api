1b21168ddaaeebf38ff8c850f196557545456e1b
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-06-10 14:51:20 +0100

    format code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/ActivitiesGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/ActivitiesGateway.kt
@@ -2,8 +2,12 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.AScheduledEvents
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 
 @Component
 class ActivitiesGateway(
@@ -19,5 +23,34 @@ class ActivitiesGateway(
     return mapOf(
       "Authorization" to "Bearer $token",
     )
+  }
+
+  fun getScheduledEvents(
+    prisonCode: String,
+    prisonerNumbers: List<String>,
+  ): Response<AScheduledEvents?> {
+    val requestBodyMap = mapOf("prisonerNumbers" to prisonerNumbers)
+    val result =
+      webClient.request<AScheduledEvents>(
+        HttpMethod.POST,
+        "/scheduled-events/prison/$prisonCode",
+        authenticationHeader(),
+        requestBody = requestBodyMap,
+        upstreamApi = UpstreamApi.ACTIVITIES,
+        forbiddenAsError = true,
+      )
+
+    return when (result) {
+      is WebClientWrapper.WebClientWrapperResponse.Success -> {
+        Response(data = result.data)
+      }
+
+      is WebClientWrapper.WebClientWrapperResponse.Error -> {
+        Response(
+          data = null,
+          errors = result.errors,
+        )
+      }
+    }
   }
 }
