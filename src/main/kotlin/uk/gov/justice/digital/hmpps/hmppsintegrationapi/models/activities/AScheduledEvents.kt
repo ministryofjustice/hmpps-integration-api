ed6e534707ca6b712ebe4e530538d8178a0954a6
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-06-11 11:16:51 +0100

    refactor model

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/activities/AScheduledEvents.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/activities/AScheduledEvents.kt
@@ -1,14 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities
 
 data class AScheduledEvents(
-  val prisonCode: String,
-  val prisonerNumbers: List<String>,
-  val startDate: String,
-  val endDate: String,
-  val appointments: List<AAppointment>,
-  val courtHearings: List<AAppointment>,
-  val visits: List<AAppointment>,
-  val activities: List<AAppointment>,
-  val externalTransfers: List<AAppointment>,
-  val adjudications: List<AAppointment>,
+  val prisonCode: String?,
+  val prisonerNumbers: List<String>?,
+  val startDate: String?,
+  val endDate: String?,
+  val appointments: List<AAppointment>?,
+  val courtHearings: List<AAppointment>?,
+  val visits: List<AAppointment>?,
+  val activities: List<AAppointment>?,
+  val externalTransfers: List<AAppointment>?,
+  val adjudications: List<AAppointment>?,
 )
