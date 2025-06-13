993eac4ddde4806c0a1cee3f04477c4440431af4
Emmalanigan <emma.lanigan@justice.gov.uk>
2025-06-13 16:23:53 +0100

    add schema descriptions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonPayBand.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonPayBand.kt
@@ -1,7 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class PrisonPayBand(
+  @Schema(description = "The internally-generated ID for this prison pay band", example = "123456")
   val id: Long,
+  @Schema(description = "The alternative text to use in place of the description", examples = ["Low", "Medium", "High"])
   val alias: String,
+  @Schema(description = "The description of pay band in this prison", example = "Pay band 1")
   val description: String,
 )
