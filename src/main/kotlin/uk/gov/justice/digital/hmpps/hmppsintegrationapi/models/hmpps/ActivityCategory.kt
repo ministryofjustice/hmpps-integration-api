9a4f69486b3d77a6158c5353657fae03d385a598
Emma Lanigan <98022697+emmalanigan@users.noreply.github.com>
2025-06-16 14:32:59 +0100

    HMAI-634 Add schema descriptions to models (#976)
    * add schema to prisonRegime
    
    * add schema to ActivityCategory
    
    * add description to RunningActivity
    
    * add required fields to schema

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ActivityCategory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ActivityCategory.kt
@@ -1,7 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class ActivityCategory(
+  @Schema(description = "The activity category code", example = "LEISURE_SOCIAL", required = true)
   val code: String,
+  @Schema(description = "The name of the activity category", example = "Leisure and social", required = true)
   val name: String,
+  @Schema(description = "The description of the activity category", example = "Library time and social clubs, like music or art")
   val description: String?,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/RunningActivity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/RunningActivity.kt
@@ -1,11 +1,20 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class RunningActivity(
+  @Schema(description = "The internally-generated ID for this activity", example = "123456", required = true)
   val id: Long,
+  @Schema(description = "The name of the activity", example = "Maths level 1")
   val activityName: String?,
+  @Schema(description = "The category for this activity", required = true)
   val category: ActivityCategory,
+  @Schema(description = "The capacity of the activity", example = "10", required = true)
   val capacity: Int,
+  @Schema(description = "The number of prisoners currently allocated to the activity", example = "8", required = true)
   val allocated: Int,
+  @Schema(description = "The number of prisoners currently on the waitlist for the activity", example = "1", required = true)
   val waitlisted: Int,
+  @Schema(description = "Whether the activity is live or archived", example = "LIVE", required = true)
   val activityState: String,
 )
