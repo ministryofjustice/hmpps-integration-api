72d4addf14c270ab7b3aaff7833de5a91f6dc9a4
Emmalanigan <emma.lanigan@justice.gov.uk>
2025-06-16 14:01:57 +0100

    add required fields to schema

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonPayBand.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonPayBand.kt
@@ -3,10 +3,10 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class PrisonPayBand(
-  @Schema(description = "The internally-generated ID for this prison pay band", example = "123456")
+  @Schema(description = "The internally-generated ID for this prison pay band", example = "123456", required = true)
   val id: Long,
-  @Schema(description = "The alternative text to use in place of the description", examples = ["Low", "Medium", "High"])
+  @Schema(description = "The alternative text to use in place of the description", examples = ["Low", "Medium", "High"], required = true)
   val alias: String,
-  @Schema(description = "The description of pay band in this prison", example = "Pay band 1")
+  @Schema(description = "The description of pay band in this prison", example = "Pay band 1", required = true)
   val description: String,
 )
