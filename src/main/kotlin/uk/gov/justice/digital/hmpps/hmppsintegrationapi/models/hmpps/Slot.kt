a3581beec592d501b3caa474cb2e707a15ad27d9
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-06-16 11:59:03 +0100

    add schema examples to model

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Slot.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Slot.kt
@@ -1,17 +1,32 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class Slot(
+  @Schema(example = "123456")
   val id: Long,
+  @Schema(example = "AM")
   val timeSlot: String,
+  @Schema(example = "1")
   val weekNumber: Int,
+  @Schema(example = "9:00")
   val startTime: String,
+  @Schema(example = "11:30")
   val endTime: String,
+  @Schema(example = "[Mon,Tue,Wed]")
   val daysOfWeek: String,
+  @Schema(example = "true")
   val mondayFlag: Boolean,
+  @Schema(example = "true")
   val tuesdayFlag: Boolean,
+  @Schema(example = "true")
   val wednesdayFlag: Boolean,
+  @Schema(example = "false")
   val thursdayFlag: Boolean,
+  @Schema(example = "false")
   val fridayFlag: Boolean,
+  @Schema(example = "false")
   val saturdayFlag: Boolean,
+  @Schema(example = "false")
   val sundayFlag: Boolean,
 )
