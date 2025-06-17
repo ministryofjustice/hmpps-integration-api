5e4c76fe5ec0d3aa358466268e91d5131ba44415
Tom Watkins <tom.watkins@justice.gov.uk>
2025-06-17 15:13:33 +0100

    New queue service

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/ActivitiesQueueService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/ActivitiesQueueService.kt
@@ -4,8 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.exception.MessageFailedException
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.AttendanceUpdateRequest
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsMessage
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsMessageResponse
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.toHmppsMessage
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
 import uk.gov.justice.hmpps.sqs.HmppsQueue
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.eventTypeMessageAttributes
@@ -14,10 +20,31 @@ import uk.gov.justice.hmpps.sqs.eventTypeMessageAttributes
 class ActivitiesQueueService(
   @Autowired private val hmppsQueueService: HmppsQueueService,
   @Autowired private val objectMapper: ObjectMapper,
+  @Autowired private val consumerPrisonAccessService: ConsumerPrisonAccessService,
 ) {
   private val activitiesQueue by lazy { hmppsQueueService.findByQueueId("activities") as HmppsQueue }
   private val activitiesQueueSqsClient by lazy { activitiesQueue.sqsClient }
   private val activitiesQueueUrl by lazy { activitiesQueue.queueUrl }
+
+  fun sendAttendanceUpdateRequest(
+    attendanceUpdateRequests: List<AttendanceUpdateRequest>,
+    who: String,
+    filters: ConsumerFilters?,
+  ): Response<HmppsMessageResponse?> {
+    for (attendanceUpdateRequest in attendanceUpdateRequests) {
+      val consumerPrisonFilterCheck = consumerPrisonAccessService.checkConsumerHasPrisonAccess<HmppsMessageResponse>(attendanceUpdateRequest.prisonId, filters)
+      if (consumerPrisonFilterCheck.errors.isNotEmpty()) {
+        return consumerPrisonFilterCheck
+      }
+
+      // TODO: Get attendance record - check if exists and also check the prison id is allowed
+    }
+
+    val hmppsMessage = attendanceUpdateRequests.toHmppsMessage(who)
+    writeMessageToQueue(hmppsMessage, "Could not send attendance update to queue")
+
+    return Response(HmppsMessageResponse(message = "Attendance update written to queue"))
+  }
 
   private fun writeMessageToQueue(
     hmppsMessage: HmppsMessage,
