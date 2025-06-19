f44b6aaa8ed12f16e78e2e37578afca5ca7fb636
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-06-19 17:32:42 +0100

    controller tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/ActivitiesControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/ActivitiesControllerTest.kt
@@ -21,12 +21,14 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.AttendanceU
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.DataResponse
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.HmppsMessageResponse
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.InternalLocation
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.ReasonForAttendance
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Slot
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.ActivitiesQueueService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetActivitiesScheduleService
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetAttendanceReasonsService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.internal.AuditService
 
 @WebMvcTest(controllers = [ActivitiesController::class])
@@ -36,6 +38,7 @@ class ActivitiesControllerTest(
   @Autowired val request: HttpServletRequest,
   @MockitoBean val auditService: AuditService,
   @MockitoBean val getActivitiesScheduleService: GetActivitiesScheduleService,
+  @MockitoBean val getAttendanceReasonsService: GetAttendanceReasonsService,
   @MockitoBean val activitiesQueueService: ActivitiesQueueService,
 ) : DescribeSpec(
     {
@@ -240,6 +243,59 @@ class ActivitiesControllerTest(
 
           val result = mockMvc.performAuthorisedPut(path, attendanceUpdateRequests)
           result.response.status.shouldBe(404)
+        }
+      }
+
+      describe("/v1/activities/attendance-reasons") {
+        val path = "$basePath/attendance-reasons"
+        val reasonForAttendance =
+          ReasonForAttendance(
+            id = 1L,
+            code = "SICK",
+            description = "Unwell - unable to attend",
+            attended = false,
+            notes = "Reported sick by cellmate.",
+          )
+
+        it("should return 200 when successful") {
+          whenever(getAttendanceReasonsService.execute())
+            .thenReturn(Response(data = listOf(reasonForAttendance)))
+
+          val result = mockMvc.performAuthorised(path)
+          result.response.status.shouldBe(HttpStatus.OK.value())
+          result.response
+            .contentAsJson<DataResponse<List<ReasonForAttendance>>>()
+            .shouldBe(DataResponse(data = listOf(reasonForAttendance)))
+        }
+
+        it("should call the audit service") {
+          whenever(getAttendanceReasonsService.execute())
+            .thenReturn(Response(data = listOf(reasonForAttendance)))
+
+          mockMvc.performAuthorised(path)
+
+          verify(auditService, times(1)).createEvent(
+            "GET_ATTENDANCE_REASONS",
+            mapOf(),
+          )
+        }
+
+        it("returns 403 when getAttendanceReasonsService returns forbidden") {
+          whenever(getAttendanceReasonsService.execute())
+            .thenReturn(
+              Response(
+                data = null,
+                errors =
+                  listOf(
+                    UpstreamApiError(
+                      type = UpstreamApiError.Type.FORBIDDEN,
+                      causedBy = UpstreamApi.ACTIVITIES,
+                    ),
+                  ),
+              ),
+            )
+          val result = mockMvc.performAuthorised(path)
+          result.response.status.shouldBe(403)
         }
       }
     },
