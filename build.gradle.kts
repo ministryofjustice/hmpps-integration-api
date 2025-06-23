a1de3eaa34fc42fec542ab63779c95d552834809
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-06-23 17:10:36 +0100

    HMAI-639 - Update HMPPS Spring Gradle Plugin (#1001)
    * Update to 8.3.0 beta
    
    * Update to non-beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
   kotlin("plugin.spring") version "2.1.21"
 }
 
