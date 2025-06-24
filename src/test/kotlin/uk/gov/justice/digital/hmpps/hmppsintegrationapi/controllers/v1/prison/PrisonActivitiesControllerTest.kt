7f16d6d95650017f6d7e5b9519fc0fd29e71ad90
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-06-24 15:24:46 +0100

    controller tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/prison/PrisonActivitiesControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/prison/PrisonActivitiesControllerTest.kt
@@ -15,14 +15,25 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.MockMvcExtensions.contentAsJson
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.helpers.IntegrationAPIMockMvc
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.ActivitiesActivity
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.ActivitiesActivityCategory
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.ActivitiesActivitySchedule
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.ActivitiesActivityScheduledInstancesForPrisoner
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.ActivitiesAdvanceAttendance
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.ActivitiesAttendance
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.ActivitiesMinimumEducationLevel
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.activities.ActivitiesSlot
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.ActivityCategory
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.ActivityScheduledInstanceForPrisoner
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.DataResponse
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.RunningActivity
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetPrisonActivitiesService
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.GetScheduledInstancesForPrisonerService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.internal.AuditService
+import java.time.LocalDateTime
 
 @WebMvcTest(controllers = [PrisonActivitiesController::class])
 @ActiveProfiles("test")
@@ -31,6 +42,7 @@ class PrisonActivitiesControllerTest(
   @Autowired val request: HttpServletRequest,
   @MockitoBean val auditService: AuditService,
   @MockitoBean val getPrisonActivitiesService: GetPrisonActivitiesService,
+  @MockitoBean val getScheduledInstancesForPrisonerService: GetScheduledInstancesForPrisonerService,
 ) : DescribeSpec(
     {
       val basePath = "/v1/prison"
@@ -106,6 +118,200 @@ class PrisonActivitiesControllerTest(
 
         it("returns 404 when getPrisonRegimeService returns not found") {
           whenever(getPrisonActivitiesService.execute(prisonId, filters)).thenReturn(
+            Response(
+              data = null,
+              errors =
+                listOf(
+                  UpstreamApiError(
+                    type = UpstreamApiError.Type.ENTITY_NOT_FOUND,
+                    causedBy = UpstreamApi.ACTIVITIES,
+                  ),
+                ),
+            ),
+          )
+
+          val result = mockMvc.performAuthorised(path)
+          result.response.status.shouldBe(404)
+        }
+      }
+
+      describe("GET /v1/prison/{prisonCode}/{prisonerId}/scheduled-instances") {
+        val prisonerId = "A1234AA"
+        val prisonCode = "MKI"
+        val filters = null
+        val path = "$basePath/$prisonCode/$prisonerId/scheduled-instances?startDate=2022-09-10&endDate=2023-09-10"
+
+        val activitiesActivityScheduledInstanceForPerson =
+          listOf(
+            ActivitiesActivityScheduledInstancesForPrisoner(
+              id = 123456L,
+              date = "2022-10-20",
+              startTime = "09:00",
+              endTime = "12:00",
+              timeSlot = "AM",
+              cancelled = false,
+              attendances =
+                listOf(
+                  ActivitiesAttendance(
+                    id = 123L,
+                    scheduledInstanceId = 12L,
+                    prisonerNumber = "A1234AA",
+                    status = "ACTIVE",
+                    editable = true,
+                    payable = false,
+                    attendanceReason = null,
+                    comment = null,
+                    recordedTime = null,
+                    recordedBy = null,
+                    payAmount = null,
+                    bonusAmount = null,
+                    pieces = null,
+                    issuePayment = null,
+                    incentiveLevelWarningIssued = null,
+                    otherAbsenceReason = null,
+                    caseNoteText = null,
+                    attendanceHistory = null,
+                  ),
+                ),
+              cancelledTime = null,
+              cancelledBy = null,
+              cancelledReason = null,
+              cancelledIssuePayment = null,
+              comment = null,
+              previousScheduledInstanceId = null,
+              previousScheduledInstanceDate = null,
+              nextScheduledInstanceId = null,
+              nextScheduledInstanceDate = null,
+              advanceAttendances =
+                listOf(
+                  ActivitiesAdvanceAttendance(
+                    id = 123L,
+                    scheduleInstanceId = 12L,
+                    prisonerNumber = "A1234AA",
+                    issuePayment = null,
+                    payAmount = null,
+                    recordedTime = null,
+                    recordedBy = null,
+                    attendanceHistory = null,
+                  ),
+                ),
+              activitySchedule =
+                ActivitiesActivitySchedule(
+                  id = 13L,
+                  description = "Monday AM Houseblock 3",
+                  internalLocation = null,
+                  capacity = 10,
+                  activity =
+                    ActivitiesActivity(
+                      id = 123456,
+                      prisonCode = "PVI",
+                      attendanceRequired = false,
+                      inCell = false,
+                      onWing = false,
+                      offWing = false,
+                      pieceWork = false,
+                      outsideWork = false,
+                      payPerSession = "H",
+                      summary = "Maths level 1",
+                      description = "A basic maths course suitable for introduction to the subject",
+                      category =
+                        ActivitiesActivityCategory(
+                          id = 1,
+                          code = "LEISURE_SOCIAL",
+                          name = "Leisure and social",
+                          description = "Such as association, library time and social clubs, like music or art",
+                        ),
+                      riskLevel = "high",
+                      minimumEducationLevel =
+                        listOf(
+                          ActivitiesMinimumEducationLevel(
+                            id = 123456,
+                            educationLevelCode = "Basic",
+                            educationLevelDescription = "Basic",
+                            studyAreaCode = "ENGLA",
+                            studyAreaDescription = "English Language",
+                          ),
+                        ),
+                      endDate = "2022-12-21",
+                      capacity = 0,
+                      allocated = 0,
+                      createdTime = LocalDateTime.parse("2022-09-01T09:01:02"),
+                      activityState = "live",
+                      paid = true,
+                    ),
+                  scheduleWeeks = 2,
+                  slots =
+                    listOf(
+                      ActivitiesSlot(
+                        id = 101L,
+                        timeSlot = "AM",
+                        weekNumber = 1,
+                        startTime = "09:00",
+                        endTime = "12:00",
+                        daysOfWeek = listOf("Mon", "Wed", "Fri"),
+                        mondayFlag = true,
+                        tuesdayFlag = false,
+                        wednesdayFlag = true,
+                        thursdayFlag = false,
+                        fridayFlag = true,
+                        saturdayFlag = false,
+                        sundayFlag = false,
+                      ),
+                    ),
+                  startDate = "2024-01-15",
+                  endDate = "2024-07-15",
+                  usePrisonRegimeTime = true,
+                ),
+            ),
+          )
+
+        val activityScheduledInstanceForPerson = activitiesActivityScheduledInstanceForPerson.map { it.toActivityScheduledInstanceForPrisoner() }
+
+        beforeEach {
+          Mockito.reset(getScheduledInstancesForPrisonerService)
+        }
+
+        it("should return 200 when success") {
+          whenever(getScheduledInstancesForPrisonerService.execute(prisonCode, prisonerId, "2022-09-10", "2023-09-10", null, null, filters)).thenReturn(Response(data = activityScheduledInstanceForPerson))
+
+          val result = mockMvc.performAuthorised(path)
+          result.response.status.shouldBe(HttpStatus.OK.value())
+          result.response.contentAsJson<DataResponse<List<ActivityScheduledInstanceForPrisoner>?>>().shouldBe(DataResponse(data = activityScheduledInstanceForPerson))
+        }
+
+        it("should call the audit service") {
+          whenever(getScheduledInstancesForPrisonerService.execute(prisonCode, prisonerId, "2022-09-10", "2023-09-10", null, null, filters)).thenReturn(Response(data = activityScheduledInstanceForPerson))
+
+          mockMvc.performAuthorised(path)
+          verify(
+            auditService,
+            times(1),
+          ).createEvent(
+            "GET_SCHEDULED_INSTANCES_FOR_PRISONER",
+            mapOf("prisonCode" to prisonCode, "prisonerId" to prisonerId, "startDate" to "2022-09-10", "endDate" to "2023-09-10", "slot" to null, "cancelled" to null.toString()),
+          )
+        }
+
+        it("returns 400 when getScheduledInstancesForPrisonerService returns bad request") {
+          whenever(getScheduledInstancesForPrisonerService.execute(prisonCode, prisonerId, "2022-09-10", "2023-09-10", null, null, filters)).thenReturn(
+            Response(
+              data = null,
+              errors =
+                listOf(
+                  UpstreamApiError(
+                    type = UpstreamApiError.Type.BAD_REQUEST,
+                    causedBy = UpstreamApi.ACTIVITIES,
+                  ),
+                ),
+            ),
+          )
+
+          val result = mockMvc.performAuthorised(path)
+          result.response.status.shouldBe(400)
+        }
+
+        it("returns 404 when getScheduledInstancesForPrisonerService returns not found") {
+          whenever(getScheduledInstancesForPrisonerService.execute(prisonCode, prisonerId, "2022-09-10", "2023-09-10", null, null, filters)).thenReturn(
             Response(
               data = null,
               errors =
