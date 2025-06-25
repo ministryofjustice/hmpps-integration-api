2f2b877fbb97fd0d49c8fe0f961986f8326ab239
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-06-25 16:55:04 +0100

    edit smoke test

--- scripts/K6/full-access-smoke-tests.js
+++ scripts/K6/full-access-smoke-tests.js
@@ -147,8 +147,14 @@ const postLocationDeactivateData = JSON.stringify({
 
 const postSearchAppointmentsEndpoint = `/v1/prison/${alternativeprisonId}/appointments/search`;
 const postSearchAppointmentsData = JSON.stringify({
+  appointmentType: "INDIVIDUAL",
   startDate: "2025-06-16",
-})
+  endDate: "2025-06-16",
+  timeSlots: ["AM", "PM", "ED"],
+  categoryCode: "GYMW",
+  inCell: false,
+  prisonerNumbers: ["A1234BC"]
+});
 
 const putAttendanceEndpoint = `/v1/activities/schedule/attendance`
 const putAttendanceData = JSON.stringify([{
