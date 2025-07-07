400e044683d44d41a8fac1cef33b93a3732aedb6
Emmalanigan <emma.lanigan@justice.gov.uk>
2025-07-07 17:06:45 +0100

    add schema descriptions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonerEducation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/PrisonerEducation.kt
@@ -1,6 +1,10 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class PrisonerEducation(
+  @Schema(description = "An enumeration of education levels.", examples = [ "PRIMARY_SCHOOL", "SECONDARY_SCHOOL_LEFT_BEFORE_TAKING_EXAMS", "SECONDARY_SCHOOL_TOOK_EXAMS", "FURTHER_EDUCATION_COLLEGE", "UNDERGRADUATE_DEGREE_AT_UNIVERSITY", "POSTGRADUATE_DEGREE_AT_UNIVERSITY", "NO_FORMAL_EDUCATION", "NOT_SURE" ], required = true)
   val educationLevel: String,
+  @Schema(description = "A list of achieved qualifications. Can be empty but not null.", required = true)
   val qualifications: List<Qualification>,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Qualification.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Qualification.kt
@@ -1,7 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class Qualification(
+  @Schema(description = "The subject of the qualification.", example = "Maths GCSE", required = true)
   val subject: String,
+  @Schema(description = "The level of an educational qualification.", examples = [ "ENTRY_LEVEL", "LEVEL_1", "LEVEL_2", "LEVEL_3", "LEVEL_4", "LEVEL_5", "LEVEL_6", "LEVEL_7", "LEVEL_8" ], required = true)
   val level: String,
+  @Schema(description = "The grade which was achieved (if known/relevant). Note: This is a free format value and there is no type or enum. Therefore values can be \"A\", \"B\", \"C\" etc, but also \"1\", \"2\", \"3\", \"Pass\", \"Distinction\", \"Merit\", \"First class honours\" etc. It is up to the consumer to interpret this data as necessary.", required = true)
   val grade: String,
 )
