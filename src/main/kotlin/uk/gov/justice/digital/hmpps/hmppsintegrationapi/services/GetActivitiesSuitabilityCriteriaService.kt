c4f9fb215dd3db8b416bdd8c345b9d5ca344d595
Emmalanigan <emma.lanigan@justice.gov.uk>
2025-07-07 16:10:02 +0100

    setup service file

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetActivitiesSuitabilityCriteriaService.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.common.ConsumerPrisonAccessService
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.ActivitiesGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+
+@Service
+class GetActivitiesSuitabilityCriteriaService(
+  @Autowired val activitiesGateway: ActivitiesGateway,
+  @Autowired val consumerPrisonAccessService: ConsumerPrisonAccessService,
+) {
+  fun execute(
+    activityId: Long,
+    filters: ConsumerFilters? = null,
+  ) {}
+}
