907fea6c669ff713d6e9d23d0c31d363a9a53570
Tom Watkins <tom.watkins@justice.gov.uk>
2025-07-08 12:29:05 +0100

    Changed fields on model to reflect changes to upstream

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ActivityScheduledInstanceForPrisoner.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/ActivityScheduledInstanceForPrisoner.kt
@@ -1,42 +1,51 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDate
+import java.time.LocalTime
 
+@Schema(description = "Describes a scheduled activity for a prisoner")
 data class ActivityScheduledInstanceForPrisoner(
   @Schema(description = "Unique ID of the scheduled instance", example = "123456")
-  val id: Long,
+  val scheduledInstanceId: Long,
+  @Schema(description = "The id of the activity", example = "1")
+  val activityId: Int,
   @Schema(description = "Date of the scheduled instance", example = "2022-10-20")
-  val date: String,
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  val sessionDate: LocalDate,
   @Schema(description = "Scheduled start time", example = "09:00")
-  val startTime: String,
+  @JsonFormat(pattern = "HH:mm")
+  val startTime: LocalTime? = null,
   @Schema(description = "Scheduled end time", example = "12:00")
-  val endTime: String,
-  @Schema(description = "Time slot for the activity", example = "AM")
+  @JsonFormat(pattern = "HH:mm")
+  val endTime: LocalTime? = null,
+  @Schema(description = "Set to true if this event will take place in the prisoner's cell", example = "false")
+  val inCell: Boolean,
+  @Schema(description = "Flag to indicate if the location of the activity is on wing", example = "false")
+  val onWing: Boolean,
+  @Schema(description = "Flag to indicate if the location of the activity is off wing and not in a listed location", example = "false")
+  val offWing: Boolean,
+  val scheduleDescription: String? = null,
+  @Schema(description = "The category for this activity, one of the high-level categories")
+  val activityCategory: String,
+  @Schema(description = "The title of the activity for this attendance record", example = "Math Level 1")
+  val activitySummary: String? = null,
+  @Schema(description = "Set to true if this event has been cancelled", example = "false")
+  val cancelled: Boolean = false,
+  @Schema(description = "Set to true if this prisoner is suspended from the scheduled event", example = "false")
+  val suspended: Boolean = false,
+  @Schema(description = "Set to true if this prisoner is auto-suspended from the scheduled event", example = "false")
+  val autoSuspended: Boolean = false,
+  @Schema(description = "Time slot of scheduled instance", example = "AM")
   val timeSlot: String,
-  @Schema(description = "Was the session cancelled?", example = "false")
-  val cancelled: Boolean,
-  @Schema(description = "Time the session was cancelled", example = "2022-10-19T15:30:00")
-  val cancelledTime: String? = null,
-  @Schema(description = "Name of the person who cancelled the session", example = "Adam Smith")
-  val cancelledBy: String? = null,
-  @Schema(description = "Reason the session was cancelled", example = "Staff unavailable")
-  val cancelledReason: String? = null,
-  @Schema(description = "Was payment still issued despite cancellation?", example = "true")
-  val cancelledIssuePayment: Boolean? = null,
-  @Schema(description = "Optional comment on the scheduled session", example = "Teacher unavailable")
-  val comment: String? = null,
-  @Schema(description = "ID of the previous scheduled instance", example = "123455")
-  val previousScheduledInstanceId: Long? = null,
-  @Schema(description = "Date of the previous scheduled instance", example = "2022-10-19")
-  val previousScheduledInstanceDate: String? = null,
-  @Schema(description = "ID of the next scheduled instance", example = "123457")
-  val nextScheduledInstanceId: Long? = null,
-  @Schema(description = "Date of the next scheduled instance", example = "2022-10-21")
-  val nextScheduledInstanceDate: String? = null,
-  @Schema(description = "Attendances recorded for this session")
-  val attendances: List<Attendance>,
-  @Schema(description = "Advance attendance history recorded for this session")
-  val advanceAttendances: List<AdvanceAttendance>,
-  @Schema(description = "Details of the associated activity schedule")
-  val activitySchedule: ActivitySchedule,
+  @Schema(description = "Should activity payment be issued for SICK, REST or OTHER", example = "true")
+  val issuePayment: Boolean? = false,
+  @Schema(description = "The activity attendance status - WAITING or COMPLETED", example = "WAITING")
+  val attendanceStatus: String? = null,
+  @Schema(description = "The code for the activity (non) attendance reason", example = "SICK")
+  val attendanceReasonCode: String? = null,
+  @Schema(description = "Set to true if this activity is a paid activity", example = "false")
+  val paidActivity: Boolean,
+  val possibleAdvanceAttendance: Boolean,
 )
