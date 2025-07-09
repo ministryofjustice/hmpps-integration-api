ac006757e12462fce6865d1b6d8edf8126150b4a
BushraAbdullahi <bushra.abdullahi@digital.justice.gov.uk>
2025-07-09 15:38:54 +0100

    add service function and tests

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/GetWaitingListApplicationsByIdService.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.ActivitiesGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.WaitingListApplication
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters
+
+@Service
+class GetWaitingListApplicationsByIdService(
+  @Autowired val activitiesGateway: ActivitiesGateway,
+  @Autowired val getScheduleDetailsService: GetScheduleDetailsService,
+) {
+  fun execute(
+    scheduleId: Long,
+    filters: ConsumerFilters?,
+  ): Response<List<WaitingListApplication>?> {
+    val checkPrisonCode = getScheduleDetailsService.execute(scheduleId, filters)
+    if (checkPrisonCode.errors.isNotEmpty()) {
+      return Response(data = null, errors = checkPrisonCode.errors)
+    }
+
+    val waitingListApplicationResponse = activitiesGateway.getWaitingListApplicationsById(scheduleId)
+    if (waitingListApplicationResponse.errors.isNotEmpty()) {
+      return Response(
+        data = null,
+        errors = waitingListApplicationResponse.errors,
+      )
+    }
+
+    return Response(
+      data = waitingListApplicationResponse.data?.map { it.toWaitingListApplication() },
+    )
+  }
+}
