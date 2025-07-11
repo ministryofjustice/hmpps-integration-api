1f3f1870af14551fd0e53bac5201b657d55df34b
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-07-11 14:36:38 +0100

    HIA-778: Create basic status endpoint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/controllers/v1/StatusController.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.controllers.v1
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Status
+
+@RestController
+@RequestMapping("/v1/status")
+@Tag(name = "Service Status")
+class StatusController {
+  @Operation(
+    summary = "Get service status.",
+    responses = [
+      ApiResponse(responseCode = "200", useReturnTypeSchema = true, description = "Service is running."),
+    ],
+  )
+  @GetMapping
+  fun getStatus(): Status = Status()
+}
