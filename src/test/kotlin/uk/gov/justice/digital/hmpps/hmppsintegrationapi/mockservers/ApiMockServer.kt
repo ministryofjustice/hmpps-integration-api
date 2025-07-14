d9e5e970457fbf4fdbc669c43bbe3cb77b0ac09e
Tom Watkins <tom.watkins@justice.gov.uk>
2025-07-14 12:01:02 +0100

    Adjust ApiMockServer to take in a config option

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/mockservers/ApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/mockservers/ApiMockServer.kt
@@ -6,38 +6,44 @@ import com.github.tomakehurst.wiremock.client.WireMock.equalToJson
 import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.matching
 import com.github.tomakehurst.wiremock.client.WireMock.post
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import org.springframework.http.HttpStatus
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 
 class ApiMockServer(
-  port: Int,
-) : WireMockServer(port) {
+  config: WireMockConfiguration,
+) : WireMockServer(config) {
   companion object {
     // These ports must match the config in the yaml files
-    fun create(upstreamApi: UpstreamApi): ApiMockServer =
-      when (upstreamApi) {
-        UpstreamApi.PRISONER_OFFENDER_SEARCH -> ApiMockServer(4000)
-        UpstreamApi.HEALTH_AND_MEDICATION -> ApiMockServer(4001)
-        UpstreamApi.MANAGE_POM_CASE -> ApiMockServer(4002)
-        UpstreamApi.PLP -> ApiMockServer(4003)
-        UpstreamApi.ACTIVITIES -> ApiMockServer(4004)
-        // USE PRISM
-        UpstreamApi.PRISON_API -> ApiMockServer(4000)
-        UpstreamApi.NDELIUS -> ApiMockServer(4003)
-        UpstreamApi.ASSESS_RISKS_AND_NEEDS -> ApiMockServer(4004)
-        UpstreamApi.EFFECTIVE_PROPOSAL_FRAMEWORK -> ApiMockServer(4005)
-        UpstreamApi.ADJUDICATIONS -> ApiMockServer(4006)
-        UpstreamApi.CVL -> ApiMockServer(4007)
-        UpstreamApi.CASE_NOTES -> ApiMockServer(4008)
-        UpstreamApi.RISK_MANAGEMENT_PLAN -> ApiMockServer(4004)
-        UpstreamApi.TEST -> TODO()
-        UpstreamApi.NON_ASSOCIATIONS -> ApiMockServer(4005)
-        UpstreamApi.PERSONAL_RELATIONSHIPS -> ApiMockServer(4006)
-        UpstreamApi.MANAGE_PRISON_VISITS -> ApiMockServer(4007)
-        UpstreamApi.INCENTIVES -> ApiMockServer(4008)
-        UpstreamApi.PRISONER_ALERTS -> ApiMockServer(4009)
-        UpstreamApi.LOCATIONS_INSIDE_PRISON -> ApiMockServer(4000)
-      }
+    fun create(upstreamApi: UpstreamApi): ApiMockServer {
+      val port =
+        when (upstreamApi) {
+          UpstreamApi.PRISONER_OFFENDER_SEARCH -> 4000
+          UpstreamApi.HEALTH_AND_MEDICATION -> 4001
+          UpstreamApi.MANAGE_POM_CASE -> 4002
+          UpstreamApi.PLP -> 4003
+          UpstreamApi.ACTIVITIES -> 4004
+          // USE PRISM
+          UpstreamApi.PRISON_API -> 4000
+          UpstreamApi.NDELIUS -> 4003
+          UpstreamApi.ASSESS_RISKS_AND_NEEDS -> 4004
+          UpstreamApi.EFFECTIVE_PROPOSAL_FRAMEWORK -> 4005
+          UpstreamApi.ADJUDICATIONS -> 4006
+          UpstreamApi.CVL -> 4007
+          UpstreamApi.CASE_NOTES -> 4008
+          UpstreamApi.RISK_MANAGEMENT_PLAN -> 4004
+          UpstreamApi.TEST -> TODO()
+          UpstreamApi.NON_ASSOCIATIONS -> 4005
+          UpstreamApi.PERSONAL_RELATIONSHIPS -> 4006
+          UpstreamApi.MANAGE_PRISON_VISITS -> 4007
+          UpstreamApi.INCENTIVES -> 4008
+          UpstreamApi.PRISONER_ALERTS -> 4009
+          UpstreamApi.LOCATIONS_INSIDE_PRISON -> 4000
+        }
+
+      val config = WireMockConfiguration.wireMockConfig().port(port)
+      return ApiMockServer(config)
+    }
   }
 
   fun stubForGet(
