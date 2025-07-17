5610f70846c58695d2959e6224f9ccb9e3ccc2c1
Emmalanigan <emma.lanigan@justice.gov.uk>
2025-07-17 16:50:58 +0100

    add validation to ManagePOMCaseGateway calls

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/PersonResponsibleOfficerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/PersonResponsibleOfficerIntegrationTest.kt
@@ -1,16 +1,24 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
 
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
 
 class PersonResponsibleOfficerIntegrationTest : IntegrationTestBase() {
+  @AfterEach
+  fun resetValidators() {
+    managePomCaseMockServer.resetValidator()
+  }
+
   @Test
   fun `returns needs for a person`() {
     callApi("$basePath/$nomsId/person-responsible-officer")
       .andExpect(status().isOk)
       .andExpect(content().json(getExpectedResponse("person-responsible-officer")))
+
+    managePomCaseMockServer.assertValidationPassed()
   }
 
   @Test
