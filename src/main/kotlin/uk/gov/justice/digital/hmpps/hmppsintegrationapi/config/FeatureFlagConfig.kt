9eac830a804240bbd215c70e0578484364ef59b1
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2025-08-13 16:53:09 +0100

    HIA-831: Remove retry feature flag (#1141)
    * HIA-831: Remove retry feature flag
    
    * HIA-831: Remove retry feature flag
    
    * HIA-831: Remove retry feature flag
    
    * HIA-831: Remove retry feature flag

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfig.kt
@@ -35,7 +35,6 @@ data class FeatureFlagConfig(
     const val USE_WAITING_LIST_ENDPOINT = "use-waiting-list-endpoint"
     const val SAN_ENDPOINT_ENABLED = "san-endpoint-enabled"
     const val SIMPLE_REDACTION = "simple-redaction"
-    const val RETRY_ALL_UPSTREAM_GETS = "retry-all-upstream-gets"
     const val EDUCATION_TRIGGER_ENABLED = "education-trigger-enabled"
     const val EDUCATION_ALN_TRIGGER_ENABLED = "education-aln-trigger-enabled"
   }
