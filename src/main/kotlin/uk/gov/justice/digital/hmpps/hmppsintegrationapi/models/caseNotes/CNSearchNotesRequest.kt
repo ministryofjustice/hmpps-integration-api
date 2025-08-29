29d8973161631185adf6432846a9a81060424b84
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-08-29 15:55:35 +0100

    MAP-2341 Add CaseNotes filter to restrict access to defined top-level Case Notes types

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/caseNotes/CNSearchNotesRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/caseNotes/CNSearchNotesRequest.kt
@@ -4,6 +4,7 @@ data class CNSearchNotesRequest(
   val includeSensitive: Boolean? = true,
   val occurredFrom: String? = null,
   val occurredTo: String? = null,
+  val typeSubTypes: List<CNTypeSubType>? = null,
   val page: Int? = null,
   val size: Int? = null,
 ) {
@@ -18,12 +19,27 @@ data class CNSearchNotesRequest(
     if (occurredTo != null) {
       map["occurredTo"] = occurredTo
     }
+    if (typeSubTypes != null) {
+      map["typeSubTypes"] = typeSubTypes.map { it.toApiConformingMap() }
+    }
     if (page != null) {
       map["page"] = page.toInt()
     }
     if (size != null) {
       map["size"] = size.toInt()
     }
+    return map
+  }
+}
+
+data class CNTypeSubType(
+  val type: String,
+  val subTypes: List<String> = emptyList(),
+) {
+  fun toApiConformingMap(): Map<String, Any> {
+    val map = mutableMapOf<String, Any>()
+    map["type"] = type
+    map["subTypes"] = subTypes
     return map
   }
 }
