8286d34c5443db7ba8ff66a35466111fdaad076c
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-09-08 14:07:57 +0100

    HIA-848: Added contact events with nDelius stub

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/ContactEventStubGenerator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/ContactEventStubGenerator.kt
@@ -27,7 +27,7 @@ object ContactEventStubGenerator {
     officerId = 123456L,
     officerName = "Officer Name",
     description = "description",
-    notes = "notes",
+    notes = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
   )
 
   fun generateNDeliusContactEvents(
