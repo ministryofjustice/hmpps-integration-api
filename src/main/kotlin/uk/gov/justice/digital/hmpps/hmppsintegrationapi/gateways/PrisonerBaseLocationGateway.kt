b150ad162c1bc5189f88e7b150733aafdf8a5885
Chris Waugh <238270+chriswaugh@users.noreply.github.com>
2025-09-10 17:37:14 +0100

    ESWE-1411: Add prisoner base location API
    Refactored the PrisonerBaseLocationGateway to call out to the new prisoner base location API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/PrisonerBaseLocationGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/PrisonerBaseLocationGateway.kt
@@ -1,14 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.LastMovementType
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper.WebClientWrapperResponse
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.PrisonerBaseLocation
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 
 /**
  * <p><This unreal gateway is pending for refactoring /p>
@@ -18,38 +18,41 @@ import java.time.format.DateTimeFormatter
  */
 @Component
 class PrisonerBaseLocationGateway(
-  @Autowired val prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway,
+  @Value("\${services.prisoner-base-location.base-url}") baseUrl: String,
 ) {
-  companion object {
-    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
-  }
+  private val webClient = WebClientWrapper(baseUrl)
+
+  @Autowired
+  lateinit var hmppsAuthGateway: HmppsAuthGateway
 
   fun getPrisonerBaseLocation(nomisNumber: String): Response<PrisonerBaseLocation?> {
-    logger.debug("Getting prisoner base location for prisonNumber=$nomisNumber")
-    val prisonResponse = prisonerOffenderSearchGateway.getPrisonOffender(nomsNumber = nomisNumber)
+    val result =
+      webClient.request<PrisonerBaseLocation>(
+        HttpMethod.GET,
+        "v1/persons/$nomisNumber/prisoner-base-location",
+        authenticationHeader(),
+        UpstreamApi.PRISONER_BASE_LOCATION,
+      )
 
-    // to be refactored: translate from reference data, at domain service
-    val baseLocation =
-      prisonResponse.data?.let {
-        val inPrison = it.inOutStatus == "IN"
-        PrisonerBaseLocation(
-          inPrison = inPrison,
-          prisonId = if (inPrison) it.prisonId else null,
-          lastPrisonId = it.lastPrisonId,
-          lastMovementType = it.lastMovementTypeCode?.let { translateLastMovementType(it) },
-          receptionDate = it.receptionDate?.let { LocalDate.parse(it, DateTimeFormatter.ISO_DATE) },
+    return when (result) {
+      is WebClientWrapperResponse.Success -> {
+        Response(data = result.data)
+      }
+
+      is WebClientWrapperResponse.Error -> {
+        Response(
+          data = null,
+          errors = result.errors,
         )
       }
-    return Response(data = baseLocation, errors = prisonResponse.errors)
+    }
   }
 
-  private fun translateLastMovementType(lastMovementTypeCode: String) =
-    when (lastMovementTypeCode) {
-      "ADM" -> LastMovementType.ADMISSION
-      "REL" -> LastMovementType.RELEASE
-      "TRN" -> LastMovementType.TRANSFERS
-      "CRT" -> LastMovementType.COURT
-      "TAP" -> LastMovementType.TEMPORARY_ABSENCE
-      else -> null
-    }
+  private fun authenticationHeader(): Map<String, String> {
+    val token = hmppsAuthGateway.getClientToken("Prisoner Base Location")
+
+    return mapOf(
+      "Authorization" to "Bearer $token",
+    )
+  }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/PrisonerBaseLocationIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/PrisonerBaseLocationIntegrationTest.kt
@@ -1,40 +1,26 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
 
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
-import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.prisonerbaselocation.FIXTURES_DIR
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
-import java.io.File
 
 class PrisonerBaseLocationIntegrationTest : IntegrationTestBase() {
-  private val knownNomisNumber = "A1234BC"
-  private val unknownNomisNumnber = "Z9876YX"
-
-  @AfterEach
-  fun resetValidators() {
-    prisonerOffenderSearchMockServer.resetValidator()
-  }
+  private val knownNomisNumber = nomsId
+  private val unknownNomisNumber = "Z9876YX"
 
   @Test
   fun `returns prisoner base location, having hmppsId being nomisID`() {
-    givenKnownPrisoner()
-
     callApi(path = makePathPrisonerBaseLocation(knownNomisNumber))
       .andExpect(status().isOk)
       .andExpect(
         content().json(getExpectedResponse("prisoner-base-location")),
       )
-    prisonerOffenderSearchMockServer.assertValidationPassed()
   }
 
   @Test
   fun `does not return prisoner base location, of an unknown prisoner`() {
-    givenUnknownPrisoner()
-
-    callApi(path = makePathPrisonerBaseLocation(unknownNomisNumnber))
+    callApi(path = makePathPrisonerBaseLocation(unknownNomisNumber))
       .andExpect(status().isNotFound)
   }
 
@@ -57,10 +43,4 @@ class PrisonerBaseLocationIntegrationTest : IntegrationTestBase() {
   }
 
   private fun makePathPrisonerBaseLocation(hmppsId: String) = "$basePath/$hmppsId/prisoner-base-location"
-
-  private fun readFixtures(fileName: String): String = File("$FIXTURES_DIR/$fileName").readText()
-
-  private fun givenKnownPrisoner() = prisonerOffenderSearchMockServer.stubForGet("/prisoner/$knownNomisNumber", readFixtures("PrisonerByIdResponse.json"))
-
-  private fun givenUnknownPrisoner() = prisonerOffenderSearchMockServer.stubForGet("/prisoner/$unknownNomisNumnber", readFixtures("PrisonerByIdNotFoundResponse.json"), HttpStatus.NOT_FOUND)
 }
