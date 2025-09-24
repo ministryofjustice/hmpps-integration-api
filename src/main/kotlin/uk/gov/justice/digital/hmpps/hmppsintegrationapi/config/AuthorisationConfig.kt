e2872862b2c4be47895c71172e4a95570815d033
SteveLeachMoj <steve.leach@justice.gov.uk>
2025-09-24 10:49:26 +0100

    HIA-956 - Implement basic permissions checking tool

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/AuthorisationConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/AuthorisationConfig.kt
@@ -1,13 +1,89 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.config
 
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerConfig
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.roles
+import java.util.*
+
 
 @Configuration
 @Component
 @ConfigurationProperties(prefix = "authorisation")
 class AuthorisationConfig {
   var consumers: Map<String, ConsumerConfig?> = emptyMap()
+}
+
+
+/**
+ * Utility to determine whether a consumer has permissions for an endpoint in a particular environment.
+ */
+class PermissionChecker(val environmentPropertyProvider: EnvironmentPropertyProvider = DefaultEnvironmentPropertyProvider()) {
+
+  /**
+   * Returns true if the user has access to an endpoint in an environment.
+   */
+  fun hasPermission(endpoint: String, environment: String, username: String): Boolean {
+    return consumersWithPermission(endpoint, environment).contains(username)
+  }
+
+  /**
+   * Returns a sorted list of all users with access to an endpoint in an environment.
+   */
+  fun consumersWithPermission(endpoint: String, environment: String): List<String> {
+    val matches = mutableSetOf<String>()
+
+    for ((key, value) in environmentPropertyProvider.getConfig(environment)) {
+      if (grantsAccess(key, value, endpoint)) {
+        matches.add(propertyConsumerName(key))
+      }
+    }
+
+    return matches.toList().sorted()
+  }
+
+  private fun propertyConsumerName(key: String): String {
+    val keyParts = key.split(".")
+    return if (keyParts.size >= 3) keyParts[2] else "{unknown}"
+  }
+
+  private fun grantsAccess(key: String, value: String, endpoint: String): Boolean =
+    isDirectAccess(key, value, endpoint) ||
+    isRoleAccess(key, value, endpoint)
+
+  private fun isRoleAccess(key: String, value: String, endpoint: String): Boolean =
+    isRoleDef(key) && roleCanAccess(value, endpoint)
+
+  private fun isDirectAccess(key: String, value: String, endpoint: String): Boolean =
+    isInclude(key) && value.equals(endpoint)
+
+  private fun isInclude(key: String): Boolean =
+    key.contains(".include[")
+
+  private fun isRoleDef(key: String): Boolean =
+    key.contains(".roles[")
+
+  private fun roleCanAccess(roleName: String, endpoint: String): Boolean =
+    roles[roleName]?.include?.contains(endpoint) == true
+}
+
+/**
+ * Provides access to the properties of an environment (as strings).
+ */
+interface EnvironmentPropertyProvider {
+  fun getConfig(environment: String): Map<String,String>
+}
+
+class DefaultEnvironmentPropertyProvider: EnvironmentPropertyProvider {
+  override fun getConfig(environment: String): Map<String,String> {
+    val yaml = YamlPropertiesFactoryBean()
+    yaml.setResources(ClassPathResource("application-$environment.yml"))
+    return stringProperties(yaml.getObject()!!)
+  }
+
+  private fun stringProperties(properties: Properties): Map<String, String> =
+    properties.map { it.key.toString() to it.value.toString() }.toMap()
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/PermissionCheckerTest.kt
@@ -0,0 +1,72 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.config
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.whenever
+import org.slf4j.LoggerFactory
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.assertTrue
+
+class PermissionCheckerTest {
+  val log = LoggerFactory.getLogger(this.javaClass)
+  val permissionChecker = PermissionChecker()
+
+  @Test
+  fun `has permission`() {
+    assertTrue(permissionChecker.hasPermission("/v1/status", "dev", "smoke-test-full-access"))
+  }
+
+  @Test
+  fun `does not have permission`() {
+    Assertions.assertFalse(permissionChecker.hasPermission("/v9/status", "dev", "smoke-test-full-access"))
+  }
+
+  @Test
+  fun `no matching consumers`() {
+    assertEquals(0, permissionChecker.consumersWithPermission("/v1/badpath", "dev").size)
+  }
+
+  @Test
+  fun `multiple matching consumers`() {
+    for (env in listOf("dev", "preprod", "prod")) {
+      val matches = permissionChecker.consumersWithPermission("/v1/status", env)
+      assertNotEquals(0, matches.size)
+      assertContains(matches, "event-service")
+      assertContains(matches, "kubernetes-health-check-client")
+    }
+  }
+
+  @Test
+  fun `show permission matches`() {
+    val endpoint = "/health/readiness"
+    val environment = "preprod"
+    val matches = permissionChecker.consumersWithPermission(endpoint, "preprod")
+
+    log.info("Consumers with access to {} in {} : {}", endpoint, environment, matches)
+
+    assertEquals(1, matches.size)
+    assertEquals("kubernetes-health-check-client", matches[0])
+  }
+
+  @Test
+  fun `validate core PermissionChecker behaviour`() {
+    val environmentProperties = mapOf(
+      "authorisation.consumers.c1.include[0]" to "/tester",
+      "authorisation.consumers.c2.include[0]" to "/other",
+      "authorisation.consumers.c2.include[1]" to "/tester",
+      "authorisation.consumers.c3.include[1]" to "/other",
+    )
+    val config = mock(EnvironmentPropertyProvider::class.java)
+    whenever(config.getConfig(any())).thenReturn(environmentProperties)
+
+    val matches = PermissionChecker(config) .consumersWithPermission("/tester", "test1")
+    assertEquals(2, matches.size)
+    assertContains(matches, "c1")
+    assertContains(matches, "c2")
+  }
+}
+
