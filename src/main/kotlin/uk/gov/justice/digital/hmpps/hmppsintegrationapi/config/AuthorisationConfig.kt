e0078cc8ddb1aca9cfcf2733b98397280e3bb14b
SteveLeachMoj <steve.leach@justice.gov.uk>
2025-09-25 09:57:32 +0100

    HIA-956 - Implement basic permissions checking tool (#1208)
    * HIA-956 - Implement basic permissions checking tool
    
    * Reformatting
    
    * More formatting changes
    
    * Use Paul's method for loading typed configuration
    
    * Fix formatting
    
    * Minor simplification
    
    * Rewrite consumersWithPermission in functional style
    
    * Move code from PermissionChecker to AuthorisationConfig
    
    * Use regular expressions to match endpoints
    
    * Add tests for regex matching paths
    
    * Remove unnecessary AuthorisationConfigProvider interface
    
    * Remove test code from src/main tree
    
    * Tidy up tests slightly
    
    * More tidy up
    
    * Make logger declaration follow house style

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/AuthorisationConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/AuthorisationConfig.kt
@@ -4,10 +4,49 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerConfig
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.roles
 
 @Configuration
 @Component
 @ConfigurationProperties(prefix = "authorisation")
 class AuthorisationConfig {
   var consumers: Map<String, ConsumerConfig?> = emptyMap()
+
+  /**
+   * Returns true if the consumer has access to the endpoint.
+   */
+  fun hasAccess(
+    consumerName: String,
+    endpoint: String,
+  ): Boolean = anyMatch(allIncludes(consumerName), endpoint)
+
+  /**
+   * Returns a list of consumers with access to a particular endpoint.
+   */
+  fun consumersWithAccess(endpoint: String): List<String> =
+    consumers
+      .filter { hasAccess(it.key, endpoint) }
+      .map { it.key }
+      .toList()
+      .sorted()
+
+  /**
+   * Returns a list of all endpoint permissions for a consumer, whether direct or from roles.
+   */
+  fun allIncludes(consumerName: String): List<String> {
+    val merged = mutableSetOf<String>()
+    merged.addAll(consumers[consumerName]?.include.orEmpty())
+    for (roleName in consumers[consumerName]?.roles ?: emptyList()) {
+      merged.addAll(roles[roleName]?.include.orEmpty())
+    }
+    return merged.toList().sorted()
+  }
+
+  /**
+   * Returns true if the endpoint matches any of the patterns.
+   */
+  private fun anyMatch(
+    patterns: List<String>?,
+    endpoint: String,
+  ): Boolean = patterns != null && patterns.any { Regex(it).matches(endpoint) }
 }
