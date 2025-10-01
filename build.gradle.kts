77534be8363b5b1489bcabc9a22f343f0da253ae
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2025-10-01 12:38:08 +0100

    HIA-855: Add GHA testing and coverage action (#1160)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import kotlinx.kover.gradle.plugin.dsl.tasks.KoverReport
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.1"
   kotlin("plugin.spring") version "2.2.20"
+  id("io.gitlab.arturbosch.detekt") version "1.23.8"
+  id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }
 
 configurations {
@@ -35,6 +38,7 @@ dependencies {
   testImplementation("io.kotest:kotest-runner-junit5-jvm:6.0.3")
   testImplementation("io.kotest:kotest-assertions-core-jvm:6.0.3")
   testImplementation("io.kotest:kotest-extensions-spring:6.0.3")
+  implementation("org.jetbrains.kotlinx:kover-cli:0.9.1")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("org.mockito:mockito-core:5.20.0")
   testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.0")
@@ -55,7 +59,6 @@ dependencies {
   testImplementation("org.eclipse.jetty:jetty-server:12.1.1")
   testImplementation("org.eclipse.jetty:jetty-http:12.1.1")
   testImplementation("org.eclipse.jetty:jetty-io:12.1.1")
-
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   testImplementation(kotlin("test"))
   testImplementation("io.mockk:mockk:1.14.5")
@@ -70,6 +73,55 @@ repositories {
 }
 
 tasks {
+  // Enables the coverage report to be created for only unit tests or integration tests
+  // This is so the unit and integration tests can be run in parallel
+  // Could have just called
+  withType<KoverReport>().configureEach {
+    val environment = System.getenv()
+    val testType = environment["TEST_TYPE"] ?: "UNIT"
+    val excluded = if (testType == "UNIT") "integrationTest" else "unitTest"
+    kover {
+      currentProject {
+        instrumentation {
+          disabledForTestTasks.add(excluded)
+          disabledForTestTasks.add("test")
+        }
+      }
+    }
+  }
+
+  val mergeCoverageReport by registering(JavaExec::class) {
+    classpath = files("lib/kover-cli.jar")
+    args =
+      listOf(
+        "merge",
+        "lib/unitTest.ic",
+        "lib/integrationTest.ic",
+        "--target=lib/merged.ic",
+      )
+  }
+
+  val createCoverageReport by registering(JavaExec::class) {
+    classpath = files("lib/kover-cli.jar")
+    args =
+      listOf(
+        "report",
+        "lib/merged.ic",
+        "--src=src/main",
+        "--classfiles=build/classes/kotlin/main",
+        "--html=coverage",
+        "--xml=coverage/report.xml",
+      )
+  }
+
+  val koverCli by registering(Copy::class) {
+    from(configurations.runtimeClasspath).include("kover-cli*.jar")
+    into("lib")
+    rename("(.*).jar", "kover-cli.jar")
+  }
+  getByName("mergeCoverageReport").dependsOn(koverCli)
+  getByName("createCoverageReport").dependsOn(mergeCoverageReport)
+
   register<Test>("unitTest") {
     group = "verification"
     filter {
@@ -97,10 +149,27 @@ tasks {
   withType<Test> {
     systemProperty("kotest.framework.config.fqn", "uk.gov.justice.digital.hmpps.hmppsintegrationapi.kotest.ProjectConfig")
   }
+
+  withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    source = source.asFileTree
+  }
+
+  getByName("check") {
+    dependsOn(":ktlintCheck", "detekt")
+  }
+}
+
+detekt {
+  config.setFrom("./detekt.yml")
+  buildUponDefaultConfig = true
+  ignoreFailures = true
+  baseline = file("./detekt-baseline.xml")
 }
 
 kotlin {
   kotlinDaemonJvmArgs = listOf("-Xmx2g")
+
+  configurations.runtimeClasspath.get().resolvedConfiguration
 }
 
 testlogger {
@@ -110,4 +179,15 @@ testlogger {
 // this is to address JLLeitschuh/ktlint-gradle#809
 ktlint {
   version = "1.5.0"
+}
+
+configurations.matching { it.name == "detekt" }.all {
+  resolutionStrategy.eachDependency {
+    if (requested.group == "org.jetbrains.kotlin") {
+      useVersion(
+        io.gitlab.arturbosch.detekt
+          .getSupportedKotlinVersion(),
+      )
+    }
+  }
 }
