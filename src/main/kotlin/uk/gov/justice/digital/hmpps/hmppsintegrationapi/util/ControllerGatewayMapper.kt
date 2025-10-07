6cea9132ab412f8bc93c6db9ad737c3d714dc3ef
SteveLeachMoj <steve.leach@justice.gov.uk>
2025-10-07 14:22:15 +0100

    Fix parameter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/ControllerGatewayMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/util/ControllerGatewayMapper.kt
@@ -14,16 +14,10 @@ class ControllerGatewayMapper {
     const val GATEWAYS = "gateways"
   }
 
-  /**
-   * Returns a map of controllers (ExtAPI endpoints) to gateways (upstream endpoints).
-   *
-   * This identifies the upstream service gateways used by each downstream API endpoint.
-   */
-
   private fun getParamList(clazz: KClass<*>): Set<KClass<*>> =
     clazz
       .takeIf {
-        isInPackage(clazz)
+        isInPackage(clazz, PACKAGE_NAME)
       }?.constructors
       ?.map { c ->
         c.parameters
@@ -34,20 +28,19 @@ class ControllerGatewayMapper {
       .flatten()
       .toSet()
 
-  private fun isInPackage(clazz: KClass<*>): Boolean = clazz.qualifiedName?.contains(PACKAGE_NAME) == true
+  private fun isInPackage(clazz: KClass<*>, packagePath: String): Boolean =
+    clazz.qualifiedName?.contains(packagePath) == true
 
   private fun toName(clazz: KClass<*>): String = clazz.javaObjectType.name.replace("$PACKAGE_NAME.", "")
 
+  private fun associations(clazz: KClass<*>) =
+    getParamList(clazz).filter { isInPackage(it, "$PACKAGE_NAME.$GATEWAYS") }.map { toName(it) }.toSet()
+
   fun getControllerGatewayMapping(context: ApplicationContext): Map<String, Set<String>> =
-    context
-      .getBeansWithAnnotation(RestController::class.java)
+    context.getBeansWithAnnotation(RestController::class.java)
       .values
-      .filter { isInPackage(ClassUtils.getUserClass(it).kotlin) }
-      .associate {
-        toName(ClassUtils.getUserClass(it).kotlin) to
-          getParamList(it::class)
-            .filter { it.qualifiedName?.contains("$PACKAGE_NAME.$GATEWAYS") == true }
-            .map { toName(it) }
-            .toSet()
-      }
+      .map { ClassUtils.getUserClass(it).kotlin }
+      .filter { isInPackage(it, PACKAGE_NAME) }
+      .associate { toName(it) to associations(it) }
+
 }
