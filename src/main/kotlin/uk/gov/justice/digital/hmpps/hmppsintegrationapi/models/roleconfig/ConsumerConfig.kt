05d36fa8e38b843e5ea17bad0bb9b06f80b7cc72
SteveLeachMoj <steve.leach@justice.gov.uk>
2025-10-29 16:01:30 +0000

    Add optional notes field to ConsumerConfig

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/roleconfig/ConsumerConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/roleconfig/ConsumerConfig.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig
 
 data class ConsumerConfig(
+  val notes: String?,
   val include: List<String>?,
   val filters: ConsumerFilters?,
   val roles: List<String>?,
