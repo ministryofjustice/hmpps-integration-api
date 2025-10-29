7061f9bfc13c1219b6ec9a10804c671a4f58c1e1
SteveLeachMoj <steve.leach@justice.gov.uk>
2025-10-29 16:20:06 +0000

    Default values for ConsumerConfig fields

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/roleconfig/ConsumerConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/roleconfig/ConsumerConfig.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig
 
 data class ConsumerConfig(
-  val notes: String?,
-  val include: List<String>?,
-  val filters: ConsumerFilters?,
-  val roles: List<String>?,
+  val include: List<String>? = null,
+  val filters: ConsumerFilters? = null,
+  val roles: List<String>? = null,
+  val notes: String? = null,
 )
