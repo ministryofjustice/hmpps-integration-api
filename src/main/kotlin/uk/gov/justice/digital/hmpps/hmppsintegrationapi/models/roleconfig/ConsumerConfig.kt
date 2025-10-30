018658100b73310a97e0832872bba1ff21ba3a2d
SteveLeachMoj <steve.leach@justice.gov.uk>
2025-10-30 13:52:02 +0000

    Add optional notes field to ConsumerConfig (#1289)
    * Add optional notes field to ConsumerConfig
    
    * Default values for ConsumerConfig fields

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/roleconfig/ConsumerConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/roleconfig/ConsumerConfig.kt
@@ -1,7 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig
 
 data class ConsumerConfig(
-  val include: List<String>?,
-  val filters: ConsumerFilters?,
-  val roles: List<String>?,
+  val include: List<String>? = null,
+  val filters: ConsumerFilters? = null,
+  val roles: List<String>? = null,
+  val notes: String? = null,
 )
