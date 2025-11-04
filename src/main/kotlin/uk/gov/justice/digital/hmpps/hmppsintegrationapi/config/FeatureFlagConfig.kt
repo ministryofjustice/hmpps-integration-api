8777c968c9ce660fc7db2cae2103e6f7d9ad9f87
Rick Choi <rick.choi@justice.gov.uk>
2025-11-04 17:42:52 +0000

    [ESWE-1502] Remove feature flags of redaction policy (#1303)
    removed feature flags
    - `redaction-policy-enabled`
    - `use-roles-dsl`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfig.kt
@@ -34,7 +34,6 @@ data class FeatureFlagConfig(
     const val SIMPLE_REDACTION = "simple-redaction"
     const val USE_CONTACT_EVENTS_ENDPOINT = "use-contact-events-endpoint"
     const val EPF_ENDPOINT_INCLUDES_LAO = "epf-endpoint-includes-lao"
-    const val REDACTION_POLICY_ENABLED = "redaction-policy-enabled"
     const val EPF_GATEWAY_DISABLED = "epf-gateway-disabled"
     const val CPR_ENABLED = "cpr-enabled"
   }
