6706b23b29784470d8e6cef845a8f7742e84c9ee
Rick Choi <rick.choi@justice.gov.uk>
2025-11-07 11:34:06 +0000

    [ESWE-1465] Remove feature `simple-redaction` and the feature flag (#1302)
    - removed feature simple-redaction
    - removed feature flag `simple-redaction`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/HmppsIntegrationApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/HmppsIntegrationApi.kt
@@ -4,10 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.FeatureFlagConfig
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.RedactionConfig
 
 @SpringBootApplication
-@EnableConfigurationProperties(RedactionConfig::class, FeatureFlagConfig::class)
+@EnableConfigurationProperties(FeatureFlagConfig::class)
 class HmppsIntegrationApi
 
 fun main(args: Array<String>) {
