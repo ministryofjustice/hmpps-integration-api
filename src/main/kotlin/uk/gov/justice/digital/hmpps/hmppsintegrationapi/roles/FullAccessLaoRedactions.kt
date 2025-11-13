263a8aa2c75bbfc125c1d2d0c33c983f8e537d16
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2025-11-13 13:53:17 +0000

    HIA-1016: Added full access with lao redactions role for testing (#1346)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/FullAccessLaoRedactions.kt
@@ -0,0 +1,25 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles
+
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.roleConstants
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.redaction.laoRedactionPolicy
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles.dsl.role
+
+val fullAccessLaoRedactions =
+  role("full-access-lao-redactions") {
+    permissions {
+      -roleConstants.allEndpoints
+    }
+    filters {
+      prisons {
+        -"*"
+      }
+      caseNotes {
+        -"*"
+      }
+    }
+    redactionPolicies(
+      listOf(
+        laoRedactionPolicy,
+      ),
+    )
+  }
