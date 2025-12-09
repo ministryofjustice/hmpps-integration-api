d213b55e06eeb24acb0c25152a7b7f50d64bf709
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2025-12-09 11:52:11 +0000

    HIA-1134: Add lao rejection for sentences endpoint (#1407)
    Co-authored-by: Paul McPhee <paul.mcphee@justice.gov.uk>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/SentencesIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/person/SentencesIntegrationTest.kt
@@ -1,13 +1,38 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.person
 
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.roles
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles.testRoleWithLaoRedactions
+import java.io.File
 
+@TestPropertySource(properties = ["services.ndelius.base-url=http://localhost:4201"])
 class SentencesIntegrationTest : IntegrationTestBase() {
-  final var path = "$basePath/$nomsId/sentences"
+  final var path = "$basePath/$crn/sentences"
   final var invalidHmppsIdPath = "$basePath/INVALID/sentences"
+
+  @BeforeEach
+  fun setUp() {
+    nDeliusMockServer.stubForGet(
+      "/case/$crn/supervisions",
+      File(
+        "src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/ndelius/fixtures/GetSupervisionsResponse.json",
+      ).readText(),
+    )
+  }
+
+  @AfterEach
+  fun tearDown() {
+    unmockkStatic("uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.RoleKt")
+  }
 
   @Test
   fun `returns sentences for a person`() {
@@ -57,5 +82,24 @@ class SentencesIntegrationTest : IntegrationTestBase() {
   fun `return a 404 for prisoner in wrong prison on latest sentence key dates and adjustments`() {
     callApiWithCN("$path/latest-key-dates-and-adjustments", limitedPrisonsCn)
       .andExpect(status().isNotFound)
+  }
+
+  @Test
+  fun `sentences returns forbidden for an LAO person when redaction policy is present`() {
+    setToLao()
+    mockkStatic("uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.RoleKt")
+    every { roles[any()] } returns testRoleWithLaoRedactions
+    callApi(path)
+      .andExpect(status().isForbidden)
+  }
+
+  @Test
+  fun `returns latest sentence key dates and adjustments for an LAO person when redaction policy is present`() {
+    setToLao()
+    mockkStatic("uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.RoleKt")
+    every { roles[any()] } returns testRoleWithLaoRedactions
+    callApi("$path/latest-key-dates-and-adjustments")
+      .andExpect(status().isOk)
+      .andExpect(content().json(getExpectedResponse("person-sentence-key-dates")))
   }
 }
