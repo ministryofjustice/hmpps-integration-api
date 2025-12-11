01971f6d366543b33e19aa278565efd2cce7cf67
Paul McPhee <paul.mcphee@justice.gov.uk>
2025-12-11 16:54:50 +0000

    HIA-1118: Set autoconfigure exclusion in yaml

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/HmppsIntegrationApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/HmppsIntegrationApi.kt
@@ -1,12 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.FeatureFlagConfig
 
-@SpringBootApplication(exclude = [FreeMarkerAutoConfiguration::class])
+@SpringBootApplication
 @EnableConfigurationProperties(FeatureFlagConfig::class)
 class HmppsIntegrationApi
 
