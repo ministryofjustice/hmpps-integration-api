73e32c9fede2f4444d8ea99193f84b3b78f4c10f
Paul McPhee <paul.mcphee@justice.gov.uk>
2025-12-18 15:52:51 +0000

    HIA-1118: Add a cache log filter and make get prison offender cacheable

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/CacheIntegrationTest.kt
@@ -0,0 +1,67 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration
+
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.caffeine.CaffeineCache
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.PrisonerOffenderSearchGateway
+import kotlin.test.assertEquals
+
+@TestPropertySource(properties = ["feature-flag.gateway-cache-enabled=true"])
+class CacheIntegrationTest : IntegrationTestBase() {
+  private final val hmppsId = "G2996UX"
+  private final val path = "/v1/persons/$hmppsId/addresses"
+
+  @Autowired
+  lateinit var cache: CaffeineCache
+
+  @MockitoSpyBean
+  private lateinit var prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway
+
+  @Test
+  fun `caches prisoner data when addresses endpoint called twice and feature enabled`() {
+    // Request 1
+    callApiWithCN(path, specificPrisonCn)
+      .andExpect(status().isOk)
+
+    // Reqyest 2
+    callApiWithCN(path, specificPrisonCn)
+      .andExpect(status().isOk)
+
+    // Calls the cached method only once
+    verify(prisonerOffenderSearchGateway, times(1)).getPrisonOffender(hmppsId)
+
+    // 1 cache miss for the first request
+    assertEquals(cache.nativeCache.stats().missCount(), 1L)
+
+    // 1 cache hit for the second request
+    assertEquals(cache.nativeCache.stats().hitCount(), 1L)
+  }
+}
+
+@TestPropertySource(properties = ["feature-flag.gateway-cache-enabled=false"])
+class CacheDisabledIntegrationTest : IntegrationTestBase() {
+  private final val hmppsId = "G2996UX"
+  private final val path = "/v1/persons/$hmppsId/addresses"
+
+  @MockitoSpyBean
+  private lateinit var prisonerOffenderSearchGateway: PrisonerOffenderSearchGateway
+
+  @Test
+  fun `does not cache prisoner data when addresses endpoint called twice and feature disabled`() {
+    // Request 1
+    callApiWithCN(path, specificPrisonCn)
+      .andExpect(status().isOk)
+
+    // Reqyest 2
+    callApiWithCN(path, specificPrisonCn)
+      .andExpect(status().isOk)
+
+    // Calls the cacheable method twice (does not cache)
+    verify(prisonerOffenderSearchGateway, times(2)).getPrisonOffender(hmppsId)
+  }
+}
