6e99b1d73f8ed2b5d01ac9354dfe01943f9744be
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-01-09 18:42:40 +0000

    HIA-1187 Unit test for new matching code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/UrlMatching.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/UrlMatching.kt
@@ -10,23 +10,22 @@ fun String.normalisePathPlaceholder(placeholder: String) = replace(placeholder, 
 
 fun normalisePath(pathPattern: String): String = pathPattern
 
-// fun normalisePath(pathPattern: String): String {
-//  return pathPattern
-//    .normalisePathPlaceholder("{hmppsId}")
-//    .normalisePathPlaceholder("{prisonId}")
-//    .normalisePathPlaceholder("{contactId}")
-//    .normalisePathPlaceholder("{visitReference}")
-//    .normalisePathPlaceholder("{scheduleId}")
-//    .normalisePathPlaceholder("{accountCode}")
-//    .normalisePathPlaceholder("{clientVisitReference}")
-//    .normalisePathPlaceholder("{imageId}")
-//    .normalisePathPlaceholder("{clientUniqueRef}")
-//    .normalisePathPlaceholder("{key}")
-//    .normalisePathPlaceholder("{id}")
-//    .normalisePathPlaceholder("[^/]*")
-//    .normalisePathPlaceholder("[^/]+")
-//    .normalisePathPlaceholder(".*")
-//    .removePrefix("^")
-//    .removeSuffix("$")
-//    .ensurePrefix("/")
-// }
+fun normalisePathFull(pathPattern: String): String =
+  pathPattern
+    .normalisePathPlaceholder("{hmppsId}")
+    .normalisePathPlaceholder("{prisonId}")
+    .normalisePathPlaceholder("{contactId}")
+    .normalisePathPlaceholder("{visitReference}")
+    .normalisePathPlaceholder("{scheduleId}")
+    .normalisePathPlaceholder("{accountCode}")
+    .normalisePathPlaceholder("{clientVisitReference}")
+    .normalisePathPlaceholder("{imageId}")
+    .normalisePathPlaceholder("{clientUniqueRef}")
+    .normalisePathPlaceholder("{key}")
+    .normalisePathPlaceholder("{id}")
+    .normalisePathPlaceholder("[^/]*")
+    .normalisePathPlaceholder("[^/]+")
+    .normalisePathPlaceholder(".*")
+    .removePrefix("^")
+    .removeSuffix("$")
+    .ensurePrefix("/")
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/UriMatchingTest.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
+
+import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+
+class UriMatchingTest {
+  fun assertAllEqual(vararg strings: String) {
+    strings.forEach { s1 ->
+      strings.forEach { s2 ->
+        s2 shouldBe s1
+      }
+    }
+  }
+
+  @Test
+  fun `test uri matching`() {
+    assertAllEqual(
+      "/v1/persons/${DEFAULT_PATH_PLACEHOLDER}",
+      normalisePathFull("/v1/persons/{hmppsId}"),
+      normalisePathFull("/v1/persons/.*"),
+    )
+  }
+}
