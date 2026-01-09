9953417ccf899ed10aa49a667e3b2b5806098908
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-01-09 18:28:01 +0000

    HIA-1187 start preparing for normalised path template matching

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/UrlMatching.kt
@@ -0,0 +1,32 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
+
+// Keep this in sync with the copy in hmpps-integration-events until the code bases are merged
+
+const val DEFAULT_PATH_PLACEHOLDER = "[a-zA-Z0-9_-]+"
+
+fun String.ensurePrefix(prefix: String) = if (startsWith(prefix)) this else "$prefix$this"
+
+fun String.normalisePathPlaceholder(placeholder: String) = replace(placeholder, DEFAULT_PATH_PLACEHOLDER)
+
+fun normalisePath(pathPattern: String): String = pathPattern
+
+// fun normalisePath(pathPattern: String): String {
+//  return pathPattern
+//    .normalisePathPlaceholder("{hmppsId}")
+//    .normalisePathPlaceholder("{prisonId}")
+//    .normalisePathPlaceholder("{contactId}")
+//    .normalisePathPlaceholder("{visitReference}")
+//    .normalisePathPlaceholder("{scheduleId}")
+//    .normalisePathPlaceholder("{accountCode}")
+//    .normalisePathPlaceholder("{clientVisitReference}")
+//    .normalisePathPlaceholder("{imageId}")
+//    .normalisePathPlaceholder("{clientUniqueRef}")
+//    .normalisePathPlaceholder("{key}")
+//    .normalisePathPlaceholder("{id}")
+//    .normalisePathPlaceholder("[^/]*")
+//    .normalisePathPlaceholder("[^/]+")
+//    .normalisePathPlaceholder(".*")
+//    .removePrefix("^")
+//    .removeSuffix("$")
+//    .ensurePrefix("/")
+// }
