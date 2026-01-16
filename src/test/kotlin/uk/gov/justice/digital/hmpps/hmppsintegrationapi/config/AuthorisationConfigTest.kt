3085ff706c1b275c63e261d8e8a2b8c39dde552e
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-01-16 09:44:11 +0000

    HIA-1191 Remove unused import

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/AuthorisationConfigTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/AuthorisationConfigTest.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.config
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.normalisePath
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.roleconfig.ConsumerFilters.Companion.NO_FILTERS
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles.dsl.role
