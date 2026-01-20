eca3ed724723aaec821efa31cfaa9ba26d79d510
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-01-20 11:11:00 +0000

    HIA-1175: Created new dataclass to handle v2 predictor outputs from new url

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/assessRisksAndNeeds/ArnRiskPredictorScore.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/assessRisksAndNeeds/ArnRiskPredictorScore.kt
@@ -23,3 +23,30 @@ data class ArnRiskPredictorScore(
       sexualPredictor = this.sexualPredictorScore.toSexualPredictor(),
     )
 }
+
+data class ArnOutput(
+  val generalPredictorScore: ArnGeneralPredictorScore = ArnGeneralPredictorScore(),
+  val violencePredictorScore: ArnViolencePredictorScore = ArnViolencePredictorScore(),
+  val groupReconvictionScore: ArnGroupReconvictionScore = ArnGroupReconvictionScore(),
+  val riskOfSeriousRecidivismScore: ArnRiskOfSeriousRecidivismScore = ArnRiskOfSeriousRecidivismScore(),
+  val sexualPredictorScore: ArnSexualPredictorScore = ArnSexualPredictorScore(),
+)
+
+data class ArnRiskPredictorScoreV2(
+  val completedDate: String? = null,
+  val status: String? = null,
+  val source: String? = null,
+  val outputVersion: String? = null,
+  val output: ArnOutput,
+) {
+  fun toRiskPredictorScore(): RiskPredictorScore =
+    RiskPredictorScore(
+      completedDate = if (!this.completedDate.isNullOrEmpty()) LocalDateTime.parse(this.completedDate) else null,
+      assessmentStatus = this.status,
+      generalPredictor = this.output.generalPredictorScore.toGeneralPredictor(),
+      violencePredictor = this.output.violencePredictorScore.toViolencePredictor(),
+      groupReconviction = this.output.groupReconvictionScore.toGroupReconviction(),
+      riskOfSeriousRecidivism = this.output.riskOfSeriousRecidivismScore.toRiskOfSeriousRecidivism(),
+      sexualPredictor = this.output.sexualPredictorScore.toSexualPredictor(),
+    )
+}
