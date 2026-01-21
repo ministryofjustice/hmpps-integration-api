e084078bda39474586db9740b42e2afc7823a7cf
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-01-21 13:17:28 +0000

    HIA-1176: Added predictor models for new V2 values

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/RiskPredictorScore.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/RiskPredictorScore.kt
@@ -22,3 +22,24 @@ data class RiskPredictorScore(
   val riskOfSeriousRecidivism: RiskOfSeriousRecidivism = RiskOfSeriousRecidivism(),
   val sexualPredictor: SexualPredictor = SexualPredictor(),
 )
+
+data class RiskPredictorScoreV2(
+  @Schema(description = "Risk scores calculation completion date", example = "2023-09-05T10:15:41")
+  val completedDate: LocalDateTime? = null,
+  @Schema(
+    description = """
+      Whether the risk score calculation is complete. Possible values are:
+      `COMPLETE`,
+      `LOCKED_INCOMPLETE`
+    """,
+    example = "COMPLETED",
+    allowableValues = ["COMPLETED", "LOCKED_INCOMPLETE"],
+  )
+  val assessmentStatus: String? = null,
+  val allReoffendingPredictor: AllReoffendingPredictor = AllReoffendingPredictor(),
+  val violentReoffendingPredictor: ViolentReoffendingPredictor = ViolentReoffendingPredictor(),
+  val seriousViolentReoffendingPredictor: SeriousViolentReoffendingPredictor = SeriousViolentReoffendingPredictor(),
+  val directContactSexualReoffendingPredictor: DirectContactSexualReoffendingPredictor = DirectContactSexualReoffendingPredictor(),
+  val indirectImageContactSexualReoffendingPredictor: IndirectImageContactSexualReoffendingPredictor = IndirectImageContactSexualReoffendingPredictor(),
+  val combinedSeriousReoffendingPredictor: CombinedSeriousReoffendingPredictor = CombinedSeriousReoffendingPredictor(),
+)
