988a39a3d41d7b53c79293458d58ae85386a145b
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-02-10 13:12:23 +0000

    HIA-1026: Reverted cache test changes

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/CacheIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/CacheIntegrationTest.kt
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cache.caffeine.CaffeineCache
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import kotlin.test.assertTrue
 
 class CacheIntegrationTest : IntegrationTestBase() {
   private final val nomsPath = "/v1/persons/$nomsId"
@@ -34,13 +33,6 @@ class CacheIntegrationTest : IntegrationTestBase() {
 
     // Calls the cached CPR method only once
     verify(corePersonRecordGateway, times(1)).corePersonRecordFor(any(), eq(nomsId))
-
-    // Address endpoint calls CPR twice per request. One for nomis and one for crn
-    // 2 cache misses for the first request
-    assertTrue { cache.nativeCache.stats().missCount() >= 2L }
-
-    // 4 cache hits after the second request
-    assertTrue { cache.nativeCache.stats().hitCount() >= 4L }
   }
 
   @Test
