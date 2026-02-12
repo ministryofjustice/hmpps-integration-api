98735a05f5b602a619ee070e481b1d2a373c4f9f
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-02-12 09:31:33 +0000

    HIA-1260 Use wrapper in tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/queues/TestQueue.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/queues/TestQueue.kt
@@ -2,9 +2,9 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.queues
 
 class TestQueue(val id: String) : Queue {
   val messages = mutableListOf<String>()
-  override fun getId() = id
 
-  override fun getName() = "Queue $id"
+  override fun queueId() = id
+  override fun queueName() = "Queue $id"
 
   override fun sendMessage(eventType: String, event: String) {
     messages.add(event)
