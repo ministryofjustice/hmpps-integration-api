87534e09d41b22fc50a3253c2994fa037a5ee392
Dan Benton <113102670+danbenton-mojdt@users.noreply.github.com>
2026-02-26 10:56:10 +0000

    MAPB-273 MAPB-210 PECS role: Add addresses, iep-level (#1568)
    * MAPB-273 PECS role: Add addresses, alphabetise permissions
    
    * MAPB-210 PECS role: Add iep-level

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/PrisonerEscortCustodyService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/PrisonerEscortCustodyService.kt
@@ -6,10 +6,12 @@ val prisonerEscortCustodyService =
   role("prisoner-escort-custody-service") {
     permissions {
       -"/v1/status"
+      -"/v1/persons/.*/addresses"
       -"/v1/persons/.*/case-notes"
-      -"/v1/persons/.*/languages"
       -"/v1/persons/.*/emergency-contacts"
       -"/v1/persons/.*/health-and-diet"
+      -"/v1/persons/.*/iep-level"
+      -"/v1/persons/.*/languages"
       -"/v1/persons/.*/protected-characteristics"
     }
     filters {
