6f92a91ddc589c057079734ddb8d91fe3b3d7836
Dan Benton <daniel.benton@justice.gov.uk>
2026-02-26 09:27:31 +0000

    MAPB-273 PECS role: Add addresses, alphabetise permissions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/PrisonerEscortCustodyService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/PrisonerEscortCustodyService.kt
@@ -6,10 +6,11 @@ val prisonerEscortCustodyService =
   role("prisoner-escort-custody-service") {
     permissions {
       -"/v1/status"
+      -"/v1/persons/.*/addresses"
       -"/v1/persons/.*/case-notes"
-      -"/v1/persons/.*/languages"
       -"/v1/persons/.*/emergency-contacts"
       -"/v1/persons/.*/health-and-diet"
+      -"/v1/persons/.*/languages"
       -"/v1/persons/.*/protected-characteristics"
     }
     filters {
