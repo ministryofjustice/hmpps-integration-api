d4d87da2621fe44fb84a86cd076db1f10d4cfbee
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-02-28 10:46:16 +0000

    HIA-1230: Change main method to be object to see if this helps coverage exclusion

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/subscriptionfilters/SubscriptionConfigGenerator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/subscriptionfilters/SubscriptionConfigGenerator.kt
@@ -1,17 +1,15 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.subscriptionfilters
 
-class SubscriptionConfigGenerator {
-  companion object {
-    /**
-     * Main method used by the Gradle task 'generateFilterPolicies' to generate subscription filter policy files for dev, preprod and prod
-     * Run from the root of the project
-     * ./gradlew generateFilterPolicies
-     */
-    @JvmStatic
-    fun main(args: Array<String>) {
-      val fileManager = FileManager()
-      val generator = SubscriptionFilterPolicyService(FilterPolicyReader(fileManager), FilterPolicyWriter(fileManager), AuthorisationConfigReader(fileManager))
-      generator.generatePolicyFiles(listOf("dev", "preprod", "prod"))
-    }
+/**
+ * Main method used by the Gradle task 'generateFilterPolicies' to generate subscription filter policy files for dev, preprod and prod
+ * Run from the root of the project
+ * ./gradlew generateFilterPolicies
+ */
+object SubscriptionConfigGenerator {
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val fileManager = FileManager()
+    val generator = SubscriptionFilterPolicyService(FilterPolicyReader(fileManager), FilterPolicyWriter(fileManager), AuthorisationConfigReader(fileManager))
+    generator.generatePolicyFiles(listOf("dev", "preprod", "prod"))
   }
 }
