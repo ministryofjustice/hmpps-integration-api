d4a281679949b141d18b953a00eb253cd167c191
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2026-03-13 15:02:24 +0000

    HIA-1295: Onboarding enhanced-reception-checks (#1590)
    * HIA-1295: Onboarding enhanced-reception-checks
    
    * HIA-1295: Added supervision status

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/EnhancedReceptionChecks.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles
+
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles.dsl.role
+
+val enhancedReceptionChecks =
+  role("enhanced-reception-checks") {
+    permissions {
+      -"/v1/persons/{hmppsId}/images"
+      -"/v1/persons/{hmppsId}/images/{id}"
+      -"/v1/status"
+    }
+    filters {
+      supervisionStatuses {
+        -"PRISONS"
+      }
+    }
+  }
