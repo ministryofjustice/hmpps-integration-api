2ff81c75fbf284055ca73434fa894de4e22d3157
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-03-17 19:13:47 +0000

    HIA-1223: Use a Conditional on bean for direct service

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/services/domain/DirectDomainEventService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/services/domain/DirectDomainEventService.kt
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.FeatureFlagConfig
