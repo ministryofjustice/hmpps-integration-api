2b730aad376ae40bc2b12221c82c7790d48f0bd9
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-03-23 09:32:16 +0000

    HIA-1283: Added canonical format to moj-prisoner roles

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/MojPrisonerEducation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/MojPrisonerEducation.kt
@@ -5,11 +5,11 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles.dsl.role
 val mojPrisonerEducation =
   role("moj-prisoner-education") {
     permissions {
-      -"/v1/persons/.*/name"
-      -"/v1/hmpps/id/nomis-number/[^/]*$"
-      -"/v1/hmpps/id/by-nomis-number/[^/]*$"
-      -"/v1/hmpps/id/nomis-number/by-hmpps-id/[^/]*$"
-      -"/v1/persons/.*/cell-location"
+      -"/v1/persons/{hmppsId}/name"
+      -"/v1/hmpps/id/nomis-number/{hmppsId}"
+      -"/v1/hmpps/id/by-nomis-number/{nomisNumber}"
+      -"/v1/hmpps/id/nomis-number/by-hmpps-id/{hmppsId}"
+      -"/v1/persons/{hmppsId}/cell-location"
       -"/v1/status"
     }
   }
