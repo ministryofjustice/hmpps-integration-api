99464a36e0498672b962a8a8a161ea0659003b80
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-03-24 14:19:34 +0000

    HIA-1322: Ignore order when comparing filter policies

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/subscriptionfilters/FilterPolicy.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/subscriptionfilters/FilterPolicy.kt
@@ -3,7 +3,13 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.subscriptionfilt
 data class FilterPolicy(
   val eventType: List<String> = listOf("default"),
   val prisonId: List<String>? = null,
-)
+) {
+  fun isDifferentTo(filterPolicy: FilterPolicy): Boolean {
+    val thisPolicy = FilterPolicy(this.eventType.sorted(), this.prisonId?.sorted())
+    val comparingPolicy = FilterPolicy(filterPolicy.eventType.sorted(), filterPolicy.prisonId?.sorted())
+    return thisPolicy != comparingPolicy
+  }
+}
 
 data class SubscriptionFilterPolicy(
   val filterPolicy: String?,
