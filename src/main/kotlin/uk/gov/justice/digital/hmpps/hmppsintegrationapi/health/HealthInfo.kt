5bb653228bd7328330fc1c42d68f4331d6d55870
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-04-08 15:25:10 +0100

    HIA-1329: Undid dependency changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/health/HealthInfo.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/health/HealthInfo.kt
@@ -1,7 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.health
 
-import org.springframework.boot.health.contributor.Health
-import org.springframework.boot.health.contributor.HealthIndicator
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.info.BuildProperties
 import org.springframework.stereotype.Component
 
