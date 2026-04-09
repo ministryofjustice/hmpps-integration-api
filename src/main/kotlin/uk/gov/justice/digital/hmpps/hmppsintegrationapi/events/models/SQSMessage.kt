1bbaa79fce88f460d98431a9165dfa26454d40bc
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-04-09 12:42:07 +0100

    HIA-1350: Give sqs message sns shape

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/models/SQSMessage.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/models/SQSMessage.kt
@@ -2,6 +2,9 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.models
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.Instant
+import java.util.UUID
+import kotlin.String
 
 /**
  * Message received from SQS Listener
@@ -16,7 +19,17 @@ data class SQSMessage(
 
 /**
  * Message sent to consumer queues
+ * Note. This is in the shape of a message that has resulted from an SNS notification to keep inline with consumers
  */
 data class DirectSQSMessage(
+  @JsonProperty("Type") val type: String = "Notification",
+  @JsonProperty("MessageId") val messageId: String = UUID.randomUUID().toString(),
+  @JsonProperty("TopicArn") val topicArn: String = "",
   @JsonProperty("Message") val message: String,
+  @JsonProperty("Timestamp") val timestamp: String = "${Instant.now()}",
+  @JsonProperty("SignatureVersion") val signatureVersion: String = "",
+  @JsonProperty("Signature") val signature: String = "",
+  @JsonProperty("SigningCertURL") val signingCertURL: String = "",
+  @JsonProperty("UnsubscribeURL") val unsubscribeURL: String = "",
+  @JsonProperty("MessageAttributes") val messageAttributes: SQSMessageAttributes,
 )
