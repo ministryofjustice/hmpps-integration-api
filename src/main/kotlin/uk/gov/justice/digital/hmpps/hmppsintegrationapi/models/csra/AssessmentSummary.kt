f995abe00a6965b4deed95103d43878763a02cd0
Dan Benton <daniel.benton@justice.gov.uk>
2026-04-28 11:08:38 +0100

    MAPB-330 Change AssessmentSummary from java to kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/csra/AssessmentSummary.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/csra/AssessmentSummary.kt
@@ -1,41 +1,29 @@
-package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.csra;
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.csra
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDate
 
-import java.time.LocalDate;
-
-public class AssessmentSummary {
-
+data class AssessmentSummary(
   @Schema(description = "Booking number", example = "123456")
-  private Long bookingId;
-
+  val bookingId: Long,
   @Schema(description = "Sequence number of assessment within booking", example = "1")
-  private Integer assessmentSeq;
-
+  val assessmentSeq: Int,
   @Schema(description = "Offender number (e.g. NOMS Number).", example = "GV09876N")
-  private String offenderNo;
-
+  val offenderNo: String,
   @Schema(description = "Classification code. This will not have a value if the assessment is incomplete or pending", example = "STANDARD")
-  private String classificationCode;
-
+  val classificationCode: String?,
   @Schema(description = "Identifies the type of assessment", example = "CSR")
-  private String assessmentCode;
-
+  val assessmentCode: String,
   @Schema(description = "Indicates whether this is a CSRA assessment")
-  private boolean cellSharingAlertFlag;
-
+  val cellSharingAlertFlag: Boolean,
   @Schema(description = "Date assessment was created", example = "2018-02-11")
-  private LocalDate assessmentDate;
-
+  val assessmentDate: LocalDate,
   @Schema(description = "The assessment creation agency id", example = "MDI")
-  private String assessmentAgencyId;
-
+  val assessmentAgencyId: String?,
   @Schema(description = "Comment from assessor", example = "Comment details")
-  private String assessmentComment;
-
+  val assessmentComment: String?,
   @Schema(description = "Username who made the assessment", example = "NGK33Y")
-  private String assessorUser;
-
+  val assessorUser: String?,
   @Schema(description = "Date of next review", example = "2018-02-11")
-  private LocalDate nextReviewDate;
-}
+  val nextReviewDate: LocalDate?,
+)
