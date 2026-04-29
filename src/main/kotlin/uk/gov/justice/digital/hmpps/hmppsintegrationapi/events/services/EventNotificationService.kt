1417768e827ef4f10279482959de86d30d2be2f2
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-04-29 12:45:01 +0100

    HIA-1354: Removed use of HmppsQueueService from IntegrationEventSqsServiceTests.kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/services/EventNotificationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/services/EventNotificationService.kt
@@ -4,24 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
-import software.amazon.awssdk.services.sns.model.MessageAttributeValue
-import software.amazon.awssdk.services.sns.model.PublishRequest
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.AuthorisationConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.FeatureFlagConfig
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.entities.EventNotification
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.enums.INTEGRATION_EVENT_TOPIC
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.messaging.QueueService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.models.DirectSQSMessage
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.models.EventType
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.models.SQSMessageAttributes
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles.dsl.SupervisionStatus
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.telemetry.TelemetryService
-import uk.gov.justice.hmpps.sqs.HmppsQueueService
 
 @ConditionalOnProperty("feature-flag.${FeatureFlagConfig.ENABLE_PUBLISH_PENDING_EVENTS}", havingValue = "true")
 @Service
 class EventNotificationService(
-  private val topicService: HmppsQueueService,
   private val queueService: QueueService,
   private val objectMapper: ObjectMapper,
   private val authorisationConfig: AuthorisationConfig,
@@ -32,39 +27,6 @@ class EventNotificationService(
   }
 
   fun sendEvent(event: EventNotification) = sendEventToQueue(event)
-
-  fun sendEventToTopic(payload: EventNotification) {
-    val hmppsEventTopic = topicService.findByTopicId(INTEGRATION_EVENT_TOPIC)
-    val topicArn = hmppsEventTopic!!.arn
-    val hmppsEventsTopicSnsClient = hmppsEventTopic.snsClient
-    val messageAttributes =
-      mutableMapOf(
-        "eventType" to
-          MessageAttributeValue
-            .builder()
-            .dataType("String")
-            .stringValue(payload.eventType)
-            .build(),
-      )
-    if (payload.prisonId != null) {
-      messageAttributes["prisonId"] =
-        MessageAttributeValue
-          .builder()
-          .dataType("String")
-          .stringValue(payload.prisonId)
-          .build()
-    }
-    hmppsEventsTopicSnsClient
-      .publish(
-        PublishRequest
-          .builder()
-          .topicArn(topicArn)
-          .message(objectMapper.writeValueAsString(payload))
-          .messageAttributes(messageAttributes)
-          .build(),
-      ).get()
-    log.info("successfully published event ${payload.eventType} to integrationeventtopic. $payload")
-  }
 
   /**
    * Determines which queues are applicable to the event and publishes directly to those queues
