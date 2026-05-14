fb27d0093f61ce5b84ef1e214406a429e3c6282e
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-05-14 09:34:09 +0100

    HIA-1372: Fixed tests not running

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/WebMvcTestConfiguration.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/WebMvcTestConfiguration.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.limitedaccess.GetCaseAccess
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.AuthorisationService
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.OboService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.internal.RoleService
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.telemetry.TelemetryService
 
@@ -30,4 +31,8 @@ class WebMvcTestConfiguration {
   @Bean
   @ConditionalOnMissingBean
   fun authorisationService(): AuthorisationService = AuthorisationService(roleService(), config())
+
+  @Bean
+  @ConditionalOnMissingBean
+  fun oboService(): OboService = OboService()
 }
