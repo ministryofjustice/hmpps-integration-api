6675a795f840c4c94e95ed7b30b74ba06a2bd760
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-05-15 12:08:51 +0100

    HIA-1372: Updated smoke test jwt

--- scripts/K6/smoke-tests.js
+++ scripts/K6/smoke-tests.js
@@ -662,26 +662,23 @@ function verify_headers() {
       'Content-Type': 'application/json',
       'x-api-key': api_key,
 // Test jwt, decoded as follows
-//      {
-//        "header": {
-//          "alg":"HS256"
-//          "typ":"JWT"
-//          "kid":"testKid"
-//        }
-//        "payload":{
-//          "sub":"1234567890"
-//          "name":"John Doe"
-//          "admin":true
-//          "iat":1516239022
-//          "aud":"testAud"
-//          "iss":"testIss"
-//          "nbf":1778083253
-//          "exp":1978087263
-//          "appid":"testId"
-//          "unique_name":"testName"
-//        }
-//      }
-      'X-On-Behalf-Of': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InRlc3RLaWQifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiYXVkIjoidGVzdEF1ZCIsImlzcyI6InRlc3RJc3MiLCJuYmYiOjE3NzgwODMyNTMsImV4cCI6MTk3ODA4NzI2MywiYXBwaWQiOiJ0ZXN0SWQiLCJ1bmlxdWVfbmFtZSI6InRlc3ROYW1lIn0.bppq2M56ruzzxwfEWK408np-22hAJ2vwrHlbHGuWDq0'
+      //      {
+      //        "header": {
+      //          "alg":"none"
+      //          "kid":"testKid"
+      //        }
+      //        "payload":{
+      //          "sub":"1234567890"
+      //          "name":"John Doe"
+      //          "admin":true
+      //          "iat":1516239022
+      //          "aud":"testAud"
+      //          "iss":"testIss"
+      //          "appid":"testId"
+      //          "unique_name":"testName"
+      //        }
+      //      }
+      'X-On-Behalf-Of': "eyJhbGciOiJub25lIiwia2lkIjoidGVzdEtpZCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiYXVkIjoidGVzdEF1ZCIsImlzcyI6InRlc3RJc3MiLCJhcHBpZCI6InRlc3RJZCIsInVuaXF1ZV9uYW1lIjoidGVzdE5hbWUifQ."
     },
   };
 
