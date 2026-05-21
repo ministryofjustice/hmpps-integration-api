9382bcd127c28e1a38c195034186a64a45478b23
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-05-21 19:59:48 +0100

    HIA-1372 Validate JWT with JWKS

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/onbehalfof/JwksOboService.kt
@@ -0,0 +1,57 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.onbehalfof
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Jwks
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.net.URI
+import java.net.URL
+import java.security.Key
+
+class JwksOboService(
+  val jwks: URL = URI.create("https://login.microsoftonline.com/common/discovery/v2.0/keys").toURL(),
+  val usernameClaim: String = "unique_name",
+) : OboService {
+  private companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  val keyCache: MutableMap<String?, Key?> = mutableMapOf()
+
+  override fun extractUsername(token: String): String? {
+    loadJwks()
+
+    val jwt =
+      Jwts
+        .parser()
+        .keyLocator { header -> keyCache[header["kid"]] }
+        .build()
+        .parseSignedClaims(token)
+
+    return jwt?.payload[usernameClaim]?.toString()
+  }
+
+  fun loadJwks() {
+    if (!keyCache.isEmpty()) {
+      log.debug("Using cached JWKS")
+      return
+    }
+
+    val jwkParser = Jwks.parser().build()
+
+    val jsonContent = jwks.openStream().bufferedReader().use { reader ->
+      jacksonObjectMapper().readTree(reader)
+    }
+
+    log.info(jsonContent.toString())
+
+    keyCache.putAll(
+      jsonContent["keys"].associate {
+        it["kid"].asText() to jwkParser.parse(it.toString()).toKey()
+      },
+    )
+
+    log.info("Loaded {} public keys from {}}", keyCache.size, jwks.toString())
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/onbehalfof/JwksOboServiceTest.kt
@@ -0,0 +1,85 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.onbehalfof
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.UnsupportedJwtException
+import io.jsonwebtoken.security.Jwks
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.engine.spec.tempfile
+import io.kotest.matchers.shouldBe
+import java.net.URL
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.PrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.time.Instant
+import java.util.Date
+
+class JwksOboServiceTest :
+  DescribeSpec(
+    {
+      val kid = "ABC-123"
+      val username = "tester1"
+
+      val keyPair = generateKeyPair()
+      val jwksUri = buildJwks(keyPair, kid)
+      val service = JwksOboService(jwksUri)
+
+      it("loads the JWKS") {
+        service.loadJwks()
+      }
+
+      it("parses a JWT using JWKS") {
+        val jwt = makeJwt(kid, username, keyPair.private)!!
+        val jwtUser = service.extractUsername(jwt)
+        jwtUser shouldBe username
+      }
+
+      it("fails if KID not found") {
+        val jwt = makeJwt("XYZ-987", username, keyPair.private)!!
+        shouldThrow<UnsupportedJwtException> {
+          service.extractUsername(jwt)
+        }
+      }
+    },
+  )
+
+private fun makeJwt(
+  kid: String,
+  username: String,
+  signingKey: PrivateKey,
+): String = Jwts
+  .builder()
+  .header()
+  .keyId(kid)
+  .and()
+  .subject(username)
+  .issuedAt(Date.from(Instant.now()))
+  .expiration(Date.from(Instant.now().plusSeconds(3600)))
+  .claim("unique_name", username)
+  .signWith(signingKey)
+  .compact()!!
+
+private fun generateKeyPair(): KeyPair {
+  val keyGenerator = KeyPairGenerator.getInstance("RSA")
+  keyGenerator.initialize(2048)
+  val keyPair = keyGenerator.generateKeyPair()
+  return keyPair!!
+}
+
+private fun DescribeSpec.buildJwks(keyPair: KeyPair, kid: String): URL {
+  val jwk = Jwks.builder().key(keyPair.public as RSAPublicKey).id(kid).algorithm("RS256").build()
+
+  val wrapper = mapOf("keys" to listOf(jwk))
+  val jwksText = ObjectMapper().writeValueAsString(wrapper)
+  println(jwksText)
+  val jwksFile = tempfile("testing", "jwks")
+  jwksFile.deleteOnExit()
+  jwksFile.writeText(jwksText)
+  val uri = jwksFile.toURI().toURL()
+
+  return uri
+}
+
+
