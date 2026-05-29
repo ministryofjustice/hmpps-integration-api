c23e3db097dd586fb5ada2b30b722b410314959a
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-05-29 10:46:07 +0100

    HIA-1401: Create Role for JusticeAI in dev for agreed endpoints

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/PersonLookup.kt
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles
+
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles.dsl.role
+
+val personLookup =
+  role("person-lookup") {
+    permissions {
+      -"/v1/persons"
+      -"/v1/persons/{hmppsId}"
+      -"/v1/persons/{hmppsId}/addresses"
+      -"/v1/persons/{hmppsId}/contacts"
+      -"/v1/persons/{hmppsId}/offences"
+      -"/v1/persons/{hmppsId}/sentences"
+      -"/v1/status"
+    }
+  }
