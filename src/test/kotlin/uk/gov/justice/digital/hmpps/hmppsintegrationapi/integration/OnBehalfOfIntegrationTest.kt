2b45eff225e6588447d9f7052cf3bd715318a845
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2026-06-03 12:28:32 +0100

    HIA-1353: Pass obo username through to the auth get token method (#1797)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/OnBehalfOfIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/OnBehalfOfIntegrationTest.kt
@@ -1,8 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.integration
 
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.atLeast
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.verify
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.RequestContext
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.services.onbehalfof.createUnsignedJwt
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class OnBehalfOfIntegrationTest : IntegrationTestBase() {
   @Test
@@ -51,5 +57,19 @@ class OnBehalfOfIntegrationTest : IntegrationTestBase() {
   fun `if oboConfig is invalid, and a Jwt is provided, return unauthorized`() {
     callApiWithCN("/v1/status", "obo-invalid", oboValue = createUnsignedJwt())
       .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+  }
+
+  @Test
+  fun `an obo username is passed to HmppsAuthGateway for the sentences endpoint`() {
+    callApiWithCN("$basePath/$crn/sentences", "obo-unsigned", oboValue = createUnsignedJwt())
+      .andExpect(MockMvcResultMatchers.status().isOk)
+    val requestContext = argumentCaptor<RequestContext?>()
+    verify(authGateway, atLeast(1)).getClientToken(eq("NOMIS"), requestContext.capture())
+    val obUserName =
+      requestContext.allValues
+        .filter { it?.oboUserName != null }
+        .map { it?.oboUserName }
+        .first()
+    assertEquals("testName", obUserName)
   }
 }
