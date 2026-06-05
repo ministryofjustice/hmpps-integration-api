e2f00ef49f5b7b3de70a0de184dcce9b8e735f9c
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-06-05 18:33:09 +0100

    HIA-1448: Remove gateway cache feature flag

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/CacheIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/integration/CacheIntegrationTest.kt
@@ -45,3 +45,40 @@ class CacheIntegrationTest : IntegrationTestBase() {
     verify(nDeliusGateway, times(1)).getOffender(crn)
   }
 }
+
+class CacheDisabledIntegrationTest : IntegrationTestBase() {
+  private final val nomsPath = "/v1/persons/$nomsId"
+  private final val crnPath = "/v1/persons/$crn"
+  private final val addressPath = "$nomsPath/addresses"
+
+  @Test
+  fun `does not cache prisoner data when addresses endpoint called twice and feature disabled`() {
+    // Request 1
+    callApiWithCN(addressPath, specificPrisonCn)
+      .andExpect(status().isOk)
+
+    // Request 2
+    callApiWithCN(addressPath, specificPrisonCn)
+      .andExpect(status().isOk)
+
+    // Calls the cacheable method twice (does not cache)
+    verify(prisonerOffenderSearchGateway, times(2)).getPrisonOffender(nomsId)
+
+    // Address endpoint calls CPR twice per request. One for nomis and one for crn
+    // Calls the cached CPR method 4 times in total across 2 requests
+    verify(corePersonRecordGateway, times(4)).corePersonRecordFor(any(), eq(nomsId))
+  }
+
+  @Test
+  fun `does not cache offender data when crn endpoint called twice and feature disabled`() {
+    // Request 1
+    callApiWithCN(crnPath, specificPrisonCn)
+      .andExpect(status().isOk)
+
+    // Request 2
+    callApiWithCN(crnPath, specificPrisonCn)
+      .andExpect(status().isOk)
+    // Calls the cacheable method twice (does not cache)
+    verify(nDeliusGateway, times(2)).getOffender(crn)
+  }
+}
