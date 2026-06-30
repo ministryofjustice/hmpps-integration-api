e3f0049382893298f1d374f233ccd5cc8389eed3
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-06-30 13:34:00 +0100

    HIA-1391: Removed the example from deprecated field

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Need.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/models/hmpps/Need.kt
@@ -9,7 +9,7 @@ data class Need(
   val riskOfHarm: Boolean? = null,
   @Schema(description = "Risk of reoffending")
   val riskOfReoffending: Boolean? = null,
-  @Schema(description = "Severity of need", example = "NO_NEED", deprecated = true)
+  @Schema(description = "Severity of need", example = "null", deprecated = true)
   @Deprecated("No longer populated by upstream. This will always be null")
   val severity: String? = null,
 )
