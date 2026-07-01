446c1e739b20eb7105bd096acd5c4f7c9fbe4ddf
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2026-07-01 10:26:07 +0100

    HIA-1534: Add completed assessment filter to ROSH event (#1882)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/listeners/DomainEventsListenerAssessmentSummaryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/listeners/DomainEventsListenerAssessmentSummaryTest.kt
@@ -1,9 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.listeners
 
-import io.mockk.verify
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.entities.EventNotification
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.enums.IntegrationEventType
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.helpers.DomainEvents
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.events.helpers.DomainEvents.ASSESSMENT_SUMMARY_PRODUCED
@@ -23,18 +20,16 @@ class DomainEventsListenerAssessmentSummaryTest : DomainEventsListenerTestCase()
       DomainEvents
         .generateDomainEvent(eventType, message)
 
-    domainEventsListener.onDomainEvent(payload)
-    // Assert
-    val events = mutableListOf<EventNotification>()
-    verify { eventNotificationRepository.insert(capture(events)) }
-
-    assertThat(events.size).isEqualTo(2)
-    assertThat(events.map { it.eventType }).contains(IntegrationEventType.RISK_OF_SERIOUS_HARM_CHANGED.name)
-    assertThat(events.map { it.eventType }).contains(IntegrationEventType.ASSESSMENT_SUMMARY_CHANGE.name)
+    // Act, Assert
+    onDomainEventShouldCreateEventNotification(
+      hmppsEventRawMessage = payload,
+      hmppsId = crn,
+      expectedNotificationType = IntegrationEventType.ASSESSMENT_SUMMARY_CHANGE.toString(),
+    )
   }
 
   @Test
-  fun `will NOT create a assessment summary notification when incorrect status - will only create a RISK_OF_SERIOUS_HARM_CHANGED`() {
+  fun `will NOT create a assessment summary notification when incorrect status`() {
     // Arrange
     val eventType = "assessment.summary.produced"
     val message = ASSESSMENT_SUMMARY_PRODUCED_LOCKED_INCOMPLETE
@@ -44,12 +39,6 @@ class DomainEventsListenerAssessmentSummaryTest : DomainEventsListenerTestCase()
       DomainEvents
         .generateDomainEvent(eventType, message)
 
-    domainEventsListener.onDomainEvent(payload)
-    // Assert
-    val events = mutableListOf<EventNotification>()
-    verify { eventNotificationRepository.insert(capture(events)) }
-
-    assertThat(events.size).isEqualTo(1)
-    assertThat(events.map { it.eventType }).contains(IntegrationEventType.RISK_OF_SERIOUS_HARM_CHANGED.name)
+    onDomainEventShouldNotCreateEventNotification(payload)
   }
 }
