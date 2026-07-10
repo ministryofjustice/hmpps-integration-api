04040fcab627c51d52514c96a1bc7fe3fb4b842d
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-07-10 08:06:02 +0100

    HIA-1293 Add unit test

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/RestApiClientTest.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito.RETURNS_DEEP_STUBS
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.whenever
+import org.springframework.http.HttpStatus
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+
+class RestApiClientTest :
+  DescribeSpec({
+    describe("basic RestApiClient functionality") {
+      fun buildMockClient(responseBody: String?) : WebClient {
+        val webClient = mock(WebClient::class.java, RETURNS_DEEP_STUBS)
+        val requestSpec = mock(WebClient.RequestBodySpec::class.java)
+        val responseSpec = mock(WebClient.ResponseSpec::class.java)
+        val mono: Mono<String> = mock(Mono::class.java) as Mono<String>
+        whenever(webClient.method(any()).uri(anyString()).headers(any())).thenReturn(requestSpec)
+        whenever(requestSpec.retrieve()).thenReturn(responseSpec)
+        whenever(responseSpec.bodyToMono(String::class.java)).thenReturn(mono)
+        whenever(mono.block()).thenReturn(responseBody)
+        return webClient
+      }
+
+      it("should handle a successful GET request") {
+        val webClient = buildMockClient("It works!")
+
+        val options = RestApiOptions(retryAttempts = 0)
+        val client = RestApiClient("TestAPI", "http://localhost:8765", webClient = webClient, defaultOptions = options)
+
+        val response = client.get("/test", String::class)
+
+        response.status shouldBe HttpStatus.OK
+        response.errors.size shouldBe 0
+        response.data shouldBe "It works!"
+      }
+    }
+  })
