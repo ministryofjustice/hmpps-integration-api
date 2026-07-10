b568ff0a4cefd45c6bb913c78a611be48b14bdda
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-07-10 09:34:43 +0100

    HIA-1293 Add negative unit test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/RestApiClientTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/RestApiClientTest.kt
@@ -9,18 +9,27 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.whenever
 import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
 
 class RestApiClientTest :
   DescribeSpec({
     describe("basic RestApiClient functionality") {
-      fun buildMockClient(responseBody: String?): WebClient {
+      fun buildMockClient(responseBody: String?, httpError: Exception? = null, retrySuccess: Boolean = false): WebClient {
         val webClient = mock(WebClient::class.java, RETURNS_DEEP_STUBS)
         val requestSpec = mock(WebClient.RequestBodySpec::class.java)
         val responseSpec = mock(WebClient.ResponseSpec::class.java)
         val mono: Mono<String> = mock(Mono::class.java) as Mono<String>
         whenever(webClient.method(any()).uri(anyString()).headers(any())).thenReturn(requestSpec)
-        whenever(requestSpec.retrieve()).thenReturn(responseSpec)
+        if (httpError == null) {
+          whenever(requestSpec.retrieve()).thenReturn(responseSpec)
+        } else {
+          if (retrySuccess) {
+            whenever(requestSpec.retrieve()).thenThrow(httpError).thenReturn(responseSpec)
+          } else {
+            whenever(requestSpec.retrieve()).thenThrow(httpError)
+          }
+        }
         whenever(responseSpec.bodyToMono(String::class.java)).thenReturn(mono)
         whenever(mono.block()).thenReturn(responseBody)
         return webClient
@@ -37,6 +46,20 @@ class RestApiClientTest :
         response.status shouldBe HttpStatus.OK
         response.errors.size shouldBe 0
         response.data shouldBe "It works!"
+      }
+
+      it("should handle HTTP errors") {
+        val webClient = buildMockClient("It fails", WebClientResponseException(500,"Server error 543", null, null, null))
+
+        val options = RestApiOptions(retryAttempts = 0)
+        val client = RestApiClient("TestAPI", "http://localhost:8765", webClient = webClient, defaultOptions = options)
+
+        val response = client.get("/test", String::class)
+
+        response.status shouldBe HttpStatus.INTERNAL_SERVER_ERROR
+        response.errors.size shouldBe 1
+        response.errors[0].message shouldBe "500 Server error 543"
+        response.data shouldBe null
       }
     }
   })
