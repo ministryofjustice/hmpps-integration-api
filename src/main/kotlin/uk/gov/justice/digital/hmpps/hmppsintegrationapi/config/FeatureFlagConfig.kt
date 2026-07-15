5893a1d739d1728ee75bbddade2ad5836c5c34e0
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-07-15 16:35:44 +0100

    HIA-1293 Start converting SANGateway over to RestApiClient

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfig.kt
@@ -35,6 +35,7 @@ data class FeatureFlagConfig(
     const val CONTACT_LINKED_PRISONERS_ENDPOINT_ENABLED = "contact-linked-prisoners-endpoint-enabled"
     const val PRISON_TIMELINE_ENDPOINT_ENABLED = "prison-timeline-endpoint-enabled"
     const val USE_PROBATION_SEARCH_FOR_PERSON_SEARCH = "use-probation-search-for-person-search"
+    const val SAN_GATEWAY_USES_RESTAPICLIENT = "san-gateway-uses-restapiclient"
 
     // Events feature flags
     const val ENABLE_DELETE_PROCESSED_EVENTS = "enable-delete-processed-events"
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/SANGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/gateways/SANGateway.kt
@@ -3,17 +3,25 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.config.FeatureFlagConfig
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.RestApiClient
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions.WebClientWrapper.WebClientWrapperResponse
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.PlanCreationSchedules
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.PlanReviewSchedules
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApiError
 
 @Component
 class SANGateway(
   @Value("\${services.san.base-url}") baseUrl: String,
+  val features: FeatureFlagConfig = FeatureFlagConfig(),
+  val apiClient: RestApiClient? = null,
 ) : UpstreamGateway {
   override fun metaData() =
     GatewayMetadata(
@@ -32,6 +40,10 @@ class SANGateway(
   lateinit var hmppsAuthGateway: HmppsAuthGateway
 
   fun getPlanCreationSchedules(prisonerNumber: String): Response<PlanCreationSchedules> {
+    if (features.isEnabled(FeatureFlagConfig.SAN_GATEWAY_USES_RESTAPICLIENT)) {
+      return getPlanCreationSchedules2(prisonerNumber)
+    }
+
     val result =
       webClient.request<PlanCreationSchedules>(
         HttpMethod.GET,
@@ -54,6 +66,42 @@ class SANGateway(
       }
     }
   }
+
+  fun getPlanCreationSchedules2(prisonerNumber: String): Response<PlanCreationSchedules> {
+    val result =
+      apiClient().get(
+        "/profile/$prisonerNumber/plan-creation-schedule?includeAllHistory=true",
+        PlanCreationSchedules::class,
+        authenticationHeader(),
+      )
+
+    return if (result.errors.isEmpty()) {
+      Response(data = result.data!!)
+    } else {
+      Response(
+        data = PlanCreationSchedules(listOf()),
+        errors = wrapErrors(result.errors),
+      )
+    }
+  }
+
+  internal fun apiClient() = apiClient ?: RestApiClient(UpstreamApi.SAN.name, "Test API")
+
+  internal fun wrapErrors(errors: List<Exception>): List<UpstreamApiError> = errors.map { mapError(it) }
+
+  internal fun mapError(error: Exception): UpstreamApiError =
+    when (error) {
+      is WebClientResponseException -> UpstreamApiError(UpstreamApi.SAN, mapStatus(error.statusCode), error.message)
+      else -> UpstreamApiError(UpstreamApi.SAN, UpstreamApiError.Type.INTERNAL_SERVER_ERROR, error.message)
+    }
+
+  internal fun mapStatus(status: HttpStatusCode): UpstreamApiError.Type =
+    when (status) {
+      HttpStatus.NOT_FOUND -> UpstreamApiError.Type.ENTITY_NOT_FOUND
+      HttpStatus.BAD_REQUEST -> UpstreamApiError.Type.BAD_REQUEST
+      HttpStatus.FORBIDDEN -> UpstreamApiError.Type.FORBIDDEN
+      else -> UpstreamApiError.Type.INTERNAL_SERVER_ERROR
+    }
 
   fun getReviewSchedules(prisonerNumber: String): Response<PlanReviewSchedules> {
     val result =
