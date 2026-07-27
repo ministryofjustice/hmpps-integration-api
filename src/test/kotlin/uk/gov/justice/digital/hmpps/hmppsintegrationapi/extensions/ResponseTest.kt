cb214b69368c1527f7ed0097d48a9ea3279142dd
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-07-27 14:50:16 +0100

    HIA-1617: Throw webclient exception when not in one of the handled exceptions

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/ResponseTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/extensions/ResponseTest.kt
@@ -3,28 +3,35 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.extensions
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import org.junit.jupiter.api.assertThrows
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.Response
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.hmpps.UpstreamApi
 
 class ResponseTest :
   DescribeSpec({
-    it("should be handle error responses") {
+    it("should handle responses") {
       val errors =
         listOf(
           WebClientResponseException(400, "Wrong", null, null, null),
           WebClientResponseException(403, "Nope", null, null, null),
           WebClientResponseException(404, "Lost it", null, null, null),
-          WebClientResponseException(502, "Bust", null, null, null),
-          RuntimeException("Unknown"),
+          WebClientResponseException(409, "Conflict", null, null, null),
         )
 
       val response = Response.error(UpstreamApi.TEST, errors, "Hello, world!")
 
       response shouldNotBe null
       response.data shouldBe "Hello, world!"
-      response.errors.size shouldBe 5
+      response.errors.size shouldBe 4
       response.errors[0].description shouldBe "400 Wrong"
       response.errors[0].causedBy shouldBe UpstreamApi.TEST
+    }
+
+    it("should throw webclient exception") {
+      val exceptions = WebClientResponseException(500, "Conflict", null, null, null)
+      assertThrows<WebClientResponseException> {
+        Response.error(UpstreamApi.TEST, listOf(exceptions), "Hello, world!")
+      }
     }
   })
