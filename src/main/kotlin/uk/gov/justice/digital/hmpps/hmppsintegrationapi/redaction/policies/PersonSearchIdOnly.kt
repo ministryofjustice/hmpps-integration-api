01089ba89f5b0070e94efb8df85a5d76d612368f
SteveLeachMoj <steve.leach@justice.gov.uk>
2026-08-03 09:59:32 +0100

    HIA-1635 Refactor redactions package

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/redaction/policies/PersonSearchIdOnly.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/redaction/policies/PersonSearchIdOnly.kt
@@ -4,28 +4,28 @@ import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.redactionconfig.R
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.redaction.redactionPolicy
 
 val personSearchIdOnly =
-    redactionPolicy(
-        "person-search-id-only",
-    ) {
-        responseRedactions {
-            jsonPath {
-                endpoints {
-                    -"/v1/persons"
-                }
-                redactions {
-                    -("$..firstName" to REMOVE)
-                    -("$..lastName" to REMOVE)
-                    -("$..middleName" to REMOVE)
-                    -("$..dateOfBirth" to REMOVE)
-                    -("$..gender" to REMOVE)
-                    -("$..ethnicity" to REMOVE)
-                    -("$..aliases" to REMOVE)
-                    -("$..contactDetails" to REMOVE)
-                    -("$..currentRestriction" to REMOVE)
-                    -("$..restrictionMessage" to REMOVE)
-                    -("$..currentExclusion" to REMOVE)
-                    -("$..exclusionMessage" to REMOVE)
-                }
-            }
+  redactionPolicy(
+    "person-search-id-only",
+  ) {
+    responseRedactions {
+      jsonPath {
+        endpoints {
+          -"/v1/persons"
         }
+        redactions {
+          -("$..firstName" to REMOVE)
+          -("$..lastName" to REMOVE)
+          -("$..middleName" to REMOVE)
+          -("$..dateOfBirth" to REMOVE)
+          -("$..gender" to REMOVE)
+          -("$..ethnicity" to REMOVE)
+          -("$..aliases" to REMOVE)
+          -("$..contactDetails" to REMOVE)
+          -("$..currentRestriction" to REMOVE)
+          -("$..restrictionMessage" to REMOVE)
+          -("$..currentExclusion" to REMOVE)
+          -("$..exclusionMessage" to REMOVE)
+        }
+      }
     }
+  }
