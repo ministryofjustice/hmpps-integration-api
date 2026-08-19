2dc9cc177ffa06bb337a236041d52821c5884a1f
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-08-19 12:22:31 +0100

    HIA-1698: Update script to generate UUI for message id

--- scripts/load-messages-to-queue/convertToExtApiMessages.sh
+++ scripts/load-messages-to-queue/convertToExtApiMessages.sh
@@ -8,13 +8,12 @@
 ##"2026-08-14T17:00:04.000000","PERSON_EDUCATION_ASSESSMENTS_CHANGED","v1/persons/A123456/education/assessments",A123456,
 ##
 ## Usage:
-## /convertToMessages.sh -e <ENV> -f <INPUT>
-## e.g ./convertToMessages.sh -e dev -f sqlResults.txt > eventMessages.txt
+## ./convertToExtApiMessages.sh -e <ENV> -f <INPUT>
+## e.g ./convertToExtApiMessages.sh -e dev -f sqlResults.txt > eventMessages.txt
 ## returns
 ## at `eventMessages.txt`
-## {"Type":"Notification","MessageId":"...","TopicArn":"...","Message":"{\"eventId\":166,\"hmppsId\":\"A1234AB\",\"eventType\":\"PRISONER_BASE_LOCATION_CHANGED\",\"prisonId\":\"TRN\",\"url\":\"https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons/A1234AB/prisoner-base-location\",\"lastModifiedDateTime\":\"2017-03-15T15:47:51.000000\"}","Timestamp":"2025-09-24T09:39:24.000Z","SignatureVersion":"1","Signature":"...","SigningCertURL":"...","UnsubscribeURL":"...","MessageAttributes":{"prisonId":{"Type":"String","Value":"TRN"},"eventType":{"Type":"String","Value":"PRISONER_BASE_LOCATION_CHANGED"}}}
-## {"Type":"Notification","MessageId":"...","TopicArn":"...","Message":"{\"eventId\":166,\"hmppsId\":\"A123456\",\"eventType\":\"PERSON_EDUCATION_ASSESSMENTS_CHANGED\",\"prisonId\":\"TRN\",\"url\":\"https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons/A123456/education/assessments\",\"lastModifiedDateTime\":\"2017-03-15T15:47:51.000000\"}","Timestamp":"2025-09-24T09:39:24.000Z","SignatureVersion":"1","Signature":"...","SigningCertURL":"...","UnsubscribeURL":"...","MessageAttributes":{"prisonId":{"Type":"String","Value":"TRN"},"eventType":{"Type":"String","Value":"PERSON_EDUCATION_ASSESSMENTS_CHANGED"}}}
-
+## {"Type":"Notification","MessageId":"f8b6adb8-1762-4b4d-b14c-3e223f5566e4","TopicArn":"","Message":"{\"eventId\":166,\"hmppsId\":\"A1234AB\",\"eventType\":\"PRISONER_BASE_LOCATION_CHANGED\",\"prisonId\":\"LPI\",\"url\":\"https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons/A1234AB/prisoner-base-location\",\"lastModifiedDateTime\":\"2017-03-15T15:47:51.000000\"}","Timestamp":"2025-09-24T09:39:24.000Z","SignatureVersion":"1","Signature":"...","SigningCertURL":"...","UnsubscribeURL":"...","MessageAttributes":{"eventType":{"Type":"String","Value":"PRISONER_BASE_LOCATION_CHANGED"}}}
+## {"Type":"Notification","MessageId":"4d8b9e08-cdf4-49b7-9cdf-5044355c9d19","TopicArn":"","Message":"{\"eventId\":166,\"hmppsId\":\"A123456\",\"eventType\":\"PERSON_EDUCATION_ASSESSMENTS_CHANGED\",\"url\":\"https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons/A123456/education/assessments\",\"lastModifiedDateTime\":\"2017-03-15T15:47:51.000000\"}","Timestamp":"2025-09-24T09:39:24.000Z","SignatureVersion":"1","Signature":"...","SigningCertURL":"...","UnsubscribeURL":"...","MessageAttributes":{"prisonId":{"Type":"String","Value":"TRN"},"eventType":{"Type":"String","Value":"PERSON_EDUCATION_ASSESSMENTS_CHANGED"}}}
 
 helpFunction()
 {
@@ -74,16 +73,16 @@ do
   ENDPOINT=$(echo $col3 | sed 's/"//g')
   HMPPS_ID=$(echo $col4 | sed 's/"//g' | sed 's/\r//g')
   PRISON_ID=$(echo $col5 | sed 's/"//g' | sed 's/\r//g')
-  TRN="TRN"
+  MESSAGE_ID=$(uuidgen | tr '[:upper:]' '[:lower:]')
   EVENT_ID=166
   TIMESTAMP=$(date -u '+%Y-%m-%dT%H:%M:%S.000Z')
   date=$(echo $(($(date +'%s * 1000 + %-N / 1000000'))))
   URL="https://${URL_PREFIX}integration-api.hmpps.service.justice.gov.uk/$ENDPOINT"
 
   if [ -z "${PRISON_ID}" ]; then
-    echo "{\"Type\":\"Notification\",\"MessageId\":\"...\",\"TopicArn\":\"...\",\"Message\":\"{\\\"eventId\\\":$EVENT_ID,\\\"hmppsId\\\":\\\"$HMPPS_ID\\\",\\\"eventType\\\":\\\"$EVENT_TYPE\\\",\\\"prisonId\\\":\\\"$TRN\\\",\\\"url\\\":\\\"$URL\\\",\\\"lastModifiedDateTime\\\":\\\"$LAST_UPDATED\\\"}\",\"Timestamp\":\"$TIMESTAMP\",\"SignatureVersion\":\"1\",\"Signature\":\"...\",\"SigningCertURL\":\"...\",\"UnsubscribeURL\":\"...\",\"MessageAttributes\":{\"prisonId\":{\"Type\":\"String\",\"Value\":\"$TRN\"},\"eventType\":{\"Type\":\"String\",\"Value\":\"$EVENT_TYPE\"}}}"
+    echo "{\"Type\":\"Notification\",\"MessageId\":\"$MESSAGE_ID\",\"TopicArn\":\"\",\"Message\":\"{\\\"eventId\\\":$EVENT_ID,\\\"hmppsId\\\":\\\"$HMPPS_ID\\\",\\\"eventType\\\":\\\"$EVENT_TYPE\\\",\\\"prisonId\\\":null,\\\"url\\\":\\\"$URL\\\",\\\"lastModifiedDateTime\\\":\\\"$LAST_UPDATED\\\"}\",\"Timestamp\":\"$TIMESTAMP\",\"SignatureVersion\":\"1\",\"Signature\":\"...\",\"SigningCertURL\":\"...\",\"UnsubscribeURL\":\"...\",\"MessageAttributes\":{\"eventType\":{\"Type\":\"String\",\"Value\":\"$EVENT_TYPE\"}}}"
   else
-    echo "{\"Type\":\"Notification\",\"MessageId\":\"...\",\"TopicArn\":\"...\",\"Message\":\"{\\\"eventId\\\":$EVENT_ID,\\\"hmppsId\\\":\\\"$HMPPS_ID\\\",\\\"eventType\\\":\\\"$EVENT_TYPE\\\",\\\"prisonId\\\":\\\"$PRISON_ID\\\",\\\"url\\\":\\\"$URL\\\",\\\"lastModifiedDateTime\\\":\\\"$LAST_UPDATED\\\"}\",\"Timestamp\":\"$TIMESTAMP\",\"SignatureVersion\":\"1\",\"Signature\":\"...\",\"SigningCertURL\":\"...\",\"UnsubscribeURL\":\"...\",\"MessageAttributes\":{\"prisonId\":{\"Type\":\"String\",\"Value\":\"$PRISON_ID\"},\"eventType\":{\"Type\":\"String\",\"Value\":\"$EVENT_TYPE\"}}}"
+    echo "{\"Type\":\"Notification\",\"MessageId\":\"$MESSAGE_ID\",\"TopicArn\":\"\",\"Message\":\"{\\\"eventId\\\":$EVENT_ID,\\\"hmppsId\\\":\\\"$HMPPS_ID\\\",\\\"eventType\\\":\\\"$EVENT_TYPE\\\",\\\"prisonId\\\":\\\"$PRISON_ID\\\",\\\"url\\\":\\\"$URL\\\",\\\"lastModifiedDateTime\\\":\\\"$LAST_UPDATED\\\"}\",\"Timestamp\":\"$TIMESTAMP\",\"SignatureVersion\":\"1\",\"Signature\":\"...\",\"SigningCertURL\":\"...\",\"UnsubscribeURL\":\"...\",\"MessageAttributes\":{\"eventType\":{\"Type\":\"String\",\"Value\":\"$EVENT_TYPE\"}}}"
   fi
 
 done < "$INPUT"
