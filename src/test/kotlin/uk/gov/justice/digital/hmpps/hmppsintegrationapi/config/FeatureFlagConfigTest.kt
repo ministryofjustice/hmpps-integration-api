fc3ede1c9d385f9326c3fb3ead143b548290e726
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-09-02 12:20:50 +0100

    HIA-1774: Bump up number of feature flags in all environments

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfigTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/config/FeatureFlagConfigTest.kt
@@ -76,7 +76,7 @@ class FeatureFlagConfigTest : ConfigTest() {
     }
 
     // Reduce the threshold in the following assertion over time
-    assertTrue(allSameFlags.size <= 6, "Too many flags enabled in all configs: $flagCounts")
+    assertTrue(allSameFlags.size <= 7, "Too many flags enabled in all configs: $flagCounts")
   }
 
   @Test
