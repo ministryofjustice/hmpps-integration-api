3d0675daef697fc8bb913b536bd8f493f56aabf1
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-09-07 17:08:54 +0100

    HIA-1643: Added hasApplicableRole function

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/ManageUsersService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/services/ManageUsersService.kt
@@ -2,11 +2,17 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationapi.services
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.gateways.ManageUsersGateway
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.models.manageUsers.NomisRole
 
 @Service
 class ManageUsersService(
   private val manageUsersGateway: ManageUsersGateway,
 ) {
+  companion object {
+    const val NOMIS_AUTH_SOURCE = "nomis"
+    const val NOMIS_GLOBAL_SEARCH_ROLE = "GLOBAL_SEARCH"
+  }
+
   fun usernameExists(
     username: String,
     authSources: List<String>,
@@ -16,5 +22,23 @@ class ManageUsersService(
       throw RuntimeException("Call to ${it.causedBy.name} failed with error: ${it.type.name}")
     }
     return usersResponse.data?.content?.any { it.enabled && !it.locked } ?: false
+  }
+
+  fun hasApplicableRole(username: String): Boolean {
+    val usersResponse = manageUsersGateway.findUser(username, listOf(NOMIS_AUTH_SOURCE))
+    usersResponse.errors.forEach {
+      throw RuntimeException("Call to ${it.causedBy.name} findUser failed with error: ${it.type.name}")
+    }
+
+    val roles =
+      usersResponse.data?.content?.filter { !it.locked }?.flatMap { user ->
+        val rolesResponse = manageUsersGateway.getRoles(user.username)
+        rolesResponse.errors.forEach {
+          throw RuntimeException("Call to ${it.causedBy.name} getRoles failed with error: ${it.type.name}")
+        }
+        rolesResponse.data ?: emptyList()
+      } ?: emptyList()
+
+    return roles.contains(NomisRole(NOMIS_GLOBAL_SEARCH_ROLE))
   }
 }
