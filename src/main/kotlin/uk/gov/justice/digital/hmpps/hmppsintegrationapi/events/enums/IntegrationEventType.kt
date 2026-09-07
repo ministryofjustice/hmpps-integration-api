55671a7371a904f32399d9f80ec3604f1dae801f
Jessica Wylde <Jessica.Wylde@justice.gov.uk>
2026-09-07 10:24:15 +0100

    HIA-1795: Removed diffrent prs work from this pr

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/enums/IntegrationEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/events/enums/IntegrationEventType.kt
@@ -284,7 +284,6 @@ val PRISON_CAPACITY_EVENTS =
     DomainEventName.LocationsInsidePrison.Location.DELETED,
     DomainEventName.LocationsInsidePrison.Location.DEACTIVATED,
     DomainEventName.LocationsInsidePrison.Location.REACTIVATED,
-    DomainEventName.LocationsInsidePrison.Location.AMENDED,
     DomainEventName.LocationsInsidePrison.SignedOpCapacity.AMENDED,
   )
 
