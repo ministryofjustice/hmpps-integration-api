f020a1095ff9697704952fc0364607b858d546d8
jessicawyldemoj <Jessica.Wylde@justice.gov.uk>
2026-09-09 14:17:38 +0100

    HIA-1611: Add back lao restrictions for all roles needed (#2021)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/ExtHmppsPublicProtection.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationapi/roles/ExtHmppsPublicProtection.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles
 
+import uk.gov.justice.digital.hmpps.hmppsintegrationapi.redaction.policies.laoRedactionPolicy
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.redaction.policies.riskScores.generalRiskScoreRedactions
 import uk.gov.justice.digital.hmpps.hmppsintegrationapi.roles.dsl.role
 
@@ -39,5 +40,6 @@ val extHmppsPublicProtection =
     }
     redactionPolicies {
       -generalRiskScoreRedactions
+      -laoRedactionPolicy
     }
   }
